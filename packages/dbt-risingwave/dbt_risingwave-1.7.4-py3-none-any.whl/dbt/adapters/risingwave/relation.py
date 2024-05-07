e40ce0e5525761ac044997f68ab5524b5a7345bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 14:03:09 +0000

    Add package dbt-risingwave/1.7.4/dbt_risingwave-1.7.4-py3-none-any.whl

---
+++ packages/dbt-risingwave/dbt_risingwave-1.7.4-py3-none-any.whl/dbt/adapters/risingwave/relation.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from typing import Optional, Type
+
+from dbt.adapters.postgres import PostgresRelation
+from dbt.dataclass_schema import StrEnum
+from dbt.utils import classproperty
+
+
+class RisingWaveRelationType(StrEnum):
+    Table = "table"
+    View = "view"
+    CTE = "cte"
+    MaterializedView = "materialized_view"
+    MaterializedView_v1_5_0 = "materializedview"
+    External = "external"
+
+    Source = "source"
+    Sink = "sink"
+
+
+@dataclass(frozen=True, eq=False, repr=False)
+class RisingWaveRelation(PostgresRelation):
+    type: Optional[RisingWaveRelationType] = None
+
+
+    @classproperty
+    def get_relation_type(cls) -> Type[RisingWaveRelationType]:
+        return RisingWaveRelationType
+
+    # RisingWave has no limitation on relation name length.
+    # We set a relatively large value right now.
+    def relation_max_name_length(self):
+        return 1024
