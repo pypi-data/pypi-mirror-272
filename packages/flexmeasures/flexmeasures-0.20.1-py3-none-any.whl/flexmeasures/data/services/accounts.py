643df13a909bd60d47bbef998591153aeda91bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:03:25 +0000

    Add package flexmeasures/0.20.1/flexmeasures-0.20.1-py3-none-any.whl

---
+++ packages/flexmeasures/flexmeasures-0.20.1-py3-none-any.whl/flexmeasures/data/services/accounts.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+from sqlalchemy import select, func
+
+from flexmeasures.data import db
+from flexmeasures.data.models.user import Account, AccountRole
+from flexmeasures.data.models.generic_assets import GenericAsset
+
+
+def get_accounts(
+    role_name: str | None = None,
+) -> list[Account]:
+    """Return a list of Account objects.
+    The role_name parameter allows to filter by role.
+    """
+    if role_name is not None:
+        role = db.session.execute(
+            select(AccountRole).filter_by(name=role_name)
+        ).scalar_one_or_none()
+        if role:
+            accounts = db.session.scalars(
+                select(Account).filter(Account.account_roles.contains(role))
+            ).all()
+        else:
+            return []
+    else:
+        accounts = db.session.scalars(select(Account)).all()
+
+    return accounts
+
+
+def get_number_of_assets_in_account(account_id: int) -> int:
+    """Get the number of assets in an account."""
+    number_of_assets_in_account = db.session.scalar(
+        select(func.count()).select_from(GenericAsset).filter_by(account_id=account_id)
+    )
+    return number_of_assets_in_account
+
+
+def get_account_roles(account_id: int) -> list[AccountRole]:
+    account = db.session.get(Account, account_id)
+    if account is None:
+        return []
+    return account.account_roles
