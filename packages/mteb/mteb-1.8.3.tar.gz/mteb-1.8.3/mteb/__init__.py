9d1159eebda9c8bf3b2c5d769040c98e0baa7ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:03:17 +0000

    Add package mteb/1.8.3/mteb-1.8.3.tar.gz

---
+++ packages/mteb/mteb-1.8.3.tar.gz/mteb-1.8.3/mteb/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from importlib.metadata import version
+
+from mteb.benchmarks import (
+    MTEB_MAIN_EN,
+    MTEB_RETRIEVAL_LAW,
+    MTEB_RETRIEVAL_WITH_INSTRUCTIONS,
+)
+from mteb.evaluation import *
+from mteb.overview import get_tasks
+
+__version__ = version("mteb")  # fetch version from install metadata
+
+
+__all__ = [
+    "MTEB_MAIN_EN",
+    "MTEB_RETRIEVAL_LAW",
+    "MTEB_RETRIEVAL_WITH_INSTRUCTIONS",
+    "get_tasks",
+]
