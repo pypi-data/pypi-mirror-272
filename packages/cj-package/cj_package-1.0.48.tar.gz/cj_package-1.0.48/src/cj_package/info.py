c2db012207bc92ed3cda9f08f0c2b1d469d73cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:02:57 +0000

    Add package cj-package/1.0.48/cj_package-1.0.48.tar.gz

---
+++ packages/cj-package/cj_package-1.0.48.tar.gz/cj_package-1.0.48/src/cj_package/info.py
@@ -0,0 +1,8 @@
+def info():
+    print("运维使用到的工具小结")
+    # 读取README.md文件打印出来
+    # with open("README.md", "r", encoding="utf-8") as f:
+    #     print(f.read())
+
+# if __name__ == '__main__':
+#     info()
