2cd7eff608827f3f94e1fa41a3ad2396b09d8d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 14:03:28 +0000

    Add package datable-ai/0.0.12/datable_ai-0.0.12.tar.gz

---
+++ packages/datable-ai/datable_ai-0.0.12.tar.gz/datable_ai-0.0.12/datable_ai/__init__.py
@@ -0,0 +1,6 @@
+from datable_ai.ocr import OCR
+from datable_ai.output import Output
+from datable_ai.structured_output import StructuredOutput
+from datable_ai.transcription import Transcription
+
+__all__ = ["StructuredOutput", "Output", "OCR", "Transcription"]
