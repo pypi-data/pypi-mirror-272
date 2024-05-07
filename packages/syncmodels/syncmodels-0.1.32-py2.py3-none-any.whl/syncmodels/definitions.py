061518f4fbf389cab497a9929a6285ab1bc902ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 14:03:38 +0000

    Add package syncmodels/0.1.32/syncmodels-0.1.32-py2.py3-none-any.whl

---
+++ packages/syncmodels/syncmodels-0.1.32-py2.py3-none-any.whl/syncmodels/definitions.py
@@ -0,0 +1,19 @@
+import re
+from enum import Enum
+
+# from .models import (
+#     BudgetTypeEnum,
+#     TravelPartyCompositionEnum,
+# )
+
+DEFAULT_LANGUAGE = "es"
+
+MODEL_TABLE = "model"
+
+UID_TYPE = str
+
+MONOTONIC_KEY = 'wave__'
+ALT_KEY = 'id__'
+# ----------------------------------------------------------
+# enumerate helpers
+# ----------------------------------------------------------
