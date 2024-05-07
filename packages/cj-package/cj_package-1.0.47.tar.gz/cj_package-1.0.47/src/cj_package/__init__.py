29b937a6bcfedac877022fd1d12dedfd42afe4b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 14:03:00 +0000

    Add package cj-package/1.0.47/cj_package-1.0.47.tar.gz

---
+++ packages/cj-package/cj_package-1.0.47.tar.gz/cj_package-1.0.47/src/cj_package/__init__.py
@@ -0,0 +1,11 @@
+from .info import info
+# from . import aliyun
+# from . import excel
+# from . import qw
+# from . import command
+# from . import domain
+# from . import sql
+# from . import utils
+# from . import email
+# from . import time
+from . import test
---
+++ packages/cj-package/cj_package-1.0.47.tar.gz/cj_package-1.0.47/src/cj_package/__version__.py
@@ -0,0 +1,13 @@
+# .-. .-. .-. . . .-. .-. .-. .-.
+# |(  |-  |.| | | |-  `-.  |  `-.
+# ' ' `-' `-`.`-' `-' `-'  '  `-'
+
+__title__ = "cj_package"
+__description__ = "cj package."
+__version__ = "1.0.47"
+__build__ = 0x023100
+__author__ = "chenxing"
+__author_email__ = "chenxing@cjdropshipping.co"
+__license__ = "Apache-2.0"
+__copyright__ = "Copyright Kenneth Reitz"
+__cake__ = "\u2728 \U0001f370 \u2728"
