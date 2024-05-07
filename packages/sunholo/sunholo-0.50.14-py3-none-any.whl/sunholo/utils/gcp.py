624e956bb416b7b427a7119ce1fbda5a6a026cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:03:17 +0000

    Add package sunholo/0.50.14/sunholo-0.50.14-py3-none-any.whl

---
+++ packages/sunholo/sunholo-0.50.14-py3-none-any.whl/sunholo/utils/gcp.py
@@ -0,0 +1,105 @@
+#   Copyright [2024] [Holosun ApS]
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+import os
+import requests
+import socket
+# can't install due to circular import sunholo.logging
+import logging
+
+def is_running_on_cloudrun():
+    if os.getenv("K_SERVICE"):
+        return True
+    return False
+
+def get_env_project_id():
+    """
+    Attempts to retrieve the project ID from environment variables.
+
+    Returns:
+        str or None: The project ID if found in environment variables, None otherwise.
+    """
+    return os.environ.get('GCP_PROJECT') or os.environ.get('GOOGLE_CLOUD_PROJECT')
+
+is_gcp_cached = None  # Global variable for caching the result
+def is_running_on_gcp():
+    global is_gcp_cached
+    if is_gcp_cached is not None:
+        return is_gcp_cached
+
+    try:
+        # Google Cloud instances can reach the metadata server
+        socket.setdefaulttimeout(1)
+        socket.socket().connect(('metadata.google.internal', 80))
+        is_gcp_cached = True
+    except (socket.timeout, socket.error):
+        is_gcp_cached = False
+
+    return is_gcp_cached
+
+def get_service_account_email():
+    service_email = os.getenv("GCP_DEFAULT_SERVICE_EMAIL")
+    if service_email:
+        return service_email
+    service_email = get_metadata('instance/service-accounts/default/email')
+    os.environ['GCP_DEFAULT_SERVICE_EMAIL'] = service_email
+    return service_email
+
+def get_gcp_project():
+    if not is_running_on_gcp():
+        return None
+    
+    project_id = get_env_project_id()
+    if project_id:
+        return project_id
+    
+    project_id = get_metadata('project/project-id')
+    if project_id:
+        os.environ["GCP_PROJECT"] = project_id 
+
+    logging.warning("GCP Project ID not found. Ensure you are running on GCP or have the GCP_PROJECT environment variable set.")
+    return None
+
+
+def get_region():
+    if not is_running_on_gcp():
+        return None
+    # The "instance/zone" metadata includes the region as part of the zone name
+    region = os.getenv("GCP_REGION")
+    if region:
+        return region
+    
+    zone = get_metadata('instance/zone')
+    if zone:
+        # Split by '/' and take the last part, then split by '-' and take all but the last part
+        region_zone = zone.split('/')[-1]
+        region = '-'.join(region_zone.split('-')[:-1])
+        os.environ["GCP_REGION"] = region
+        return region
+    return None
+
+def get_metadata(stem):
+    if not is_running_on_gcp():
+        return None
+    
+    metadata_server_url = f'http://metadata.google.internal/computeMetadata/v1/{stem}'
+
+    headers = {'Metadata-Flavor': 'Google'}
+
+    response = requests.get(metadata_server_url, headers=headers)
+
+    if response.status_code == 200:
+        return response.text
+    else:
+        print(f"Request failed with status code {response.status_code}")
+        return None
