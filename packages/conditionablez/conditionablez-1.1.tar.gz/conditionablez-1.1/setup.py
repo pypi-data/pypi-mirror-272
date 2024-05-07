814b5e08ea7b23bb583b5c39e344aab0ae0939fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 14:03:11 +0000

    Add package conditionablez/1.1/conditionablez-1.1.tar.gz

---
+++ packages/conditionablez/conditionablez-1.1.tar.gz/conditionablez-1.1/setup.py
@@ -0,0 +1,46 @@
+from distutils.core import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+
+setup(
+    name = 'conditionablez',
+    packages = ['conditionablez'],
+    version = '1.1',
+    license='MIT',
+    description = 'conditionablez',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author = 'ochaaz',
+    author_email = 'ocha@masrizky.com',
+    url = 'https://github.com/ochaaz/conditionablez',
+    download_url = 'https://github.com/ochaaz/conditionablez/archive/v_01.tar.gz',
+    project_urls={
+        'Documentation': 'https://github.com/ochaaz/conditionablez',
+        'Funding': 'https://donate.pypi.org',
+        'Say Thanks!': 'http://saythanks.io/to/example',
+        'Source': 'https://github.com/ochaaz/conditionablez/',
+        'Tracker': 'https://github.com/ochaaz/conditionablez/issues',
+    },
+    keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],
+    install_requires=[
+            'validators',
+            'beautifulsoup4',
+            'discordautochat',
+            'discorudo',
+        ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+     dependencies= [
+        'discordautochat',
+        'discorudo'
+    ]
+)
