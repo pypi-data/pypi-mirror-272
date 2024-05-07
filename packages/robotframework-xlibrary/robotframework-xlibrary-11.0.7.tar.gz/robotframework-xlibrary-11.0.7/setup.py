bb6b1bf850a1d1d22a333da129ad81fae4f69753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:03:07 +0000

    Add package robotframework-xlibrary/11.0.7/robotframework-xlibrary-11.0.7.tar.gz

---
+++ packages/robotframework-xlibrary/robotframework-xlibrary-11.0.7.tar.gz/robotframework-xlibrary-11.0.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="robotframework-xlibrary",
+    version="11.0.7",
+    author="Tassana Khrueawan",
+    author_email="tassana.khr@gmail.com",
+    description="Custom Library for MyAIS 2.0 Automation",
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Khrx1999/robotframework-xlibrary.git",
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=[
+        'robotframework>=6.0.0',
+        'pymongo>=3.0.0',
+        
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
