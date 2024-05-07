4b6607383cb37a1e5415393a5583617a809b804a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 14:03:38 +0000

    Add package autorunpy/8.1.11/autorunpy-8.1.11-py2.py3-none-any.whl

---
+++ packages/autorunpy/autorunpy-8.1.11-py2.py3-none-any.whl/autorunpy/rm_venv.py
@@ -0,0 +1,26 @@
+"""
+
+    Removes virtualenv using pyenv if specified in the config file
+
+    """
+
+import subprocess
+import sys
+
+from .util import Conf
+from .util import read_json
+
+c = Conf()
+
+def rm_venv(conf_stem) :
+    _ , j = read_json(conf_stem)
+
+    if j[c.rm_venv] :
+        _cmds = ['pyenv' , 'virtualenv-delete' , '-f' , conf_stem]
+        _cmds += ['&> /dev/null']
+        
+        subprocess.run(_cmds)
+
+if __name__ == '__main__' :
+    arg = sys.argv[1]
+    rm_venv(arg)
