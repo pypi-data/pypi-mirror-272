6854ade94decaa50a835be5fe256452e61394b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:03:27 +0000

    Add package myswiki/1.0.0/myswiki-1.0.0.tar.gz

---
+++ packages/myswiki/myswiki-1.0.0.tar.gz/myswiki-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='myswiki',
+    version='1.0.0',
+    description='A small library for formatting decimals',
+    py_modules=['myswiki'],
+    author='k0ng999',
+    author_email='baydar_14@mail.ru',
+    license='MIT',
+)
