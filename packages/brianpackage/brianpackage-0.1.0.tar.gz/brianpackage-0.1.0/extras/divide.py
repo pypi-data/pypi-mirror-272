47798a9dce78150edc387a14d72d4309bb9c9b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 14:03:16 +0000

    Add package brianpackage/0.1.0/brianpackage-0.1.0.tar.gz

---
+++ packages/brianpackage/brianpackage-0.1.0.tar.gz/brianpackage-0.1.0/extras/divide.py
@@ -0,0 +1,5 @@
+def devide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        raise ValueError("Cannot divide by zero")
---
+++ packages/brianpackage/brianpackage-0.1.0.tar.gz/brianpackage-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'A simple Python package that provides basic math operations.'
+
+setup(
+    name="brianpackage", 
+    version=VERSION,
+    author="Brian",
+    author_email="brian@nexa4ai.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[], 
+    
+    keywords=['python', 'math', 'basic operations'],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
