c1c0d0ba2c24f3aea58964535419503fc8f0c555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:02:52 +0000

    Add package UQPyL/2.0.3/UQPyL-2.0.3-cp310-cp310-win_amd64.whl

---
+++ packages/UQPyL/UQPyL-2.0.3-cp310-cp310-win_amd64.whl/UQPyL/utility/__init__.py
@@ -0,0 +1,17 @@
+from .scalers import MinMaxScaler, StandardScaler, Scaler
+from .model_selections import KFold, RandSelect
+from .polynomial_features import PolynomialFeatures
+from .grid_search import GridSearch
+from .metrics import r2_score, rank_score, sort_score
+
+__all__=[
+    'Scaler',
+    'MinMaxScaler',
+    'StandardScaler',
+    'KFold',
+    'RandSelect',
+    'PolynomialFeatures',
+    'r2_score',
+    'rank_score',
+    'GridSearch'
+]
