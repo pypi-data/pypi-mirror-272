ae83c2e4a144a13dc1c8833be795d1d34b36aa22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:03:25 +0000

    Add package HusBot/0.1/husbot-0.1.tar.gz

---
+++ packages/HusBot/husbot-0.1.tar.gz/husbot-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='HusBot',
+    version='0.1',
+    author= 'HUSSAM.FA',
+    description= 'Module Telegram Bot For DEVELOPRS',
+    packages=['HusBot'],
+    install_requires=['httpx'],
+    classifiers=["Programming Language :: Python :: 3","Operating System :: OS Independent","License :: OSI Approved :: MIT License"]
+)
