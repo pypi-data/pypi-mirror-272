d929e34207712d7ba97cfee9c2628dfb65b6ed95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:03:16 +0000

    Add package pyOpenTTDAdmin/0.1.3/pyopenttdadmin-0.1.3.tar.gz

---
+++ packages/pyOpenTTDAdmin/pyopenttdadmin-0.1.3.tar.gz/pyopenttdadmin-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = 'pyOpenTTDAdmin',
+    version = '0.1.3',
+    packages = ['pyopenttdadmin'],
+    install_requires = [],  # Add any dependencies here
+    author = 'liki-mc',
+    description = 'Python library to communicate with OpenTTD Admin port',
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/liki-mc/pyOpenTTDAdmin/',
+    license = 'MIT',  # Choose an appropriate license
+)
