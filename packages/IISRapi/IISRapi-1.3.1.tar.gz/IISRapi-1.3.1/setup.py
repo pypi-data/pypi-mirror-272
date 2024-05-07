8267a6b7e6f83e58e2c66e81fb2636ac5aec5bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:03:08 +0000

    Add package IISRapi/1.3.1/IISRapi-1.3.1.tar.gz

---
+++ packages/IISRapi/IISRapi-1.3.1.tar.gz/IISRapi-1.3.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+def readmd():
+    with open(Path("readme.md"), "r", encoding="utf-8") as f:
+        return f.read()
+    
+with open("requirement.txt", "r") as f:
+    requirements = f.read().splitlines()
+    
+setup(
+    name='IISRapi',
+    version='1.3.1',
+    packages=find_packages(),
+    license='MIT',
+    long_description=readmd(),
+    long_description_content_type='text/markdown',
+    install_requires=requirements,
+)
