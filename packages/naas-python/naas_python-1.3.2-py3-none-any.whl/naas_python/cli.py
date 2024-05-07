6590dba8c0fab817cc97b66cf49bec2fbe791405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:03:23 +0000

    Add package naas-python/1.3.2/naas_python-1.3.2-py3-none-any.whl

---
+++ packages/naas-python/naas_python-1.3.2-py3-none-any.whl/naas_python/cli.py
@@ -0,0 +1,34 @@
+import typer
+
+from naas_python.domains.registry.handlers.CLIRegistryHandler import (
+    primaryAdaptor as typerRegistryAdaptor,
+)
+
+# from naas_python.domains import spaceCliAdaptor  # , registryCliAdaptor
+from naas_python.domains.space.handlers.CLISpaceHandler import (
+    primaryAdaptor as typerSpaceAdaptor,
+)
+
+# from naas_python.domains import secretCliAdaptor  # , secretCliAdaptor
+from naas_python.domains.secret.handlers.CLISecretHandler import (
+    primaryAdaptor as typerSecretAdaptor,
+)
+
+
+def _create_cli_app():
+    app = typer.Typer(
+        epilog="Found a bug? Report it at https://github.com/jupyter-naas/naas-python/issues",
+        pretty_exceptions_show_locals=False,
+        pretty_exceptions_short=True,
+        # pretty_exceptions_enable=False,
+    )
+
+    # Registry domain's related commands
+    app.add_typer(typerSpaceAdaptor.app, name="space")
+    app.add_typer(typerRegistryAdaptor.app, name="registry")
+    app.add_typer(typerSecretAdaptor.app, name="secret")
+
+    return app
+
+
+app = _create_cli_app()
