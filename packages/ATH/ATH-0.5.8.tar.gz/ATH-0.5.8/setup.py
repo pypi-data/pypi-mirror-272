5d61e38ea7b7eef3be1e2187768e523c4c3b9640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:03:08 +0000

    Add package ATH/0.5.8/ATH-0.5.8.tar.gz

---
+++ packages/ATH/ATH-0.5.8.tar.gz/ATH-0.5.8/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ATH",
+    version="0.5.8",
+    description="""Info: https://discord.com/channels/831614817458323537/1213477275232641044""",
+    packages=find_packages(),
+    install_requires=["pyttsx3", "cryptography", "speechrecognition", "asyncio", "discord", "discord.py", "telebot", "pyscreenshot", "python-dotenv", "qrcode[pil]", "c2pa-python", "beautifulsoup4"]
+)
