746b5ec79a38b908f500c1b16b55bc0de88ad5d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:02:57 +0000

    Add package nucliadb-node-binding/3.0.3.post1259/nucliadb_node_binding-3.0.3.post1259.tar.gz

---
+++ packages/nucliadb-node-binding/nucliadb_node_binding-3.0.3.post1259.tar.gz/nucliadb_node_binding-3.0.3.post1259/local_dependencies/nucliadb_protos/src/knowledgebox.rs
@@ -0,0 +1,272 @@
+// ID
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct KnowledgeBoxId {
+    #[prost(string, tag="1")]
+    pub slug: ::prost::alloc::string::String,
+    #[prost(string, tag="2")]
+    pub uuid: ::prost::alloc::string::String,
+}
+// CONFIG
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct KnowledgeBoxConfig {
+    #[prost(string, tag="1")]
+    pub title: ::prost::alloc::string::String,
+    #[prost(string, tag="2")]
+    pub description: ::prost::alloc::string::String,
+    #[deprecated]
+    #[prost(string, repeated, tag="3")]
+    pub enabled_filters: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+    #[deprecated]
+    #[prost(string, repeated, tag="4")]
+    pub enabled_insights: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+    #[prost(string, tag="5")]
+    pub slug: ::prost::alloc::string::String,
+    #[deprecated]
+    #[prost(bool, tag="6")]
+    pub disable_vectors: bool,
+    #[prost(int64, tag="7")]
+    pub migration_version: i64,
+    /// DEPRECATED: duplicated field also stored in `writer.proto Shards`
+    #[deprecated]
+    #[prost(enumeration="super::utils::ReleaseChannel", tag="8")]
+    pub release_channel: i32,
+}
+// NEW
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct KnowledgeBoxNew {
+    #[prost(string, tag="1")]
+    pub slug: ::prost::alloc::string::String,
+    #[prost(message, optional, tag="2")]
+    pub config: ::core::option::Option<KnowledgeBoxConfig>,
+    /// this fields are only set by backend when creating hosted KBs
+    #[prost(string, tag="3")]
+    pub forceuuid: ::prost::alloc::string::String,
+    #[prost(enumeration="super::utils::VectorSimilarity", tag="4")]
+    pub similarity: i32,
+    #[prost(int32, optional, tag="5")]
+    pub vector_dimension: ::core::option::Option<i32>,
+    #[prost(float, optional, tag="6")]
+    pub default_min_score: ::core::option::Option<f32>,
+    #[prost(uint32, repeated, tag="9")]
+    pub matryoshka_dimensions: ::prost::alloc::vec::Vec<u32>,
+    /// this field are only used by NucliaDB Writer API when creating a KB. Used
+    /// in onprem scenarios
+    #[prost(string, tag="8")]
+    pub learning_config: ::prost::alloc::string::String,
+    /// release channel, although not used when backend creates hosted KBs, it's
+    /// recomputed and changed depending on the environment
+    #[prost(enumeration="super::utils::ReleaseChannel", tag="7")]
+    pub release_channel: i32,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct NewKnowledgeBoxResponse {
+    #[prost(enumeration="KnowledgeBoxResponseStatus", tag="1")]
+    pub status: i32,
+    #[prost(string, tag="2")]
+    pub uuid: ::prost::alloc::string::String,
+}
+// UPDATE
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct KnowledgeBoxUpdate {
+    #[prost(string, tag="1")]
+    pub slug: ::prost::alloc::string::String,
+    #[prost(string, tag="2")]
+    pub uuid: ::prost::alloc::string::String,
+    #[prost(message, optional, tag="3")]
+    pub config: ::core::option::Option<KnowledgeBoxConfig>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct UpdateKnowledgeBoxResponse {
+    #[prost(enumeration="KnowledgeBoxResponseStatus", tag="1")]
+    pub status: i32,
+    #[prost(string, tag="2")]
+    pub uuid: ::prost::alloc::string::String,
+}
+// GC
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct GcKnowledgeBoxResponse {
+}
+// DELETE
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct DeleteKnowledgeBoxResponse {
+    #[prost(enumeration="KnowledgeBoxResponseStatus", tag="1")]
+    pub status: i32,
+}
+// Labels on a Knowledge Box
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Label {
+    #[prost(string, tag="2")]
+    pub title: ::prost::alloc::string::String,
+    #[prost(string, tag="3")]
+    pub related: ::prost::alloc::string::String,
+    #[prost(string, tag="4")]
+    pub text: ::prost::alloc::string::String,
+    #[prost(string, tag="5")]
+    pub uri: ::prost::alloc::string::String,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct LabelSet {
+    #[prost(string, tag="1")]
+    pub title: ::prost::alloc::string::String,
+    #[prost(string, tag="2")]
+    pub color: ::prost::alloc::string::String,
+    #[prost(message, repeated, tag="3")]
+    pub labels: ::prost::alloc::vec::Vec<Label>,
+    #[prost(bool, tag="4")]
+    pub multiple: bool,
+    #[prost(enumeration="label_set::LabelSetKind", repeated, tag="5")]
+    pub kind: ::prost::alloc::vec::Vec<i32>,
+}
+/// Nested message and enum types in `LabelSet`.
+pub mod label_set {
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+    #[repr(i32)]
+    pub enum LabelSetKind {
+        Resources = 0,
+        Paragraphs = 1,
+        Sentences = 2,
+        Selections = 3,
+    }
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Labels {
+    #[prost(map="string, message", tag="1")]
+    pub labelset: ::std::collections::HashMap<::prost::alloc::string::String, LabelSet>,
+}
+// Entities on a Knowledge Box
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Entity {
+    #[prost(string, tag="2")]
+    pub value: ::prost::alloc::string::String,
+    #[prost(string, repeated, tag="4")]
+    pub represents: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+    #[prost(bool, tag="3")]
+    pub merged: bool,
+    #[prost(bool, tag="5")]
+    pub deleted: bool,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct EntitiesGroupSummary {
+    #[prost(string, tag="2")]
+    pub title: ::prost::alloc::string::String,
+    #[prost(string, tag="3")]
+    pub color: ::prost::alloc::string::String,
+    #[prost(bool, tag="4")]
+    pub custom: bool,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct EntitiesGroup {
+    #[prost(map="string, message", tag="1")]
+    pub entities: ::std::collections::HashMap<::prost::alloc::string::String, Entity>,
+    #[prost(string, tag="2")]
+    pub title: ::prost::alloc::string::String,
+    #[prost(string, tag="3")]
+    pub color: ::prost::alloc::string::String,
+    #[prost(bool, tag="4")]
+    pub custom: bool,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct DeletedEntitiesGroups {
+    #[prost(string, repeated, tag="1")]
+    pub entities_groups: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct EntitiesGroups {
+    #[prost(map="string, message", tag="1")]
+    pub entities_groups: ::std::collections::HashMap<::prost::alloc::string::String, EntitiesGroup>,
+}
+///
+/// Structure to represent all duplicates defined in a kb
+///     - call it an "Index" because it should include flattened version of all duplicated entries
+///     - this allows 1 call to pull all duplicates
+///
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct EntityGroupDuplicateIndex {
+    #[prost(map="string, message", tag="1")]
+    pub entities_groups: ::std::collections::HashMap<::prost::alloc::string::String, entity_group_duplicate_index::EntityGroupDuplicates>,
+}
+/// Nested message and enum types in `EntityGroupDuplicateIndex`.
+pub mod entity_group_duplicate_index {
+    #[derive(Clone, PartialEq, ::prost::Message)]
+    pub struct EntityDuplicates {
+        #[prost(string, repeated, tag="1")]
+        pub duplicates: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+    }
+    #[derive(Clone, PartialEq, ::prost::Message)]
+    pub struct EntityGroupDuplicates {
+        #[prost(map="string, message", tag="1")]
+        pub entities: ::std::collections::HashMap<::prost::alloc::string::String, EntityDuplicates>,
+    }
+}
+// Vectorsets
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct VectorSet {
+    #[prost(int32, tag="1")]
+    pub dimension: i32,
+    #[prost(enumeration="super::utils::VectorSimilarity", tag="2")]
+    pub similarity: i32,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct VectorSets {
+    #[prost(map="string, message", tag="1")]
+    pub vectorsets: ::std::collections::HashMap<::prost::alloc::string::String, VectorSet>,
+}
+// Synonyms of a Knowledge Box
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct TermSynonyms {
+    #[prost(string, repeated, tag="1")]
+    pub synonyms: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Synonyms {
+    #[prost(map="string, message", tag="1")]
+    pub terms: ::std::collections::HashMap<::prost::alloc::string::String, TermSynonyms>,
+}
+/// Metadata of the model associated to the KB
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct SemanticModelMetadata {
+    #[prost(enumeration="super::utils::VectorSimilarity", tag="1")]
+    pub similarity_function: i32,
+    #[prost(int32, optional, tag="2")]
+    pub vector_dimension: ::core::option::Option<i32>,
+    #[prost(float, optional, tag="3")]
+    pub default_min_score: ::core::option::Option<f32>,
+    /// list of possible subdivisions of the matryoshka embeddings (if the model
+    /// supports it)
+    #[prost(uint32, repeated, tag="4")]
+    pub matryoshka_dimensions: ::prost::alloc::vec::Vec<u32>,
+}
+// Do not update this model without confirmation of internal Learning Config API
+
+/// Deprecated
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct KbConfiguration {
+    #[prost(string, tag="2")]
+    pub semantic_model: ::prost::alloc::string::String,
+    #[prost(string, tag="3")]
+    pub generative_model: ::prost::alloc::string::String,
+    #[prost(string, tag="4")]
+    pub ner_model: ::prost::alloc::string::String,
+    #[prost(string, tag="5")]
+    pub anonymization_model: ::prost::alloc::string::String,
+    #[prost(string, tag="6")]
+    pub visual_labeling: ::prost::alloc::string::String,
+}
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[repr(i32)]
+pub enum KnowledgeBoxResponseStatus {
+    Ok = 0,
+    Conflict = 1,
+    Notfound = 2,
+    Error = 3,
+}
---
+++ packages/nucliadb-node-binding/nucliadb_node_binding-3.0.3.post1259.tar.gz/nucliadb_node_binding-3.0.3.post1259/local_dependencies/nucliadb_protos/src/noderesources.rs
@@ -0,0 +1,285 @@
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct TextInformation {
+    #[prost(string, tag="1")]
+    pub text: ::prost::alloc::string::String,
+    #[prost(string, repeated, tag="2")]
+    pub labels: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct IndexMetadata {
+    /// Tantivy doc & para
+    #[prost(message, optional, tag="1")]
+    pub modified: ::core::option::Option<::prost_types::Timestamp>,
+    /// Tantivy doc & para
+    #[prost(message, optional, tag="2")]
+    pub created: ::core::option::Option<::prost_types::Timestamp>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ShardId {
+    #[prost(string, tag="1")]
+    pub id: ::prost::alloc::string::String,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ShardIds {
+    #[prost(message, repeated, tag="1")]
+    pub ids: ::prost::alloc::vec::Vec<ShardId>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ShardCreated {
+    #[prost(string, tag="1")]
+    pub id: ::prost::alloc::string::String,
+    #[prost(enumeration="shard_created::DocumentService", tag="2")]
+    pub document_service: i32,
+    #[prost(enumeration="shard_created::ParagraphService", tag="3")]
+    pub paragraph_service: i32,
+    #[prost(enumeration="shard_created::VectorService", tag="4")]
+    pub vector_service: i32,
+    #[prost(enumeration="shard_created::RelationService", tag="5")]
+    pub relation_service: i32,
+}
+/// Nested message and enum types in `ShardCreated`.
+pub mod shard_created {
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+    #[repr(i32)]
+    pub enum DocumentService {
+        DocumentV0 = 0,
+        DocumentV1 = 1,
+        DocumentV2 = 2,
+    }
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+    #[repr(i32)]
+    pub enum ParagraphService {
+        ParagraphV0 = 0,
+        ParagraphV1 = 1,
+        ParagraphV2 = 2,
+        ParagraphV3 = 3,
+    }
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+    #[repr(i32)]
+    pub enum VectorService {
+        VectorV0 = 0,
+        VectorV1 = 1,
+    }
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+    #[repr(i32)]
+    pub enum RelationService {
+        RelationV0 = 0,
+        RelationV1 = 1,
+        RelationV2 = 2,
+    }
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ResourceId {
+    #[prost(string, tag="1")]
+    pub shard_id: ::prost::alloc::string::String,
+    #[prost(string, tag="2")]
+    pub uuid: ::prost::alloc::string::String,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Shard {
+    #[prost(message, optional, tag="5")]
+    pub metadata: ::core::option::Option<ShardMetadata>,
+    #[prost(string, tag="1")]
+    pub shard_id: ::prost::alloc::string::String,
+    #[prost(uint64, tag="2")]
+    pub fields: u64,
+    #[prost(uint64, tag="3")]
+    pub paragraphs: u64,
+    #[prost(uint64, tag="4")]
+    pub sentences: u64,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct EmptyResponse {
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct EmptyQuery {
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Position {
+    #[prost(uint64, tag="1")]
+    pub index: u64,
+    #[prost(uint64, tag="2")]
+    pub start: u64,
+    #[prost(uint64, tag="3")]
+    pub end: u64,
+    /// For pdfs/documents only
+    #[prost(uint64, tag="4")]
+    pub page_number: u64,
+    #[prost(bool, tag="7")]
+    pub in_page: bool,
+    /// For multimedia only
+    #[prost(uint32, repeated, tag="5")]
+    pub start_seconds: ::prost::alloc::vec::Vec<u32>,
+    #[prost(uint32, repeated, tag="6")]
+    pub end_seconds: ::prost::alloc::vec::Vec<u32>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Representation {
+    #[prost(bool, tag="1")]
+    pub is_a_table: bool,
+    #[prost(string, tag="2")]
+    pub file: ::prost::alloc::string::String,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct SentenceMetadata {
+    #[prost(message, optional, tag="1")]
+    pub position: ::core::option::Option<Position>,
+    #[prost(bool, tag="2")]
+    pub page_with_visual: bool,
+    #[prost(message, optional, tag="3")]
+    pub representation: ::core::option::Option<Representation>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct VectorSentence {
+    #[prost(float, repeated, tag="1")]
+    pub vector: ::prost::alloc::vec::Vec<f32>,
+    #[prost(message, optional, tag="9")]
+    pub metadata: ::core::option::Option<SentenceMetadata>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ParagraphMetadata {
+    #[prost(message, optional, tag="1")]
+    pub position: ::core::option::Option<Position>,
+    #[prost(bool, tag="2")]
+    pub page_with_visual: bool,
+    #[prost(message, optional, tag="3")]
+    pub representation: ::core::option::Option<Representation>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct IndexParagraph {
+    /// Start end position in field text
+    #[prost(int32, tag="1")]
+    pub start: i32,
+    /// Start end position in field text
+    #[prost(int32, tag="2")]
+    pub end: i32,
+    /// Paragraph specific labels
+    #[prost(string, repeated, tag="3")]
+    pub labels: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+    /// key is full id for vectors
+    #[prost(map="string, message", tag="4")]
+    pub sentences: ::std::collections::HashMap<::prost::alloc::string::String, VectorSentence>,
+    #[prost(string, tag="5")]
+    pub field: ::prost::alloc::string::String,
+    /// split were it belongs
+    #[prost(string, tag="6")]
+    pub split: ::prost::alloc::string::String,
+    #[prost(uint64, tag="7")]
+    pub index: u64,
+    #[prost(bool, tag="8")]
+    pub repeated_in_field: bool,
+    #[prost(message, optional, tag="9")]
+    pub metadata: ::core::option::Option<ParagraphMetadata>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct VectorSetId {
+    #[prost(message, optional, tag="1")]
+    pub shard: ::core::option::Option<ShardId>,
+    #[prost(string, tag="2")]
+    pub vectorset: ::prost::alloc::string::String,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct VectorSetList {
+    #[prost(message, optional, tag="1")]
+    pub shard: ::core::option::Option<ShardId>,
+    #[prost(string, repeated, tag="2")]
+    pub vectorset: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct IndexParagraphs {
+    /// id of the paragraph f"{self.rid}/{field_key}/{paragraph.start}-{paragraph.end}"
+    #[prost(map="string, message", tag="1")]
+    pub paragraphs: ::std::collections::HashMap<::prost::alloc::string::String, IndexParagraph>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Resource {
+    #[prost(message, optional, tag="1")]
+    pub resource: ::core::option::Option<ResourceId>,
+    #[prost(message, optional, tag="2")]
+    pub metadata: ::core::option::Option<IndexMetadata>,
+    /// Doc index
+    ///
+    /// Tantivy doc filled by field allways full
+    #[prost(map="string, message", tag="3")]
+    pub texts: ::std::collections::HashMap<::prost::alloc::string::String, TextInformation>,
+    // Key is RID/FIELDID
+
+    /// Document labels always serialized full
+    #[prost(string, repeated, tag="4")]
+    pub labels: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+    /// Tantivy doc
+    #[prost(enumeration="resource::ResourceStatus", tag="5")]
+    pub status: i32,
+    /// Paragraph
+    ///
+    /// Paragraphs by field
+    #[prost(map="string, message", tag="6")]
+    pub paragraphs: ::std::collections::HashMap<::prost::alloc::string::String, IndexParagraphs>,
+    #[prost(string, repeated, tag="7")]
+    pub paragraphs_to_delete: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+    #[prost(string, repeated, tag="8")]
+    pub sentences_to_delete: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+    /// Relations
+    #[prost(message, repeated, tag="9")]
+    pub relations: ::prost::alloc::vec::Vec<super::utils::Relation>,
+    #[prost(string, tag="11")]
+    pub shard_id: ::prost::alloc::string::String,
+    /// vectorset is the key
+    #[prost(map="string, message", tag="12")]
+    pub vectors: ::std::collections::HashMap<::prost::alloc::string::String, super::utils::UserVectors>,
+    /// Vectorset prefix vector id
+    #[prost(map="string, message", tag="13")]
+    pub vectors_to_delete: ::std::collections::HashMap<::prost::alloc::string::String, super::utils::UserVectorsList>,
+    #[prost(message, optional, tag="14")]
+    pub security: ::core::option::Option<super::utils::Security>,
+}
+/// Nested message and enum types in `Resource`.
+pub mod resource {
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+    #[repr(i32)]
+    pub enum ResourceStatus {
+        Processed = 0,
+        Empty = 1,
+        Error = 2,
+        Delete = 3,
+        Pending = 4,
+        Blocked = 5,
+        Expired = 6,
+    }
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ShardMetadata {
+    #[prost(string, tag="1")]
+    pub kbid: ::prost::alloc::string::String,
+    #[prost(enumeration="super::utils::ReleaseChannel", tag="2")]
+    pub release_channel: i32,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct NodeMetadata {
+    #[deprecated]
+    #[prost(float, tag="1")]
+    pub load_score: f32,
+    #[prost(uint64, tag="2")]
+    pub shard_count: u64,
+    #[prost(map="string, message", tag="3")]
+    pub shards: ::std::collections::HashMap<::prost::alloc::string::String, node_metadata::ShardMetadata>,
+    #[prost(string, tag="4")]
+    pub node_id: ::prost::alloc::string::String,
+    #[prost(string, optional, tag="5")]
+    pub primary_node_id: ::core::option::Option<::prost::alloc::string::String>,
+    #[prost(uint64, tag="6")]
+    pub available_disk: u64,
+    #[prost(uint64, tag="7")]
+    pub total_disk: u64,
+}
+/// Nested message and enum types in `NodeMetadata`.
+pub mod node_metadata {
+    #[derive(Clone, PartialEq, ::prost::Message)]
+    pub struct ShardMetadata {
+        #[prost(string, tag="1")]
+        pub kbid: ::prost::alloc::string::String,
+        #[deprecated]
+        #[prost(float, tag="2")]
+        pub load_score: f32,
+    }
+}
