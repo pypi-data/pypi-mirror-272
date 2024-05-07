03bb511a024e4d2995161ee23889e31cced2daf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:03:22 +0000

    Add package rdb-json-parser/0.4/rdb_json_parser-0.4.tar.gz

---
+++ packages/rdb-json-parser/rdb_json_parser-0.4.tar.gz/rdb_json_parser-0.4/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+long_description = None
+with open("README.txt","r",encoding="utf8") as f:
+    long_description = f.read() 
+
+setuptools.setup(
+    name = "rdb_json_parser",
+    version = "0.4",
+    author = "Nikunj Pathak",
+    author_email = "nik.twister@gmail.com",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    packages = ["rdb_json_parser"],
+    package_data = {"rdb_json_parser":["*.so"]}
+)
+
