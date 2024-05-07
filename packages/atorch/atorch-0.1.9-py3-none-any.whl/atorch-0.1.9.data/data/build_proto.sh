ad5bd0feb1e86d89017aae6c2d0694250e1315e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:02:54 +0000

    Add package atorch/0.1.9/atorch-0.1.9-py3-none-any.whl

---
+++ packages/atorch/atorch-0.1.9-py3-none-any.whl/atorch-0.1.9.data/data/build_proto.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+set -x
+
+python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. atorch/protos/acceleration.proto
+python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. atorch/protos/coworker.proto
---
+++ packages/atorch/atorch-0.1.9-py3-none-any.whl/atorch/ops/git_version_info_installed.py
@@ -0,0 +1,6 @@
+version = '0.1.9+dc54a95b'
+git_hash = 'dc54a95b'
+git_branch = 'update_20240426'
+installed_ops = {'quantization_optimizer': False, 'quantizer': False}
+compatible_ops = {'quantization_optimizer': True, 'quantizer': True, 'atorch_not_implemented': False}
+torch_info = {'version': '2.1', 'bf16_support': False, 'cuda_version': '12.1', 'nccl_version': '2.18'}
