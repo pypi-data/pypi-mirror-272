2112e4a447c5fc9fb3fb54e6b723674a50a0069c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 14:03:38 +0000

    Add package nowgg/1.1.0/nowgg-1.1.0.tar.gz

---
+++ packages/nowgg/nowgg-1.1.0.tar.gz/nowgg-1.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from pathlib import Path
+import os
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(
+    name='nowgg',
+    version='1.1.0',
+    scripts=['nowgg.py'],
+    author_email='cloudmaster@now.gg',
+    install_requires=[
+        'requests'
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    entry_points={
+        'console_scripts': [
+            'nowgg = nowgg:main',
+        ],
+    }
+)
