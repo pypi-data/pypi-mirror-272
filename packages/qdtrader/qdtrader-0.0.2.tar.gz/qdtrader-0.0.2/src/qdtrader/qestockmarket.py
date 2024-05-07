abb039c2e2f7eab0f59b80b0ad1cbea3b6724057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 14:03:38 +0000

    Add package qdtrader/0.0.2/qdtrader-0.0.2.tar.gz

---
+++ packages/qdtrader/qdtrader-0.0.2.tar.gz/qdtrader-0.0.2/src/qdtrader/qestockmarket.py
@@ -0,0 +1,12 @@
+import platform 
+
+
+def checkStockTime(now):
+    return True
+    
+def changeStockInstIDs(stratname, instids):
+    pass
+
+
+def runStockMarketProcess(user, passwd, strats,runmode):
+    pass
