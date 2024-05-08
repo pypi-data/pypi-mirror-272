c37a844bfa72b455205e62502577c0dd39430c90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 04:03:09 +0000

    Add package vcf2seq/0.6.5a0/vcf2seq-0.6.5a0.tar.gz

---
+++ packages/vcf2seq/vcf2seq-0.6.5a0.tar.gz/vcf2seq-0.6.5a0/vcf2seq/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+
+import sys, os
+
+app_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, app_path)
+
+from vcf2seq.vcf2seq import compute, input_ctrl
+
---
+++ packages/vcf2seq/vcf2seq-0.6.5a0.tar.gz/vcf2seq-0.6.5a0/vcf2seq/info.py
@@ -0,0 +1,8 @@
+
+
+APPNAME   = "vcf2seq"
+SHORTDESC = "Inputing a VCF file, it returns the genomic sequence at the specified length (31 by default)."
+LICENCE   = "GPL3"
+VERSION   = "0.6.5-alpha"
+AUTHOR    = "Benoit Guibert"
+AUTHOR_EMAIL = "benoit.guibert@free.fr"
