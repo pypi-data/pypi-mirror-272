cea976783252d1a08c27dddc42914ddaec7785f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 15:04:24 +0000

    Add package wonder-diffusion-sdk/0.0.6.dev2/wonder-diffusion-sdk-0.0.6.dev2.tar.gz

---
+++ packages/wonder-diffusion-sdk/wonder-diffusion-sdk-0.0.6.dev2.tar.gz/wonder-diffusion-sdk-0.0.6.dev2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wonder-diffusion-sdk',
+    version='0.0.6-dev2',
+    description='Python SDK for common Wonder diffusion models',
+    author='basri',
+    author_email='basri@wonder.co',
+    packages=find_packages(),
+    install_requires=[
+      'diffusers==0.26.3'
+    ],
+)
