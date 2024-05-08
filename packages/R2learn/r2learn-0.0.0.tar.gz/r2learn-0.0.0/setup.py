5b3ec75ad720a94541254d5da865c96195de1a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 15:03:50 +0000

    Add package R2learn/0.0.0/r2learn-0.0.0.tar.gz

---
+++ packages/R2learn/r2learn-0.0.0.tar.gz/r2learn-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='R2learn',  
+    version='0.0.0',  # initial version for package name
+    description='A Python package for Representation Retrieval Learning',
+    packages=find_packages(),
+    author='Qi Xu',
+    author_email='qxu6@uci.edu',
+    # Add any additional fields as needed (license, keywords, etc.)
+)
