a8f5ca8a689dd13f878e7301272e71bd5c0cdc9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 07:03:32 +0000

    Add package Impactrueno/1.0.1/Impactrueno-1.0.1.tar.gz

---
+++ packages/Impactrueno/Impactrueno-1.0.1.tar.gz/Impactrueno-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Impactrueno',
+    version='1.0.1',
+    author='Missael Angel Cardenas',
+    author_email= 'missaelanggel@gmail.com',
+    description= 'Una Biblioteca que contiene la clase Random_pokemon para mostranos poquemones locos',
+    packages=["Impactrueno"],
+    package_data= {'Impactrueno': ['pokemon.csv']},
+    install_requires= [
+        'pandas', 'pkg_resources', 'setuptools', 'twine', 'wheel',
+    ],
+)
