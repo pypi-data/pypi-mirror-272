c149fb3fd2f565b7f00514d750b5c1a60207c6a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 03:04:02 +0000

    Add package wauo/0.1/wauo-0.1.tar.gz

---
+++ packages/wauo/wauo-0.1.tar.gz/wauo-0.1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='wauo',
+    version='0.1',
+    description='More convenient use of requests',
+    url='https://github.com/markadc/wauo',
+    author='WangTuo',
+    author_email='markadc@126.com',
+    packages=['wauo'],
+    zip_safe=False
+)
