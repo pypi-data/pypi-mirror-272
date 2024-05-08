32555239612096d8a0a7f7b24cb7cf3509262c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 03:03:38 +0000

    Add package green-tsetlin/1.0.0/green_tsetlin-1.0.0.tar.gz

---
+++ packages/green-tsetlin/green_tsetlin-1.0.0.tar.gz/green_tsetlin-1.0.0/README.md
@@ -0,0 +1,10 @@
+
+green_tsetlin
+==============
+![logo](docs/image/Green%20Tsetlin%20transparent%20gray.png)
+
+
+See examples for examples. More examples will be added asap.
+
+
+
---
+++ packages/green-tsetlin/green_tsetlin-1.0.0.tar.gz/green_tsetlin-1.0.0/docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+
+project = 'green_tsetlin'
+copyright = "2024, Sondre 'Ooki' Glimsdal"
+author = "Sondre 'Ooki' Glimsdal"
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc', 'nbsphinx']
+
+nbsphinx_execute = "never"
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'renku'
+html_static_path = ['_static']
