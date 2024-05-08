c1d6f6cd2d9ce80dc56cc3493bb437a81da73774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 16:04:52 +0000

    Add package dsp-tools/7.2.0.post0/dsp_tools-7.2.0.post0.tar.gz

---
+++ packages/dsp-tools/dsp_tools-7.2.0.post0.tar.gz/dsp_tools-7.2.0.post0/src/dsp_tools/commands/resume_xmlupload/resume_xmlupload.py
@@ -0,0 +1,96 @@
+import pickle
+import sys
+from copy import deepcopy
+from dataclasses import replace
+
+from loguru import logger
+from termcolor import colored
+
+from dsp_tools.commands.xmlupload.iri_resolver import IriResolver
+from dsp_tools.commands.xmlupload.list_client import ListClient
+from dsp_tools.commands.xmlupload.list_client import ListClientLive
+from dsp_tools.commands.xmlupload.models.sipi import Sipi
+from dsp_tools.commands.xmlupload.models.upload_state import UploadState
+from dsp_tools.commands.xmlupload.project_client import ProjectClient
+from dsp_tools.commands.xmlupload.project_client import ProjectClientLive
+from dsp_tools.commands.xmlupload.upload_config import UploadConfig
+from dsp_tools.commands.xmlupload.xmlupload import cleanup_upload
+from dsp_tools.commands.xmlupload.xmlupload import upload_resources
+from dsp_tools.utils.connection_live import ConnectionLive
+
+
+def resume_xmlupload(server: str, user: str, password: str, sipi: str, skip_first_resource: bool = False) -> bool:
+    """
+    Resume an interrupted xmlupload.
+
+    Args:
+        server: the DSP server where the data should be imported
+        user: the user (e-mail) with which the data should be imported
+        password: the password of the user with which the data should be imported
+        sipi: the sipi instance to be used
+        skip_first_resource: if this flag is set, the first resource of the pending resources is removed
+
+    Returns:
+        True if all resources could be uploaded without errors; False if one of the resources could not be
+        uploaded because there is an error in it
+    """
+    upload_state = _read_upload_state_from_disk(server)
+    if skip_first_resource:
+        if len(upload_state.pending_resources) > 0:
+            new_pending = deepcopy(upload_state.pending_resources)
+            new_pending.pop(0)
+            upload_state = replace(upload_state, pending_resources=new_pending)
+        else:
+            msg = (
+                "The list of pending resources is empty.\n"
+                "It is not yet possible to skip the first item of the stashed properties.\n"
+                "Do you want to continue with the upload of the stashed properties anyway? [y/n]"
+            )
+            resp = None
+            while resp not in ["y", "n"]:
+                resp = input(colored(msg, color="red"))
+            if resp == "n":
+                sys.exit(1)
+
+    previous_successful = len(upload_state.iri_resolver_lookup)
+    previous_failed = len(upload_state.failed_uploads)
+    previous_total = previous_successful + previous_failed
+    msg = (
+        f"Resuming upload for project {upload_state.config.shortcode} on server {server}. "
+        f"Number of resources uploaded until now: {previous_total}"
+    )
+    if previous_failed:
+        msg += f" ({previous_failed} of them failed)"
+    logger.info(msg)
+    print("\n==========================\n" + msg + "\n==========================\n")
+
+    con = ConnectionLive(server)
+    con.login(user, password)
+    sipi_con = ConnectionLive(sipi, token=con.get_token())
+    sipi_server = Sipi(sipi_con)
+
+    project_client: ProjectClient = ProjectClientLive(con, upload_state.config.shortcode)
+    list_client: ListClient = ListClientLive(con, project_client.get_project_iri())
+
+    iri_resolver, failed_uploads, nonapplied_stash = upload_resources(
+        resources=upload_state.pending_resources,
+        failed_uploads=upload_state.failed_uploads,
+        imgdir=".",
+        sipi_server=sipi_server,
+        permissions_lookup=upload_state.permissions_lookup,
+        con=con,
+        stash=upload_state.pending_stash,
+        config=upload_state.config,
+        project_client=project_client,
+        list_client=list_client,
+        iri_resolver=IriResolver(upload_state.iri_resolver_lookup),
+    )
+
+    return cleanup_upload(iri_resolver, upload_state.config, failed_uploads, nonapplied_stash)
+
+
+def _read_upload_state_from_disk(server: str) -> UploadState:
+    save_location = UploadConfig().with_server_info(server, "foo").diagnostics.save_location
+    with open(save_location, "rb") as f:
+        saved_state: UploadState = pickle.load(f)  # noqa: S301 (deserialization of untrusted data)
+    return saved_state
---
+++ packages/dsp-tools/dsp_tools-7.2.0.post0.tar.gz/dsp_tools-7.2.0.post0/src/dsp_tools/commands/xmlupload/stash/upload_stashed_resptr_props.py
@@ -0,0 +1,114 @@
+from __future__ import annotations
+
+from datetime import datetime
+from typing import Any
+
+from loguru import logger
+
+from dsp_tools.commands.xmlupload.iri_resolver import IriResolver
+from dsp_tools.commands.xmlupload.stash.stash_models import LinkValueStash
+from dsp_tools.commands.xmlupload.stash.stash_models import LinkValueStashItem
+from dsp_tools.models.exceptions import BaseError
+from dsp_tools.utils.connection import Connection
+
+
+def upload_stashed_resptr_props(
+    iri_resolver: IriResolver,
+    con: Connection,
+    stashed_resptr_props: LinkValueStash,
+    context: dict[str, str],
+) -> LinkValueStash | None:
+    """
+    After all resources are uploaded, the stashed resptr props must be applied to their resources in DSP.
+
+    Args:
+        iri_resolver: resolver with a mapping of ids from the XML file to IRIs in DSP
+        con: connection to DSP
+        stashed_resptr_props: all resptr props that have been stashed
+        context: the JSON-LD context of the resource
+
+    Returns:
+        nonapplied_resptr_props: the resptr props that could not be uploaded
+    """
+
+    print(f"{datetime.now()}: Upload the stashed resptrs...")
+    logger.info("Upload the stashed resptrs...")
+    not_uploaded: list[LinkValueStashItem] = []
+    for res_id, stash_items in stashed_resptr_props.res_2_stash_items.copy().items():
+        res_iri = iri_resolver.get(res_id)
+        if not res_iri:
+            # resource could not be uploaded to DSP, so the stash cannot be uploaded either
+            # no action necessary: this resource will remain in nonapplied_resptr_props,
+            # which will be handled by the caller
+            continue
+        print(f"{datetime.now()}:   Upload resptrs of resource '{res_id}'...")
+        logger.info(f"  Upload resptrs of resource '{res_id}'...")
+        for stash_item in stash_items:
+            target_iri = iri_resolver.get(stash_item.target_id)
+            if not target_iri:
+                continue
+            if _upload_stash_item(stash_item, res_iri, target_iri, con, context):
+                stashed_resptr_props.res_2_stash_items[res_id].remove(stash_item)
+            else:
+                not_uploaded.append(stash_item)
+        if not stashed_resptr_props.res_2_stash_items[res_id]:
+            del stashed_resptr_props.res_2_stash_items[res_id]
+    return LinkValueStash.make(not_uploaded)
+
+
+def _upload_stash_item(
+    stash: LinkValueStashItem,
+    res_iri: str,
+    target_iri: str,
+    con: Connection,
+    context: dict[str, str],
+) -> bool:
+    """
+    Upload a single stashed link value to DSP.
+
+    Args:
+        stash: the stashed link value to upload
+        res_iri: the iri of the resource
+        target_iri: the iri of the target resource
+        con: connection to DSP
+        context: the JSON-LD context of the resource
+
+    Returns:
+        True, if the upload was successful, False otherwise
+    """
+    payload = _create_resptr_prop_json_object_to_update(stash, res_iri, target_iri, context)
+    try:
+        con.post(route="/v2/values", data=payload)
+    except BaseError as err:
+        _log_unable_to_upload_link_value(err.message, stash.res_id, stash.prop_name)
+        return False
+    logger.debug(f'  Successfully uploaded resptr links of "{stash.prop_name}"')
+    return True
+
+
+def _log_unable_to_upload_link_value(msg: str, res_id: str, prop_name: str) -> None:
+    err_msg = f"Unable to upload the resptr prop of '{prop_name}' of resource '{res_id}'."
+    print(f"{datetime.now()}:     WARNING: {err_msg} Original error message: {msg}")
+    logger.opt(exception=True).warning(err_msg)
+
+
+def _create_resptr_prop_json_object_to_update(
+    stash: LinkValueStashItem,
+    res_iri: str,
+    target_iri: str,
+    context: dict[str, str],
+) -> dict[str, Any]:
+    """This function creates a JSON object that can be sent as an update request to the DSP-API."""
+    linkVal = {
+        "@type": "knora-api:LinkValue",
+        "knora-api:linkValueHasTargetIri": {"@id": target_iri},
+    }
+    if stash.permission:
+        linkVal["knora-api:hasPermissions"] = stash.permission
+    jsonobj = {
+        "@id": res_iri,
+        "@type": stash.res_type,
+        f"{stash.prop_name}Value": linkVal,
+        "@context": context,
+    }
+    return jsonobj
