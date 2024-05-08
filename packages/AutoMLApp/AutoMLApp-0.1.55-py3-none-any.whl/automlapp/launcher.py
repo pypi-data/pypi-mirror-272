786a1b61ccb2dd7feb0733350663d088aeb2c050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 03:03:05 +0000

    Add package AutoMLApp/0.1.55/AutoMLApp-0.1.55-py3-none-any.whl

---
+++ packages/AutoMLApp/AutoMLApp-0.1.55-py3-none-any.whl/automlapp/launcher.py
@@ -0,0 +1,14 @@
+# automlapp/launcher.py
+import subprocess
+import os
+
+def launch_streamlit():
+    # Determine the path to the app.py within the installed package
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    app_path = os.path.join(dir_path, 'app.py')
+    
+    # Run the streamlit command
+    subprocess.run(["streamlit", "run", app_path])
+
+if __name__ == "__main__":
+    launch_streamlit()
