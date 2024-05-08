aaa5eb842535a2df0ccf60cc1568947a4227adb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 15:03:55 +0000

    Add package Way2DSSAT/0.0.10/way2dssat-0.0.10.tar.gz

---
+++ packages/Way2DSSAT/way2dssat-0.0.10.tar.gz/way2dssat-0.0.10/Way2DSSAT/__init__.py
@@ -0,0 +1,12 @@
+#init funciton
+"""Create soil file and store the dictionary and list objects.
+then weather
+"""
+from .gssurgo2soil import SOL
+"""Creates .SOL file."""
+
+from .WTH import WTH
+"""cretes weather file."""
+
+from .xfiles import Xfile
+"""Creates X file"""
