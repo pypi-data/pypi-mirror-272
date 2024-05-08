57c3dd9e706ccd4d1e184433388dbf391367ac3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 05:02:55 +0000

    Add package dslog/0.1.8/dslog-0.1.8-py3-none-any.whl

---
+++ packages/dslog/dslog-0.1.8-py3-none-any.whl/dslog/logger.py
@@ -0,0 +1,76 @@
+from typing import Literal, Protocol, Generic, TypeVarTuple
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+from .types import Level, Handler, Formatter, value
+from . import loggers, formatters
+
+Objs = TypeVarTuple('Objs')
+
+class LogFn(Protocol, Generic[*Objs]):
+  def __call__(self, *objs: *Objs, level: Level = 'INFO'):
+    ...
+
+class Logger(ABC, LogFn[*Objs], Generic[*Objs]):
+  @abstractmethod
+  def __call__(self, *objs: *Objs, level: Level = 'INFO'):
+    ...
+
+  @classmethod
+  def of(cls, handler: Handler) -> 'Logger':
+    return LoggerOf(handler)
+
+  @classmethod
+  def click(cls) -> 'Logger':
+    """`print` logger formatted with `click`"""
+    return Logger.of(print).format(formatters.click) 
+
+  @classmethod
+  def rich(cls) -> 'Logger':
+    """Nicely formatted rich logger (use `loggers.rich` aka `Logger.of(rich.print)` for a non-formatted version)"""
+    return loggers.rich().format(formatters.rich) 
+  
+  @classmethod
+  def file(cls, filepath: str, *, mode: Literal['w', 'a'] = 'a') -> 'Logger[*Objs]':
+    """Default formatted file logger (use `loggers.file` for a non-formatted version)"""
+    return loggers.file(filepath, mode=mode).format(formatters.default)
+  
+  @classmethod
+  def empty(cls) -> 'Logger':
+    """*Objs logger that doesn't do anything"""
+    return LoggerOf(lambda *_, **_kw: None)
+
+  def limit(self, min_level: Level) -> 'Logger':
+    return Limited(min_level, self)
+  
+  def format(self, format: Formatter[*Objs]) -> 'Logger[*Objs]':
+    return Formatted(format, self)
+  
+  def prefix(self, prefix: str) -> 'Logger[*Objs]':
+    return self.format(lambda *objs, **_: (prefix, *objs)) # type: ignore
+  
+  def postfix(self, postfix: str) -> 'Logger[*Objs]':
+    return self.format(lambda *objs, **_: (*objs, postfix)) # type: ignore
+  
+@dataclass
+class LoggerOf(Logger):
+  handler: Handler
+  def __call__(self, *objs, **_):
+    self.handler(*objs)
+
+@dataclass
+class Limited(Logger):
+  min_level: Level | int
+  logger: Logger
+
+  def __call__(self, *objs, level: Level = 'INFO'):
+    if value(level) >= value(self.min_level):
+      self.logger(*objs, level=level)
+
+@dataclass
+class Formatted(Logger[*Objs]):
+  formatter: Formatter[*Objs]
+  logger: Logger
+
+  def __call__(self, *objs, level: Level = 'INFO'):
+    formatted_objs = self.formatter(*objs, level=level)
+    self.logger(*formatted_objs, level=level)
