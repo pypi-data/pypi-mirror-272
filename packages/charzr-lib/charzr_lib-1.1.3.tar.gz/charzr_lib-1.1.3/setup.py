7d59e3bc6db8b97bb4bba32e6b2284dcfd9aee29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 15:03:49 +0000

    Add package charzr-lib/1.1.3/charzr_lib-1.1.3.tar.gz

---
+++ packages/charzr-lib/charzr_lib-1.1.3.tar.gz/charzr_lib-1.1.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='charzr_lib',
+    version='1.1.3'
+        ,
+    author='Missael Angel Cardenas',
+    author_email= 'missaelanggel@gmail.com',
+    description= 'Una Biblioteca que contiene la clase Random_pokemon para mostranos poquemones locos',
+    packages=["charzr_lib"],
+    package_data={'charzr_lib': ['pokemon.csv']},
+    install_requires=[
+        'pandas', 'setuptools', 'twine', 'wheel'
+    ],
+)
