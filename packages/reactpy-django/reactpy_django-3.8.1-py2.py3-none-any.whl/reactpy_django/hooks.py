3898497b742eb770581ab3393e70f2f878462ea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 14:04:26 +0000

    Add package reactpy-django/3.8.1/reactpy_django-3.8.1-py2.py3-none-any.whl

---
+++ packages/reactpy-django/reactpy_django-3.8.1-py2.py3-none-any.whl/reactpy_django/hooks.py
@@ -0,0 +1,496 @@
+from __future__ import annotations
+
+import asyncio
+import logging
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Awaitable,
+    Callable,
+    DefaultDict,
+    Sequence,
+    Union,
+    cast,
+    overload,
+)
+from uuid import uuid4
+
+import orjson
+from channels import DEFAULT_CHANNEL_LAYER
+from channels.db import database_sync_to_async
+from channels.layers import InMemoryChannelLayer, get_channel_layer
+from reactpy import use_callback, use_effect, use_memo, use_ref, use_state
+from reactpy import use_connection as _use_connection
+from reactpy import use_location as _use_location
+from reactpy import use_scope as _use_scope
+from reactpy.backend.types import Location
+
+from reactpy_django.exceptions import UserNotFoundError
+from reactpy_django.types import (
+    AsyncMessageReceiver,
+    AsyncMessageSender,
+    ConnectionType,
+    FuncParams,
+    Inferred,
+    Mutation,
+    MutationOptions,
+    Query,
+    QueryOptions,
+    UserData,
+)
+from reactpy_django.utils import generate_obj_name, get_pk
+
+if TYPE_CHECKING:
+    from channels_redis.core import RedisChannelLayer
+    from django.contrib.auth.models import AbstractUser
+
+
+_logger = logging.getLogger(__name__)
+_REFETCH_CALLBACKS: DefaultDict[Callable[..., Any], set[Callable[[], None]]] = (
+    DefaultDict(set)
+)
+
+
+# TODO: Deprecate this once the equivalent hook gets moved to reactpy.hooks.*
+def use_location() -> Location:
+    """Get the current route as a `Location` object"""
+    return _use_location()
+
+
+def use_origin() -> str | None:
+    """Get the current origin as a string. If the browser did not send an origin header,
+    this will be None."""
+    scope = _use_scope()
+    try:
+        if scope["type"] == "websocket":
+            return next(
+                (
+                    header[1].decode("utf-8")
+                    for header in scope["headers"]
+                    if header[0] == b"origin"
+                ),
+                None,
+            )
+        if scope["type"] == "http":
+            host = next(
+                (
+                    header[1].decode("utf-8")
+                    for header in scope["headers"]
+                    if header[0] == b"host"
+                )
+            )
+            return f"{scope['scheme']}://{host}" if host else None
+    except Exception:
+        _logger.info("Failed to get origin")
+    return None
+
+
+# TODO: Deprecate this once the equivalent hook gets moved to reactpy.hooks.*
+def use_scope() -> dict[str, Any]:
+    """Get the current ASGI scope dictionary"""
+    scope = _use_scope()
+
+    if isinstance(scope, dict):
+        return scope
+
+    raise TypeError(f"Expected scope to be a dict, got {type(scope)}")
+
+
+# TODO: Deprecate this once the equivalent hook gets moved to reactpy.hooks.*
+def use_connection() -> ConnectionType:
+    """Get the current `Connection` object"""
+    return _use_connection()
+
+
+@overload
+def use_query(
+    options: QueryOptions,
+    /,
+    query: Callable[FuncParams, Awaitable[Inferred]] | Callable[FuncParams, Inferred],
+    *args: FuncParams.args,
+    **kwargs: FuncParams.kwargs,
+) -> Query[Inferred]: ...
+
+
+@overload
+def use_query(
+    query: Callable[FuncParams, Awaitable[Inferred]] | Callable[FuncParams, Inferred],
+    *args: FuncParams.args,
+    **kwargs: FuncParams.kwargs,
+) -> Query[Inferred]: ...
+
+
+def use_query(*args, **kwargs) -> Query[Inferred]:
+    """This hook is used to execute functions in the background and return the result, \
+        typically to read data the Django ORM.
+
+    Args:
+        options: An optional `QueryOptions` object that can modify how the query is executed.
+        query: A callable that returns a Django `Model` or `QuerySet`.
+        *args: Positional arguments to pass into `query`.
+
+    Keyword Args:
+        **kwargs: Keyword arguments to pass into `query`."""
+
+    should_execute, set_should_execute = use_state(True)
+    data, set_data = use_state(cast(Inferred, None))
+    loading, set_loading = use_state(True)
+    error, set_error = use_state(cast(Union[Exception, None], None))
+    if isinstance(args[0], QueryOptions):
+        query_options, query, query_args, query_kwargs = _use_query_args_1(
+            *args, **kwargs
+        )
+    else:
+        query_options, query, query_args, query_kwargs = _use_query_args_2(
+            *args, **kwargs
+        )
+    query_ref = use_ref(query)
+    if query_ref.current is not query:
+        raise ValueError(f"Query function changed from {query_ref.current} to {query}.")
+
+    async def execute_query() -> None:
+        """The main running function for `use_query`"""
+        try:
+            # Run the query
+            if asyncio.iscoroutinefunction(query):
+                new_data = await query(*query_args, **query_kwargs)
+            else:
+                new_data = await database_sync_to_async(
+                    query,
+                    thread_sensitive=query_options.thread_sensitive,
+                )(*query_args, **query_kwargs)
+
+            # Run the postprocessor
+            if query_options.postprocessor:
+                if asyncio.iscoroutinefunction(query_options.postprocessor):
+                    new_data = await query_options.postprocessor(
+                        new_data, **query_options.postprocessor_kwargs
+                    )
+                else:
+                    new_data = await database_sync_to_async(
+                        query_options.postprocessor,
+                        thread_sensitive=query_options.thread_sensitive,
+                    )(new_data, **query_options.postprocessor_kwargs)
+
+        # Log any errors and set the error state
+        except Exception as e:
+            set_data(cast(Inferred, None))
+            set_loading(False)
+            set_error(e)
+            _logger.exception(f"Failed to execute query: {generate_obj_name(query)}")
+            return
+
+        # Query was successful
+        else:
+            set_data(new_data)
+            set_loading(False)
+            set_error(None)
+
+    @use_effect(dependencies=None)
+    def schedule_query() -> None:
+        """Schedule the query to be run when needed"""
+        # Make sure we don't re-execute the query unless we're told to
+        if not should_execute:
+            return
+        set_should_execute(False)
+
+        # Execute the query in the background
+        asyncio.create_task(execute_query())
+
+    @use_callback
+    def refetch() -> None:
+        """Callable provided to the user, used to re-execute the query"""
+        set_should_execute(True)
+        set_loading(True)
+        set_error(None)
+
+    @use_effect(dependencies=[])
+    def register_refetch_callback() -> Callable[[], None]:
+        """Track the refetch callback so we can re-execute the query"""
+        # By tracking callbacks globally, any usage of the query function will be re-run
+        # if the user has told a mutation to refetch it.
+        _REFETCH_CALLBACKS[query].add(refetch)
+        return lambda: _REFETCH_CALLBACKS[query].remove(refetch)
+
+    # The query's user API
+    return Query(data, loading, error, refetch)
+
+
+@overload
+def use_mutation(
+    options: MutationOptions,
+    mutation: (
+        Callable[FuncParams, bool | None] | Callable[FuncParams, Awaitable[bool | None]]
+    ),
+    refetch: Callable[..., Any] | Sequence[Callable[..., Any]] | None = None,
+) -> Mutation[FuncParams]: ...
+
+
+@overload
+def use_mutation(
+    mutation: (
+        Callable[FuncParams, bool | None] | Callable[FuncParams, Awaitable[bool | None]]
+    ),
+    refetch: Callable[..., Any] | Sequence[Callable[..., Any]] | None = None,
+) -> Mutation[FuncParams]: ...
+
+
+def use_mutation(*args: Any, **kwargs: Any) -> Mutation[FuncParams]:
+    """This hook is used to modify data in the background, typically to create/update/delete \
+    data from the Django ORM.
+        
+    Mutation functions can `return False` to prevent executing your `refetch` function. All \
+    other returns are ignored. Mutation functions can be sync or async.
+
+    Args:
+        mutation: A callable that performs Django ORM create, update, or delete \
+            functionality. If this function returns `False`, then your `refetch` \
+            function will not be used.
+        refetch:  A query function (the function you provide to your `use_query` \
+            hook) or a sequence of query functions that need a `refetch` if the \
+            mutation succeeds. This is useful for refreshing data after a mutation \
+            has been performed.
+    """
+
+    loading, set_loading = use_state(False)
+    error, set_error = use_state(cast(Union[Exception, None], None))
+    if isinstance(args[0], MutationOptions):
+        mutation_options, mutation, refetch = _use_mutation_args_1(*args, **kwargs)
+    else:
+        mutation_options, mutation, refetch = _use_mutation_args_2(*args, **kwargs)
+
+    # The main "running" function for `use_mutation`
+    async def execute_mutation(exec_args, exec_kwargs) -> None:
+        # Run the mutation
+        try:
+            if asyncio.iscoroutinefunction(mutation):
+                should_refetch = await mutation(*exec_args, **exec_kwargs)
+            else:
+                should_refetch = await database_sync_to_async(
+                    mutation, thread_sensitive=mutation_options.thread_sensitive
+                )(*exec_args, **exec_kwargs)
+
+        # Log any errors and set the error state
+        except Exception as e:
+            set_loading(False)
+            set_error(e)
+            _logger.exception(
+                f"Failed to execute mutation: {generate_obj_name(mutation)}"
+            )
+
+        # Mutation was successful
+        else:
+            set_loading(False)
+            set_error(None)
+
+            # `refetch` will execute unless explicitly told not to
+            # or if `refetch` was not defined.
+            if should_refetch is not False and refetch:
+                for query in (refetch,) if callable(refetch) else refetch:
+                    for callback in _REFETCH_CALLBACKS.get(query) or ():
+                        callback()
+
+    # Schedule the mutation to be run when needed
+    @use_callback
+    def schedule_mutation(
+        *exec_args: FuncParams.args, **exec_kwargs: FuncParams.kwargs
+    ) -> None:
+        # Set the loading state.
+        # It's okay to re-execute the mutation if we're told to. The user
+        # can use the `loading` state to prevent this.
+        set_loading(True)
+
+        # Execute the mutation in the background
+        asyncio.ensure_future(
+            execute_mutation(exec_args=exec_args, exec_kwargs=exec_kwargs)
+        )
+
+    # Used when the user has told us to reset this mutation
+    @use_callback
+    def reset() -> None:
+        set_loading(False)
+        set_error(None)
+
+    # The mutation's user API
+    return Mutation(schedule_mutation, loading, error, reset)
+
+
+def use_user() -> AbstractUser:
+    """Get the current `User` object from either the WebSocket or HTTP request."""
+    connection = use_connection()
+    user = connection.scope.get("user") or getattr(connection.carrier, "user", None)
+    if user is None:
+        raise UserNotFoundError("No user is available in the current environment.")
+    return user
+
+
+def use_user_data(
+    default_data: (
+        None | dict[str, Callable[[], Any] | Callable[[], Awaitable[Any]] | Any]
+    ) = None,
+    save_default_data: bool = False,
+) -> UserData:
+    """Get or set user data stored within the REACTPY_DATABASE.
+    
+    Kwargs:
+        default_data: A dictionary containing `{key: default_value}` pairs. \
+            For computationally intensive defaults, your `default_value` \
+            can be sync or async functions that return the value to set.
+        save_default_data: If True, `default_data` values will automatically be stored \
+            within the database if they do not exist.
+    """
+    from reactpy_django.models import UserDataModel
+
+    user = use_user()
+
+    async def _set_user_data(data: dict):
+        if not isinstance(data, dict):
+            raise TypeError(f"Expected dict while setting user data, got {type(data)}")
+        if user.is_anonymous:
+            raise ValueError("AnonymousUser cannot have user data.")
+
+        pk = get_pk(user)
+        model, _ = await UserDataModel.objects.aget_or_create(user_pk=pk)
+        model.data = orjson.dumps(data)
+        await model.asave()
+
+    query: Query[dict | None] = use_query(
+        QueryOptions(postprocessor=None),
+        _get_user_data,
+        user=user,
+        default_data=default_data,
+        save_default_data=save_default_data,
+    )
+    mutation = use_mutation(_set_user_data, refetch=_get_user_data)
+
+    return UserData(query, mutation)
+
+
+def use_channel_layer(
+    name: str | None = None,
+    *,
+    group_name: str | None = None,
+    group_add: bool = True,
+    group_discard: bool = True,
+    receiver: AsyncMessageReceiver | None = None,
+    layer: str = DEFAULT_CHANNEL_LAYER,
+) -> AsyncMessageSender:
+    """
+    Subscribe to a Django Channels layer to send/receive messages.
+
+    Args:
+        name: The name of the channel to subscribe to. If you define a `group_name`, you \
+            can keep `name` undefined to auto-generate a unique name.
+        group_name: If configured, any messages sent within this hook will be broadcasted \
+            to all channels in this group.
+        group_add: If `True`, the channel will automatically be added to the group \
+            when the component mounts.
+        group_discard: If `True`, the channel will automatically be removed from the \
+            group when the component dismounts.
+        receiver: An async function that receives a `message: dict` from a channel. \
+            If more than one receiver waits on the same channel name, a random receiver \
+            will get the result.
+        layer: The channel layer to use. This layer must be defined in \
+            `settings.py:CHANNEL_LAYERS`.
+    """
+    channel_layer: InMemoryChannelLayer | RedisChannelLayer = get_channel_layer(layer)
+    channel_name = use_memo(lambda: str(name or uuid4()))
+
+    if not name and not group_name:
+        raise ValueError("You must define a `name` or `group_name` for the channel.")
+
+    if not channel_layer:
+        raise ValueError(
+            f"Channel layer '{layer}' is not available. Are you sure you"
+            " configured settings.py:CHANNEL_LAYERS properly?"
+        )
+
+    # Add/remove a group's channel during component mount/dismount respectively.
+    @use_effect(dependencies=[])
+    async def group_manager():
+        if group_name and group_add:
+            await channel_layer.group_add(group_name, channel_name)
+
+        if group_name and group_discard:
+            return lambda: asyncio.run(
+                channel_layer.group_discard(group_name, channel_name)
+            )
+
+    # Listen for messages on the channel using the provided `receiver` function.
+    @use_effect
+    async def message_receiver():
+        if not receiver:
+            return
+
+        while True:
+            message = await channel_layer.receive(channel_name)
+            await receiver(message)
+
+    # User interface for sending messages to the channel
+    async def message_sender(message: dict):
+        if group_name:
+            await channel_layer.group_send(group_name, message)
+        else:
+            await channel_layer.send(channel_name, message)
+
+    return message_sender
+
+
+def use_root_id() -> str:
+    """Get the root element's ID. This value is guaranteed to be unique. Current versions of \
+        ReactPy-Django return a `uuid4` string."""
+    scope = use_scope()
+
+    return scope["reactpy"]["id"]
+
+
+def _use_query_args_1(options: QueryOptions, /, query: Query, *args, **kwargs):
+    return options, query, args, kwargs
+
+
+def _use_query_args_2(query: Query, *args, **kwargs):
+    return QueryOptions(), query, args, kwargs
+
+
+def _use_mutation_args_1(options: MutationOptions, mutation: Mutation, refetch=None):
+    return options, mutation, refetch
+
+
+def _use_mutation_args_2(mutation, refetch=None):
+    return MutationOptions(), mutation, refetch
+
+
+async def _get_user_data(
+    user: AbstractUser, default_data: None | dict, save_default_data: bool
+) -> dict | None:
+    """The mutation function for `use_user_data`"""
+    from reactpy_django.models import UserDataModel
+
+    if not user or user.is_anonymous:
+        return None
+
+    pk = get_pk(user)
+    model, _ = await UserDataModel.objects.aget_or_create(user_pk=pk)
+    data = orjson.loads(model.data) if model.data else {}
+
+    if not isinstance(data, dict):
+        raise TypeError(f"Expected dict while loading user data, got {type(data)}")
+
+    # Set default values, if needed
+    if default_data:
+        changed = False
+        for key, value in default_data.items():
+            if key not in data:
+                new_value: Any = value
+                if asyncio.iscoroutinefunction(value):
+                    new_value = await value()
+                elif callable(value):
+                    new_value = value()
+                data[key] = new_value
+                changed = True
+        if changed:
+            model.data = orjson.dumps(data)
+            if save_default_data:
+                await model.asave()
+
+    return data
---
+++ packages/reactpy-django/reactpy_django-3.8.1-py2.py3-none-any.whl/reactpy_django/templatetags/reactpy.py
@@ -0,0 +1,221 @@
+from __future__ import annotations
+
+from logging import getLogger
+from uuid import uuid4
+
+import dill as pickle
+from django import template
+from django.http import HttpRequest
+from django.urls import NoReverseMatch, reverse
+from reactpy.backend.hooks import ConnectionContext
+from reactpy.backend.types import Connection, Location
+from reactpy.core.types import ComponentConstructor
+from reactpy.utils import vdom_to_html
+
+from reactpy_django import config, models
+from reactpy_django.exceptions import (
+    ComponentCarrierError,
+    ComponentDoesNotExistError,
+    ComponentParamError,
+    InvalidHostError,
+    OfflineComponentMissing,
+)
+from reactpy_django.types import ComponentParams
+from reactpy_django.utils import SyncLayout, strtobool, validate_component_args
+
+try:
+    RESOLVED_WEB_MODULES_PATH = reverse("reactpy:web_modules", args=["/"]).strip("/")
+except NoReverseMatch:
+    RESOLVED_WEB_MODULES_PATH = ""
+register = template.Library()
+_logger = getLogger(__name__)
+
+
+@register.inclusion_tag("reactpy/component.html", takes_context=True)
+def component(
+    context: template.RequestContext,
+    dotted_path: str,
+    *args,
+    host: str | None = None,
+    prerender: str = str(config.REACTPY_PRERENDER),
+    offline: str = "",
+    **kwargs,
+):
+    """This tag is used to embed an existing ReactPy component into your HTML template.
+
+    Args:
+        dotted_path: The dotted path to the component to render.
+        *args: The positional arguments to provide to the component.
+
+    Keyword Args:
+        class: The HTML class to apply to the top-level component div.
+        key: Force the component's root node to use a specific key value. Using \
+            key within a template tag is effectively useless.
+        host: The host to use for the ReactPy connections. If set to `None`, \
+            the host will be automatically configured. \
+            Example values include: `localhost:8000`, `example.com`, `example.com/subdir`
+        prerender: Configures whether to pre-render this component, which \
+            enables SEO compatibility and reduces perceived latency.
+        offline: The dotted path to the component to render when the client is offline.
+        **kwargs: The keyword arguments to provide to the component.
+
+    Example ::
+
+        {% load reactpy %}
+        <!DOCTYPE html>
+        <html>
+        <body>
+            {% component "example_project.my_app.components.hello_world" recipient="World" %}
+        </body>
+        </html>
+    """
+    request: HttpRequest | None = context.get("request")
+    perceived_host = (request.get_host() if request else "").strip("/")
+    host = (
+        host
+        or (next(config.REACTPY_DEFAULT_HOSTS) if config.REACTPY_DEFAULT_HOSTS else "")
+    ).strip("/")
+    is_local = not host or host.startswith(perceived_host)
+    uuid = str(uuid4())
+    class_ = kwargs.pop("class", "")
+    has_args = bool(args or kwargs)
+    user_component: ComponentConstructor | None = None
+    _prerender_html = ""
+    _offline_html = ""
+
+    # Validate the host
+    if host and config.REACTPY_DEBUG_MODE:
+        try:
+            validate_host(host)
+        except InvalidHostError as e:
+            return failure_context(dotted_path, e)
+
+    # Fetch the component
+    if is_local:
+        user_component = config.REACTPY_REGISTERED_COMPONENTS.get(dotted_path)
+        if not user_component:
+            msg = f"Component '{dotted_path}' is not registered as a root component. "
+            _logger.error(msg)
+            return failure_context(dotted_path, ComponentDoesNotExistError(msg))
+
+    # Validate the component args & kwargs
+    if is_local and config.REACTPY_DEBUG_MODE:
+        try:
+            validate_component_args(user_component, *args, **kwargs)
+        except ComponentParamError as e:
+            _logger.error(str(e))
+            return failure_context(dotted_path, e)
+
+    # Store args & kwargs in the database (fetched by our websocket later)
+    if has_args:
+        try:
+            save_component_params(args, kwargs, uuid)
+        except Exception as e:
+            _logger.exception(
+                "An unknown error has occurred while saving component params for '%s'.",
+                dotted_path,
+            )
+            return failure_context(dotted_path, e)
+
+    # Pre-render the component, if requested
+    if strtobool(prerender):
+        if not is_local:
+            msg = "Cannot pre-render non-local components."
+            _logger.error(msg)
+            return failure_context(dotted_path, ComponentDoesNotExistError(msg))
+        if not user_component:
+            msg = "Cannot pre-render component that is not registered."
+            _logger.error(msg)
+            return failure_context(dotted_path, ComponentDoesNotExistError(msg))
+        if not request:
+            msg = (
+                "Cannot pre-render component without a HTTP request. Are you missing the "
+                "request context processor in settings.py:TEMPLATES['OPTIONS']['context_processors']?"
+            )
+            _logger.error(msg)
+            return failure_context(dotted_path, ComponentCarrierError(msg))
+        _prerender_html = prerender_component(
+            user_component, args, kwargs, uuid, request
+        )
+
+    # Fetch the offline component's HTML, if requested
+    if offline:
+        offline_component = config.REACTPY_REGISTERED_COMPONENTS.get(offline)
+        if not offline_component:
+            msg = f"Cannot render offline component '{offline}'. It is not registered as a component."
+            _logger.error(msg)
+            return failure_context(dotted_path, OfflineComponentMissing(msg))
+        if not request:
+            msg = (
+                "Cannot render an offline component without a HTTP request. Are you missing the "
+                "request context processor in settings.py:TEMPLATES['OPTIONS']['context_processors']?"
+            )
+            _logger.error(msg)
+            return failure_context(dotted_path, ComponentCarrierError(msg))
+        _offline_html = prerender_component(offline_component, [], {}, uuid, request)
+
+    # Return the template rendering context
+    return {
+        "reactpy_class": class_,
+        "reactpy_uuid": uuid,
+        "reactpy_host": host or perceived_host,
+        "reactpy_url_prefix": config.REACTPY_URL_PREFIX,
+        "reactpy_component_path": f"{dotted_path}/{uuid}/{int(has_args)}/",
+        "reactpy_resolved_web_modules_path": RESOLVED_WEB_MODULES_PATH,
+        "reactpy_reconnect_interval": config.REACTPY_RECONNECT_INTERVAL,
+        "reactpy_reconnect_max_interval": config.REACTPY_RECONNECT_MAX_INTERVAL,
+        "reactpy_reconnect_backoff_multiplier": config.REACTPY_RECONNECT_BACKOFF_MULTIPLIER,
+        "reactpy_reconnect_max_retries": config.REACTPY_RECONNECT_MAX_RETRIES,
+        "reactpy_prerender_html": _prerender_html,
+        "reactpy_offline_html": _offline_html,
+    }
+
+
+def failure_context(dotted_path: str, error: Exception):
+    return {
+        "reactpy_failure": True,
+        "reactpy_debug_mode": config.REACTPY_DEBUG_MODE,
+        "reactpy_dotted_path": dotted_path,
+        "reactpy_error": type(error).__name__,
+    }
+
+
+def save_component_params(args, kwargs, uuid):
+    params = ComponentParams(args, kwargs)
+    model = models.ComponentSession(uuid=uuid, params=pickle.dumps(params))
+    model.full_clean()
+    model.save()
+
+
+def validate_host(host: str):
+    if "://" in host:
+        protocol = host.split("://")[0]
+        msg = (
+            f"Invalid host provided to component. Contains a protocol '{protocol}://'."
+        )
+        _logger.error(msg)
+        raise InvalidHostError(msg)
+
+
+def prerender_component(
+    user_component: ComponentConstructor, args, kwargs, uuid, request: HttpRequest
+):
+    search = request.GET.urlencode()
+    scope = getattr(request, "scope", {})
+    scope["reactpy"] = {"id": str(uuid)}
+
+    with SyncLayout(
+        ConnectionContext(
+            user_component(*args, **kwargs),
+            value=Connection(
+                scope=scope,
+                location=Location(
+                    pathname=request.path, search=f"?{search}" if search else ""
+                ),
+                carrier=request,
+            ),
+        )
+    ) as layout:
+        vdom_tree = layout.render()["model"]
+
+    return vdom_to_html(vdom_tree)
