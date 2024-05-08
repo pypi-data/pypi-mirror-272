2f521f3d4f15376e89fd19517e7d292fcb59c9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 04:03:52 +0000

    Add package edgegap-logging/1.7.0/edgegap_logging-1.7.0-py3-none-any.whl

---
+++ packages/edgegap-logging/edgegap_logging-1.7.0-py3-none-any.whl/edgegap_logging/__init__.py
@@ -0,0 +1,10 @@
+from ._configuration import LoggingConfiguration
+from ._format import Format, Color
+from ._logging import DefaultFormatter
+
+__all__ = [
+    'Color',
+    'Format',
+    'DefaultFormatter',
+    'LoggingConfiguration',
+]
