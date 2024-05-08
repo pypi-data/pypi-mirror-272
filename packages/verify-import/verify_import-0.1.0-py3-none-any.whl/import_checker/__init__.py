d0cb5fa1ea02885d98c39aecf97b91342f601a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 04:03:39 +0000

    Add package verify-import/0.1.0/verify_import-0.1.0-py3-none-any.whl

---
+++ packages/verify-import/verify_import-0.1.0-py3-none-any.whl/import_checker/__init__.py
@@ -0,0 +1,6 @@
+"""
+### Import Checker
+> Simple tool to verify all submodules import without error
+"""
+import lazy_loader as lazy
+__getattr__, __dir__, __all__ = lazy.attach_stub(__name__, __file__)
