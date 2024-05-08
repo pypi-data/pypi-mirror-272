ccd3858e2dbf886b40ebd056710ef09af75c7d42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 16:03:55 +0000

    Add package arvi/0.1.11/arvi-0.1.11-py3-none-any.whl

---
+++ packages/arvi/arvi-0.1.11-py3-none-any.whl/arvi/__init__.py
@@ -0,0 +1,18 @@
+__all__ = ['RV']
+
+from .timeseries import RV
+
+_ran_once = False
+
+def __getattr__(name: str):
+    if name in (
+        '_ipython_canary_method_should_not_exist_',
+        '_repr_mimebundle_',
+    ):
+        return
+
+    global _ran_once  # can't do it any other way :(
+    if _ran_once:
+        return RV(name)
+    else:
+        _ran_once = True
