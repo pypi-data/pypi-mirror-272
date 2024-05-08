bfabcbdca5121e8de2c31ebb5269e892b14f5e67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 15:03:48 +0000

    Add package Latifa/0.1/Latifa-0.1.tar.gz

---
+++ packages/Latifa/Latifa-0.1.tar.gz/Latifa-0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name = 'Latifa',
+    version = '0.1',
+    author = 'Latifa',
+    description = 'Simple code that encrypts',
+    packages = setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
