27c426cb8f87b93385a62fa199da45f3ef7cd30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 05:03:37 +0000

    Add package angr-management/9.2.102/angr_management-9.2.102-py3-none-any.whl

---
+++ packages/angr-management/angr_management-9.2.102-py3-none-any.whl/angrmanagement/utils/layout.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Iterable
+
+if TYPE_CHECKING:
+    from PySide6.QtWidgets import QGridLayout, QWidget
+
+
+def add_to_grid(lyt: QGridLayout, cols: int, widgets: Iterable[QWidget]) -> None:
+    """
+    Adds widgets to a grid layout given a desired column count.
+    """
+    r = lyt.rowCount()
+    c = 0
+    for item in widgets:
+        lyt.addWidget(item, r, c)
+        c += 1
+        if c >= cols:
+            c = 0
+            r += 1
