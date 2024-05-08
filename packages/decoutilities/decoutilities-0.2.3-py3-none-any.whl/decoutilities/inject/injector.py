7c099870f2e04d57860c8c5f4a623a9f401dd6be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 16:04:37 +0000

    Add package decoutilities/0.2.3/decoutilities-0.2.3-py3-none-any.whl

---
+++ packages/decoutilities/decoutilities-0.2.3-py3-none-any.whl/decoutilities/inject/injector.py
@@ -0,0 +1,19 @@
+class injector:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self):
+        self.registry = {}
+
+    def register(self, key):
+        def decorator(func):
+            self.registry[key] = func
+            return func
+        return decorator
+
+    def inject(self, key):
+        return self.registry.get(key)
