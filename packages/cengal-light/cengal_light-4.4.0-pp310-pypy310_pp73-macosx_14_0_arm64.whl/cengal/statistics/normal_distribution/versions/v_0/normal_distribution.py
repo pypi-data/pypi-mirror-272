84116c311d65bf13ca1b7d3736dee6884555ce9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 15:03:46 +0000

    Add package cengal-light/4.4.0/cengal_light-4.4.0-pp310-pypy310_pp73-macosx_14_0_arm64.whl

---
+++ packages/cengal-light/cengal_light-4.4.0-pp310-pypy310_pp73-macosx_14_0_arm64.whl/cengal/statistics/normal_distribution/versions/v_0/normal_distribution.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Copyright © 2012-2024 ButenkoMS. All rights reserved. Contacts: <gtalk@butenkoms.space>
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+__all__ = ['count_99_95_68', 'guess_99_95_68', 'standard_deviation', 'variance', 'statistics', 'average', 'Number']
+
+
+from typing import Tuple, Sequence, Iterable, Union, Callable
+from collections import deque
+from math import sqrt
+from cengal.math.numbers import *
+
+
+"""
+Module Docstring
+Docstrings: http://www.python.org/dev/peps/pep-0257/
+"""
+
+__author__ = "ButenkoMS <gtalk@butenkoms.space>"
+__copyright__ = "Copyright © 2012-2024 ButenkoMS. All rights reserved. Contacts: <gtalk@butenkoms.space>"
+__credits__ = ["ButenkoMS <gtalk@butenkoms.space>", ]
+__license__ = "Apache License, Version 2.0"
+__version__ = "4.4.0"
+__maintainer__ = "ButenkoMS <gtalk@butenkoms.space>"
+__email__ = "gtalk@butenkoms.space"
+# __status__ = "Prototype"
+__status__ = "Development"
+# __status__ = "Production"
+
+
+def statistics(iterable_readings: Iterable[Number]) -> Tuple[float, Sequence[Number]]:
+    """Example
+    
+        ```
+        diff_mean, readings = statistics(iterable_readings)
+        ```
+
+    Args:
+        iterable_readings (Iterable[Number]): _description_
+
+    Returns:
+        Tuple[float, Sequence[Number]]: _description_
+    """    
+    readings = deque()
+    data_sum = 0
+    for reading in iterable_readings:
+        readings.append(reading)
+        data_sum += reading
+
+    try:
+        diff_mean = data_sum / len(readings)
+    except ZeroDivisionError:
+        diff_mean = 0
+    
+    return diff_mean, readings
+
+
+def variance(diff_mean: float, readings: Sequence[Number]) -> Tuple[float, float, float, float]:
+    """Example:
+
+        ```
+        diff_mean, readings = statistics(iterable_readings)
+        diff_mean, variance, max_deviation, min_deviation = variance(diff_mean, readings)
+        ```
+
+    Alternative example:
+
+        ```
+        diff_mean, variance, max_deviation, min_deviation = variance(*statistics(iterable_readings))
+        ```
+
+    Args:
+        diff_mean (float): _description_
+        readings (Sequence[Number]): _description_
+
+    Returns:
+        Tuple[float, float, float, float]: _description_
+    """    
+    max_deviation = None
+    min_deviation = None
+    deviation_square_sum = 0
+    for reading in readings:
+        deviation = reading - diff_mean
+        deviation_square_sum += deviation ** 2
+        if max_deviation is None:
+            max_deviation = deviation
+        max_deviation = max(max_deviation, deviation)
+        if min_deviation is None:
+            min_deviation = deviation
+        min_deviation = min(min_deviation, deviation)
+
+    try:
+        variance = deviation_square_sum / len(readings)
+    except ZeroDivisionError:
+        variance = 0
+
+    return diff_mean, variance, max_deviation, min_deviation
+
+
+def standard_deviation(diff_mean, variance, max_deviation, min_deviation) -> Tuple[float, float, float, float]:
+    """Example:
+
+        ```
+        diff_mean, readings = statistics(iterable_readings)
+        diff_mean, variance, max_deviation, min_deviation = variance(diff_mean, readings)
+        diff_mean, sd, max_deviation, min_deviation = standard_deviation(diff_mean, variance, max_deviation, min_deviation)
+        ```
+
+    Alternative example:
+
+        ```
+        diff_mean, sd, max_deviation, min_deviation = standard_deviation(*variance(*statistics(iterable_readings)))
+        ```
+
+    Args:
+        diff_mean (_type_): _description_
+        variance (_type_): _description_
+        max_deviation (_type_): _description_
+        min_deviation (_type_): _description_
+
+    Returns:
+        Tuple[float, float, float, float]: _description_
+    """    
+    return diff_mean, sqrt(variance), max_deviation, min_deviation
+
+
+def guess_99_95_68(mean, sd, max_deviation, min_deviation) -> Tuple[float, float, float, float, float]:
+    """Example:
+
+        ```
+        diff_mean, readings = statistics(iterable_readings)
+        diff_mean, variance, max_deviation, min_deviation = variance(diff_mean, readings)
+        diff_mean, sd, max_deviation, min_deviation = standard_deviation(diff_mean, variance, max_deviation, min_deviation)
+        val_99, val_95, val_68, max_deviation, min_deviation = guess_99_95_68(diff_mean, sd, max_deviation, min_deviation)
+        ```
+
+    Alternative example:
+
+        ```
+        val_99, val_95, val_68, max_deviation, min_deviation = guess_99_95_68(*standard_deviation(*variance(*statistics(iterable_readings)))
+        ```
+
+    Args:
+        mean (_type_): _description_
+        sd (_type_): _description_
+        max_deviation (_type_): _description_
+        min_deviation (_type_): _description_
+
+    Returns:
+        Tuple[float, float, float, float, float]: _description_
+    """    
+    # See: https://en.wikipedia.org/wiki/68%E2%80%9395%E2%80%9399.7_rule
+    val_68 = mean + sd
+    val_95 = mean + 2 * sd
+    val_99 = mean + 3 * sd
+    return val_99, val_95, val_68, max_deviation, min_deviation
+
+
+def average(data: Sequence[Number]) -> Number:
+    data_len = len(data)
+    data_sum = sum(data)
+    try:
+        return data_sum / data_len
+    except ZeroDivisionError:
+        return 0
+
+
+def count_99_95_68(iterable_readings: Iterable[Number], operation: Callable=average) -> Tuple[float, float, float, float, float]:
+    """_summary_
+
+    Args:
+        iterable_readings (Iterable[Number]): _description_
+        operation (Callable, optional): _description_. Defaults to average. You can use other functions like min, max, median, etc.
+
+    Returns:
+        Tuple[float, float, float, float, float]: _description_
+    """    
+    diff_mean, readings = statistics(iterable_readings)
+    _, _, max_deviation, min_deviation = variance(diff_mean, readings)
+    sorted_readings = sorted(readings)
+    readings_qnt = len(sorted_readings)
+
+    def get_slice(data: Sequence, divider: int) -> Sequence:
+        data_slice = data[:divider]
+        if len(data_slice) == len(data):
+            data_slice = data[:len(data)-1]
+        return data_slice
+
+    div_68 = round(readings_qnt * 0.68)
+    val_68 = operation(get_slice(sorted_readings, div_68))
+
+    div_95 = round(readings_qnt * 0.95)
+    val_95 = operation(get_slice(sorted_readings, div_95))
+
+    div_99 = round(readings_qnt * 0.99)
+    val_99 = operation(get_slice(sorted_readings, div_99))
+
+    return val_99, val_95, val_68, max_deviation, min_deviation
+
---
+++ packages/cengal-light/cengal_light-4.4.0-pp310-pypy310_pp73-macosx_14_0_arm64.whl/cengal/text_processing/encoding_detection/versions/v_0/encoding_detection.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Copyright © 2012-2024 ButenkoMS. All rights reserved. Contacts: <gtalk@butenkoms.space>
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+__all__ = [
+    'decode',
+    'detect_and_decode',
+    'is_utf8_text',
+    'is_text_is_7bit_utf8_compatible',
+    'is_probably_utf8',
+    'is_utf8',
+]
+
+
+from typing import Tuple, Union
+import cchardet as chardet
+from cengal.modules_management.alternative_import import alt_import
+with alt_import('cchardet') as chardet:
+    if chardet is None:
+        CHARDET_PRESENT: bool = False
+    else:
+        CHARDET_PRESENT = True
+
+from charset_normalizer import detect as cn_detect
+from cengal.text_processing.text_processing import Text, normalize_text
+from cengal.text_processing.utf_bom_processing import *
+
+
+"""
+Module Docstring
+Docstrings: http://www.python.org/dev/peps/pep-0257/
+"""
+
+__author__ = "ButenkoMS <gtalk@butenkoms.space>"
+__copyright__ = "Copyright © 2012-2024 ButenkoMS. All rights reserved. Contacts: <gtalk@butenkoms.space>"
+__credits__ = ["ButenkoMS <gtalk@butenkoms.space>", ]
+__license__ = "Apache License, Version 2.0"
+__version__ = "4.4.0"
+__maintainer__ = "ButenkoMS <gtalk@butenkoms.space>"
+__email__ = "gtalk@butenkoms.space"
+# __status__ = "Prototype"
+__status__ = "Development"
+# __status__ = "Production"
+
+
+def detect_and_decode(text: Union[bytes, bytearray], detect_as_utf8_when_possible: bool = True, check_text_for_utf8_compliance: bool = True) -> Tuple[str, str, bytes]:
+    if not text:
+        return str(), 'utf-8', bytes()
+
+    text = normalize_text(text, bytes)
+    possible_utf_bom = determine_text_bom(text)
+    text = remove_bom(text, possible_utf_bom)
+    possible_encoding = determine_bom_encoding(possible_utf_bom)
+    if possible_encoding is not None:
+        return decode_text_and_remove_all_wrong_symbols(text, possible_encoding), possible_encoding, possible_utf_bom
+    else:
+        try_charset_normalizer = False
+        try:
+            if CHARDET_PRESENT:
+                detection = chardet.detect(text)
+            else:
+                try_charset_normalizer = True
+        except LookupError:
+            try_charset_normalizer = True
+        
+        if try_charset_normalizer:
+            detection = cn_detect(text)
+            
+        encoding = detection["encoding"]
+        if detect_as_utf8_when_possible:
+            if check_text_for_utf8_compliance:
+                result_encoding = 'utf-8' if is_utf8_text(encoding, text) else encoding
+            else:
+                result_encoding = 'utf-8' if (is_utf8(encoding) or is_probably_utf8(encoding)) else encoding
+        else:
+            result_encoding = encoding
+        
+        bom_bytes = bytes()
+        return text.decode(encoding), result_encoding, bom_bytes
+
+
+utf8_compatible_encodings = {
+    'utf-8',
+    'ISO-8859-1',
+    'Latin 1',
+}
+utf8_compatible_encodings_lower = {encoding.lower() for encoding in utf8_compatible_encodings}
+
+
+utf8_half_compatible_encodings = {
+    'US-ASCII',
+    'ASCII',
+    'ANSI_X3.4-1968',
+    'iso-ir-6',
+    'ANSI_X3.4-1986',
+    'ISO_646.irv:1991',
+    'ASCII-7',
+    'ASCII-8',
+    'ISO646-US',
+    'us',
+    'IBM367',
+    'cp367',
+    'csASCII',
+}
+utf8_half_compatible_encodings_lower = {encoding.lower() for encoding in utf8_half_compatible_encodings}
+
+
+def is_utf8(encoding: str) -> bool:
+    return encoding.lower() in utf8_compatible_encodings_lower
+
+
+def is_probably_utf8(encoding: str) -> bool:
+    return encoding.lower() in utf8_half_compatible_encodings_lower
+
+
+def is_text_is_7bit_utf8_compatible(text: Union[bytes, bytearray]) -> bool:
+    return all(b <= 127 for b in text)
+
+
+def is_utf8_text(encoding: str, text: Union[bytes, bytearray]) -> bool:
+    if is_utf8(encoding):
+        return True
+    elif is_probably_utf8(encoding):
+        return is_text_is_7bit_utf8_compatible(text)
+    else:
+        return False
+
+
+def decode(text: Union[bytes, bytearray]) -> str:
+    text, encoding, bom_bytes = detect_and_decode(text)
+    return text
