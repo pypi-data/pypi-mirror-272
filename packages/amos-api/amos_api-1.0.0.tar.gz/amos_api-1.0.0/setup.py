bbeef32ba8c68e3ab2d03a9beb2d2231ce3abb0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 16:05:02 +0000

    Add package amos-api/1.0.0/amos_api-1.0.0.tar.gz

---
+++ packages/amos-api/amos_api-1.0.0.tar.gz/amos_api-1.0.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='amos_api',
+    version='1.0.0',
+    description='AMOS common api',
+    author='ilex.h',
+    author_email='390132625@qq.com',
+    url='https://github.com/aptray/python-pkg',
+    packages=setuptools.find_packages(),
+    py_modules=['amos_api'],
+)
