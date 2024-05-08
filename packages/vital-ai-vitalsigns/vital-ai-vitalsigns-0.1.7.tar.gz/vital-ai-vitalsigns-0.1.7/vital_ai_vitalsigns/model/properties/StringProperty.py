a7d370d82fcced0879d294068eae4e4bd8c8a3e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 04:03:05 +0000

    Add package vital-ai-vitalsigns/0.1.7/vital-ai-vitalsigns-0.1.7.tar.gz

---
+++ packages/vital-ai-vitalsigns/vital-ai-vitalsigns-0.1.7.tar.gz/vital-ai-vitalsigns-0.1.7/vital_ai_vitalsigns/model/properties/StringProperty.py
@@ -0,0 +1,29 @@
+from .IProperty import IProperty
+
+
+class StringProperty(IProperty):
+    def __init__(self, value: str):
+        super().__init__(value)
+
+    def __bool__(self):
+        return bool(self.value)
+
+    def __getattr__(self, attr):
+        return getattr(self.value, attr)
+
+    def __str__(self):
+        return str(self.value)
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.value == other
+        elif isinstance(other, StringProperty):
+            return self.value == other.value
+        return NotImplemented
+
+    def __rshift__(self, other):
+        return self == other
+
+    def to_json(self):
+        return {"value": self.value}
+
