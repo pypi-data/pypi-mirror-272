ef4814086619f777d69e18caa743592ee5c7a751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 07:03:37 +0000

    Add package baseapp-url-shortening/0.1.3/baseapp-url-shortening-0.1.3.tar.gz

---
+++ packages/baseapp-url-shortening/baseapp-url-shortening-0.1.3.tar.gz/baseapp-url-shortening-0.1.3/baseapp_url_shortening/models.py
@@ -0,0 +1,20 @@
+import short_url
+from django.db import models
+from django.urls import reverse
+from model_utils.models import TimeStampedModel
+
+
+class ShortUrl(TimeStampedModel):
+    short_code = models.CharField(max_length=30, editable=False, unique=True, db_index=True)
+    full_url = models.URLField(max_length=2000)
+
+    @property
+    def short_url_path(self) -> str:
+        return reverse("v1:short_url_redirect_full_url", kwargs=dict(short_code=self.short_code))
+
+    def save(self, *args, **kwargs):
+        creating = not self.pk
+        super().save(*args, **kwargs)
+        if creating:
+            self.short_code = short_url.encode_url(self.pk)
+            super().save(update_fields=["short_code"])
