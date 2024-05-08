917df6f94a74146bdae79a21fa345e5cec680fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 15:04:24 +0000

    Add package wonder-diffusion-sdk/0.0.6.dev2/wonder_diffusion_sdk-0.0.6.dev2-py3-none-any.whl

---
+++ packages/wonder-diffusion-sdk/wonder_diffusion_sdk-0.0.6.dev2-py3-none-any.whl/wonder_diffusion_sdk/config/__init__.py
@@ -0,0 +1,5 @@
+from .sdk_config import WonderDiffusionSdkConfig
+from .model_config import WonderDiffusionModelConfig
+from .globals import DEVICE
+from .lora_config import WonderLora, WonderLoraConfig
+from .controlnet_config import WonderControlNet, WonderControlNetConfig
