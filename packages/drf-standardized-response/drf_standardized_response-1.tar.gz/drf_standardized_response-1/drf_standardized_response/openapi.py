07b8b12312dfe22dd62321a9e2fe03487e5f7a71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 03:03:29 +0000

    Add package drf-standardized-response/1/drf_standardized_response-1.tar.gz

---
+++ packages/drf-standardized-response/drf_standardized_response-1.tar.gz/drf_standardized_response-1/drf_standardized_response/openapi.py
@@ -0,0 +1,86 @@
+from django.test.client import RequestFactory
+from rest_framework.response import Response
+from drf_standardized_errors.openapi import AutoSchema as BaseAutoSchema
+from drf_spectacular.utils import OpenApiResponse
+from .settings import package_settings
+
+
+class StandardizedSchemaMixin:
+    def _get_response_for_code(
+        self, serializer, status_code, media_types=None, direction="response"
+    ):
+        dummy_request = RequestFactory().get(self.path)
+        self._response_standardizer = (
+            package_settings.RESPONSE_STANDARDIZER_CLASS(
+                view=self._view,
+                request=dummy_request,
+            )
+        )
+
+        response = super()._get_response_for_code(
+            serializer, status_code, media_types, direction
+        )
+        if not (content := response.get("content")):
+            return response
+        if "application/json" not in content:
+            return response
+
+        schema = content["application/json"]["schema"]
+        reference = schema.get("$ref", schema.get("items", {}).get("$ref"))
+        if not reference:
+            return response
+        if "ErrorResponse" in reference:
+            return response
+
+        if isinstance(serializer, OpenApiResponse):
+            serializer = serializer.response
+        serializer_meta = getattr(serializer, "Meta", {})
+
+        if not (
+            getattr(serializer_meta, "should_standardize_schema", True)
+            and self._response_standardizer.should_standardize()
+        ):
+            return response
+
+        formatted_schema = self._standardize_response_schema(reference)
+        content["application/json"]["schema"] = formatted_schema
+        return response
+
+    def _standardize_response_schema(self, reference):
+        dummy_response = Response(status=200)
+        standardized_schema = {
+            "type": "object",
+            "properties": {
+                "success": {"type": "boolean"},
+                "message": {"type": "string"},
+            },
+        }
+
+        if not self._response_standardizer.should_wrap(dummy_response):
+            return {
+                "allOf": [
+                    {"$ref": reference},
+                    standardized_schema,
+                ]
+            }
+
+        schema_name = reference.split("/")[-1]
+        schema_key = (schema_name, "schemas")
+        schema = self.registry._components[schema_key].schema
+        unwrapped_fields = (
+            self._response_standardizer.get_wrapping_excluded_fields()
+        )
+        unwrapped_data = {
+            field: schema["properties"].pop(field, None)
+            for field in unwrapped_fields
+            if field is not None
+        }
+
+        standardized_schema["properties"].update(unwrapped_data)
+        standardized_schema["properties"]["data"] = schema
+
+        return standardized_schema
+
+
+class AutoSchema(BaseAutoSchema, StandardizedSchemaMixin):
+    pass
