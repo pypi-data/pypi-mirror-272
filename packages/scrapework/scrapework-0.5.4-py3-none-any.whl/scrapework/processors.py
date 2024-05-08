90f2c3bf8f832041baba6810281d59b1593aa125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 15:03:44 +0000

    Add package scrapework/0.5.4/scrapework-0.5.4-py3-none-any.whl

---
+++ packages/scrapework/scrapework-0.5.4-py3-none-any.whl/scrapework/processors.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from typing import Any, Dict, Iterable, Union
+
+from scrapework.core.logger import Logger
+
+
+class Processor(ABC):
+    """Processor _summary_
+
+    _extended_summary_
+
+    Args:
+        ABC (_type_): _description_
+    """
+
+    def __init__(self) -> None:
+        self.logger = Logger().get_logger()
+        self.logger.debug(f"Using processor: {self.__class__.__name__}")
+
+    @abstractmethod
+    def process_items(
+        self,
+        items: Union[Dict[str, Any], Iterable[Dict[str, Any]]],
+    ):
+        pass
