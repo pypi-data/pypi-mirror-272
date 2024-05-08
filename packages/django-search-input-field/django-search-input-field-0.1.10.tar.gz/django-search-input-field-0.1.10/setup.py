cde8695929c67a1e0a95a203d05b43ac88308002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 03:04:13 +0000

    Add package django-search-input-field/0.1.10/django-search-input-field-0.1.10.tar.gz

---
+++ packages/django-search-input-field/django-search-input-field-0.1.10.tar.gz/django-search-input-field-0.1.10/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+# Remove the unused import statement for glob
+# Add the templates folder to the build configuration
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-search-input-field',
+    version='0.1.10',
+    license='MIT',
+    author="Treyd Services AB",
+    author_email='support@treyd.io',
+    packages=find_packages(include=['django_search_input_field', 'django_search_input_field.*',
+                                    'django_search_input_field.templates', 'django_search_input_field.templates.*']),
+    include_package_data=True,
+    package_data={'django_search_input_field': ['django_search_input_field/templates/*']},
+    url='https://github.com/minaaaatef/django-search-input-form',
+    install_requires=[
+
+    ],
+)
