ab5eef66b0466570589fd6f35a85725947efe83d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 16:03:58 +0000

    Add package grasp-planning/0.4.2/grasp_planning-0.4.2.tar.gz

---
+++ packages/grasp-planning/grasp_planning-0.4.2.tar.gz/grasp_planning-0.4.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['grasp_planning',
+ 'grasp_planning.constraints',
+ 'grasp_planning.cost',
+ 'grasp_planning.solver']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['casadi>3.6',
+ 'forwardkinematics>=1.1.3',
+ 'numpy>=1.15.3,<2.0.0',
+ 'scipy<=1.10.1',
+ 'spatial-casadi>=1.1.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'grasp_planning',
+    'version': '0.4.2',
+    'description': '"Grasp and Motion Planning Python Package."',
+    'author': 'Tomas Merva',
+    'author_email': 'tmerva7@gmail.com',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.10',
+}
+
+
+setup(**setup_kwargs)
+
