199e71e36d0d84996f7939e153c78074e6a919e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 15:03:49 +0000

    Add package pulse-coupled-nn/0.0.2/pulse_coupled_nn-0.0.2.tar.gz

---
+++ packages/pulse-coupled-nn/pulse_coupled_nn-0.0.2.tar.gz/pulse_coupled_nn-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+with open("pulse_coupled_nn/README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="pulse_coupled_nn",
+    version="0.0.2",
+    description="Python implementation of the Pulse Coupled Neural Network (PCNN)",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArdeleanRichard/PCNN",
+    author="Eugen-Richard Ardelean",
+    author_email="ardeleaneugenrichard@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["numpy", "scipy", "matplotlib", "astropy"],
+    extras_require={
+        "dev": ["twine>=4.0.2"],
+    },
+    python_requires=">=3.7",
+)
