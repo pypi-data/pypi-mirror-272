5fc314091cf371fa814aae7c05d59e400478a7f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 03:04:45 +0000

    Add package DesalSim/0.2/DesalSim-0.2.tar.gz

---
+++ packages/DesalSim/DesalSim-0.2.tar.gz/DesalSim-0.2/setup.py
@@ -0,0 +1,22 @@
+#setup.py
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f: 
+    description =f.read()
+
+setup(
+      name='DesalSim',
+      version='0.2',
+      packages=find_packages(),
+      url="https://github.com/rodoulak/Desalination-and-Brine-Treatment-Simulation-",
+      author="rodoulak",
+      author_email="r.ktori@tudelft.nl",
+      license="MIT",
+      install_requires=[
+          "numpy>=1.0",
+          "pandas>=1.0",  # Include pandas as a dependency
+      ],
+      long_description= description,
+      long_description_content_type="text/markdown",
+      )
