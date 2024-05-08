ac5173bdea19b29673bed842b58aadc1a3274012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 04:03:06 +0000

    Add package moveread-pipelines-dfy/0.1.3/moveread_pipelines_dfy-0.1.3-py3-none-any.whl

---
+++ packages/moveread-pipelines-dfy/moveread_pipelines_dfy-0.1.3-py3-none-any.whl/moveread/pipelines/dfy/pipelines/_inputval.py
@@ -0,0 +1,21 @@
+from typing import Unpack
+import uvicorn
+from moveread.pipelines.input_validation import Input, Result, InputValidationAPI, fastapi, Params
+from ..generated import Inputval
+
+def pre(input: Inputval.In) -> tuple[Input, Inputval.In]:
+  return Input(gameId=input.gameId, imgs=input.imgs), input
+
+def post(entry: tuple[Result, Inputval.In]) -> Inputval.Out:
+  res, state = entry
+  return Inputval.next('preprocess', Inputval.In(gameId=res.gameId, imgs=res.imgs, model=state.model))
+
+def inputval(
+  Qin: Inputval.QueueIn, Qout: Inputval.QueueOut, *,
+  port: int = 8002,
+  host: str = '0.0.0.0',
+  **params: Unpack[Params]
+):
+  sdk = InputValidationAPI(Qin.map(pre), Qout.premap(post))
+  app = fastapi(sdk, **params)
+  uvicorn.run(app, port=port, host=host)
