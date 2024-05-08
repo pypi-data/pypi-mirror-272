c8a297e018084988b5d7808da2f42b8fc2647317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 05:03:07 +0000

    Add package phenomApis/0.0.49/phenomApis-0.0.49.tar.gz

---
+++ packages/phenomApis/phenomApis-0.0.49.tar.gz/phenomApis-0.0.49/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.49'
+DESCRIPTION = 'A Package To Access Phenom Apis'
+LONG_DESCRIPTION = '<div style="display: flex;align-items: center;gap: 16px"><p><img src="https://www.phenom.com/images/favicon-180x180.png"><br></p><p style="font-size: 80px;font-weight: 800;font-family: sans-serif;color: black;">phenom</p></div>'
+
+# Setting up
+setup(
+    name="phenomApis",
+    version=VERSION,
+    author="phenom",
+    author_email="8297991468h@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['PyJWT==2.4.0', 'certifi==2024.2.2', 'urllib3==1.26.18', 'six==1.16.0'],
+    keywords=['resumeparser', 'exsearch'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
