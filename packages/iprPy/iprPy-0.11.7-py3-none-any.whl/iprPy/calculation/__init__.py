33e71818188c53d0f54c27b47d4517825be7be49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 06:05:37 +0000

    Add package iprPy/0.11.7/iprPy-0.11.7-py3-none-any.whl

---
+++ packages/iprPy/iprPy-0.11.7-py3-none-any.whl/iprPy/calculation/__init__.py
@@ -0,0 +1,94 @@
+from io import IOBase
+import sys
+from pathlib import Path
+from importlib import import_module, resources
+from typing import Optional, Union
+
+from yabadaba.tools import ModuleManager
+
+calculationmanager = ModuleManager('Calculation')
+from .. import recordmanager
+
+from .Calculation import Calculation
+
+__all__ = ['Calculation', 'calculationmanager', 'load_calculation']
+
+# Define subfolders to ignore
+ignorelist = ['__pycache__', 'subsets']
+
+# Dynamically import calculation styles
+for style in resources.contents(__name__):
+
+    # Only import subfolders not in ignorelist
+    if '.' not in style and style not in ignorelist:
+        try:
+            # Try importing calculation
+            module = import_module(f'.{style}', __name__)
+            
+        except Exception as e:
+            # Add failed imports to managers
+            recordmanager.failed_styles[f'calculation_{style}'] = '%s: %s' % sys.exc_info()[:2]
+            calculationmanager.failed_styles[style] = '%s: %s' % sys.exc_info()[:2]
+
+        else:
+            # Add successful imports to managers
+            classname = module.__all__[0]
+            classmodule = getattr(module, classname)
+            recordmanager.loaded_styles[f'calculation_{style}'] = classmodule
+            calculationmanager.loaded_styles[style] = classmodule
+
+def load_calculation(style, **kwargs):
+    """
+    Loads a Calculation subclass associated with a given calculation style
+
+    Parameters
+    ----------
+    style : str
+        The calculation style
+    
+    Returns
+    -------
+    subclass of iprPy.calculation.Calculation 
+        A Calculation object for the style
+    """
+    return calculationmanager.init(style, **kwargs)
+
+def run_calculation(params: Union[str, dict, IOBase, None] = None,
+                    calc_style: Optional[str] = None,
+                    raise_error: bool = False,
+                    verbose: bool = True):
+    """
+    Runs a calculation from a parameter file and outputs results to
+    results.json.
+
+    Parameters
+    ----------
+    params : dict, str or file-like object, optional
+        The parameters or parameter file to read in.  If not given, will
+        run based on the current object attribute values.
+    calc_style : str, optional
+        Specifies the style of calculation to run.  Optional if params is a
+        path to a file where the file's name is calc_<calc_style>.in.
+    raise_error : bool, optional
+        The default behavior of run is to take any error messages from the
+        calculation and set them to class attributes and save to
+        results.json. This allows for calculations to successfully fail.
+        Setting this to True will instead raise the errors, which can
+        provide more details for debugging.
+    verbose : bool, optional
+        If True, a message relating to the calculation's status will be
+        printed upon completion.  Default value is True.
+    """
+    if calc_style is None:
+        # Extract calc_style from filename
+        if isinstance(params, (str, Path)):
+            calc_style = Path(params).stem.replace('calc_', '')
+        else:
+            raise ValueError('calc_style not given and cannot be determined from params')
+
+    # Load calculation style and read in paramfile
+    calculation = load_calculation(calc_style, params=params)
+    
+    # Run and create results_json
+    calculation.run(results_json=True, raise_error=raise_error, verbose=verbose)
+    
---
+++ packages/iprPy/iprPy-0.11.7-py3-none-any.whl/iprPy/calculation/energy_check/EnergyCheck.py
@@ -0,0 +1,296 @@
+# coding: utf-8
+# Standard Python libraries
+from io import IOBase
+from pathlib import Path
+from typing import Optional, Union
+
+
+from DataModelDict import DataModelDict as DM
+
+# https://github.com/usnistgov/atomman
+import atomman.unitconvert as uc
+
+# iprPy imports
+from .. import Calculation
+from .energy_check import energy_check
+from ...calculation_subset import (LammpsPotential, LammpsCommands, Units,
+                                   AtommanSystemLoad)
+
+class EnergyCheck(Calculation):
+    """Class for managing potential energy checks of structures"""
+
+############################# Core properties #################################
+
+    def __init__(self,
+                 model: Union[str, Path, IOBase, DM, None]=None,
+                 name: Optional[str]=None,
+                 database = None,
+                 params: Union[str, Path, IOBase, dict] = None,
+                 **kwargs: any):
+        """
+        Initializes a Calculation object for a given style.
+
+        Parameters
+        ----------
+        model : str, file-like object or DataModelDict, optional
+            Record content in data model format to read in.  Cannot be given
+            with params.
+        name : str, optional
+            The name to use for saving the record.  By default, this should be
+            the calculation's key.
+        database : yabadaba.Database, optional
+            A default Database to associate with the Record, typically the
+            Database that the Record was obtained from.  Can allow for Record
+            methods to perform Database operations without needing to specify
+            which Database to use.
+        params : str, file-like object or dict, optional
+            Calculation input parameters or input parameter file.  Cannot be
+            given with model.
+        **kwargs : any
+            Any other core Calculation record attributes to set.  Cannot be
+            given with model.
+        """
+
+        # Initialize subsets used by the calculation
+        self.__potential = LammpsPotential(self)
+        self.__commands = LammpsCommands(self)
+        self.__units = Units(self)
+        self.__system = AtommanSystemLoad(self)
+        subsets = (self.commands, self.potential, self.system, self.units)
+
+        # Initialize unique calculation attributes
+        self.__potential_energy = None
+
+        # Define calc shortcut
+        self.calc = energy_check
+
+        # Call parent constructor
+        super().__init__(model=model, name=name, database=database, params=params,
+                         subsets=subsets, **kwargs)
+
+    @property
+    def filenames(self) -> list:
+        """list: the names of each file used by the calculation."""
+        return [
+            'energy_check.py',
+            'run0.template'
+        ]
+
+############################## Class attributes ###############################
+
+    @property
+    def commands(self) -> LammpsCommands:
+        """LammpsCommands subset"""
+        return self.__commands
+
+    @property
+    def potential(self) -> LammpsPotential:
+        """LammpsPotential subset"""
+        return self.__potential
+
+    @property
+    def units(self) -> Units:
+        """Units subset"""
+        return self.__units
+
+    @property
+    def system(self) -> AtommanSystemLoad:
+        """AtommanSystemLoad subset"""
+        return self.__system
+
+    @property
+    def potential_energy(self) -> float:
+        """float: The measured potential energy per atom for the system"""
+        if self.__potential_energy is None:
+            raise ValueError('No results yet!')
+        return self.__potential_energy
+
+####################### Parameter file interactions ###########################
+
+    def load_parameters(self,
+                        params: Union[dict, str, IOBase],
+                        key: Optional[str] = None):
+        """
+        Reads in and sets calculation parameters.
+
+        Parameters
+        ----------
+        params : dict, str or file-like object
+            The parameters or parameter file to read in.
+        key : str, optional
+            A new key value to assign to the object.  If not given, will use
+            calc_key field in params if it exists, or leave the key value
+            unchanged.
+        """
+        # Load universal content
+        input_dict = super().load_parameters(params, key=key)
+
+        # Load input/output units
+        self.units.load_parameters(input_dict)
+
+        # Change default values for subset terms
+
+        # Load calculation-specific strings
+
+        # Load calculation-specific booleans
+
+        # Load calculation-specific integers
+
+        # Load calculation-specific unitless floats
+
+        # Load calculation-specific floats with units
+
+        # Load LAMMPS commands
+        self.commands.load_parameters(input_dict)
+
+        # Load LAMMPS potential
+        self.potential.load_parameters(input_dict)
+
+        # Load initial system
+        self.system.load_parameters(input_dict)
+
+    @property
+    def singularkeys(self) -> list:
+        """list: Calculation keys that can have single values during prepare."""
+
+        keys = (
+            # Universal keys
+            super().singularkeys
+
+            # Subset keys
+            + self.commands.keyset
+            + self.units.keyset
+
+            # Calculation-specific keys
+        )
+        return keys
+
+    @property
+    def multikeys(self) -> list:
+        """list: Calculation key sets that can have multiple values during prepare."""
+
+        keys = (
+            # Universal multikeys
+            super().multikeys +
+
+            # Combination of potential and system keys
+            [
+                self.potential.keyset +
+                self.system.keyset
+            ]
+        )
+        return keys
+
+########################### Data model interactions ###########################
+
+    @property
+    def modelroot(self) -> str:
+        """str: The root element of the content"""
+        return 'calculation-energy-check'
+
+    def build_model(self) -> DM:
+        """
+        Generates and returns model content based on the values set to object.
+        """
+        # Build universal content
+        model = super().build_model()
+        calc = model[self.modelroot]
+
+        # Build subset content
+        self.commands.build_model(calc, after='atomman-version')
+        self.potential.build_model(calc, after='calculation')
+        self.system.build_model(calc, after='potential-LAMMPS')
+
+        # Build results
+        if self.status == 'finished':
+
+            # Save the final cohesive energy
+            calc['potential-energy'] = uc.model(self.potential_energy,
+                                                self.units.energy_unit,
+                                                None)
+
+        self._set_model(model)
+        return model
+
+    def load_model(self,
+                   model: Union[str, DM],
+                   name: Optional[str] = None):
+        """
+        Loads record contents from a given model.
+
+        Parameters
+        ----------
+        model : str or DataModelDict
+            The model contents of the record to load.
+        name : str, optional
+            The name to assign to the record.  Often inferred from other
+            attributes if not given.
+        """
+        # Load universal and subset content
+        super().load_model(model, name=name)
+        calc = self.model[self.modelroot]
+
+        # Load results
+        if self.status == 'finished':
+            self.__potential_energy = uc.value_unit(calc['potential-energy'])
+
+########################## Metadata interactions ##############################
+
+    def metadata(self) -> dict:
+        """
+        Generates a dict of simple metadata values associated with the record.
+        Useful for quickly comparing records and for building pandas.DataFrames
+        for multiple records of the same style.
+        """
+        # Call super to extract universal and subset content
+        meta = super().metadata()
+
+        # Extract results
+        if self.status == 'finished':
+            meta['E_pot'] = self.potential_energy
+
+        return meta
+
+    @property
+    def compare_terms(self) -> list:
+        """list: The terms to compare metadata values absolutely."""
+        return [
+            'script',
+
+            'parent_key',
+            'load_options',
+            'symbols',
+
+            'potential_LAMMPS_key',
+            'potential_key',
+        ]
+
+########################### Calculation interactions ##########################
+
+    def calc_inputs(self) -> dict:
+        """Builds calculation inputs from the class's attributes"""
+
+        # Initialize input_dict
+        input_dict = {}
+
+        # Add subset inputs
+        for subset in self.subsets:
+            subset.calc_inputs(input_dict)
+
+        # Rename ucell to system
+        input_dict['system'] = input_dict.pop('ucell')
+
+        # Return input_dict
+        return input_dict
+
+    def process_results(self, results_dict: dict):
+        """
+        Processes calculation results and saves them to the object's results
+        attributes.
+
+        Parameters
+        ----------
+        results_dict: dict
+            The dictionary returned by the calc() method.
+        """
+        self.__potential_energy = results_dict['E_pot']
