fcdf5fe6dbb60739b00e6d5057d779f1cf834824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 16:04:49 +0000

    Add package manhelp/0.1.31/manhelp-0.1.31.tar.gz

---
+++ packages/manhelp/manhelp-0.1.31.tar.gz/manhelp-0.1.31/iman/__init__.py
@@ -0,0 +1,7 @@
+"""
+iman.
+
+iman is an intuitive interface for finding Linux commands. It uses natural language processing to understand human language queries and returns the most relevant Linux commands. Ideal for both beginners who are learning Linux and experienced users who need a quick reference.",
+"""
+
+__version__ = "0.1.0"
---
+++ packages/manhelp/manhelp-0.1.31.tar.gz/manhelp-0.1.31/iman/main.py
@@ -0,0 +1,4 @@
+from iman.search import app
+
+if __name__ == "__main__":
+    app()
