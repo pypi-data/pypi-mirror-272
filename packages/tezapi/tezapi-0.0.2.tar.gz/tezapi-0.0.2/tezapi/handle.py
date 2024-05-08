f98ba340ec17dd015e3e0e3ecc84bb423cf34060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 16:05:27 +0000

    Add package tezapi/0.0.2/tezapi-0.0.2.tar.gz

---
+++ packages/tezapi/tezapi-0.0.2.tar.gz/tezapi-0.0.2/tezapi/handle.py
@@ -0,0 +1,64 @@
+from aiohttp import web
+from aiohttp.web_request import Request
+
+from .routes import Route
+
+
+class AioAPI(web.Application):
+    def __init__(self, host: str = '127.0.0.1', port: int = 8000, **kwargs):
+        self.host = host
+        self.port = port
+        self.routes = []
+        super().__init__(**kwargs)
+
+    def _add_route(self, path, handler, method: str, **kwargs):
+        self.routes.append(web.route(method, path, handler, **kwargs))
+
+    def get(self, path: str, **kwargs):
+        def wrapper(func):
+            async def decorated(request: Request):
+                route = Route(func)
+                return await route(request)
+
+            self._add_route(path, decorated, 'GET', name=func.__name__, **kwargs)
+            return decorated
+
+        return wrapper
+
+    def post(self, path: str, **kwargs):
+        def wrapper(func):
+            async def decorated(request: Request):
+                route = Route(func)
+                return await route(request)
+
+            self._add_route(path, decorated, 'POST', name=func.__name__, **kwargs)
+            return decorated
+
+        return wrapper
+
+    def delete(self, path: str, **kwargs):
+        def wrapper(func):
+            async def decorated(request: Request):
+                route = Route(func)
+                return await route(request)
+
+            self._add_route(path, decorated, 'DELETE', name=func.__name__, **kwargs)
+            return decorated
+
+        return wrapper
+
+    def put(self, path: str, **kwargs):
+        def wrapper(func):
+            async def decorated(request: Request):
+                route = Route(func)
+                return await route(request)
+
+            self._add_route(path, decorated, 'PUT', name=func.__name__, **kwargs)
+            return decorated
+
+        return wrapper
+
+    def run(self):
+        app = self
+        app.add_routes(self.routes)
+        web.run_app(app, host=self.host, port=self.port)
---
+++ packages/tezapi/tezapi-0.0.2.tar.gz/tezapi-0.0.2/tezapi/routes.py
@@ -0,0 +1,60 @@
+import inspect
+import logging
+
+from aiohttp.web_request import Request
+
+from .exceptions import APIError, ClassBaseError
+from .responses import JSONResponse, HTMLResponse
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s - %(pathname)s:%(lineno)d')
+
+
+class Route:
+
+    def __init__(self, handler):
+        self.handler = handler
+        signature = inspect.signature(handler)
+        arguments = {}
+        for parameter in signature.parameters.values():
+            arguments[parameter.name] = parameter.annotation
+        self.arguments = arguments
+        self.signature = signature
+
+    async def __call__(self, request: Request):
+        try:
+            request_path_data = self._request_path(request)
+        except APIError as error:
+            return error.__response__
+        try:
+            result = await self.handler(**request_path_data)
+            if isinstance(result, dict):
+                return JSONResponse(result).__response__
+            try:
+                return result.__response__
+            except AttributeError:
+                return HTMLResponse(result).__response__
+        except APIError as error:
+            return error.__response__
+        except ClassBaseError as error:
+            return error.__response__
+
+
+    def _request_path(self, request: Request):
+        data = {}
+        for key, value in self.arguments.items():
+            item = request.match_info.get(key)
+            if value == Request:
+                item = request
+            elif value == int:
+                try:
+                    item = int(item)
+                except ValueError:
+                    raise APIError('PathValidationError', 400, f'{key} is must be an integer')
+            elif value == float:
+                try:
+                    item = float(item)
+                except ValueError:
+                    raise APIError('PathValidationError', 400, f'{key} must be a float')
+            data[key] = item
+        return data
