1cda667228dd29dbfff982ef506af044842f94c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 06:05:52 +0000

    Add package netbox-config-backup/2.0.1/netbox_config_backup-2.0.1.tar.gz

---
+++ packages/netbox-config-backup/netbox_config_backup-2.0.1.tar.gz/netbox_config_backup-2.0.1/netbox_config_backup/querysets/__init__.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.db.models import Count
+
+from extras.choices import JobResultStatusChoices
+from utilities.querysets import RestrictedQuerySet
+
+
+
+class BackupQuerySet(RestrictedQuerySet):
+    def default_annotate(self):
+        from netbox_config_backup.models import BackupJob, BackupCommitTreeChange
+
+        return self.annotate(
+            last_backup=models.Subquery(
+                BackupJob.objects.filter(backup=models.OuterRef('id'), status=JobResultStatusChoices.STATUS_COMPLETED).order_by('-completed').values('completed')[:1]
+            ),
+            next_attempt=models.Subquery(
+                BackupJob.objects.filter(backup=models.OuterRef('id'), status__in=['pending', 'running']).order_by('-scheduled').values('scheduled')[:1]
+            ),
+            last_change=models.Subquery(
+                BackupCommitTreeChange.objects.filter(backup=models.OuterRef('id')).order_by('-id').values('commit__time')[:1]
+            )
+        )
