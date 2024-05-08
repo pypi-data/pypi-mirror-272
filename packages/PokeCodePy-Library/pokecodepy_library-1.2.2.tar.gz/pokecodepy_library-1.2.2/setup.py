5d873739f3618236a87cff255a6aef1697f754bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 14:03:49 +0000

    Add package PokeCodePy-Library/1.2.2/pokecodepy_library-1.2.2.tar.gz

---
+++ packages/PokeCodePy-Library/pokecodepy_library-1.2.2.tar.gz/pokecodepy_library-1.2.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PokeCodePy_Library',
+    version='1.2.2',
+    author='Luz Lizeth Vazquez Garcia',
+    author_email='luzlizet35@gmail.com',
+    description='Biblioteca que con la clase RandomPokemon para generar un pokemon aleatorio',
+    packages=["PokeCodePy_Library"],
+    package_data={'PokeCodePy_Library': ['pokemon.csv']},
+    install_requires=[
+        'pandas',
+        'twine',
+        'wheel',
+        'setuptools',
+    ],
+)
