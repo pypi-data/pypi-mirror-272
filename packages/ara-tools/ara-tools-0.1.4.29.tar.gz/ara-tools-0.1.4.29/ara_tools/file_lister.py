2834637a5271c590d7ebbb8c390d3c4dd470fd45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 06:05:40 +0000

    Add package ara-tools/0.1.4.29/ara-tools-0.1.4.29.tar.gz

---
+++ packages/ara-tools/ara-tools-0.1.4.29.tar.gz/ara-tools-0.1.4.29/ara_tools/file_lister.py
@@ -0,0 +1,31 @@
+import os
+import fnmatch
+
+def generate_markdown_listing(directories, file_types_to_be_listed, output_file_path="config_prompt_givens.md"):
+    """
+    Recursively walks through directories starting from each directory in 'directories',
+    formats the files and directories in Markdown format using only files matching 'file_types_to_be_listed',
+    and writes to a specified Markdown file in a specified path.
+    """
+    markdown_lines = []
+    # markdown_lines.append(f"### GIVENS")  # optional Adding prompt tag already in file listing
+    for start_directory in directories:
+        start_level = start_directory.count(os.sep)
+        directory_name = os.path.basename(start_directory)
+        markdown_lines.append(f"# {directory_name}")  # Adding root directory listing
+        for root, dirs, files in os.walk(start_directory):
+            level = root.count(os.sep) - start_level
+            indent = '    ' * level
+            if level > 0:
+                directory_name = os.path.basename(root)
+                markdown_lines.append(f"{'#' * (level + 1)} {directory_name}")
+            
+            for file in files:
+                if any(fnmatch.fnmatch(file, pattern) for pattern in file_types_to_be_listed):
+                    markdown_lines.append(f"{indent} - [] {file}")
+
+    with open(output_file_path, "w") as md_file:
+        md_file.write('\n'.join(markdown_lines))
+
+# Example usage
+# generate_markdown_listing(['ara'], ['*.py'])
