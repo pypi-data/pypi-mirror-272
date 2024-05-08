8b7f75934d1804d4ec02f2d1d24a8cc61f67c0dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 16:04:00 +0000

    Add package pinjected/0.2.53/pinjected-0.2.53-py3-none-any.whl

---
+++ packages/pinjected/pinjected-0.2.53-py3-none-any.whl/pinjected/v2/provide_context.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+from pinjected.v2.keys import IBindKey
+
+
+@dataclass
+class ProvideContext:
+    resolver:'AsyncResolver'
+    key: Optional[IBindKey]
+    parent: Optional['ProvideContext']
+
+    @property
+    def trace(self):
+        if self.parent is None:
+            return [self]
+        else:
+            return self.parent.trace + [self]
+
+    @property
+    def trace_str(self):
+        return ' -> '.join([str(x.key.ide_hint_string()) for x in self.trace])
+
+
+
+
+
