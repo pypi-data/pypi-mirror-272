6aa103157d8b1eb5fcc8a32546a4cdd4495cca5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 03:03:22 +0000

    Add package eurmlsdk/0.0.71/eurmlsdk-0.0.71.tar.gz

---
+++ packages/eurmlsdk/eurmlsdk-0.0.71.tar.gz/eurmlsdk-0.0.71/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eurmlsdk',
+    version='0.0.71',
+    packages=find_packages(),
+    description='eUR ML SDK',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'boto3',
+        'python-dotenv',
+        'paramiko',
+        'ultralytics'
+    ],
+    author='eUR',
+    author_email='aiml@embedur.com',
+    license='MIT',
+    entry_points={
+        "console_scripts": [
+            "eurmlsdk = eurmlsdk.__main__:main"
+        ]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
