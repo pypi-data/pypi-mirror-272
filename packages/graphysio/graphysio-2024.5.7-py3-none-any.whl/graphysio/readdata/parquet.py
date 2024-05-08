4b16f726532302d26e1817701f7f84d41d9107c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 05:03:57 +0000

    Add package graphysio/2024.5.7/graphysio-2024.5.7-py3-none-any.whl

---
+++ packages/graphysio/graphysio-2024.5.7-py3-none-any.whl/graphysio/readdata/parquet.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+
+from graphysio.dialogs import DlgListChoice
+from graphysio.readdata.baseclass import BaseReader
+from graphysio.structures import PlotData
+
+try:
+    import pyarrow.parquet as pa
+except ImportError:
+    is_available = False
+else:
+    is_available = True
+
+
+class ParquetReader(BaseReader):
+    is_available = is_available
+
+    def askUserInput(self):
+        filepath = self.userdata["filepath"]
+        s = pa.read_schema(filepath)
+        colnames = s.names
+
+        def cb(columns):
+            self.userdata["columns"] = columns
+
+        dlgchoice = DlgListChoice(colnames, "Open Parquet", "Choose curves to load")
+        dlgchoice.dlgdata.connect(cb)
+        dlgchoice.exec_()
+
+    def __call__(self) -> PlotData:
+        filepath = self.userdata["filepath"]
+        data = pd.read_parquet(filepath, columns=self.userdata["columns"])
+
+        data = data.dropna(axis="columns", how="all")
+        data = data.sort_index()
+        data.index = data.index.astype(np.int64)
+
+        return PlotData(data=data, filepath=filepath)
---
+++ packages/graphysio/graphysio-2024.5.7-py3-none-any.whl/graphysio/utils.py
@@ -0,0 +1,90 @@
+import imp
+import os
+import sys
+from functools import partial
+from itertools import cycle
+
+import numpy as np
+import pathvalidate
+from pyqtgraph.Qt import QtGui, QtWidgets
+
+sanitize_filename = partial(pathvalidate.sanitize_filename, platform="auto")
+sanitize_filepath = partial(pathvalidate.sanitize_filepath, platform="auto")
+
+
+def Colors():
+    qtcolors = [
+        QtGui.QColor(0, 114, 189),
+        QtGui.QColor(217, 83, 25),
+        QtGui.QColor(237, 177, 32),
+        QtGui.QColor(126, 47, 142),
+        QtGui.QColor(119, 172, 48),
+        QtGui.QColor(77, 190, 238),
+        QtGui.QColor(162, 20, 47),
+    ]
+    return cycle(qtcolors)
+
+
+def estimateSampleRate(series):
+    intervals = np.diff(series.index)
+    # 1e9 to account for ns -> Hz
+    fs = 1e9 / np.median(intervals)
+    if fs == np.inf:
+        fs = 0
+    if fs > 1:
+        fs = int(round(fs))
+    return fs
+
+
+def loadmodule():
+    defaultdir = os.path.expanduser("~")
+    filepath = QtWidgets.QFileDialog.getOpenFileName(
+        caption="Import module", filter="Python files (*.py)", directory=defaultdir
+    )
+    if not isinstance(filepath, str):
+        # PyQt5 API change
+        filepath = filepath[0]
+    if not filepath:
+        # Cancel pressed
+        return
+
+    folder, filename = os.path.split(filepath)
+    modulename, _ = os.path.splitext(filename)
+    f, filename, description = imp.find_module(modulename, [folder])
+
+    bcbak = sys.dont_write_bytecode
+    try:
+        sys.dont_write_bytecode = True
+        imp.load_module("graphysio.plugin", f, filename, description)
+    finally:
+        sys.dont_write_bytecode = bcbak
+        f.close()
+
+
+def clip(vec, vrange):
+    if vrange is None:
+        return vec
+    xmin, xmax = vrange
+    cond = (vec > xmin) & (vec < xmax)
+    return vec[cond]
+
+
+def truncatevecs(vecs):
+    # Ensure all vectors have the same length by truncating the end
+    maxidx = min(map(len, vecs))
+    return [vec[0:maxidx] for vec in vecs]
+
+
+def getshell(ui=None):
+    import IPython
+
+    IPython.embed(ui=ui)
+
+
+def displayError(errmsg):
+    msgbox = QtWidgets.QMessageBox()
+    msgbox.setWindowTitle("Error")
+    msgbox.setText(str(errmsg))
+    msgbox.setStandardButtons(QtWidgets.QMessageBox.Ok)
+    msgbox.setIcon(QtWidgets.QMessageBox.Critical)
+    msgbox.exec_()
