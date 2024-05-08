493408264eed9c95a17555566329d2b55ff68528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 06:04:26 +0000

    Add package latex-rubber/1.6.6/latex_rubber-1.6.6.tar.gz

---
+++ packages/latex-rubber/latex_rubber-1.6.6.tar.gz/latex_rubber-1.6.6/rubber/latex_modules/glossaries.py
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+import rubber.depend
+import rubber.module_interface
+
+
+class Module(rubber.module_interface.Module):
+
+    def __init__(self, document, opt):
+        job = document.basename()
+        glo = job + '.glo'
+
+        document.add_product(job + '.ist')
+        document.add_source(glo)
+
+        dep = rubber.depend.Shell(('makeglossaries', job))
+        # FIXME: does probably fail with --inplace and friends.
+        dep.add_product(glo)
+        dep.add_product(job + '.gls')
+        dep.add_product(job + '.glg')
+        dep.add_source(job + '.aux')
