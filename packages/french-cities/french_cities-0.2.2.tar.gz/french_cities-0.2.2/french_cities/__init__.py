59a7f499132bde63ced2155399bcc2247c7d198a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 03:03:07 +0000

    Add package french-cities/0.2.2/french_cities-0.2.2.tar.gz

---
+++ packages/french-cities/french_cities-0.2.2.tar.gz/french_cities-0.2.2/french_cities/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from dotenv import load_dotenv
+from importlib_metadata import version
+
+from french_cities.city_finder import find_city
+from french_cities.departement_finder import find_departements
+from french_cities.vintage import set_vintage
+
+load_dotenv(override=True)
+
+__version__ = version(__package__)
+
+
+__all__ = [
+    "find_city",
+    "find_departements",
+    "set_vintage",
+]
