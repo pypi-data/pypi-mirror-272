6ca28a04724d4b85df0424341cf5e7677d3f8d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 07:03:37 +0000

    Add package statsnet-python-sdk/0.0.3/statsnet_python_sdk-0.0.3.tar.gz

---
+++ packages/statsnet-python-sdk/statsnet_python_sdk-0.0.3.tar.gz/statsnet_python_sdk-0.0.3/statsnet_python_sdk/__init__.py
@@ -0,0 +1,5 @@
+from .client import Client
+from .exceptions import ClientException, InvalidParamsException
+
+__all__ = ["ClientException", "InvalidParamsException", "Client"]
+__version__ = "0.0.3"
