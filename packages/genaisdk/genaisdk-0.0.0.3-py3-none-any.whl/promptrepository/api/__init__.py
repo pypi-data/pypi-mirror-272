11ff44c1cdd2b3d4ee0a965dbba7c36d4ee8b586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 04:03:24 +0000

    Add package genaisdk/0.0.0.3/genaisdk-0.0.0.3-py3-none-any.whl

---
+++ packages/genaisdk/genaisdk-0.0.0.3-py3-none-any.whl/promptrepository/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from promptrepository.api.public_prompts_api import PublicPromptsApi
+from promptrepository.api.public_tags_api import PublicTagsApi
+
