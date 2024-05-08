2664a9f512151d5a77bfc750143e295b79990eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 15:03:51 +0000

    Add package typedmongo/1.5.0/typedmongo-1.5.0-py3-none-any.whl

---
+++ packages/typedmongo/typedmongo-1.5.0-py3-none-any.whl/typedmongo/asyncio/__init__.py
@@ -0,0 +1,52 @@
+from typedmongo.shortcuts import Contains, EndsWith, StartsWith
+
+from .client import (
+    DeleteMany,
+    DeleteOne,
+    InsertOne,
+    ReplaceOne,
+    UpdateMany,
+    UpdateOne,
+    initial_collections,
+)
+from .fields import (
+    BooleanField,
+    DateTimeField,
+    DecimalField,
+    DictField,
+    EmbeddedField,
+    FieldParamters,
+    FloatField,
+    IntegerField,
+    ListField,
+    ObjectIdField,
+    StringField,
+)
+from .table import Index, MongoTable, Table
+
+__all__ = [
+    "Contains",
+    "StartsWith",
+    "EndsWith",
+    "DeleteMany",
+    "DeleteOne",
+    "InsertOne",
+    "ReplaceOne",
+    "UpdateMany",
+    "UpdateOne",
+    "initial_collections",
+    "Index",
+    "MongoTable",
+    "Table",
+    "BooleanField",
+    "DateTimeField",
+    "DecimalField",
+    "DictField",
+    "EmbeddedField",
+    "FieldParamters",
+    "FloatField",
+    "IntegerField",
+    "ListField",
+    "ObjectIdField",
+    "StringField",
+]
