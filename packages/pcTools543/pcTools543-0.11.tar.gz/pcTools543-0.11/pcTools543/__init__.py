ca7c0fdc4a9771a2633ed35c03d3423a6af3b8dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 15:03:51 +0000

    Add package pcTools543/0.11/pcTools543-0.11.tar.gz

---
+++ packages/pcTools543/pcTools543-0.11.tar.gz/pcTools543-0.11/pcTools543/__init__.py
@@ -0,0 +1,2 @@
+print("Initializing pcTools543_package...")
+from .pcTools import show_num
---
+++ packages/pcTools543/pcTools543-0.11.tar.gz/pcTools543-0.11/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pcTools543",
+    version="0.11",
+    author="Su",
+    author_email="aaaaaaaa@qq.com",
+    description="Just",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'pcTools543=pcTools543:main'
+        ],
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
