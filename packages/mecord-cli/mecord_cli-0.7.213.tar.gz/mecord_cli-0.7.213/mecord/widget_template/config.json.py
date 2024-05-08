a6aefbf24082a78590fb87b2d1584c685f227cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 03:03:21 +0000

    Add package mecord-cli/0.7.213/mecord_cli-0.7.213.tar.gz

---
+++ packages/mecord-cli/mecord_cli-0.7.213.tar.gz/mecord_cli-0.7.213/mecord/widget_template/config.json.py
@@ -0,0 +1,14 @@
+{
+  "name": "Widget-Demo",
+  "version": "1.0",
+  "parent_widget_id": "",
+  "widget_id": "",
+  "group_id": "",
+  "cmd": "",
+  "max_task_number": "",
+  "data": {
+    "fn_name": [
+      "txt2image"
+    ]
+  }
+}
