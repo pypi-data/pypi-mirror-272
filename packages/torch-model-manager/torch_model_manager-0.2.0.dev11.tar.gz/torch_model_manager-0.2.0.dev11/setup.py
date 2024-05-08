427c71aae74c776ec048dfc61f9732586478709c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 03:03:41 +0000

    Add package torch-model-manager/0.2.0.dev11/torch_model_manager-0.2.0.dev11.tar.gz

---
+++ packages/torch-model-manager/torch_model_manager-0.2.0.dev11.tar.gz/torch_model_manager-0.2.0.dev11/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='torch-model-manager',
+    version='0.2.0.dev11',
+    description='A package for managing PyTorch models',
+    author='Billal MOKHTARI',
+    author_email='mokhtaribillal1@gmail.com',
+    packages=find_packages(exclude=['tests', 
+                                    'docs', 
+                                    'build.sh', 
+                                    'requirements.sh', 
+                                    'resources.md',
+                                    'torch_model_manager/src/run_ids.json',
+                                    '.pypirc',
+                                    'torch_model_manager/src/__pycache__',
+                                    'torch_model_manager/src/.neptune',
+                                    'upload.py']),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/Billal-MOKHTARI/torch-model-manager',
+    keywords=['PyTorch', 'Deep Learning', 'Machine Learning', 'High Level Programming'],
+    install_requires=[
+        'torch',
+        'numpy',
+        'torchvision',
+        'torch',
+        'torchcam',
+        'neptune'
+        # Add any other dependencies here
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+
+)
