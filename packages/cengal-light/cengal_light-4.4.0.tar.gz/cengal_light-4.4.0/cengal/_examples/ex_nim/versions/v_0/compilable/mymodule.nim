f43fbf828d800a9d61082d17bfe49f60394dc30b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 16:05:05 +0000

    Add package cengal-light/4.4.0/cengal_light-4.4.0.tar.gz

---
+++ packages/cengal-light/cengal_light-4.4.0.tar.gz/cengal_light-4.4.0/cengal/_examples/ex_nim/versions/v_0/compilable/mymodule.nim
@@ -0,0 +1,23 @@
+# mymodule.nim - file name should match the module name you're going to import from python
+import nimpy
+import compile_time_py_definitions
+import mystrutils
+
+# Use the exported `toUpper` function
+let myString = "Hello, Worldd!"
+
+proc greet(name: string): string {.exportpy.} =
+  let upperString = mystrutils.toUpper(myString)
+  echo upperString  # This will print: "HELLO, WORLDd!"
+
+  when defined(NIMF_Linux):
+    echo "mymodule: NIMF_Linux"
+  else:
+    echo "mymodule: not NIMF_Linux"
+
+  when defined(NIMF_IS_ARM):
+    echo "mymodule: NIMF_IS_ARM"
+  else:
+    echo "mymodule: not NIMF_IS_ARM"
+
+  return "Hello, " & name & "!" & toUpper("me") & NIMD_OS_TYPE
---
+++ packages/cengal-light/cengal_light-4.4.0.tar.gz/cengal_light-4.4.0/cengal/time_management/cpu_clock_cycles/versions/v_0/compilable/cpu_clock_cycles.c
@@ -0,0 +1,69 @@
+// Copyright © 2012-2024 ButenkoMS. All rights reserved. Contacts: <gtalk@butenkoms.space>
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#if defined(CF_IS_X86)
+
+#ifdef _MSC_VER
+
+#include <intrin.h>
+
+extern unsigned long long c_cpu_clock_cycles() {
+    unsigned int ui;
+    unsigned long long cycles = __rdtscp(&ui);
+    return cycles;
+}
+
+#else
+
+extern unsigned long long c_cpu_clock_cycles()
+{
+    unsigned long long result;
+    asm volatile(
+        "RDTSCP;"
+        "SHLQ $32,%%rdx;"
+        "ORQ %%rdx,%%rax;"
+        "MOVQ %%rax,%0;"
+        :"=r"(result)
+        :
+        :"rdx","rax", "rcx"
+    );
+    return result;
+}
+
+#endif
+
+#elif defined(CF_IS_ARM)
+
+#include <stdint.h>
+
+extern uint64_t c_cpu_clock_cycles()
+{
+    uint64_t result;
+    asm volatile(
+        "mrs %0, cntvct_el0"
+        : "=r" (result)
+    );
+
+    return result;
+}
+
+#else
+
+extern unsigned long long c_cpu_clock_cycles()
+{
+    return 0;
+}
+
+#endif
