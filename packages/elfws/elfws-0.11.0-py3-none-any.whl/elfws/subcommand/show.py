2f25a87f380342d846af7cb91045ece004276c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 15:03:43 +0000

    Add package elfws/0.11.0/elfws-0.11.0-py3-none-any.whl

---
+++ packages/elfws/elfws-0.11.0-py3-none-any.whl/elfws/subcommand/show.py
@@ -0,0 +1,25 @@
+
+import re
+import sys
+
+from elfws import display
+from elfws.report import junit
+from elfws import suppression_list
+from elfws import utils
+
+
+def show(cla):
+
+    oSupList = suppression_list.create()
+
+    lLogFile = utils.read_log_file(cla.log_file)
+
+    oWarnList = utils.create_warning_list(lLogFile, cla.log_file)
+
+    display.results(cla.log_file, '<None>', oSupList, oWarnList)
+
+    if cla.junit:
+        lJUnitFile = junit.generate_junit_xml_file(cla, oWarnList, oSupList)
+        utils.write_junit_file(cla.junit, lJUnitFile)
+
+    sys.exit(0)
---
+++ packages/elfws/elfws-0.11.0-py3-none-any.whl/elfws/subcommand/version.py
@@ -0,0 +1,10 @@
+
+import sys
+
+from elfws import version
+
+
+def print_version():
+
+    print('EDA Log File Warning Suppressor (ELFWS) version ' + version.version)
+    sys.exit(0)
