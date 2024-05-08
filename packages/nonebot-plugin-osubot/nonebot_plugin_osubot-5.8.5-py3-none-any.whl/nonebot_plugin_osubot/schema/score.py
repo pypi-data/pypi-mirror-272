ee77f58043d057981781c42599eb5f4c6dbe6ca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 15:03:53 +0000

    Add package nonebot-plugin-osubot/5.8.5/nonebot_plugin_osubot-5.8.5-py3-none-any.whl

---
+++ packages/nonebot-plugin-osubot/nonebot_plugin_osubot-5.8.5-py3-none-any.whl/nonebot_plugin_osubot/schema/score.py
@@ -0,0 +1,38 @@
+from typing import Literal, Optional
+
+from .basemodel import Base
+from .beatmap import Beatmap, Beatmapset
+
+
+class Statistics(Base):
+    count_50: Optional[int]
+    count_100: Optional[int]
+    count_300: Optional[int]
+    count_geki: Optional[int]
+    count_katu: Optional[int]
+    count_miss: Optional[int]
+
+
+class Score(Base):
+    id: int
+    best_id: Optional[int]
+    user_id: int
+    accuracy: float
+    mods: list[str]
+    score: int
+    max_combo: int
+    perfect: int
+    statistics: Statistics
+    passed: bool
+    pp: Optional[float]
+    rank: str
+    created_at: str
+    mode: Literal["fruits", "mania", "osu", "taiko"]
+    mode_int: int
+    beatmap: Optional[Beatmap]
+    beatmapset: Optional[Beatmapset]
+
+
+class BeatmapUserScore(Base):
+    position: int
+    score: Score
