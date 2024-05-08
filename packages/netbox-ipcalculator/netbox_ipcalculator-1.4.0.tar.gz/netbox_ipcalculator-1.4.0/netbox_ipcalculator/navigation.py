691219eb24dbf39edcba98a112478b279ad865a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 04:03:04 +0000

    Add package netbox-ipcalculator/1.4.0/netbox_ipcalculator-1.4.0.tar.gz

---
+++ packages/netbox-ipcalculator/netbox_ipcalculator-1.4.0.tar.gz/netbox_ipcalculator-1.4.0/netbox_ipcalculator/navigation.py
@@ -0,0 +1,8 @@
+from netbox.plugins import PluginMenuItem
+
+menu_items = (
+    PluginMenuItem(
+        link='plugins:netbox_ipcalculator:ipcalculator',
+        link_text='IP Calculator',
+    ),
+)
