0e01963e5d06144416e6c901f38f66b2a1281dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 15:04:18 +0000

    Add package trio-bybit/0.2.0/trio_bybit-0.2.0.tar.gz

---
+++ packages/trio-bybit/trio_bybit-0.2.0.tar.gz/trio_bybit-0.2.0/tests/test_rsa.py
@@ -0,0 +1,28 @@
+import os
+
+from trio_bybit.client import AsyncClient
+
+# Generate key pair by 3 steps: generate pairs file, extract pub key and private key
+# openssl genrsa -out keypair.pem 2048
+# openssl rsa -in keypair.pem -pubout -out publickey.crt
+# openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in keypair.pem -out pkcs8.key
+
+
+async def test_get_wallet():
+    with open(os.getenv("BYBIT_PRIVATE_KEY_PATH"), "r") as f:
+        private_key = f.read()
+
+    client = await AsyncClient.create(
+        api_key=os.getenv("BYBIT_API_KEY"),
+        api_secret=private_key,
+        sign_style="RSA",
+    )
+
+    async with client:
+        wallet = await client.get_wallet_balance(accountType="UNIFIED")
+        assert wallet["retCode"] == 0
+        assert wallet["retMsg"] == "OK"
+        assert wallet["result"]["list"][0]["accountType"] == "UNIFIED"
+        position = await client.get_position_info(category="linear", settleCoin="USDT")
+        assert position["retCode"] == 0
+        assert position["retMsg"] == "OK"
