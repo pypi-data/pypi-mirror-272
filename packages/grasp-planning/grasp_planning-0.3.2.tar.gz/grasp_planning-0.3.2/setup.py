4107c7952aefef9bfc6400560ecf684acc6e9684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 16:04:51 +0000

    Add package grasp-planning/0.3.2/grasp_planning-0.3.2.tar.gz

---
+++ packages/grasp-planning/grasp_planning-0.3.2.tar.gz/grasp_planning-0.3.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+import os
+
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = f"{lib_folder}/requirements.txt"
+install_requires = [] # Here we'll add: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
+
+setuptools.setup(
+    name="grasp_planning",
+    version="0.3.2", 
+    author="Tomas Merva", 
+    author_email="tmerva7@gmail.com",
+    description="Grasp and Motion Planning Python Package",
+    long_description="Grasp and Motion Planning Python Package",
+    packages=setuptools.find_packages('.'),
+    install_requires=install_requires
+)
