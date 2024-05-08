00459baaf79a7d559ef7e9b698c7e7234b6337af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 16:04:46 +0000

    Add package odoo-addon-l10n-th-gov-purchase-guarantee/15.0.1.1.2/odoo_addon_l10n_th_gov_purchase_guarantee-15.0.1.1.2-py3-none-any.whl

---
+++ packages/odoo-addon-l10n-th-gov-purchase-guarantee/odoo_addon_l10n_th_gov_purchase_guarantee-15.0.1.1.2-py3-none-any.whl/odoo/addons/l10n_th_gov_purchase_guarantee/models/purchase_order.py
@@ -0,0 +1,34 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    purchase_guarantee_ids = fields.One2many(
+        comodel_name="purchase.guarantee",
+        inverse_name="purchase_id",
+        string="Guarantee",
+    )
+    purchase_guarantee_count = fields.Integer(
+        string="Guarantee Count",
+        compute="_compute_purchase_guarantee_count",
+    )
+
+    @api.depends("purchase_guarantee_ids")
+    def _compute_purchase_guarantee_count(self):
+        for rec in self.sudo():
+            rec.purchase_guarantee_count = len(rec.purchase_guarantee_ids)
+
+    def action_view_purchase_guarantee(self):
+        self.ensure_one()
+        action = self.env["ir.actions.act_window"]._for_xml_id(
+            "l10n_th_gov_purchase_guarantee.purchase_guarantee_action"
+        )
+        action["domain"] = [("purchase_id", "=", self.id)]
+        action["context"] = {
+            "default_reference": "purchase.order,%s" % (str(self.id),),
+        }
+        return action
