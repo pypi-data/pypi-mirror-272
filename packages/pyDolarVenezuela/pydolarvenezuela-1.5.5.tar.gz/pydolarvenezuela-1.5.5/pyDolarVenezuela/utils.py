e18bb6c7b68687173718e0e6cf94c4130660a27a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 15:04:15 +0000

    Add package pyDolarVenezuela/1.5.5/pydolarvenezuela-1.5.5.tar.gz

---
+++ packages/pyDolarVenezuela/pydolarvenezuela-1.5.5.tar.gz/pydolarvenezuela-1.5.5/pyDolarVenezuela/utils.py
@@ -0,0 +1,58 @@
+currencies = {
+    "eur": {"name": "Euro", "id": "euro"},
+    "cny": {"name": "Yuan chino", "id": "yuan"},
+    "try": {"name": "Lira turca", "id": "lira"},
+    "rub": {"name": "Rublo ruso", "id": "rublo"},
+    "usd": {"name": "Dólar estadounidense", "id": "dolar"}
+}
+
+code_currencies = {
+    'USD': 'Dólar estadounidense',
+    'DKK': 'Corona danesa',
+    'COP': 'Peso colombiano',
+    'NOK': 'Corona noruega',
+    'GBP': 'Libra esterlina',
+    'SEK': 'Corona sueca',
+    'CLP': 'Peso chileno',
+    'CHF': 'Franco suizo',
+    'HKD': 'Dólar de Hong Kong',
+    'TWD': 'Nuevo dólar taiwanés',
+    'BRL': 'Real brasileño',
+    'CAD': 'Dólar canadiense',
+    'EUR': 'Euro',
+    'BOB': 'Boliviano',
+    'NIO': 'Córdoba nicaragüense',
+    'ARS': 'Peso argentino',
+    'CNY': 'Yuan chino',
+    'ILS': 'Nuevo séquel israelí',
+    'JPY': 'Yen japonés',
+    'PEN': 'Sol peruano',
+    'DOP': 'Peso dominicano',
+    'TTD': 'Dólar de Trinidad y Tobago',
+    'UYU': 'Peso uruguayo',
+    'ANG': 'Florín antillano neerlandés',
+    'AUD': 'Dólar australiano'
+}
+
+currencies_list = ['usd', 'dkk', 'cop', 'nok', 'gbp', 'sek', 'clp', 'chf', 'hkd', 'twd', 'brl', 'cad',
+                   'eur', 'bob', 'nio', 'ars', 'cny', 'ils', 'jpy', 'pen', 'dop', 'ttd', 'uyu', 'ang', 'aud']
+
+monitors = {'binance': 'Binance', 'dolartoday': 'DolarToday',
+            'yadio': 'Yadio', 'airtm': 'Airtm', 'cambiosrya': 'Cambios R&A',
+            'mkambio': 'Mkambio', 'bcv': 'BCV', 'promediovip': 'EnParaleloVzlaVip',
+            'prom_epv': 'EnParalelovzla'}
+
+monitors_exchange = ["dolar-em", "monitor_dolar_venezuela", "enparalelovzla", "monitor_dolar_vzla", "petro",
+"bcv", "remesas_zoom", "italcambio","bancamiga","banco_de_venezuela","banco-exterior",
+"banplus","bnc","banesco","bbva_provincial","mercantil","otras_instituciones",
+"binance","airtm","reserve","syklo","yadio",
+"dolartoday","mkambio" ,"cambios-r&a" ,"paypal" ,"zinli" ,
+"skrill" ,"amazon_gift_card"]
+
+time_units = {
+    "semana": "weeks", "semanas": "weeks",
+    "día": "days", "días": "days",
+    "horas": "hours", "hora": "hours",
+    "minutos": "minutes", "minuto": "minutes",
+    "segundos": "seconds", "segundo": "seconds"
+}
