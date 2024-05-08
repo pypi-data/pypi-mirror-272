e93537bca7df92b63f29ab41842186fe02a3c1be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 16:04:53 +0000

    Add package miner-ai-beta/0.1.11/miner_ai_beta-0.1.11.tar.gz

---
+++ packages/miner-ai-beta/miner_ai_beta-0.1.11.tar.gz/miner_ai_beta-0.1.11/miner_ai_beta/loader/__init__.py
@@ -0,0 +1,7 @@
+from .docs import IndexFromDocs
+from .excels import IndexFromXlss
+from .web_pages import IndexFromWebPages
+from .pdfs import IndexFromPdfs
+from .tubes import IndexFromTubes
+from .ppts import IndexFromPPTs
+from .union import MergeIndexes
