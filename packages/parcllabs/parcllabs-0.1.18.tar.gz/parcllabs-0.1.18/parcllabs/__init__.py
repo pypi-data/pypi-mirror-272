51dcc1a05eee951bb8d8b470ba7f6baf3a256db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 06:05:24 +0000

    Add package parcllabs/0.1.18/parcllabs-0.1.18.tar.gz

---
+++ packages/parcllabs/parcllabs-0.1.18.tar.gz/parcllabs-0.1.18/parcllabs/__init__.py
@@ -0,0 +1,41 @@
+from typing import Optional
+from parcllabs.__version__ import VERSION as __version__
+
+# Constants
+DEFAULT_API_BASE = "https://api.parcllabs.com"
+
+api_key: Optional[str] = None
+api_base = DEFAULT_API_BASE
+
+
+from parcllabs.parcllabs_client import ParclLabsClient
+
+
+from parcllabs.services.investor_metrics import (
+    InvestorMetricsHousingStockOwnership,
+    InvesetorMetricsNewListingsForSaleRollingCounts,
+    InvestorMetricsPurchaseToSaleRatio,
+    InvestorMetricsHousingEventCounts,
+)
+
+from parcllabs.services.market_metrics import (
+    MarketMetricsHousingEventPrices,
+    MarketMetricsHousingStock,
+    MarketMetricsHousingEventCounts,
+)
+
+from parcllabs.services.for_sale_market_metrics import (
+    ForSaleMarketMetricsNewListingsRollingCounts,
+)
+
+from parcllabs.services.rental_market_metrics import (
+    RentalMarketMetricsRentalUnitsConcentration,
+    RentalMarketMetricsGrossYield,
+    RentalMarketMetricsNewListingsForRentRollingCounts,
+)
+
+from parcllabs.services.portfolio_metrics import (
+    PortfolioMetricsSFHousingStockOwnership,
+    PortfolioMetricsSFNewListingsForSaleRollingCounts,
+)
+from parcllabs.services.search import SearchMarkets
