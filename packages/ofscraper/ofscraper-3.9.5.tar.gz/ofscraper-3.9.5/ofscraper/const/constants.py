7a7f8f978f39a886bc8bfeea32ef5a28289de095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-08 05:02:57 +0000

    Add package ofscraper/3.9.5/ofscraper-3.9.5.tar.gz

---
+++ packages/ofscraper/ofscraper-3.9.5.tar.gz/ofscraper-3.9.5/ofscraper/const/constants.py
@@ -0,0 +1,33 @@
+r"""
+                                                             
+        _____                                    ay           
+r"""
+                                                             
+ _______  _______         _______  _______  _______  _______  _______  _______  _______ 
+(  ___  )(  ____ \       (  ____ \(  ____ \(  ____ )(  ___  )(  ____ )(  ____ \(  ____ )
+| (   ) || (    \/       | (    \/| (    \/| (    )|| (   ) || (    )|| (    \/| (    )|
+| |   | || (__     _____ | (_____ | |      | (____)|| (___) || (____)|| (__    | (____)|
+| |   | ||  __)   (_____)(_____  )| |      |     __)|  ___  ||  _____)|  __)   |     __)
+| |   | || (                   ) || |      | (\ (   | (   ) || (      | (      | (\ (   
+| (___) || )             /\____) || (____/\| ) \ \__| )   ( || )      | (____/\| ) \ \__
+(_______)|/              \_______)(_______/|/   \__/|/     \||/       (_______/|/   \__/
+                                                                                      
+"""
+                 \/     \/           \/            \/         
+"""
+
+from ofscraper.const.binary import *
+from ofscraper.const.config import *
+from ofscraper.const.date import *
+from ofscraper.const.download import *
+from ofscraper.const.files import *
+from ofscraper.const.general import *
+from ofscraper.const.logger import *
+from ofscraper.const.metadata import *
+from ofscraper.const.other_url import *
+from ofscraper.const.prompts import *
+from ofscraper.const.req import *
+from ofscraper.const.table import *
+from ofscraper.const.test_constants import *
+from ofscraper.const.time import *
+from ofscraper.const.url import *
