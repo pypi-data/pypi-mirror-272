a3c44451f8b2cc39561e7c8c671127214f3d46c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 05:04:19 +0000

    Add package cellworld-gym/0.0.96/cellworld_gym-0.0.96.tar.gz

---
+++ packages/cellworld-gym/cellworld_gym-0.0.96.tar.gz/cellworld_gym-0.0.96/cellworld_gym/envs/__init__.py
@@ -0,0 +1,19 @@
+from gymnasium.envs.registration import register
+from .bot_evade import BotEvadeEnv, BotEvadeObservation
+from .oasis import OasisEnv, OasisObservation
+from .dual_evade import DualEvadeEnv, DualEvadeObservation
+
+register(
+    id='CellworldBotEvade-v0',
+    entry_point='cellworld_gym.envs:BotEvadeEnv'
+)
+
+register(
+    id='CellworldOasis-v0',
+    entry_point='cellworld_gym.envs:OasisEnv'
+)
+
+register(
+    id='CellworldDualEvade-v0',
+    entry_point='cellworld_gym.envs:DualEvadeEnv'
+)
