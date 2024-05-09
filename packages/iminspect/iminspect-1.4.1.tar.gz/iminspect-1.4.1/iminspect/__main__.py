dea745da79006f2cde2f6c07d569d314212141e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 06:04:27 +0000

    Add package iminspect/1.4.1/iminspect-1.4.1.tar.gz

---
+++ packages/iminspect/iminspect-1.4.1.tar.gz/iminspect-1.4.1/iminspect/__main__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+Main utility to start iminspect like a "standalone" application.
+
+For convenience, you should create a launcher to execute:
+path/to/venv/.../python -m iminspect
+
+See https://stackoverflow.com/questions/14132789/relative-imports-for-the-billionth-time
+why running a script from this package via (path/to/script.py) won't work.
+"""
+
+
+from . import inspector
+import argparse
+from pathlib import Path
+from vito import imutils
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Inspect an images or multiple images')
+    parser.add_argument(
+        'image', nargs='*', type=Path, help='Image file(s) to inspect', default=None)
+    args = parser.parse_args()
+    
+    if (args.image is None) or (len(args.image) == 0):
+        to_inspect = None
+    elif len(args.image) == 1:
+        to_inspect = imutils.imread(args.image[0])
+    else:
+        to_inspect = [imutils.imread(img) for img in args.image]
+    
+    inspector.inspect(data=to_inspect)
