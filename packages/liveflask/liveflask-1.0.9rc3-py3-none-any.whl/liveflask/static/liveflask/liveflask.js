ed9d1a70be5ead887a279dc45a5305ee949db096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 06:03:57 +0000

    Add package liveflask/1.0.9rc3/liveflask-1.0.9rc3-py3-none-any.whl

---
+++ packages/liveflask/liveflask-1.0.9rc3-py3-none-any.whl/liveflask/static/liveflask/liveflask.js
@@ -0,0 +1,44 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// INITIALIZERS
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+window.liveflask = {
+    components: [],
+    first: function () {
+        return this.components.length > 0 ? this.components[0] : null;
+    },
+    find: function (id) {
+        return this.components.find(component => component.data.key === id) || null;
+    },
+    get_by_name: function (name) {
+        return this.components.filter(component => component.class === name);
+    },
+    all: function () {
+        return this.components;
+    }
+}
+
+document.querySelectorAll('[data-component]').forEach(el => {
+    let live_flask_children = [];
+
+    el.__liveflask = JSON.parse(el.getAttribute('data-snapshot'));
+    el.removeAttribute('data-snapshot')
+
+
+    init_model(el)
+    init_action(el)
+    init_polling(el)
+
+
+    el.__liveflask.set = function (key, value) {
+        el.__liveflask[key] = value
+        send_request(el, {update_property: [key, value]}, undefined)
+    }
+
+    window.liveflask.components.push(el.__liveflask);
+
+    // register event named liveflask:initialized
+    document.dispatchEvent(new CustomEvent('liveflask:initialized', {detail: el.__liveflask, target: el}))
+    init_inits(el)
+
+})
