015394acb9b7abdaf0b75f0d245526d2fd66f13e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 04:04:11 +0000

    Add package agentql/0.4.4/agentql-0.4.4.tar.gz

---
+++ packages/agentql/agentql-0.4.4.tar.gz/agentql-0.4.4/src/agentql/ext/playwright/__init__.py
@@ -0,0 +1,10 @@
+from .playwright_driver_async import (
+    PlaywrightWebDriver as PlaywrightWebDriverAsync,
+    Locator as LocatorAsync,
+    Page as PageAsync,
+)
+from .playwright_driver_sync import (
+    PlaywrightWebDriver as PlaywrightWebDriverSync,
+    Locator as LocatorSync,
+    Page as PageSync,
+)
---
+++ packages/agentql/agentql-0.4.4.tar.gz/agentql-0.4.4/src/agentql/trail_logger/__init__.py
@@ -0,0 +1,19 @@
+from .trail_logger import (
+    TrailLogger,
+    TrailLoggerStore,
+    add_event,
+    finalize,
+    init,
+    spy_on_method,
+    spy_on_object,
+)
+
+__all__ = [
+    "add_event",
+    "finalize",
+    "init",
+    "spy_on_method",
+    "TrailLogger",
+    "TrailLoggerStore",
+    "spy_on_object",
+]
