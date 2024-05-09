065264fc82a214921f8b744bea7a1066072ee49d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 05:04:06 +0000

    Add package CloakSDK/0.0.0.1/cloaksdk-0.0.0.1.tar.gz

---
+++ packages/CloakSDK/cloaksdk-0.0.0.1.tar.gz/cloaksdk-0.0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CloakSDK',
+    version='0.0.0.1',
+    packages=find_packages(),
+    author='frane',
+    author_email='autismguy843@gmail.com',
+    description='Simple framework to text',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Natural Language :: Polish",
+        "Programming Language :: Python :: 3.12",
+    ]
+)
