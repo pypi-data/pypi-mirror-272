64a60318799f70d47d5b7b68bcc1f8172f2f7461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 15:04:26 +0000

    Add package xyscreens/0.1.2.dev2/xyscreens-0.1.2.dev2.tar.gz

---
+++ packages/xyscreens/xyscreens-0.1.2.dev2.tar.gz/xyscreens-0.1.2.dev2/xyscreens/__init__.py
@@ -0,0 +1,11 @@
+"""
+Implements the XYScreens library for controlling XY Screens projector screens and projector lifts.
+
+Created on 17 Nov 2022
+
+@author: Rogier van Staveren
+"""
+
+from _version import __version__
+
+from xyscreens.xyscreens import XYScreens, XYScreensState
