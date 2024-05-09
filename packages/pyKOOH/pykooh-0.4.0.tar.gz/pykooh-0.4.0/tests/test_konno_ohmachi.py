f8b06f2674db8e4db6ec146d1535afbbbf82a444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 06:04:43 +0000

    Add package pyKOOH/0.4.0/pykooh-0.4.0.tar.gz

---
+++ packages/pyKOOH/pykooh-0.4.0.tar.gz/pykooh-0.4.0/tests/test_konno_ohmachi.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pykooh
+import pytest
+
+from . import DATA_PATH
+
+try:
+    import Cython  # noqa: F401
+
+    has_cython = True
+except ImportError:
+    has_cython = False
+
+use_cython = (
+    [True, False]
+    if has_cython
+    else [
+        False,
+    ]
+)
+
+# Load test data
+data = np.load(str(DATA_PATH / "test_data.npz"))
+freqs = data["freqs"]
+raw_amps = data["fourier_amps"]
+smooth_amps = data["ko_amps"]
+b = data["b"]
+
+
+@pytest.mark.parametrize("use_cython", use_cython)
+def test_smooth(use_cython):
+    calculated = pykooh.smooth(freqs, freqs, raw_amps, b, use_cython=use_cython)
+    np.testing.assert_allclose(calculated, smooth_amps, rtol=1e-3)
