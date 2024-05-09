872e405c21999e121992fb7fad8d77a439475ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 15:04:51 +0000

    Add package subshift/0.10.4/subshift-0.10.4-py2.py3-none-any.whl

---
+++ packages/subshift/subshift-0.10.4-py2.py3-none-any.whl/subshift/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+from subshift.version import __version__, __author__, __credits__
+from subshift.subshift import subshift, main
+
+"""
+subsync
+
+A small package to synchronize your subtitle files.
+"""
+
+__version__ = __version__
+__author__ = __author__
+__credits__ = __credits__
