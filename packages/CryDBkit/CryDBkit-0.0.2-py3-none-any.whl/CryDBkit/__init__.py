1a5b1fcb0b9ca54349df7ccd806c2774cf32ad63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 03:03:45 +0000

    Add package CryDBkit/0.0.2/CryDBkit-0.0.2-py3-none-any.whl

---
+++ packages/CryDBkit/CryDBkit-0.0.2-py3-none-any.whl/CryDBkit/__init__.py
@@ -0,0 +1,10 @@
+import datetime
+
+__description__ = 'ToolKit for CryDB'
+__email__ = 'binjacobcao@gmail.com'
+
+now = datetime.datetime.now()
+formatted_date_time = now.strftime('%Y-%m-%d %H:%M:%S')
+print('CryDB, Bin CAO, HKUST.GZ' )
+print('Executed on :',formatted_date_time, ' | Have a great day.')  
+print('='*80)
