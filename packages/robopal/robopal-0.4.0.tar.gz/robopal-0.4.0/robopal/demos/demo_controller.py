30e8acc76446fdb1676b2d03e1bcba4a76bef209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 04:04:17 +0000

    Add package robopal/0.4.0/robopal-0.4.0.tar.gz

---
+++ packages/robopal/robopal-0.4.0.tar.gz/robopal-0.4.0/robopal/demos/demo_controller.py
@@ -0,0 +1,52 @@
+import numpy as np
+import logging
+
+from robopal.robots.diana_med import DianaMed
+from robopal.robots.panda import Panda
+from robopal.envs import RobotEnv
+
+logging.basicConfig(level=logging.INFO)
+
+MANIPULATORS = {
+    'DianaMed': DianaMed,
+    'Panda' : Panda,
+}
+
+if __name__ == "__main__":
+
+    options = {}
+    # Choose the manipulator
+    print("available manipulator:\n DianaMed, Panda")
+    options['manipulator'] = input("Choose the manipulator:")
+    assert options['manipulator'] in MANIPULATORS.keys(), 'Invalid manipulator'
+    options['manipulator'] = MANIPULATORS[options['manipulator']]
+
+    # Choose the controller
+    print("available controllers:\n JNTIMP, JNTVEL, CARTIMP, CARTIK")
+    options['ctrl'] = input("Choose the controller:")
+    assert options['ctrl'] in ['JNTIMP', 'JNTVEL', 'CARTIMP', 'CARTIK'], 'Invalid controller'
+
+    env = RobotEnv(
+        robot=options['manipulator'],
+        render_mode='human',
+        control_freq=200,
+        is_interpolate=False,
+        controller=options['ctrl'],
+    )
+
+    if options['ctrl'] == 'JNTIMP':
+        action = np.array([0.3, -0.4, 0.7, -0.3, -0.4, 0.7, 0])
+
+    elif options['ctrl'] == 'JNTVEL':
+        action = np.array([0.00, 0.1, 0.0, 0.0, 0., 0., 0])
+
+    elif options['ctrl'] == 'CARTIMP':
+        action = np.array([0.33, -0.39, 0.66, 1.0, 0.0, 0.0, 0])
+
+    elif options['ctrl'] == 'CARTIK':
+        action = np.array([0.33, -0.3, 0.5, 1, 0, 0, 0])
+
+    env.reset()
+    for _ in range(int(2e4)):
+        env.step(action)
+    env.close()
---
+++ packages/robopal/robopal-0.4.0.tar.gz/robopal-0.4.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='UTF-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='robopal',
+    packages=[package for package in find_packages() if package.startswith("robopal")],
+    version='0.4.0',
+    author="Haoran Zhou, Yichao Huang, Yuhan Zhao, Yang Lu",
+    author_email="jou072@126.com",
+    description="robopal: A Simulation Framework based Mujoco",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NoneJou072/robopal",
+    python_requires=">=3.8",
+    include_package_data=True,
+    install_requires=[
+        'numpy>=1.23.5',
+        'mujoco>=3.1.4',
+        'gymnasium'
+    ],
+    extras_require={
+        'planning': ['ruckig~=0.9.2'],
+    },
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+    ],
+)
