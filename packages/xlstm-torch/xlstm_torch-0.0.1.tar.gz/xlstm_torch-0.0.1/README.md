1f07522217d456aac7d8e72eb8a10844c4416988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 03:05:27 +0000

    Add package xlstm-torch/0.0.1/xlstm_torch-0.0.1.tar.gz

---
+++ packages/xlstm-torch/xlstm_torch-0.0.1.tar.gz/xlstm_torch-0.0.1/README.md
@@ -0,0 +1,10 @@
+[![Multi-Modality](agorabanner.png)](https://discord.gg/qUtxnK2NMf)
+
+# xLSTM
+Implementation of xLSTM in Pytorch from the paper: "xLSTM: Extended Long Short-Term Memory"
+
+
+
+
+# License
+MIT
---
+++ packages/xlstm-torch/xlstm_torch-0.0.1.tar.gz/xlstm_torch-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['xlstm_torch']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['swarms', 'zetascale']
+
+setup_kwargs = {
+    'name': 'xlstm-torch',
+    'version': '0.0.1',
+    'description': 'xLSTM - Pytorch',
+    'long_description': '[![Multi-Modality](agorabanner.png)](https://discord.gg/qUtxnK2NMf)\n\n# xLSTM\nImplementation of xLSTM in Pytorch from the paper: "xLSTM: Extended Long Short-Term Memory"\n\n\n\n\n# License\nMIT\n',
+    'author': 'Kye Gomez',
+    'author_email': 'kye@apac.ai',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/kyegomez/xLSTM',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
