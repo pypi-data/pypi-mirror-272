a572932305ef8452c2805f94b31a8b2d4d895c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 06:04:09 +0000

    Add package iminspect/1.4.0/iminspect-1.4.0-py3-none-any.whl

---
+++ packages/iminspect/iminspect-1.4.0-py3-none-any.whl/iminspect/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+Python Qt5 utils to easily visualize image data for faster development.
+"""
+
+
+__version__ = '1.4.0'
+
+
+__all__ = ['imgview', 'inputs', 'inspection_utils', 'inspection_widgets', 'inspector']
+
+
+def show(data, **kwargs):
+    """Just a "symlink" to iminspector.inspector.inspect() for convenience/out
+    of laziness. Refer to inspector.inspect() for the actual documentation.
+
+    How to save a few characters using this wrapper:
+    import numpy as np
+    import iminspect
+    iminspect.show(np.random.rand(16, 16))
+    """
+    from . import inspector
+    inspector.inspect(data, **kwargs)
