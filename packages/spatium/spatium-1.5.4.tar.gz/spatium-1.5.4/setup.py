cade20caacd627337ae2ccb6ed78d3ed9718290f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 15:05:11 +0000

    Add package spatium/1.5.4/spatium-1.5.4.tar.gz

---
+++ packages/spatium/spatium-1.5.4.tar.gz/spatium-1.5.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, Extension, find_packages
+from Cython.Compiler import Options
+
+Options.docstrings = True
+# Options.annotate = True
+
+setup(
+    ext_modules=[
+        Extension(
+            "spatium._spatium",
+            ["src/spatium/_spatium.pyx"],
+            # extra_compile_args=["-std=c++20", "/std:c++20"],
+        ),
+    ],
+    packages=find_packages(
+        where="src",
+        exclude=["tests", "spatium/*.c", "spatium/*.cpp"]
+    ),
+    package_dir={'': 'src'},
+)
