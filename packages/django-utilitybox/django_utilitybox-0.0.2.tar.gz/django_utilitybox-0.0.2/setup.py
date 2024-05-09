6dc9485e1ef0c050184cc8c79285280f41590d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 06:04:50 +0000

    Add package django-utilitybox/0.0.2/django_utilitybox-0.0.2.tar.gz

---
+++ packages/django-utilitybox/django_utilitybox-0.0.2.tar.gz/django_utilitybox-0.0.2/setup.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+VERSION = '0.0.2'
+DESCRIPTION = '...'
+PACKAGE_NAME = 'django_utilitybox'
+AUTHOR = 'Jose Angel Colin Najera'
+EMAIL = 'josecolin99@gmail.com'
+GITHUB_URL = 'https://github.com/Josecolin99/...'
+
+setup(
+    name = PACKAGE_NAME,
+    packages = find_packages(),
+    entry_points={
+        "console_scripts": [
+            "djangoutilitycmd=dj_utilitybox.__main__:main"
+        ]
+    },
+    version = VERSION,
+    license='MIT',
+    description = DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    author = AUTHOR,
+    author_email = EMAIL,
+    url = GITHUB_URL,
+    keywords = [
+    ],
+    install_requires=[ 
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
