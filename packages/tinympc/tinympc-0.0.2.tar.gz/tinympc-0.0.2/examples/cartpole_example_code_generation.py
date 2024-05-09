c192d88479bdad2628b6f851ae9dd06be6e47429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 14:04:02 +0000

    Add package tinympc/0.0.2/tinympc-0.0.2.tar.gz

---
+++ packages/tinympc/tinympc-0.0.2.tar.gz/tinympc-0.0.2/examples/cartpole_example_code_generation.py
@@ -0,0 +1,26 @@
+import tinympc
+import numpy as np
+
+A = np.array([[1.0, 0.01, 0.0, 0.0],
+              [0.0, 1.0, 0.039, 0.0],
+              [0.0, 0.0, 1.002, 0.01],
+              [0.0, 0.0, 0.458, 1.002]])
+B = np.array([[0.0  ],
+              [0.02 ],
+              [0.0  ],
+              [0.067]])
+Q = np.diag([10.0, 1, 10, 1])
+R = np.diag([1.0])
+
+N = 20
+
+prob = tinympc.TinyMPC()
+
+u_min = np.array([-0.5])
+u_max = np.array([0.5])
+prob.setup(A, B, Q, R, N, rho=1, max_iter=10, u_min=u_min, u_max=u_max)
+
+x0 = np.array([0.5, 0, 0, 0])
+prob.set_x0(x0)
+
+prob.codegen("out", verbose=1)
