11ad6e6a08bf2aced056c8b0d73851ed9e73d7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 16:05:09 +0000

    Add package CTG-Utils/1.0.0/CTG_Utils-1.0.0-py3-none-any.whl

---
+++ packages/CTG-Utils/CTG_Utils-1.0.0-py3-none-any.whl/CTG_Utils/CTG_Func/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.1.0"
+__author__ = 'F. Bertin'
+__license__ = "MIT"
+
+from .CTG_plot import *
+from .CTG_effectif import *
+from .CTG_config import *
+from .CTG_synthese import *
+from .CTGClasses import *
+from .CTG_utility import *
+
+
---
+++ packages/CTG-Utils/CTG_Utils-1.0.0-py3-none-any.whl/CTG_Utils/CTG_GUI/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '4.0.0'
+__author__  = 'BiblioMeter team'
+__license__ = 'MIT'
+
+from CTG_Utils.CTG_GUI.GUI_Globals import *
+from CTG_Utils.CTG_GUI.Useful_Functions import *
+from CTG_Utils.CTG_GUI.Useful_Classes import *
+from CTG_Utils.CTG_GUI.Page_Classes import *
