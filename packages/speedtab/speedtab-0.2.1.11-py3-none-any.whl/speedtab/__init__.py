5e716cbe48ccb212a4d88d2cbc7ed25537bae818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 03:04:15 +0000

    Add package speedtab/0.2.1.11/speedtab-0.2.1.11-py3-none-any.whl

---
+++ packages/speedtab/speedtab-0.2.1.11-py3-none-any.whl/speedtab/__init__.py
@@ -0,0 +1,3 @@
+from speedtab.client import Client, create_token, BooleanCondition, SpreadSheet
+from speedtab.enums import MergeType, BorderSides, BorderStyle, BooleanConditionTypes, HorizontalAlignment, VerticalAlignment, WrapStrategy, ShareRole, ChartType, StackedType, LegendPosition, AxisPosition, DefaultColors, ReadyFormats, CustomFormats, ClearSpecific
+from speedtab.formats import Color
