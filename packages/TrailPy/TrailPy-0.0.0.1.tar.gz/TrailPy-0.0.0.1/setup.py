6072d5d0911e7c30ab98822d88a052b1d9d24057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 03:04:39 +0000

    Add package TrailPy/0.0.0.1/TrailPy-0.0.0.1.tar.gz

---
+++ packages/TrailPy/TrailPy-0.0.0.1.tar.gz/TrailPy-0.0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    long_description= f.read()
+
+# This call to setup() does all the work
+# This call to setup() does all the work
+setup(
+    name="TrailPy",
+    version="0.0.0.1",
+    description="TRIAL",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Tabish Izhar",
+    author_email="tizhar@iul.ac.in",
+    license="GNU LESSER GENERAL PUBLIC LICENSE v2.1 or later (GNU LGPLv2.1)",
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+    ],
+    package_dir= {"":"src"},
+    packages=find_packages(where='src'),
+    include_package_data=False,
+    install_requires=["numpy==1.23.3", "pandas==1.4.4", "plotly==5.10.0", "ray==2.6.1", "openpyxl==3.0.10" ],
+    extras_require={
+        "dev": ["pytest>=7.0","twine>=4.0.2"],
+    },
+    python_requires=">=3.10",
+)
+
+
+
---
+++ packages/TrailPy/TrailPy-0.0.0.1.tar.gz/TrailPy-0.0.0.1/src/TrailPy/mats.py
@@ -0,0 +1,16 @@
+
+class mat():
+  
+    def __init__(self, a,b):
+        self.a=a
+        self.b=b
+        print ("Working: ", a, b)
+
+    def add(self):
+        return (self.a+self.b)
+
+    def sub(self):
+        return (self.a-self.b)
+
+    def multiple(self):
+        return (self.a*self.b)        
