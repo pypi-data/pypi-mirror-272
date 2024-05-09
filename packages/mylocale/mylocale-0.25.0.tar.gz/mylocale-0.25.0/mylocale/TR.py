c2e7d5e3fe18d8415aa5de0d208051a2c7927779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 14:03:47 +0000

    Add package mylocale/0.25.0/mylocale-0.25.0.tar.gz

---
+++ packages/mylocale/mylocale-0.25.0.tar.gz/mylocale-0.25.0/mylocale/TR.py
@@ -0,0 +1,23 @@
+import csv
+from babel import default_locale
+
+
+def tr(
+    csv_file,
+    target_key,
+    varnames: list = [],
+    vars: list = [],
+    langcode=default_locale(),
+):
+    f = open(csv_file, newline="")
+    locale_csv = csv.DictReader(f=f, delimiter=",")
+    for item in locale_csv:
+        if item["stringname"] == target_key:
+            try:
+                if item[langcode] == "":
+                    return item["en_EN"]
+                else:
+                    return item[langcode]
+            except:
+                return item["en_EN"]
+    f.close()
