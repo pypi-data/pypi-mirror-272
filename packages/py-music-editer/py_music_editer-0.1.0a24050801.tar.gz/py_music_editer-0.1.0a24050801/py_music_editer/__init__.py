cc8fb9d313ff0a0388c43934f44b0464f6581093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 04:04:11 +0000

    Add package py-music-editer/0.1.0a24050801/py_music_editer-0.1.0a24050801.tar.gz

---
+++ packages/py-music-editer/py_music_editer-0.1.0a24050801.tar.gz/py_music_editer-0.1.0a24050801/py_music_editer/__init__.py
@@ -0,0 +1,12 @@
+"""
+py_music_editer/__init__.py
+Version: 0.1.0.alpha24050801
+Copyright @CooooldWind_
+Protected by Apache License Version 2.0.
+https://www.apache.org/licenses/LICENSE-2.0
+"""
+
+from mutagen.id3 import ID3
+
+class Music:
+    pass
