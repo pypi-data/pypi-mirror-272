c518444df0a9561f8e41f79049ce6b9c12072b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 16:04:44 +0000

    Add package mxlm/0.0.8/mxlm-0.0.8-py3-none-any.whl

---
+++ packages/mxlm/mxlm-0.0.8-py3-none-any.whl/mxlm/mxlm_utils.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 29 16:15:58 2024
+
+@author: yl
+"""
+
+
+def df_to_html(df, *args, max_width=400, **argkws):
+    """
+    Pretty print DataFrame to html
+    """
+    if hasattr(df, "to_frame"):
+        df = df.to_frame()
+    argkws.setdefault(
+        "formatters",
+        {
+            col: lambda x: f'<div style="max-width:{max_width}px;"><span style="white-space: pre-wrap; font-family: Monospace;">%s</span></div>'
+            % x
+            for col in df.columns
+        },
+    )
+    argkws.setdefault("escape", False)
+    return df.to_html(*args, **argkws)
