e44c9497fd9a5f5eeb63a3489c70b97bdd9dee91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 03:05:43 +0000

    Add package cntime-nlp/0.1/cntime-nlp-0.1.tar.gz

---
+++ packages/cntime-nlp/cntime-nlp-0.1.tar.gz/cntime-nlp-0.1/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+
+import setuptools
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+with open(HERE / "README.md", encoding="utf-8") as f:
+    README = f.read()
+
+
+with open("requirements.txt") as f:
+    install_requires = [line for line in f if line and line[0] not in "#-"]
+
+setup(
+    name="cntime-nlp",
+    version="0.1",
+    keywords=["nlp", "time nlp"],
+    url="https://github.com/KelabDev/ChineseTimeNLP",
+    author="Taccisum",
+    author_email="taccisum@gmail.com",
+    long_description_content_type="text/markdown",
+    description="将中文时间表达词转为相应的时间字符串，支持时间点，时间段，时间间隔。",
+    long_description=README,
+    license="MIT Licence",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    classifiers=["Programming Language :: Python :: 3.9"],
+)
