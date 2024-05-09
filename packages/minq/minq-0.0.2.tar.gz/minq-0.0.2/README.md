1cba180d59c761c9cfa7a8f0ad169707f42d37a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 14:03:44 +0000

    Add package minq/0.0.2/minq-0.0.2.tar.gz

---
+++ packages/minq/minq-0.0.2.tar.gz/minq-0.0.2/README.md
@@ -0,0 +1,6 @@
+# Minq
+
+[![PyPI](https://img.shields.io/pypi/v/minq)](https://pypi.org/project/minq)
+[![Typecheck](https://github.com/fwcd/minq/actions/workflows/typecheck.yml/badge.svg)](https://github.com/fwcd/minq/actions/workflows/typecheck.yml)
+
+Tiny quantum gate library based on NumPy.
---
+++ packages/minq/minq-0.0.2.tar.gz/minq-0.0.2/minq/__init__.py
@@ -0,0 +1,55 @@
+from functools import reduce
+from numpy.typing import ArrayLike, NDArray
+from typing import cast
+
+import numpy as np
+
+I = np.identity(2)
+'''Identity transform'''
+
+X = np.array([
+    [0, 1],
+    [1, 0],
+])
+'''Bit-flip transform'''
+
+Z = np.array([
+    [1, 0],
+    [0, -1],
+])
+'''Phase-flip transform'''
+
+H = (1 / np.sqrt(2)) * np.array([
+    [1, 1],
+    [1, -1],
+])
+'''Hadamard transform'''
+
+def ket(*args: int | str):
+    '''Creates a standard basis vector'''
+    if len(args) == 0:
+        return np.array([1])
+    elif isinstance(args[0], int):
+        assert all(b == 0 or b == 1 for b in args)
+        x = np.zeros(2 ** len(args))
+        x[from_binary(*cast(tuple[int, ...], args))] = 1
+        return x
+    else:
+        assert len(args) == 1
+        x = np.zeros(2 ** len(args[0]))
+        x[int(args[0], base=2)] = 1
+        return x
+
+def kron(*arrays: ArrayLike) -> NDArray:
+    '''Computes the Kronecker product (tensor product) of the given arrays'''
+    assert len(arrays) > 0
+    return reduce(np.kron, arrays)
+
+def kronpow(array: ArrayLike, n: int) -> NDArray:
+    '''Applies the Kronecker product n times'''
+    assert n > 0
+    return reduce(lambda acc, _: np.kron(acc, array), range(n - 1), array)
+
+def from_binary(*bits: int) -> int:
+    '''Converts a list of bits to the corresponding number when interpreted in base 2'''
+    return 0 if len(bits) == 0 else int(''.join(map(str, bits)), base=2)
