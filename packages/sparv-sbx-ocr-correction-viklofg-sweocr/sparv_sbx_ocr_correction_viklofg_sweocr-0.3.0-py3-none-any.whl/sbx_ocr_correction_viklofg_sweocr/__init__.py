2c39dfaebb7f526d73e78824fea0c365e1a8f1d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 03:04:43 +0000

    Add package sparv-sbx-ocr-correction-viklofg-sweocr/0.3.0/sparv_sbx_ocr_correction_viklofg_sweocr-0.3.0-py3-none-any.whl

---
+++ packages/sparv-sbx-ocr-correction-viklofg-sweocr/sparv_sbx_ocr_correction_viklofg_sweocr-0.3.0-py3-none-any.whl/sbx_ocr_correction_viklofg_sweocr/__init__.py
@@ -0,0 +1,7 @@
+from sbx_ocr_correction_viklofg_sweocr.annotations import annotate_ocr_correction
+
+__all__ = ["annotate_ocr_correction"]
+
+__description__ = "Annotate words with corrections of OCR-errors."
+
+__version__ = "0.3.0"
