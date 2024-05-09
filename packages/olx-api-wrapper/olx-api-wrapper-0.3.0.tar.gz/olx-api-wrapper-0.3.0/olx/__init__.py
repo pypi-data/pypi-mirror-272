129a50c57a50d5f23726e1a316665602313a2c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 04:03:45 +0000

    Add package olx-api-wrapper/0.3.0/olx-api-wrapper-0.3.0.tar.gz

---
+++ packages/olx-api-wrapper/olx-api-wrapper-0.3.0.tar.gz/olx-api-wrapper-0.3.0/olx/__init__.py
@@ -0,0 +1,7 @@
+__title__ = "olx-api-wrapper"
+__version__ = "0.3.0"
+__author__ = "Paweł Stawikowski"
+__copyright__ = "Copyright (c) 2024 Paweł Stawikowski"
+__license__ = "MIT"
+
+from . import partner
