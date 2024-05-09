e8ae9f27df9a72fb5a5f51f15c41823b1e77952c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 03:05:11 +0000

    Add package vital-ai-vitalsigns/0.1.10/vital_ai_vitalsigns-0.1.10-py3-none-any.whl

---
+++ packages/vital-ai-vitalsigns/vital_ai_vitalsigns-0.1.10-py3-none-any.whl/vital_ai_vitalsigns/model/properties/TruthProperty.py
@@ -0,0 +1,22 @@
+from .IProperty import IProperty
+
+
+class TruthProperty(IProperty):
+    def __init__(self, value: str):
+        super().__init__(value)
+
+    def __str__(self):
+        return str(self.value)
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.value == other
+        elif isinstance(other, TruthProperty):
+            return self.value == other.value
+        return NotImplemented
+
+    def __rshift__(self, other):
+        return self == other
+
+    def to_json(self):
+        return {"value": self.value}
