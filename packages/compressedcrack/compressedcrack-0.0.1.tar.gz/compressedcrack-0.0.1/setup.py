7d176430b7a5627011920ff0edd2947d1c60526f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 04:04:25 +0000

    Add package compressedcrack/0.0.1/compressedcrack-0.0.1.tar.gz

---
+++ packages/compressedcrack/compressedcrack-0.0.1.tar.gz/compressedcrack-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='compressedcrack',
+    version='0.0.1',
+    description='A command-line tool to crack password-protected compressed files using brute force.',
+    author='Thanh Minh',
+    author_email='thanhdoantranminh@gmail.com',
+    url='https://github.com/mnismt/CompressedCrack',
+    packages=find_packages(),
+    install_requires=[
+        'patoolib',
+    ],
+    entry_points={
+        'console_scripts': [
+            'compressedcrack = main:main',
+        ],
+    },
+)
