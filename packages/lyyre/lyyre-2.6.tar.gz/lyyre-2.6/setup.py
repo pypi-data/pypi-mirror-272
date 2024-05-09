ec7a9a0241b3ed1f87b6a03c8639bee85f68bdda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 03:04:44 +0000

    Add package lyyre/2.6/lyyre-2.6.tar.gz

---
+++ packages/lyyre/lyyre-2.6.tar.gz/lyyre-2.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='lyyre',
+    version='2.6',
+    author='lyy',
+    author_email='',
+    description='userfull application for  lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=['re'
+
+
+    ]
+)
