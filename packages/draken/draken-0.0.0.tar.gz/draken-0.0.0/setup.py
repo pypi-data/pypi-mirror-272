98031c0aecf4c706a3400060635b5d9d622c43eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 14:03:47 +0000

    Add package draken/0.0.0/draken-0.0.0.tar.gz

---
+++ packages/draken/draken-0.0.0.tar.gz/draken-0.0.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+LIBRARY = "draken"
+
+__author__ = "notset"
+__version__ = "0.0.0"
+#with open(f"{LIBRARY}/__version__.py", mode="r") as v:
+#    vers = v.read()
+#exec(vers)  # nosec
+
+with open("README.md", "r") as rm:
+    long_description = rm.read()
+
+try:
+    with open("requirements.txt", "r") as f:
+        required = f.read().splitlines()
+except:
+    with open("draken.egg-info/requires.txt", "r") as f:
+        required = f.read().splitlines()
+
+setup(
+    name=LIBRARY,
+    version=__version__,
+    description="External Index",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    maintainer="@joocer",
+    author=__author__,
+    author_email="justin.joyce@joocer.com",
+    packages=find_packages(include=[LIBRARY, f"{LIBRARY}.*"]),
+    url=f"https://github.com/mabel-dev/{LIBRARY}/",
+    install_requires=required,
+)
