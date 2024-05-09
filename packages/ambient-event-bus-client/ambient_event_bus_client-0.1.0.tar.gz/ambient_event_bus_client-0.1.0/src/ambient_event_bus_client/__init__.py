289baa4d6e03f1619ba52c83b2c1555c4fbe6ed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 14:04:06 +0000

    Add package ambient-event-bus-client/0.1.0/ambient_event_bus_client-0.1.0.tar.gz

---
+++ packages/ambient-event-bus-client/ambient_event_bus_client-0.1.0.tar.gz/ambient_event_bus_client-0.1.0/src/ambient_event_bus_client/__init__.py
@@ -0,0 +1,8 @@
+"""A library to interact with the Ambient Labs Event Bus.
+"""
+
+__version__ = "0.1.0"
+
+from .client import Client
+from .models.options import ClientOptions
+from .models.event_api_models import *
