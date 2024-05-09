7d4316e4df1e087db578c0342205787751dd010e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 05:04:10 +0000

    Add package metaflow-dbt-extension/1.0.0/metaflow-dbt-extension-1.0.0.tar.gz

---
+++ packages/metaflow-dbt-extension/metaflow-dbt-extension-1.0.0.tar.gz/metaflow-dbt-extension-1.0.0/metaflow_extensions/dbt_ext/config/mfextinit_dbt_ext.py
@@ -0,0 +1,16 @@
+from metaflow.metaflow_config_funcs import from_conf
+
+###
+# CONFIGURE: You can also specify additional debugging options that are
+#            activated using the METAFLOW_DEBUG_XXX environment variable. See debug.py
+###
+DEBUG_OPTIONS = ["dbt"]
+
+# Which adapter to use with DBT. Default to Postgres.
+# This will also determine which dependency to install for conda environments as the dbt-[adapter-name] python package.
+# see https://docs.getdbt.com/docs/supported-data-platforms for a list of adapters
+DBT_ADAPTER_NAME = from_conf("DBT_ADAPTER_NAME", "postgres")
+
+
+def get_pinned_conda_libs(python_version, datastore_type):
+    return {"pyyaml": "6.0", f"dbt-{DBT_ADAPTER_NAME}": ">=1.7.0"}
