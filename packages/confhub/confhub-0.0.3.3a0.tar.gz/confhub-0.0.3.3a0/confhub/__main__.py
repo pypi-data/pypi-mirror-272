41d2229ff16a705a7804b6a3a993fdb481b32296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 03:05:18 +0000

    Add package confhub/0.0.3.3a0/confhub-0.0.3.3a0.tar.gz

---
+++ packages/confhub/confhub-0.0.3.3a0.tar.gz/confhub-0.0.3.3a0/confhub/__main__.py
@@ -0,0 +1,45 @@
+import argparse
+
+import structlog
+
+import confhub.commands as commands
+from confhub.setup_logger import SetupLogger, LoggerReg
+from confhub.__meta__ import __version__
+
+SetupLogger(
+    name_registration=[
+        LoggerReg(name="confhub_configuration", level=LoggerReg.Level.INFO),
+    ],
+    default_development=True
+)
+logger: structlog.BoundLogger = structlog.getLogger('confhub_configuration')
+
+
+class Config:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description=f"Confhub v{__version__} help")
+        self.init_args()
+
+        self.__args = self.parser.parse_args()
+        logger.debug('Initializing config')
+
+    def parsing(self) -> None:
+        logger.debug('Argument handling..')
+
+        if self.__args.create:
+            commands.create(folder=self.__args.create)
+
+        logger.debug('Argument processing completed!')
+
+    def init_args(self):
+        # Добавляем аргументы
+        self.parser.add_argument(
+            '-c', '--create',
+            metavar="folder", type=str,
+            help='Generating configuration files'
+        )
+
+
+if __name__ == '__main__':
+    config = Config()
+    config.parsing()
