f818645640a5f7fa9af67fa10c192454a8dc8796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 03:04:47 +0000

    Add package hatyan/2.8.0/hatyan-2.8.0.tar.gz

---
+++ packages/hatyan/hatyan-2.8.0.tar.gz/hatyan-2.8.0/hatyan/metadata.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  9 20:48:46 2023
+
+@author: veenstra
+"""
+
+import numpy as np
+import pandas as pd
+
+
+def metadata_add_to_obj(obj,metadata):
+    """
+    add metadata to components/timeseries object (is pd.DataFrame)
+    via attrs, but this function prevents dropping existing attrs that are not in metadata dict
+    """
+    for key in metadata.keys():
+        obj.attrs[key] = metadata[key]
+    
+    return obj
+
+
+def metadata_from_diablocks(diablocks_pd, block_id):
+    diablocks_pd_onerow = diablocks_pd.iloc[block_id]
+    
+    metadata_keys = ['station', 'grootheid', 'eenheid', 
+                     'vertref', 
+                     'tstart', 'tstop',
+                     'timestep_min', 'timestep_unit',
+                     'TYP', 'groepering']
+    
+    #TODO: align with metadata from hatyan.read_components()
+    metadata = {key:diablocks_pd_onerow[key] for key in metadata_keys}
+    
+    # add origin
+    metadata['origin'] = 'from timeseries dia file'
+    
+    # replace nan with None (otherwise metadata_compare fails)
+    #TODO: avoid nan in metadata (timestep for hoek_har.dia)
+    if np.isnan(metadata['timestep_min']): #non-equidistant, nan in py38 and none in py39 (pandas 2.1.2)
+        metadata['timestep_min'] = None
+        metadata['timestep_unit'] = None
+    return metadata
+
+
+def metadata_from_obj(obj):
+    metadata = obj.attrs.copy()
+    return metadata
+
+
+def metadata_compare(metadata_list):
+    
+    # remove tstart/tstop since they cannot be compared on equality in case of multifile dia
+    metadata_list_notstartstop = []
+    for meta in metadata_list:
+        meta_new = meta.copy()
+        if 'tstart' in meta_new:
+            meta_new.pop('tstart')
+        if 'tstop' in meta_new:
+            meta_new.pop('tstop')
+        metadata_list_notstartstop.append(meta_new)
+    
+    nmeta = len(metadata_list_notstartstop)
+    for i in range(1,nmeta):
+        meta1 = metadata_list_notstartstop[i-1]
+        meta2 = metadata_list_notstartstop[i]
+        if meta1!=meta2:
+            meta_12_df = pd.concat([pd.Series(meta1), pd.Series(meta2)],axis=1)
+            meta_12_df["equal"] = meta_12_df[0]==meta_12_df[1]
+            raise ValueError(f'metadata for two datasets is not equal, cannot be merged:\n{meta_12_df}')
+
+
+def wns_from_metadata(metadata):
+    """
+    Gets waarnemingssoort from metadata grootheid (quantity), eenheid (unit) and vertref (vertical reference).
+    Idea from: https://repos.deltares.nl/repos/lib_tide/trunk/src/hatyan_fortran/HATYAN00/wnstab.f
+    Deliberately left out domeincode (type), I(int) and F(float) since we always have floats 
+    that are only converted to int upon file writing
+    """
+    
+    meta_sel = {key:metadata[key] for key in ['grootheid','eenheid','vertref']}
+    
+    if meta_sel == {'grootheid':'WATHTE', 'eenheid':'cm', 'vertref':'NAP'}:
+        wns = 1
+    elif meta_sel == {'grootheid':'WATHTE', 'eenheid':'cm', 'vertref':'MSL'}:
+        wns = 54
+    elif meta_sel == {'grootheid':'WATHTBRKD', 'eenheid':'cm', 'vertref':'NAP'}:
+        wns = 18
+    elif meta_sel == {'grootheid':'WATHTBRKD', 'eenheid':'cm', 'vertref':'MSL'}:
+        wns = 55
+    else:
+        raise ValueError(f'combination of quantity/unit/vertref not found available in wns_from_metadata():\n{meta_sel}')
+    
+    return wns
---
+++ packages/hatyan/hatyan-2.8.0.tar.gz/hatyan-2.8.0/tests/test_astrog.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 21 11:17:44 2021
+
+@author: laan_st
+
+"""
+
+
+import pytest
+import numpy as np
+import pandas as pd
+import datetime as dt
+import hatyan
+
+
+@pytest.mark.systemtest
+def test_astrog_dT():
+    # 1. Input
+    timeInput = pd.date_range(start=dt.datetime(1970,12,31,12,31),end=dt.datetime(2020,12,31,12,31),freq='YS')
+    
+    # 2. Expectations
+    dTLastExp = np.array([39.468, 40.144, 40.82 , 41.496, 42.172, 42.848, 43.524, 44.2  ,
+           44.876, 45.552, 46.228, 46.904, 47.58 , 48.256, 48.932, 49.608,
+           50.284, 50.96 , 51.636, 52.312, 52.988, 53.664, 54.34 , 55.016,
+           55.692, 56.368, 57.044, 57.72 , 58.396, 59.072, 59.748, 60.424,
+           61.1  , 61.776, 62.452, 63.128, 63.804, 64.48 , 65.156, 65.832,
+           66.508, 67.184, 67.86 , 68.536, 69.212, 69.888, 70.564, 71.24 ,
+           71.916, 72.592])
+    dTExctExp = np.array([42.184, 42.184, 44.184, 45.184, 46.184, 47.184, 48.184, 49.184,
+           50.184, 51.184, 51.184, 52.184, 53.184, 54.184, 54.184, 55.184,
+           55.184, 56.184, 56.184, 57.184, 58.184, 58.184, 59.184, 60.184,
+           61.184, 62.184, 62.184, 63.184, 64.184, 64.184, 64.184, 64.184,
+           64.184, 64.184, 64.184, 65.184, 65.184, 65.184, 66.184, 66.184,
+           66.184, 66.184, 67.184, 67.184, 67.184, 68.184, 69.184, 69.184,
+           69.184, 69.184])
+    
+    # 3. Run test
+    dTLastOut = hatyan.astrog.dT(timeInput,dT_fortran=True)
+    dTExctOut = hatyan.astrog.dT(timeInput,dT_fortran=False)
+    
+    # 4. Vefication
+    assert type(dTLastOut) == np.ndarray
+    assert type(dTExctOut) == np.ndarray
+    assert np.allclose(dTLastExp, dTLastOut)
+    assert np.allclose(dTExctExp, dTExctOut)
+
+
+@pytest.mark.systemtest
+def test_astrog_astrab():
+    
+    # 1. Input
+    timeInput = dt.datetime(2008,1,1,12,31)
+    
+    # 2. Expectations
+    parExpect = {'EHMOON': np.array([448.08691901]),
+                 'DECMOO': np.array([-13.02683311]),
+                 'PARLAX': np.array([3262.092635]),
+                 'DPAXDT': np.array([-18.05431966]),
+                 'ALTMOO': np.array([-13.09124181]),
+                 'ELONG':  np.array([283.08899401]),
+                 'ALTSUN': np.array([14.08083711]),
+                 'LONSUN': np.array([280.45809545]),
+                 'EQELON': np.array([278.83322595]),
+                 'DECSUN': np.array([-23.02828019]),
+                 'DISSUN': np.array([0.98330485]),
+                 'EHARI':  np.array([5.0317035]),
+                 'RASUN':  np.array([281.37522918]),
+                 'EHSUN':  np.array([6.92014497]),
+                 'LONMOO': np.array([203.54708946]),
+                 'LATMOO': np.array([-4.18226297]),
+                 'RAMOON': np.array([200.20845513]),
+                 'ANM':    np.array([151.15606465])}
+    
+    # 3. Run test
+    parOutput = hatyan.astrog.astrab(timeInput,hatyan.astrog.dT(timeInput,dT_fortran=True))
+    
+    # 4. Vefication
+    assert type(parOutput) == dict
+    assert len(parOutput)  == 18
+    for parName in parExpect:
+        assert len(parOutput[parName]) == 1
+        assert abs(parExpect[parName][0]-parOutput[parName][0]) < 10E-9
+    
+    
+@pytest.mark.systemtest
+def test_astrog_astrac():
+    
+    # 1. Input
+    timeInput  = dt.datetime(2008,1,1,6,31)
+    
+    # 2. Expectations
+    timeExpect = [pd.DatetimeIndex(['2007-12-31 18:08:06.634848001']),
+                  pd.DatetimeIndex(['2008-01-01 06:28:32.856047999']),
+                  pd.DatetimeIndex(['2007-12-17 10:18:26.216188801']),
+                  pd.DatetimeIndex(['2007-12-24 01:16:42.818976000']),
+                  pd.DatetimeIndex(['2007-12-31 07:51:54.693312000']),
+                  pd.DatetimeIndex(['2008-01-08 11:38:04.174895999']),
+                  pd.DatetimeIndex(['2008-01-01 00:54:56.645894400']),
+                  pd.DatetimeIndex(['2008-01-01 11:07:33.697843200']),
+                  pd.DatetimeIndex(['2008-01-01 07:48:34.139385600']),
+                  pd.DatetimeIndex(['2008-01-01 07:48:34.139471999']),
+                  pd.DatetimeIndex(['2008-03-20 05:49:10.054166400']),
+                  pd.DatetimeIndex(['2007-06-21 18:07:03.793017600']),
+                  pd.DatetimeIndex(['2007-09-23 09:52:02.142432000']),
+                  pd.DatetimeIndex(['2007-12-22 06:08:46.055884800']),
+                  pd.DatetimeIndex(['2008-01-03 08:22:45.903254400']),
+                  pd.DatetimeIndex(['2008-01-03 08:22:45.696239999'])]
+     
+    # 3. Run test
+    timeOutput = []
+    for iMode in range(1,17):
+        timeOutput.append(hatyan.astrog.astrac(timeInput,dT_fortran=False,mode=np.array(iMode)))
+    
+    # 4. Vefication
+    for iMode in range(1,17):
+        print(iMode)
+        assert type(timeOutput[iMode-1]) == pd.DatetimeIndex
+        assert abs(timeExpect[iMode-1]-timeOutput[iMode-1]).total_seconds() < 10E-5
+
+
+def test_astrog_moonriseset():
+    """
+    This part of code resulted in timesteps being dropped in newer pandas versions.
+    This was due to 0 RATES in astract, resulting in inf values in 'addtime'.
+    This is solved by supplying posinf=0 to np.nan_to_num().
+    This test therefore checks if the length of the resulting dataframe is correct.
+    """
+    
+    # script settings
+    tstart = dt.datetime(2003,1,1)
+    tstop = dt.datetime(2003,6,1)
+    
+    # moonrise and -set
+    moonriseset_python = hatyan.astrog_moonriseset(tFirst=tstart, tLast=tstop)
+    
+    assert len(moonriseset_python) == 292
+
+
+@pytest.mark.systemtest
+def test_astrog_leapsecondslist():
+    leap_seconds_pd, expirydate = hatyan.astrog.get_leapsecondslist_fromurlorfile()
+    
