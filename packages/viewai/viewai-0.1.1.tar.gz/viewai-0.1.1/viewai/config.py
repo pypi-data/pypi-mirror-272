a3050e8100655d48b97169ea64a4409cb22a3d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 15:04:21 +0000

    Add package viewai/0.1.1/viewai-0.1.1.tar.gz

---
+++ packages/viewai/viewai-0.1.1.tar.gz/viewai-0.1.1/viewai/config.py
@@ -0,0 +1,78 @@
+# Copyright (c) 2024 The View AI Contributors
+# Distributed under the MIT software license
+
+from .imports import *
+
+class ClassificationType(Enum):
+    """Enum Class for Classification Type."""
+    BINARY = "Binary"
+    MULTICLASS = "Multiclass"
+
+class RegressionType(Enum):
+    """Enum Class for Regression Type."""
+    LINEAR = "Linear"
+    NONLINEAR = "Nonlinear"
+
+class Config:
+    """Getter Class for Configurations for XAI. (HARD CODED)"""
+
+    def __init__(self) -> None:
+        pass
+
+    def get_missing_value_threshold(self):
+        """Returns the threshold for missing value percentage in a feature."""
+        return 60
+
+    def get_supervised_type_threshold(self):
+        """Returns the threshold for determining classification task based on nunique of target feature."""
+        return 20 + 1
+
+    def get_categorical_feature_threshold(self):
+        """Returns the threshold for determining categorical feature based on nunique of feature."""
+        return 50 + 1
+
+    def high_cardinality_features_threshold(self):
+        """Returns the threshold for determining high cardinality feature based on nunique of feature."""
+        return 50 + 1
+    
+    def get_shap_sample_percentage(self):
+        """Returns the sample size for SHAP."""
+        return 0.4
+
+    def get_classification_type(self, binary=True):
+        """Returns Supervised Task"""
+        if(binary):
+            return ClassificationType.BINARY
+        else:
+            return ClassificationType.MULTICLASS
+
+    def get_regression_type(self, linear=True):
+        """Returns Supervised Task"""
+        if(linear):
+            return RegressionType.LINEAR
+        else:
+            return RegressionType.NONLINEAR
+
+
+URL_REGEX = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
+
+SUMMARY_PLOT_FILE_NAME = os.path.join("tmp", "Summary_plot.json")
+FEATURE_IMPORTANCE_PLOT_FILE_NAME = os.path.join("tmp", "FImportance_plot.json")
+
+RANDOM_SEED = 42
+random.seed(RANDOM_SEED)
+np.random.seed(RANDOM_SEED)  # Set seed for reproducibility
+
+
+def create_folder(folder_path):
+    """Create a folder if it doesn't exist."""
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        print(f"Folder '{folder_path}' created.")
+    else:
+        print(f"Folder '{folder_path}' already exists.")
+
+
+# Example usage
+folder_path = "./tmp"
+create_folder(folder_path)
