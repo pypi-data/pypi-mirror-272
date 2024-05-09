c620dc07edd94ea752a3f55f3c385d0ba7b20c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 06:04:38 +0000

    Add package pychacha/1.0.3/pychacha-1.0.3.tar.gz

---
+++ packages/pychacha/pychacha-1.0.3.tar.gz/pychacha-1.0.3/README.md
@@ -0,0 +1,5 @@
+# pychacha
+
+This is an implementation of ChaCha20 encryption in Python, with optional RSA encryption of the ChaCha20 key.
+
+This code is provided as-is, with no warranty of safety, security, or functionality. Use at your own risk.
---
+++ packages/pychacha/pychacha-1.0.3.tar.gz/pychacha-1.0.3/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as file:
+    readme = file.read()
+
+setuptools.setup(
+    name="pychacha",
+    version="1.0.3",
+    description="ChaCha20 encryption tools for Python.",
+    url="https://github.com/thecodeforge/pychacha",
+    py_modules=["pychacha"],
+    install_requires=[],
+    packages=setuptools.find_packages(),
+    package_dir={
+        "pychacha":"pychacha"
+    },
+    python_requires= '>=3.11',
+    long_description = readme,
+    long_description_content_type = "text/markdown"
+)
