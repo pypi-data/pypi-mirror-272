2a4a9106ec1aa2c7329f99530c44930a7951603d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 04:03:52 +0000

    Add package sparklestock/0.0.0.1/sparklestock-0.0.0.1.tar.gz

---
+++ packages/sparklestock/sparklestock-0.0.0.1.tar.gz/sparklestock-0.0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sparklestock", ## 소문자 영단어
+    version="0.0.0.1", ##
+    author="Seyong Ahn", ## ex) Sunkyeong Lee
+    author_email="hiseyong1008@gmail.com", ##
+    description="sparkle stock package", ##
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hiseyong", ##
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
