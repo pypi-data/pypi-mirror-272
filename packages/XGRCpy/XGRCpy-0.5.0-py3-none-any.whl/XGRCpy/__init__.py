70c36a87f122c20f4c4ba3e6283e6e96b92b152e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 03:05:28 +0000

    Add package XGRCpy/0.5.0/XGRCpy-0.5.0-py3-none-any.whl

---
+++ packages/XGRCpy/XGRCpy-0.5.0-py3-none-any.whl/XGRCpy/__init__.py
@@ -0,0 +1,8 @@
+# Import key functions and classes
+from .XGRCpy import XGRCViewTable
+
+# Optional: Define a version number for the package
+__version__ = "0.5.0"
+
+# Optional: Define the package's public interface
+__all__ = ["XGRCViewTable"]
