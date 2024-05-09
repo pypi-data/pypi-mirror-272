3a3642b09830fd19615df37aace527d68c6dbbb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 06:04:21 +0000

    Add package jenkins-pysdk/1.3.2/jenkins_pysdk-1.3.2.tar.gz

---
+++ packages/jenkins-pysdk/jenkins_pysdk-1.3.2.tar.gz/jenkins_pysdk-1.3.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setup(
+    name='jenkins_pysdk',
+    version='1.3.2',  # Don't forget to update version.py & conf.py :)
+    packages=find_packages(),
+    install_requires=[
+        'httpx',
+        'orjson',
+        'pydantic',
+        'urllib3',
+    ],
+    author='KnownZero',
+    author_email="gihjeefs@protonmail.com",
+    description='2024 Python SDK for Jenkins',
+    url='https://github.com/KnownZero/JenkinsPythonSDK',
+    python_requires='>=3.6',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    keywords=['python', 'Jenkins', 'SDK', 'API', 'REST']
+)
