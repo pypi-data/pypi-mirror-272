11f363bb02695d04eafd356f0928ce5ab5325db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 03:03:53 +0000

    Add package frykit/0.6.0/frykit-0.6.0.tar.gz

---
+++ packages/frykit/frykit-0.6.0.tar.gz/frykit-0.6.0/script/run_example.py
@@ -0,0 +1,11 @@
+'''运行example目录下的所有脚本'''
+
+# TODO: 换用pytest
+import subprocess
+from pathlib import Path
+
+code_filepath = Path(__file__).absolute()
+example_dirpath = code_filepath.parent.parent / 'example'
+for example_filepath in example_dirpath.iterdir():
+    subprocess.run(['python', str(example_filepath)])
+    print(f'> {example_filepath.name}')
