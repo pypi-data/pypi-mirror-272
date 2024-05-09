7256fef7f07799dd50612147bc10232fdb0b8bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 06:04:22 +0000

    Add package python-cmr/0.11.0/python_cmr-0.11.0.tar.gz

---
+++ packages/python-cmr/python_cmr-0.11.0.tar.gz/python_cmr-0.11.0/cmr/__init__.py
@@ -0,0 +1,3 @@
+from .queries import GranuleQuery, CollectionQuery, ToolQuery, ServiceQuery, VariableQuery, CMR_OPS, CMR_UAT, CMR_SIT
+
+__all__ = ["GranuleQuery", "CollectionQuery", "ToolQuery", "ServiceQuery", "VariableQuery", "CMR_OPS", "CMR_UAT", "CMR_SIT"]
