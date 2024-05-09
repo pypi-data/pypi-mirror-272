cc0139223a1a5a95f227eeb1c4e2bacec578d518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 06:04:19 +0000

    Add package naludaq/0.26.5/naludaq-0.26.5-py3-none-any.whl

---
+++ packages/naludaq/naludaq-0.26.5-py3-none-any.whl/naludaq/daq/workers/packager/__init__.py
@@ -0,0 +1,13 @@
+from naludaq.daq.workers.packager.worker_packager import PackagerLight
+from naludaq.daq.workers.packager.worker_packager_debug import DebugPackager
+from naludaq.daq.workers.packager.worker_packager_hdsoc import HDSoCPackager
+
+
+def get_packager(board, *args, **kwargs):
+    """Returns a Packager object for the given board."""
+    if board.params["model"] in ["hdsocv1", "hdsocv1_evalr1", "hdsocv1_evalr2"]:
+        return HDSoCPackager(board, *args, **kwargs)
+    else:
+        return PackagerLight(*args, **kwargs)
+    # else:
+    #     raise ValueError('Unknown board model: {}'.format(board.params['model']))
