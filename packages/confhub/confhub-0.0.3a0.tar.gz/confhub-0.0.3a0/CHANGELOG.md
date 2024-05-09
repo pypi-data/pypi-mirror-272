154a553cb05c04a7c68623ffb33046a627297bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 03:04:00 +0000

    Add package confhub/0.0.3a0/confhub-0.0.3a0.tar.gz

---
+++ packages/confhub/confhub-0.0.3a0.tar.gz/confhub-0.0.3a0/CHANGELOG.md
@@ -0,0 +1,17 @@
+# Changelog
+---
+
+# v0.0.3a
+- Instructions for installation and use are written in README.md
+
+---
+
+# v0.0.2a
+- Adaptation for the library
+- A command has been created to generate configuration files.
+
+---
+
+# v0.0.1a
+- A test version was developed and tested on several projects.
+- Preparation and testing continues for the creation of the boot package.
---
+++ packages/confhub/confhub-0.0.3a0.tar.gz/confhub-0.0.3a0/confhub/reader.py
@@ -0,0 +1,97 @@
+import os
+from pathlib import Path
+from typing import Optional, Any
+
+import structlog
+from dynaconf import Dynaconf, LazySettings
+from dynaconf.utils.boxing import DynaBox
+
+from confhub.enums import Service
+from confhub.exceptions import PathError, ModuleException
+from confhub.setup_logger import SetupLogger, LoggerReg
+
+logger: structlog.BoundLogger = structlog.get_logger("confhub")
+
+
+class ReaderConf:
+    """
+
+    Class for reading configuration files.
+    Supported configuration formats:
+        - .toml - Default and recommended file format.
+        - .yaml|.yml - Recommended for Django applications.
+        - .json - Useful to reuse existing or exported settings.
+        - .ini - Useful to reuse legacy settings.
+        - .py - Not Recommended but supported for backwards compatibility.
+        - .env - Useful to automate the loading of environment variables.
+
+    It also allows you to specify separate `env` files for reading project environment variables.
+
+    Methods:
+    data_export(): Returns collected data in dictionary format.
+    create_service_urls(): Finds services in the configuration and creates URLs for them.
+
+    """
+    def __init__(
+            self,
+            *paths: str | Path,
+            env: str | Path = '.env',
+            dev: bool = False,
+            logger_registrations: Optional[list[LoggerReg]] = None,
+    ) -> None:
+        """
+        :param paths: str | Path - Configuration path
+        :param env: str| Path - Path to the project environment variable file
+        :param dev: Local priority for configuration determination,
+        :param logger_registrations: Logger configuration, default: LoggerReg(name="", level=LoggerReg.Level.DEBUG)
+        """
+        if logger_registrations is None:
+            logger_registrations = [LoggerReg(name="", level=LoggerReg.Level.DEBUG)]
+
+        SetupLogger(name_registration=logger_registrations, default_development=dev)
+
+        self.dev = dev
+        PathError.checking_paths(*paths)
+        settings_files = [*paths]
+
+        try:
+            PathError.checking_paths(env)
+            settings_files.append(env)
+        except PathError as _:
+            logger.warning('The `.env` file was not found and will not be loaded!', env_path=env)
+
+        __data: LazySettings = Dynaconf(
+            load_dotenv=True,
+            settings_files=settings_files
+        )
+
+        self.data = self.data_export(data=__data)
+
+    def data_export(self, data: LazySettings) -> dict:
+        """
+        Returns collected data in dictionary format
+
+        :param data: LazySettings - LazySettings object
+        :return: dict - collected data in dictionary format depending on development conditions
+        """
+        if os.getenv('DEV', False) or self.dev:
+            __data: DynaBox = data.get('development')
+        else:
+            __data: DynaBox = data.get('release')
+
+        if __data is None:
+            raise ModuleException('The configuration is empty! Please check your configuration data.')
+
+        return __data.to_dict()
+
+    def create_service_urls(self) -> None:
+        """
+        Finds services in the configuration and creates URLs for them.
+
+        :return: None
+        """
+        self.data.update({
+            f"{service_name.value}_url": service_name.create_url(data=self.data.get(service_name.value))
+            for service_name in Service
+            if service_name.value in self.data
+        })
