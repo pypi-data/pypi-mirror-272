019eaf4c080beeb2623338851cb6c8fc46957ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-09 06:04:25 +0000

    Add package mesa2-preprocessing/0.1.0/mesa2_preprocessing-0.1.0.tar.gz

---
+++ packages/mesa2-preprocessing/mesa2_preprocessing-0.1.0.tar.gz/mesa2_preprocessing-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mesa2_preprocessing',
+    version='0.1.0',
+    author='Nadir Nadirov',
+    author_email='nadirnadirov1999@gmail.com',
+    description='A tool for post-processing MESA2.0 measurement data',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'pandas>=1.0',
+        'numpy>=1.18',
+        'nptdms>=0.25'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  # Ensure this matches your chosen license
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Scientific/Engineering :: Visualization'
+    ],
+    python_requires='>=3.8',
+)
