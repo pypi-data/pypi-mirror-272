37e4f2cb51d73e1dd7c7c5d7caed78b7efc9773e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 14:05:15 +0000

    Add package ba-tsconcat/0.1.4/ba_tsconcat-0.1.4.tar.gz

---
+++ packages/ba-tsconcat/ba_tsconcat-0.1.4.tar.gz/ba_tsconcat-0.1.4/src/tsconcat/__init__.py
@@ -0,0 +1,10 @@
+"""Concatenate MRI time series data.
+
+Usage:
+    tsconcat bids_dir output_dir group [options]
+
+Arguments:
+    bids_dir (str): Input BIDS folder path.
+    output_dir (str): Output BIDS folder path.
+    analysis_level (str): Processing stage, must be "group".
+"""
