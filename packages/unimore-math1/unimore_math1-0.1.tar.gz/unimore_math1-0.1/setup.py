1ca1c543b1effb77ef29b9c87ff47c86835c3e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 15:05:00 +0000

    Add package unimore-math1/0.1/unimore_math1-0.1.tar.gz

---
+++ packages/unimore-math1/unimore_math1-0.1.tar.gz/unimore_math1-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup 
+  
+setup( 
+    name='unimore_math1', 
+    version='0.1', 
+    description='A sample Python package to test CI/CD', 
+    long_description='A sample Python package to test CI/CD',
+    author='Angelo Ferrando', 
+    author_email='angelo.ferrando42@gmail.com', 
+    packages=['unimore_math1'], 
+    install_requires=[], 
+) 
