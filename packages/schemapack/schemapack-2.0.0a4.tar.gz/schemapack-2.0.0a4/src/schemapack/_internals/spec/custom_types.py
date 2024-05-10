fee4725e4e5a9f3dcef2389cad210c8093ac57ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 16:04:47 +0000

    Add package schemapack/2.0.0a4/schemapack-2.0.0a4.tar.gz

---
+++ packages/schemapack/schemapack-2.0.0a4.tar.gz/schemapack-2.0.0a4/src/schemapack/_internals/spec/custom_types.py
@@ -0,0 +1,29 @@
+# Copyright 2021 - 2024 Universität Tübingen, DKFZ, EMBL, and Universität zu Köln
+# for the German Human Genome-Phenome Archive (GHGA)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""Custom types annotations used for type hinting."""
+
+from typing import Annotated
+
+from pydantic import Field as _Field
+from typing_extensions import TypeAlias
+
+_NonEmptyStr: TypeAlias = Annotated[str, _Field(..., min_length=1)]
+ClassName: TypeAlias = _NonEmptyStr
+ResourceId: TypeAlias = _NonEmptyStr
+RelationPropertyName: TypeAlias = _NonEmptyStr
+ContentPropertyName: TypeAlias = _NonEmptyStr
+IdPropertyName: TypeAlias = _NonEmptyStr
