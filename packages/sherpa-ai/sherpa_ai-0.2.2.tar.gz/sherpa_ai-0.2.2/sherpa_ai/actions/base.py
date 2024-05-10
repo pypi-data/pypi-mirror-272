cc9ed0f7c1e77a1976bbc40b48e268d91bbe57ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 05:05:56 +0000

    Add package sherpa-ai/0.2.2/sherpa_ai-0.2.2.tar.gz

---
+++ packages/sherpa-ai/sherpa_ai-0.2.2.tar.gz/sherpa_ai-0.2.2/sherpa_ai/actions/base.py
@@ -0,0 +1,55 @@
+import json
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from functools import cached_property
+
+
+@dataclass
+class ActionResource:
+    """
+    Resource used for an action.
+
+    Attributes:
+        source (str): Source of the resource, such as document id or url.
+        content (str): Content of the resource.
+    """
+
+    source: str
+    content: str
+
+
+class BaseAction(ABC):
+    @abstractmethod
+    def execute(self, **kwargs):
+        pass
+
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def args(self) -> dict:
+        pass
+
+    @cached_property
+    def resources(self) -> list:
+        return []
+
+    def __str__(self):
+        tool_desc = {
+            "name": self.name,
+            "args": self.args,
+        }
+
+        return json.dumps(tool_desc, indent=4)
+
+    def add_resources(self, resources: list[dict]):
+        action_resources = self.resources
+        action_resources.clear()
+
+        for resource in resources:
+            action_resources.append(
+                ActionResource(source=resource["Source"], content=resource["Document"])
+            )
---
+++ packages/sherpa-ai/sherpa_ai-0.2.2.tar.gz/sherpa_ai-0.2.2/sherpa_ai/agents/base.py
@@ -0,0 +1,160 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, List
+
+from loguru import logger
+
+from sherpa_ai.action_planner import ActionPlanner
+from sherpa_ai.actions.base import BaseAction
+from sherpa_ai.events import EventType
+from sherpa_ai.output_parsers.base import BaseOutputProcessor
+from sherpa_ai.verbose_loggers.base import BaseVerboseLogger
+from sherpa_ai.verbose_loggers.verbose_loggers import DummyVerboseLogger
+
+
+# Avoid circular import
+if TYPE_CHECKING:
+    from sherpa_ai.memory import Belief, SharedMemory
+
+
+class BaseAgent(ABC):
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        shared_memory: SharedMemory = None,
+        belief: Belief = None,
+        action_planner: ActionPlanner = None,
+        num_runs: int = 1,
+        verbose_logger: BaseVerboseLogger = DummyVerboseLogger(),
+        actions: List[BaseAction] = [],
+        validation_steps: int = 1,
+        validations: List[BaseOutputProcessor] = [],
+        feedback_agent_name: str = "critic",
+    ):
+        self.name = name
+        self.description = description
+        self.shared_memory = shared_memory
+        self.belief = belief
+        self.action_planner = action_planner
+        self.num_runs = num_runs
+
+        self.subscribed_events = []
+
+        self.verbose_logger = verbose_logger
+        self.actions = actions
+        self.validation_steps = validation_steps
+        self.validations = validations
+        self.feedback_agent_name = feedback_agent_name
+
+    @abstractmethod
+    def create_actions(self) -> List[BaseAction]:
+        pass
+
+    @abstractmethod
+    def synthesize_output(self) -> str:
+        pass
+
+    def run(self):
+        self.verbose_logger.log(f"⏳{self.name} is thinking...")
+        logger.debug(f"```⏳{self.name} is thinking...```")
+
+        self.shared_memory.observe(self.belief)
+
+        actions = self.actions if len(self.actions) > 0 else self.create_actions()
+        self.belief.set_actions(actions)
+
+        for _ in range(self.num_runs):
+            result = self.action_planner.select_action(self.belief)
+            logger.info(f"Action selected: {result}")
+
+            if result is None:
+                # this means the action selector choose to finish
+                break
+
+            action_name, inputs = result
+            action = self.belief.get_action(action_name)
+
+            self.verbose_logger.log(
+                f"```🤖{self.name} is executing {action_name}\n Input: {inputs}...```"
+            )
+            logger.debug(f"🤖{self.name} is executing {action_name}...```")
+
+            self.belief.update_internal(
+                EventType.action, self.name, action_name + str(inputs)
+            )
+
+            if action is None:
+                logger.error(f"Action {action_name} not found")
+                continue
+
+            action_output = self.act(action, inputs)
+
+            self.verbose_logger.log(f"```Action output: {action_output}```")
+            logger.debug(f"```Action output: {action_output}```")
+
+            self.belief.update_internal(
+                EventType.action_output, self.name, action_output
+            )
+
+        result = self.validate_output()
+
+        logger.debug(f"```🤖{self.name} wrote: {result}```")
+
+        self.shared_memory.add(EventType.result, self.name, result)
+        return result
+
+    def validate_output(self):
+        """
+        Validate the synthesized output through a series of validation steps.
+
+        This method iterates through each validation in the 'validations' list, and for each validation,
+        it performs 'validation_steps' attempts to synthesize output using 'synthesize_output' method.
+        If the output doesn't pass validation, feedback is incorporated into the belief system.
+
+        If a validation fails after all attempts, the error messages from the last failed validation
+        are appended to the final result.
+
+        Returns:
+            str: The synthesized output after validation.
+        """
+        failed_validation = []
+        result = self.synthesize_output()
+        for validation in self.validations:
+            for count in range(self.validation_steps):
+                self.belief.update_internal(EventType.result, self.name, result)
+
+                validation_result = validation.process_output(
+                    text=result, belief=self.belief, iteration_count=count
+                )
+
+                if validation_result.is_valid:
+                    result = validation_result.result
+                    break
+                else:
+                    self.belief.update_internal(
+                        EventType.feedback,
+                        self.feedback_agent_name,
+                        validation_result.feedback,
+                    )
+                    result = self.synthesize_output()
+
+            if count >= self.validation_steps:
+                failed_validation.append(validation)
+
+        if len(failed_validation) > 0:
+            # if the validation failed after all steps, append the error messages to the result
+            result += "\n".join(
+                failed_validation.get_failure_message()
+                for failed_validation in failed_validation
+            )
+
+        self.belief.update_internal(EventType.result, self.name, result)
+        return result
+
+    def observe(self):
+        return self.shared_memory.observe(self.belief)
+
+    def act(self, action, inputs):
+        return action.execute(**inputs)
