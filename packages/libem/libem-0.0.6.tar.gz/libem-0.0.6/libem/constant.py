f2b7331a2f1e0e13a7f8b1e81cb95753e3cc0e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 14:05:19 +0000

    Add package libem/0.0.6/libem-0.0.6.tar.gz

---
+++ packages/libem/libem-0.0.6.tar.gz/libem-0.0.6/libem/constant.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import yaml
+import logging
+
+LIBEM_DIR = os.path.join(os.path.expanduser("~"), ".libem")
+LIBEM_CONFIG_FILE = os.path.join(LIBEM_DIR, "config.yaml")
+LIBEM_SOURCE = os.path.dirname(os.path.abspath(__file__))
+
+try:
+    with open(LIBEM_CONFIG_FILE, "r") as f:
+        LIBEM_CONFIG = yaml.load(f, Loader=yaml.SafeLoader)
+except FileNotFoundError:
+    print(f"Config file not found at {LIBEM_CONFIG_FILE}")
+    sys.exit(1)
+
+LIBEM_DO_LOG = False
+LIBEM_LOG_DIR = os.path.join(
+    os.path.dirname(__file__),
+    "..", "logs")
+# DEBUG: 10, INFO: 20, WARNING: 30, ERROR: 40
+LIBEM_LOG_LEVEL = \
+    os.environ.get("LIBEM_LOG_LEVEL", logging.INFO)
+LIBEM_3RD_PARTY_LOG_LEVEL = \
+    os.environ.get("LIBEM_3RD_PARTY_LOG_LEVEL", logging.WARNING)
