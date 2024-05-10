20a81dd5d9b5c7d66a4f06de5ea1f391bc5573d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 16:04:46 +0000

    Add package NatanStanek/0.14.1/NatanStanek-0.14.1-py3-none-any.whl

---
+++ packages/NatanStanek/NatanStanek-0.14.1-py3-none-any.whl/NatanStanek/__init__.py
@@ -0,0 +1,30 @@
+# NatanStanek/__init__.py
+import time
+import random
+def greet():
+    print("Hello from NatanStanek!")
+
+def DoesNatanSmell():
+    print("Natan stinks")
+
+def McDonaldsBreak(sec):
+    time.sleep(sec)
+    print("Break over, back to work!")
+
+def DriveCar():
+    num = random.randint(0, 10)
+    if (num > 8):
+        print("For once Natan is driving safely")
+    else:
+        print("Natan is driving like a maniac")
+    time.sleep(2)
+    print("Natan has fallen asleep at the wheel")
+
+startTime = 0
+def StartOfTest():
+    global startTime
+    startTime = time.time()
+
+def EndOfTest():
+    print("Test took: ", time.time() - startTime, " seconds")
+
