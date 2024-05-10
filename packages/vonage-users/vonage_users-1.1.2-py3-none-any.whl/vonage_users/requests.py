5c8dddcb84154ad5c6cb5eace9b53a828fa52ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 04:04:14 +0000

    Add package vonage-users/1.1.2/vonage_users-1.1.2-py3-none-any.whl

---
+++ packages/vonage-users/vonage_users-1.1.2-py3-none-any.whl/vonage_users/requests.py
@@ -0,0 +1,15 @@
+from typing import Literal, Optional
+
+from pydantic import BaseModel, Field
+
+
+class ListUsersFilter(BaseModel):
+    """Request object for listing users."""
+
+    page_size: Optional[int] = Field(100, ge=1, le=100)
+    order: Optional[Literal['asc', 'desc', 'ASC', 'DESC']] = None
+    cursor: Optional[str] = Field(
+        None,
+        description="The cursor to start returning results from. You are not expected to provide this manually, but to follow the url provided in _links.next.href or _links.prev.href in the response which contains a cursor value.",
+    )
+    name: Optional[str] = None
