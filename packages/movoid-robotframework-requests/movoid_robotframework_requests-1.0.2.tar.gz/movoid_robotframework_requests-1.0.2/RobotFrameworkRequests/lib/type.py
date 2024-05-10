c159af176eded65942267ca7a8ca522a8474dddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 03:03:44 +0000

    Add package movoid-robotframework-requests/1.0.2/movoid_robotframework_requests-1.0.2.tar.gz

---
+++ packages/movoid-robotframework-requests/movoid_robotframework_requests-1.0.2.tar.gz/movoid_robotframework_requests-1.0.2/RobotFrameworkRequests/lib/type.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : type
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/1/30 23:00
+# Description   : 
+"""
+from movoid_function import reset_function_default_value
+from movoid_function.type import check_parameters_type
+
+
+@reset_function_default_value(check_parameters_type)
+def robot_check_parameters_type(convert=True):
+    pass
