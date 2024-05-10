2e660fd201a86ff55cda6e7bc96bd1b138506dc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 03:03:53 +0000

    Add package elementary-data/0.15.1/elementary_data-0.15.1-py3-none-any.whl

---
+++ packages/elementary-data/elementary_data-0.15.1-py3-none-any.whl/elementary/utils/package.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+import pkg_resources
+import requests
+
+_PYPI_URL = "https://pypi.org/pypi/elementary-data/json"
+
+
+def get_package_version() -> str:
+    return pkg_resources.get_distribution("elementary-data").version
+
+
+def get_latest_package_version() -> Optional[str]:
+    try:
+        resp = requests.get(_PYPI_URL, timeout=5)
+        resp.raise_for_status()
+        return resp.json().get("info", {}).get("version")
+    except Exception:
+        return None
