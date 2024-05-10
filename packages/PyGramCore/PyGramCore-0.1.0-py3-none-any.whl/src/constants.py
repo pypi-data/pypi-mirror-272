9e856224d8955d07f1f09bb94c22e3c602e3ea97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 03:03:45 +0000

    Add package PyGramCore/0.1.0/PyGramCore-0.1.0-py3-none-any.whl

---
+++ packages/PyGramCore/PyGramCore-0.1.0-py3-none-any.whl/src/constants.py
@@ -0,0 +1,15 @@
+MEDIA_FORMATS = {
+    "jpeg",
+    "png",
+    "heic",
+    "heif",
+    "mp4",
+    "quicktime",
+}
+
+# Driver values
+IMPLICIT_WAIT = 10  # in sec.
+
+# URLs
+INSTAGRAM_URL = "https://www.instagram.com"
+INSTAGRAM_LOGIN_URL = f"{INSTAGRAM_URL}/accounts/login/"
