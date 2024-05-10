9b4ad85ddd8ceaf386f5799d8d87fb3c5f7d307e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 03:04:03 +0000

    Add package whaox-wapi/1.0.30/whaox-wapi-1.0.30.tar.gz

---
+++ packages/whaox-wapi/whaox-wapi-1.0.30.tar.gz/whaox-wapi-1.0.30/wapi/features/POST.py
@@ -0,0 +1,32 @@
+from functools import wraps
+
+from jsons import load, dump
+from requests import Response, post
+
+from wapi.static.T import T
+from wapi.utils.get_path import get_path
+
+
+def POST(
+        path: str = "",
+        _T: T = None,
+) -> T | Response:
+    def decorator(func):
+        @wraps(func)
+        def wrapper(self, **kwargs):
+            headers = kwargs.pop('headers', None)
+            data = dict()
+
+            if body := dump(kwargs.pop('body', None)):
+                data.update(body)
+
+            url = get_path(self, func, path).format(**kwargs)
+            response = post(url, json=data, headers=headers)
+
+            if _T is None:
+                return response
+            return load(response.json(), _T)
+
+        return wrapper
+
+    return decorator
