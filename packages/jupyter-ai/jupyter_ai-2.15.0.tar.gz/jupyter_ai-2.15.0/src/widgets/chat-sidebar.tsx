f5a88a0e3676d4f1c0530f70c3f20c60a558edd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 06:03:58 +0000

    Add package jupyter-ai/2.15.0/jupyter_ai-2.15.0.tar.gz

---
+++ packages/jupyter-ai/jupyter_ai-2.15.0.tar.gz/jupyter_ai-2.15.0/src/widgets/chat-sidebar.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { ReactWidget } from '@jupyterlab/apputils';
+import type { IThemeManager } from '@jupyterlab/apputils';
+import type { Awareness } from 'y-protocols/awareness';
+
+import { Chat } from '../components/chat';
+import { chatIcon } from '../icons';
+import { SelectionWatcher } from '../selection-watcher';
+import { ChatHandler } from '../chat_handler';
+import { IJaiCompletionProvider } from '../tokens';
+import { IRenderMimeRegistry } from '@jupyterlab/rendermime';
+
+export function buildChatSidebar(
+  selectionWatcher: SelectionWatcher,
+  chatHandler: ChatHandler,
+  globalAwareness: Awareness | null,
+  themeManager: IThemeManager | null,
+  rmRegistry: IRenderMimeRegistry,
+  completionProvider: IJaiCompletionProvider | null,
+  openInlineCompleterSettings: () => void
+): ReactWidget {
+  const ChatWidget = ReactWidget.create(
+    <Chat
+      selectionWatcher={selectionWatcher}
+      chatHandler={chatHandler}
+      globalAwareness={globalAwareness}
+      themeManager={themeManager}
+      rmRegistry={rmRegistry}
+      completionProvider={completionProvider}
+      openInlineCompleterSettings={openInlineCompleterSettings}
+    />
+  );
+  ChatWidget.id = 'jupyter-ai::chat';
+  ChatWidget.title.icon = chatIcon;
+  ChatWidget.title.caption = 'Jupyter AI Chat'; // TODO: i18n
+  return ChatWidget;
+}
