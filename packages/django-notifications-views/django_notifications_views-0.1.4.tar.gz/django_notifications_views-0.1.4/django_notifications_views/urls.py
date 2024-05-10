f56bc86eb5df8543e969aa0a54f23f31b5013f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 04:04:29 +0000

    Add package django-notifications-views/0.1.4/django_notifications_views-0.1.4.tar.gz

---
+++ packages/django-notifications-views/django_notifications_views-0.1.4.tar.gz/django_notifications_views-0.1.4/django_notifications_views/urls.py
@@ -0,0 +1,17 @@
+from rest_framework.routers import DefaultRouter
+from django_notifications_views.views import NotificationsViewSet, ManageDeviceExpo
+from django_notifications_views.app_settings import api_settings as settings
+
+
+router = DefaultRouter()
+router.register(r"user-notifications", NotificationsViewSet, basename="notifications")
+
+if settings.get_user_setting("USE_EXPO_NOTIFICATIONS"):
+    router.register(r"expo-devices", ManageDeviceExpo, basename="expo")
+
+
+# fmt: off
+urlpatterns = [
+
+]
+# fmt: on
