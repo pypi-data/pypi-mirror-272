6defbcf09ce2efbba038b61765b33039a5e0f07f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 16:04:53 +0000

    Add package NatanStanek/0.13/natanstanek-0.13.tar.gz

---
+++ packages/NatanStanek/natanstanek-0.13.tar.gz/natanstanek-0.13/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='NatanStanek',
+    version='0.13',
+    description='A simple demonstration package for NatanStanek',
+    url='https://github.com/JacoBSmitH101/NatanStanek',
+    author='James Mortimer',
+    author_email='jamesmortimer@bournside.smells.uk',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False
+)
