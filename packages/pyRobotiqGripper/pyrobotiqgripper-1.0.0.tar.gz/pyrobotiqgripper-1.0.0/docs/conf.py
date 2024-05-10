7d9103158a6f464cae709c4daee96f40b37f07b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 15:04:26 +0000

    Add package pyRobotiqGripper/1.0.0/pyrobotiqgripper-1.0.0.tar.gz

---
+++ packages/pyRobotiqGripper/pyrobotiqgripper-1.0.0.tar.gz/pyrobotiqgripper-1.0.0/docs/conf.py
@@ -0,0 +1,53 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+# Get the project root dir, which is the parent dir of this
+cwd = os.getcwd()
+project_root = os.path.dirname(cwd)
+
+
+
+# Insert the project root dir as the first element in the PYTHONPATH.
+# This lets us ensure that the source package is imported, and that its
+# version is used.
+sys.path.insert(0, project_root)
+
+project = 'pyRobotiqGripper'
+copyright = '2024, Benoit CASTETS'
+author = 'Benoit CASTETS'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.duration"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+"""
+Tells the project to use sphinx pygments for color coding code examples.
+"""
+
+pygments_style = 'sphinx'
