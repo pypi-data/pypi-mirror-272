3e0ff7939b5e1de211dca4f0d206da6a4092a316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 15:04:17 +0000

    Add package create-flask-package/0.1.1/create_flask_package-0.1.1.tar.gz

---
+++ packages/create-flask-package/create_flask_package-0.1.1.tar.gz/create_flask_package-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='create_flask_package',
+    version='0.1.1',
+    author='Nasir iqbal',
+    packages=find_packages(),
+    install_requires=[
+        'Flask',
+        'Flask-SQLAlchemy',
+        'Flask-CORS',
+        'Flask-Login',
+        'Flask-Session'
+    ],
+    entry_points={
+        'console_scripts': [
+            'create_flask_package = your_package_name.create_flask_package:create_package_entry_point'
+        ]
+    },
+)
