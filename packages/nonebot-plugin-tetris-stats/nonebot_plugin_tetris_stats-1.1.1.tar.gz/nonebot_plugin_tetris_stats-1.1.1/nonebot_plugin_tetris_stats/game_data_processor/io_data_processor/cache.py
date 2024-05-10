225ec31471c67101e47687f562a863c8253de1c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 14:05:14 +0000

    Add package nonebot-plugin-tetris-stats/1.1.1/nonebot_plugin_tetris_stats-1.1.1.tar.gz

---
+++ packages/nonebot-plugin-tetris-stats/nonebot_plugin_tetris_stats-1.1.1.tar.gz/nonebot_plugin_tetris_stats-1.1.1/nonebot_plugin_tetris_stats/game_data_processor/io_data_processor/cache.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timezone
+
+from aiocache import Cache as ACache  # type: ignore[import-untyped]
+from nonebot.compat import type_validate_json
+from nonebot.log import logger
+
+from ...utils.request import Request
+from .schemas.base import FailedModel, SuccessModel
+
+UTC = timezone.utc
+
+
+class Cache:
+    cache = ACache(ACache.MEMORY)
+
+    @classmethod
+    async def get(cls, url: str) -> bytes:
+        cached_data = await cls.cache.get(url)
+        if cached_data is None:
+            response_data = await Request.request(url)
+            parsed_data: SuccessModel | FailedModel = type_validate_json(SuccessModel | FailedModel, response_data)  # type: ignore[arg-type]
+            if isinstance(parsed_data, SuccessModel):
+                await cls.cache.add(
+                    url,
+                    response_data,
+                    (parsed_data.cache.cached_until - datetime.now(UTC)).total_seconds(),
+                )
+            return response_data
+        logger.debug(f'{url}: Cache hit!')
+        return cached_data
