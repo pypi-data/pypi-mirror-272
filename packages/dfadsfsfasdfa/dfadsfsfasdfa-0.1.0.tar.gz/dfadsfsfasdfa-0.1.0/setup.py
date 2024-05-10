f4dced6ebbe3d7ba815733bed116257eec150724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 04:04:27 +0000

    Add package dfadsfsfasdfa/0.1.0/dfadsfsfasdfa-0.1.0.tar.gz

---
+++ packages/dfadsfsfasdfa/dfadsfsfasdfa-0.1.0.tar.gz/dfadsfsfasdfa-0.1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dfadsfsfasdfa',
+    version='0.1.0',
+    description='A brief description of your package',
+    author='hrewew',
+)
