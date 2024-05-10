bbcb54bfdaa3f3f1011dd566e1711f7b9a813fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 15:04:28 +0000

    Add package esercitazione-cicd/0.1/esercitazione_cicd-0.1-py3-none-any.whl

---
+++ packages/esercitazione-cicd/esercitazione_cicd-0.1-py3-none-any.whl/math_lab/calc.py
@@ -0,0 +1,25 @@
+class MathOperations():
+    
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+
+    def add(self):
+        return self.num1 + self.num2
+    
+    def sub(self):
+        return self.num1 - self.num2
+    
+    def mul(self):
+        return self.num1 * self.num2
+    
+    def div(self):
+        if self.num2 == 0:
+            raise Exception("Cannot divide by 0!")
+        else:
+            return self.num1 / self.num2
+    
+
+    
+
