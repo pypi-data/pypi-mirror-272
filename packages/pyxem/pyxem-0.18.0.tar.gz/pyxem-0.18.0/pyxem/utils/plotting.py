4c57f4e3e5d52e3deee1856842d0bda42ac517ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 03:04:25 +0000

    Add package pyxem/0.18.0/pyxem-0.18.0.tar.gz

---
+++ packages/pyxem/pyxem-0.18.0.tar.gz/pyxem-0.18.0/pyxem/utils/plotting.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2024 The pyXem developers
+#
+# This file is part of pyXem.
+#
+# pyXem is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyXem is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyXem.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Utils for plotting 2D Diffraction Patterns."""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from pyxem.utils.polar_transform_utils import (
+    get_template_cartesian_coordinates,
+    get_template_polar_coordinates,
+    image_to_polar,
+)
+from pyxem.utils.diffraction import find_beam_center_blur
+
+__all__ = [
+    "plot_template_over_pattern",
+]
+
+
+def plot_template_over_pattern(
+    pattern,
+    simulation,
+    ax=None,
+    in_plane_angle=0.0,
+    max_r=None,
+    find_direct_beam=True,
+    direct_beam_position=None,
+    mirrored_template=False,
+    coordinate_system="cartesian",
+    marker_color="red",
+    marker_type="x",
+    size_factor=1.0,
+    **kwargs
+):
+    """A quick utility function to plot a simulated pattern over an experimental image
+
+    Parameters
+    ----------
+    pattern : 2D np.ndarray
+        The diffraction pattern
+    simulation : :class:`diffsims.sims.diffraction_simulation.DiffractionSimulation`
+        The simulated diffraction pattern. It must be calibrated.
+    axis : matplotlib.AxesSubplot, optional
+        An axis object on which to plot. If None is provided, one will be created.
+    in_plane_angle : float, optional
+        An in-plane rotation angle to apply to the template in degrees
+    max_r : float, optional
+        Maximum radius to consider in the polar transform in pixel coordinates.
+        Will only influence the result if ``coordinate_system="polar"``.
+    find_direct_beam: bool, optional
+        Roughly find the optimal direct beam position if it is not centered.
+    direct_beam_position: 2-tuple
+        The (x, y) position of the direct beam in pixel coordinates. Takes
+        precedence over `find_direct_beam`
+    mirrored_template: bool, optional
+        Whether to mirror the given template
+    coordinate_system : str, optional
+        Type of coordinate system to plot the image and template in. Either
+        ``cartesian`` or ``polar``
+    marker_color : str, optional
+        Color of the spot markers
+    marker_type : str, optional
+        Type of marker used for the spots
+    size_factor : float, optional
+        Scaling factor for the spots. See notes on size.
+    **kwargs :
+        See :func:`matplotlib.pyplot.imshow`
+
+    Returns
+    -------
+    ax : matplotlib.AxesSubplot
+        The axes object
+    im : matplotlib.image.AxesImage
+        The representation of the image on the axes
+    sp : matplotlib.collections.PathCollection
+        The scatter plot representing the diffraction pattern
+
+    Notes
+    -----
+    The spot marker sizes are scaled by the square root of their intensity
+    """
+    if ax is None:
+        _, ax = plt.subplots()
+    if coordinate_system == "polar":
+        pattern = image_to_polar(
+            pattern,
+            max_r=max_r,
+            find_direct_beam=find_direct_beam,
+            direct_beam_position=direct_beam_position,
+        )
+        x, y, intensities = get_template_polar_coordinates(
+            simulation,
+            in_plane_angle=in_plane_angle,
+            max_r=max_r,
+            mirrored=mirrored_template,
+        )
+    elif coordinate_system == "cartesian":
+        if direct_beam_position is not None:
+            c_x, c_y = direct_beam_position
+        elif find_direct_beam:
+            c_x, c_y = find_beam_center_blur(pattern, 1)
+        else:
+            c_y, c_x = pattern.shape[0] / 2, pattern.shape[1] / 2
+        x, y, intensities = get_template_cartesian_coordinates(
+            simulation,
+            center=(c_x, c_y),
+            in_plane_angle=in_plane_angle,
+            window_size=(pattern.shape[1], pattern.shape[0]),
+            mirrored=mirrored_template,
+        )
+        y = pattern.shape[0] - y
+    else:
+        raise NotImplementedError(
+            "Only polar and cartesian are accepted coordinate systems"
+        )
+    im = ax.imshow(pattern, **kwargs)
+    sp = ax.scatter(
+        x,
+        y,
+        s=size_factor * np.sqrt(intensities),
+        marker=marker_type,
+        color=marker_color,
+    )
+    return (ax, im, sp)
