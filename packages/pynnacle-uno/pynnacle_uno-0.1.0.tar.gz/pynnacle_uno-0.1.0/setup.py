272cffeb5ea229dce35136dd0157f57a357a4aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 15:04:28 +0000

    Add package pynnacle-uno/0.1.0/pynnacle_uno-0.1.0.tar.gz

---
+++ packages/pynnacle-uno/pynnacle_uno-0.1.0.tar.gz/pynnacle_uno-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pynnacle_uno',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A Python module that provides hands-on robotics coding experience in Python that closely mirrors the structure and functionality of Arduino\'s programming language.',
+    url='https://github.com/Red-Pula/Pynnacle-Uno',
+    author='Red Hizon, Jenel Justo, Serena Lee',
+    author_email='redhizon@gmail.com, jenel.just88@gmail.com, nmae.lee@gmail.com',
+    license='GNU Affero General Public License',
+    install_requires=['pymata4>=1.15'],
+
+    classifiers=[
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+    ],
+)
