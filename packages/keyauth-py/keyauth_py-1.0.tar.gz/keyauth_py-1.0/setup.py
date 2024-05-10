c63332c103f2ec892a2d40c2ddda2bf91fab1c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 04:03:48 +0000

    Add package keyauth-py/1.0/keyauth_py-1.0.tar.gz

---
+++ packages/keyauth-py/keyauth_py-1.0.tar.gz/keyauth_py-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='keyauth-py',
+    version='1.0',
+    packages=['keyauth-py'],
+    install_requires=['requests'],
+    url='https://github.com/Herels2/keyauth-py',
+    license='',
+    author='Sterrist',
+    author_email='griodred@gmail.com',
+    description='Key Authorization'
+)
