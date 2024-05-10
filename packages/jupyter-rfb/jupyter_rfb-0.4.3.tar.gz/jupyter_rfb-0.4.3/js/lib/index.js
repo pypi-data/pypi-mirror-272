d2c1d709dff22b9059245f29523de9e0156d53b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 15:04:59 +0000

    Add package jupyter-rfb/0.4.3/jupyter_rfb-0.4.3.tar.gz

---
+++ packages/jupyter-rfb/jupyter_rfb-0.4.3.tar.gz/jupyter_rfb-0.4.3/js/lib/index.js
@@ -0,0 +1,4 @@
+// Export widget models and views, and the npm package version number.
+
+export {RemoteFrameBufferModel, RemoteFrameBufferView} from './widget';
+export {version} from '../package.json';
