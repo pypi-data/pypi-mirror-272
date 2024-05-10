4da2db83d72ba407d0c929db21feddcdcef48e41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 03:03:52 +0000

    Add package ml-indie-tools/0.12.1/ml_indie_tools-0.12.1-py3-none-any.whl

---
+++ packages/ml-indie-tools/ml_indie_tools-0.12.1-py3-none-any.whl/ml_indie_tools/train_utils.py
@@ -0,0 +1,167 @@
+import logging
+import asyncio
+import math
+from datetime import datetime
+from datetime import timezone
+import numpy as np
+import json
+
+
+class TrainUtils:
+    def __init__(self, indraserver_profile=None):
+        self.log = logging.getLogger(__name__)
+        self.indraserver_profile = None
+        self.indra_active = False
+        if indraserver_profile is not None:
+            self.indraserver_profile = indraserver_profile
+            try:
+                from indralib.indra_event import IndraEvent  # type: ignore
+                from indralib.indra_client import IndraClient  # type: ignore
+            except ImportError:
+                self.log.error("indralib is required to use the indraserver_profile")
+                self.indraserver_profile = None
+        if self.indraserver_profile is not None:
+            self.icl = IndraClient(verbose=False, profile="default")
+        if self.icl is None:
+            logging.error("Could not create Indrajala client")
+        else:
+            asyncio.run(self.init_indra())
+
+    async def init_indra(self):
+        ws = await self.icl.init_connection(verbose=False)
+        if ws is None:
+            logging.error("Could not connect to Indrajala")
+            self.indraserver_profile = None
+        else:
+            self.indra_active = True
+            self.log.info("Connected to Indrajala server {indraserver_profile}")
+
+    @staticmethod
+    def progress_bar_string(progress, max_progress, bar_length=20):
+        """Create a Unicode progress bar string
+
+        This creates a string of length bar_length with a Unicode progress bar using
+        fractional Unicode block characters. The returned string is always of constant
+        length and is suitable for printing to a terminal or notebook.
+
+        This pretty much obsoletes the `tqdm` or similar package for simple progress bars.
+
+        :param progress: current progress
+        :param max_progress: maximum progress
+        :param bar_length: length of the progress bar
+        :return: Unicode progress bar string of length `bar_length`
+        """
+        progress_frac = progress / max_progress
+        num_blocks = int(bar_length * progress_frac)
+        rem = bar_length * progress_frac - num_blocks
+        blocks = " ▏▎▍▌▋▊▉█"
+        remainder_index = int(rem * len(blocks))
+        bar = blocks[-1] * num_blocks
+        if remainder_index > 0:
+            bar += blocks[remainder_index]
+        bar += " " * (bar_length - len(bar))
+        return bar
+
+    def train_session_start(
+        self,
+        model_name,
+        model_description,
+        model_version,
+        model_params,
+        indra_subdomain=None,
+    ):
+        if self.train_session_active is True:
+            self.log.warning(
+                "Training session already active, closing existing session"
+            )
+            self.train_session_end()
+        self.model_name = model_name
+        self.model_description = model_description
+        self.model_version = model_version
+        self.model_params = model_params
+        if indra_subdomain is None:
+            subdomain = f"{model_name}/{model_version}".replace(" ", "_")
+            self.log.warning(f"No Indrajala subdomain set, using {subdomain}")
+        self.indra_subdomain = indra_subdomain
+        self.model_loss_history = []
+        self.losses = np.array([])
+        self.train_session_active = True
+
+    def train_session_end(self):
+        if not self.train_session_active:
+            self.log.error(
+                "No active training session: use train_session_start() first"
+            )
+            return
+        self.train_session_active = False
+
+    async def indra_report(self, record):
+        if not self.indra_active:
+            self.log.error("Indrajala not active")
+            return
+        event = IndraEvent()
+        event.domain = f"$event/ml/model/train/{self.indra_subdomain}/record"
+        event.from_id = "python/ml_indie_tools"
+        event.to_scope = "public"
+        event.data_type = "json/ml/trainrecord"
+        event.data = json.dumps(record)
+        await self.icl.send_event(event)
+
+        event = IndraEvent()
+        event.domain = f"$event/ml/model/train/{self.indra_subdomain}/loss"
+        event.from_id = "python/ml_indie_tools"
+        event.to_scope = "public"
+        event.data_type = "number/float"
+        event.data = json.dumps(record["mean_loss"])
+        await self.icl.send_event(event)
+
+    def train_state(
+        self,
+        current_epoch,
+        current_batch,
+        num_batches,
+        loss,
+        val_loss=None,
+        mean_loss_window=20,
+    ):
+        if not self.train_session_active:
+            self.log.error(
+                "No active training session: use train_session_start() first"
+            )
+            return
+        # Calculate perplexity, accuracy from loss:
+        perplexity = math.exp(loss)
+        if val_loss is not None:
+            val_perplexity = math.exp(val_loss)
+        else:
+            val_perplexity = None
+        accuracy = 1 - loss
+        if val_loss is not None:
+            val_accuracy = 1 - val_loss
+        else:
+            val_accuracy = None
+        self.model_loss_history.append(record)
+        self.losses = np.append(self.losses, loss)
+        mean_loss = np.mean(self.losses[-mean_loss_window:])
+        record = {
+            "epoch": current_epoch,
+            "loss": loss,
+            "mean_loss": mean_loss,
+            "perplexity": perplexity,
+            "accuracy": accuracy,
+            "val_loss": val_loss,
+            "val_perplexity": val_perplexity,
+            "val_accuracy": val_accuracy,
+            "timestamp": datetime.now(timezone.utc).isoformat(),
+        }
+
+        if self.indra_active:
+            if self.indra_domain is None:
+                self.log.error("No Indrajala domain set")
+            else:
+                asyncio.run(self.indra_report(record))
+
+        pbar = self.progress_bar_string(current_batch, num_batches)
+
+        status_string = f"{current_batch:6d} ⦊{pbar}⦉ loss: {mean_loss:.4f}    "
+        return status_string
