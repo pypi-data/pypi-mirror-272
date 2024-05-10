4a76fd56b44a6641ddb281129e8d819d62f54289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 05:04:30 +0000

    Add package scrapetools/1.1.9/scrapetools-1.1.9-py3-none-any.whl

---
+++ packages/scrapetools/scrapetools-1.1.9-py3-none-any.whl/scrapetools/__init__.py
@@ -0,0 +1,7 @@
+from .email_scraper import scrape_emails
+from .input_scraper import scrape_inputs
+from .link_scraper import LinkScraper
+from .phone_scraper import scrape_phone_numbers
+
+__version__ = "1.1.9"
+__all__ = ["scrape_emails", "scrape_inputs", "LinkScraper", "scrape_phone_numbers"]
