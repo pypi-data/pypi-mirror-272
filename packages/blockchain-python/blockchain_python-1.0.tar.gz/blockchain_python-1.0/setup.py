40f32b77327eee00c0ac252de39595a212c6d4cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 15:04:46 +0000

    Add package blockchain-python/1.0/blockchain_python-1.0.tar.gz

---
+++ packages/blockchain-python/blockchain_python-1.0.tar.gz/blockchain_python-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='blockchain_python',
+    version='1.0',
+    description='Description of your package',
+    long_description='Long description of your package',
+    author='Your Name',
+    author_email='your@email.com',
+    packages=find_packages(),
+    install_requires=[
+        # List of dependencies
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        # Add other classifiers as needed
+    ],
+)
