ac89535af22d2e6ee92ec277ab41a637ac21b2ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 05:06:03 +0000

    Add package infrable/0.1.15/infrable-0.1.15.tar.gz

---
+++ packages/infrable/infrable-0.1.15.tar.gz/infrable-0.1.15/tests/unit/test_infra.py
@@ -0,0 +1,56 @@
+from infrable import Host, infra, init
+
+
+def setup_module():
+    init.init()
+    infra.switches["env"].set("dev")
+
+
+def test_items():
+    assert len(infra.items) == 27
+    assert infra.items["dev"] == "dev"
+
+
+def test_item_types():
+    assert len(infra.item_types) == 13
+    assert infra.item_types[infra.Host]["dev_host"].fqdn == "dev.example.com"
+    assert infra.item_types[Host]["dev_host"].fqdn == "dev.example.com"
+    assert infra.item_types[infra.Service]["nginx"].host.fqdn == "dev.example.com"
+
+
+def test_hosts():
+    assert len(infra.hosts) == 3
+    assert infra.hosts["dev_host"].fqdn == "dev.example.com"
+
+
+def test_services():
+    assert len(infra.services) == 2
+    assert infra.services["nginx"].host.fqdn == "dev.example.com"
+
+
+def test_host_groups():
+    assert len(infra.host_groups) == 1
+    assert infra.host_groups["managed_hosts"][0].fqdn == "dev.example.com"
+
+
+def test_typers():
+    assert len(infra.typers) == 3
+    assert infra.typers["nginx"].registered_commands[0].name == "reload"
+
+
+def test_metas():
+    assert len(infra.metas) == 0
+
+
+def test_by_fqdn():
+    assert infra.by_fqdn["dev.example.com"].fqdn == "dev.example.com"
+
+
+def test_by_ip():
+    assert infra.by_ip["127.0.0.1"].fqdn == "dev.example.com"
+
+
+def test_get_host():
+    assert infra.get_host("dev_host").fqdn == "dev.example.com"
+    assert infra.get_host("dev.example.com").fqdn == "dev.example.com"
+    assert infra.get_host("127.0.0.1").fqdn == "dev.example.com"
