20049493202d67403d909a56de051d9e0193e8aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 03:04:34 +0000

    Add package tools-new-ada/2024.0.0.3/tools_new_ada-2024.0.0.3.tar.gz

---
+++ packages/tools-new-ada/tools_new_ada-2024.0.0.3.tar.gz/tools_new_ada-2024.0.0.3/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+# @time     : 2024/5/9 17:47
+# @Author   : new
+# @File      : setup.py
+from setuptools import setup, find_packages
+
+GFICLEE_VERSION = '2024.0.0.3'
+
+setup(
+    name='tools_new_ada',
+    version=GFICLEE_VERSION,
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+
+        "console_scripts": ['tools_new_ada = tools_py.main:main']
+    },
+    install_requires=['portion', 'python-dateutil', 'paramiko', 'redis', 'jsonpath', 'pymysql',
+                      'apscheduler', 'requests', 'xlrd', 'pymongo', 'SQLAlchemy'],
+    url='http://git.ppdaicorp.com/pengying/tools_py.git',
+    license='MIT',
+    author='py_new',
+    author_email='pengying@ppdai.com',
+    description='More convenient to create tools project'
+)
