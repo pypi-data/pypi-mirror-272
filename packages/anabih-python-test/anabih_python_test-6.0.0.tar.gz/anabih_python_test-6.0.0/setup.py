9f8b4a31bfb9b7d5fe4b7ec51503fc638f546015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 04:04:28 +0000

    Add package anabih-python-test/6.0.0/anabih_python_test-6.0.0.tar.gz

---
+++ packages/anabih-python-test/anabih_python_test-6.0.0.tar.gz/anabih_python_test-6.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="anabih_python_test",
+    version="6.0.0",
+    author="Ahmed Nabih",
+    author_email="anabih2000@yahoo.com",
+    description="Python test",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anabih/python-test",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
