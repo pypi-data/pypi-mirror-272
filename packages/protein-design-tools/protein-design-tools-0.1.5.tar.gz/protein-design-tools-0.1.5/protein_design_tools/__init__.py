72bb31c00ae558a08a5ecb811d4d3b351d956e25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 05:06:03 +0000

    Add package protein-design-tools/0.1.5/protein-design-tools-0.1.5.tar.gz

---
+++ packages/protein-design-tools/protein-design-tools-0.1.5.tar.gz/protein-design-tools-0.1.5/protein_design_tools/__init__.py
@@ -0,0 +1,8 @@
+"""
+protein_design_tools
+A Python package for protein design tools.
+"""
+
+# Import the public parts of your package using their full package.subpackage.module notation.
+from .protein_structure import ProteinStructure
+from .protein_structure_utils import *
