a805f11b1eaa8b3edee924d7de4a73ee465efec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 16:04:43 +0000

    Add package NatanStanek/0.14.0/natanstanek-0.14.0.tar.gz

---
+++ packages/NatanStanek/natanstanek-0.14.0.tar.gz/natanstanek-0.14.0/NatanStanek/__init__.py
@@ -0,0 +1,11 @@
+# NatanStanek/__init__.py
+import time
+def greet():
+    print("Hello from NatanStanek!")
+
+def DoesNatanSmell():
+    print("Natan stinks")
+
+def McDonaldsBreak(sec):
+    time.sleep(sec)
+    print("Break over, back to work!")
---
+++ packages/NatanStanek/natanstanek-0.14.0.tar.gz/natanstanek-0.14.0/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='NatanStanek',
+    version='0.14.0',
+    description='The real Natan Stanek python package',
+    url='https://github.com/JacoBSmitH101/NatanStanek',
+    author='James Mortimer',
+    author_email='jamesmortimer@bournside.smells.uk',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False
+)
