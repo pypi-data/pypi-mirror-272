5893ed12bc453728ee60337cea12458eb084dbb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 04:04:14 +0000

    Add package unicorn-binance-websocket-api/2.6.0/unicorn_binance_websocket_api-2.6.0-pp310-pypy310_pp73-win_amd64.whl

---
+++ packages/unicorn-binance-websocket-api/unicorn_binance_websocket_api-2.6.0-pp310-pypy310_pp73-win_amd64.whl/unicorn_binance_websocket_api/sockets.pyi
@@ -0,0 +1,21 @@
+from .exceptions import *
+from .connection import BinanceWebSocketApiConnection as BinanceWebSocketApiConnection
+from _typeshed import Incomplete
+
+logger: Incomplete
+
+class BinanceWebSocketApiSocket:
+    manager: Incomplete
+    stream_id: Incomplete
+    channels: Incomplete
+    markets: Incomplete
+    symbols: Incomplete
+    output: Incomplete
+    unicorn_fy: Incomplete
+    exchange: Incomplete
+    websocket: Incomplete
+    def __init__(self, manager, stream_id, channels, markets) -> None: ...
+    async def __aenter__(self): ...
+    async def __aexit__(self, exc_type, exc_val, exc_tb) -> None: ...
+    async def start_socket(self) -> None: ...
+    def raise_exceptions(self) -> None: ...
