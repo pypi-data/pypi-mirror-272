ae5cc60d9ba91b9cb20da48d1fc80b6233ab32e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 16:04:51 +0000

    Add package mycall-parth/0.0.1/mycall_parth-0.0.1.tar.gz

---
+++ packages/mycall-parth/mycall_parth-0.0.1.tar.gz/mycall_parth-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mycall_parth",
+    version="0.0.1",
+    packages=find_packages(),
+    author="parth",
+    description="A simple calculator utility library",
+    python_requires=">=3.6",
+)
