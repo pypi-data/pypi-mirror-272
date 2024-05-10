b4bbbced1f3485b12d922daa535c6dca8abaa1c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 15:04:17 +0000

    Add package unimore-math1/0.2/unimore_math1-0.2-py3-none-any.whl

---
+++ packages/unimore-math1/unimore_math1-0.2-py3-none-any.whl/unimore_math1/my_math.py
@@ -0,0 +1,19 @@
+class MathOperations:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+    
+    def add(self):
+        return self.num1 + self.num2
+    
+    def subtract(self):
+        return self.num1 - self.num2
+    
+    def multiply(self):
+        return self.num1 * self.num2
+    
+    def divide(self):
+        if self.num2 == 0:
+            return "Cannot divide by zero"
+        else:
+            return self.num1 / self.num2
