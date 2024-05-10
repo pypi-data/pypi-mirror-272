91a8534655d7b33f218215457af933a968f073b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 15:04:55 +0000

    Add package wagtail-birdsong/2.0.0/wagtail_birdsong-2.0.0-py3-none-any.whl

---
+++ packages/wagtail-birdsong/wagtail_birdsong-2.0.0-py3-none-any.whl/birdsong/models.py
@@ -0,0 +1,97 @@
+import uuid
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
+from taggit.models import TaggedItemBase
+from wagtail.admin.panels import FieldPanel
+from wagtail.coreutils import camelcase_to_underscore
+from wagtail.models import Site
+
+
+class ContactTag(TaggedItemBase):
+    content_object = ParentalKey(
+        "birdsong.Contact", on_delete=models.CASCADE, related_name="tagged_items"
+    )
+
+
+class Contact(ClusterableModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(verbose_name=_("email"))
+    tags = ClusterTaggableManager(
+        through=ContactTag,
+        verbose_name=_("tags"),
+        blank=True,
+    )
+
+    panels = [
+        FieldPanel("email"),
+        FieldPanel("tags"),
+    ]
+
+    def __str__(self):
+        return self.email
+
+
+class CampaignStatus(models.IntegerChoices):
+    UNSENT = 0, _("unsent")
+    SENDING = 1, _("sending")
+    SENT = 2, _("sent")
+    FAILED = 3, _("failed")
+
+
+class Campaign(models.Model):
+    name = models.CharField(
+        verbose_name=_("name"),
+        max_length=255,
+        help_text=_("The name of the campaign"),
+    )
+    subject = models.TextField(verbose_name=_("subject"))
+    sent_date = models.DateTimeField(
+        verbose_name=_("sent date"),
+        blank=True,
+        null=True,
+    )
+    receipts = models.ManyToManyField(
+        Contact,
+        verbose_name=_("receipts"),
+        through="Receipt",
+    )
+    status = models.IntegerField(
+        verbose_name=_("status"),
+        choices=CampaignStatus.choices,
+        default=CampaignStatus.UNSENT,
+    )
+
+    panels = [
+        FieldPanel("name"),
+        FieldPanel("subject"),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    def get_template(self, request):
+        return "mail/%s.html" % (camelcase_to_underscore(self.__class__.__name__))
+
+    def get_text_template(self, request):
+        return "mail/%s.txt" % (camelcase_to_underscore(self.__class__.__name__))
+
+    def get_context(self, request, contact):
+        site = Site.find_for_request(request)
+        return {
+            "self": self,
+            "request": request,
+            "contact": contact,
+            "site": site,
+        }
+
+
+class Receipt(models.Model):
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    sent_date = models.DateTimeField(auto_now=True)
+    # Probably not necessary, but might come in useful later
+    success = models.BooleanField(default=True)
---
+++ packages/wagtail-birdsong/wagtail_birdsong-2.0.0-py3-none-any.whl/birdsong/views/editor.py
@@ -0,0 +1,92 @@
+from django.http.response import JsonResponse
+from django.shortcuts import render
+from django.template.loader import render_to_string
+from wagtail_modeladmin.views import CreateView, EditView, InspectView
+
+from birdsong.conf import BIRDSONG_TEST_CONTACT
+
+
+def preview(request, campaign, test_contact):
+    return render(
+        request,
+        campaign.get_template(request),
+        campaign.get_context(request, test_contact)
+    )
+
+
+def confirm_send(request, campaign, form, send_url, index_url):
+    context = {
+        'self': campaign,
+        'form': form,
+        'request': request,
+        'send_url': send_url,
+        'index_url': index_url
+    }
+
+    return render(request, "birdsong/editor/send_confirm.html", context)
+
+
+def confirm_test(request, campaign, form, send_url, index_url):
+    context = {
+        'self': campaign,
+        'form': form,
+        'request': request,
+        'send_url': send_url,
+        'index_url': index_url
+    }
+    return render(request, "birdsong/editor/test_confirm.html", context)
+
+
+class InspectCampaign(InspectView):
+    def __init__(self, model_admin, instance_pk):
+        super().__init__(model_admin, instance_pk)
+        self.contact_class = model_admin.contact_class
+
+    def get_context_data(self, **kwargs):
+        preview_contact = self.contact_class(**BIRDSONG_TEST_CONTACT)
+        # Should this be frozen? Changes to templates will change old campaigns
+        preview = render_to_string(
+            self.instance.get_template(self.request),
+            self.instance.get_context(self.request, preview_contact),
+        )
+        context = {
+            'receipts': self.instance.receipt_set.all(),
+            'preview': preview
+        }
+        context.update(kwargs)
+        return super().get_context_data(**context)
+
+
+def ajax_preview(request, view):
+    FormClass = view.get_form_class()
+    form = FormClass(request.POST)
+    if form.is_valid():
+        campaign = form.save(commit=False)
+        test_contact = view.model_admin.contact_class(**BIRDSONG_TEST_CONTACT)
+        content = render_to_string(
+            campaign.get_template(request),
+            campaign.get_context(request, test_contact)
+        )
+        return JsonResponse({
+            'success': True,
+            'preview': content,
+        })
+    else:
+        return JsonResponse({
+            'success': False,
+            'errors': form.errors,
+        })
+
+
+class EditCampaignView(EditView):
+    def post(self, request, *args, **kwargs):
+        if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+            return ajax_preview(request, self)
+        return super().post(request, *args, **kwargs)
+
+
+class CreateCampaignView(CreateView):
+    def post(self, request, *args, **kwargs):
+        if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+            return ajax_preview(request, self)
+        return super().post(request, *args, **kwargs)
