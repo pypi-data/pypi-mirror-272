132b3e22fce7857fa8f180e6b163aaafdc4e1e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 03:03:44 +0000

    Add package infini-transformer-pytorch/0.1.0/infini_transformer_pytorch-0.1.0-py3-none-any.whl

---
+++ packages/infini-transformer-pytorch/infini_transformer_pytorch-0.1.0-py3-none-any.whl/infini_transformer_pytorch/__init__.py
@@ -0,0 +1,9 @@
+from infini_transformer_pytorch.infini_transformer import (
+    InfiniTransformer,
+    detach_memories_,
+    detach_cached_kv_
+)
+
+from infini_transformer_pytorch.wrapper import (
+    InfiniTransformerWrapper
+)
