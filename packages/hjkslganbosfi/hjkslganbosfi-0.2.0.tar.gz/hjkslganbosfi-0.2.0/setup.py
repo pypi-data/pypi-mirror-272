a92e273da247b08558c4df164a78725064e486bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 04:04:32 +0000

    Add package hjkslganbosfi/0.2.0/hjkslganbosfi-0.2.0.tar.gz

---
+++ packages/hjkslganbosfi/hjkslganbosfi-0.2.0.tar.gz/hjkslganbosfi-0.2.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hjkslganbosfi',
+    version='0.2.0',
+    description='A brief description of your package',
+    author='hrewew',
+)
