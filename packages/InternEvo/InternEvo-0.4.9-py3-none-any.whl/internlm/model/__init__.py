01ba3690a458b65901cd29efc809a90450047dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 03:04:24 +0000

    Add package InternEvo/0.4.9/InternEvo-0.4.9-py3-none-any.whl

---
+++ packages/InternEvo/InternEvo-0.4.9-py3-none-any.whl/internlm/model/__init__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from .metrics import AccPerplex
+from .modeling_internlm import build_model_with_cfg
+from .modeling_internlm2 import build_model_with_cfg as build_model_with_cfg2
+from .modeling_llama import build_model_with_cfg as build_model_with_llama_cfg
+from .modeling_llava import build_model_with_cfg as build_model_with_llava_cfg
+from .modeling_moe import build_model_with_moe_cfg
+from .modules.embedding import Embedding1D, RotaryEmbedding
+from .modules.mlp import FeedForward
+from .modules.multi_head_attention import MHA, DistributedAttention
+from .moe.moe import MoE
+from .ops.linear import RewardModelLinear, ScaleColumnParallelLinear
+from .utils import gather_forward_split_backward
+
+__all__ = [
+    "Embedding1D",
+    "FeedForward",
+    "MoE",
+    "RotaryEmbedding",
+    "RewardModelLinear",
+    "ScaleColumnParallelLinear",
+    "AccPerplex",
+    "MHA",
+    "DistributedAttention",
+    "gather_forward_split_backward",
+    "build_model_with_cfg",
+    "build_model_with_cfg2",
+    "build_model_with_moe_cfg",
+    "build_model_with_llama_cfg",
+    "build_model_with_llava_cfg",
+]
