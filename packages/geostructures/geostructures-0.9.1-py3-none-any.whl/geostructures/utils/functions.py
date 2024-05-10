6bd9e4cc9f0c2fabf85495c44690de65007345c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 04:04:19 +0000

    Add package geostructures/0.9.1/geostructures-0.9.1-py3-none-any.whl

---
+++ packages/geostructures/geostructures-0.9.1-py3-none-any.whl/geostructures/utils/functions.py
@@ -0,0 +1,20 @@
+"""Module for miscellaneous multi-use functions"""
+
+__all__ = ['round_half_up']
+
+
+def round_half_up(value: float, precision) -> float:
+    """
+    Rounds numbers to the nearest whole, where a value exactly between the two nearest
+    wholes is rounded to the higher whole.
+
+    Args:
+        value:
+            The float value to be rounded
+        precision:
+            The precision to round the float value to
+
+    """
+    mod = value + 10 ** -(precision + 12)
+
+    return round(mod, precision)
