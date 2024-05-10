ee545065ff97c403188f6c7c2204bed012f4d02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 16:04:55 +0000

    Add package airtestProject/0.1.0/airtestproject-0.1.0.tar.gz

---
+++ packages/airtestProject/airtestproject-0.1.0.tar.gz/airtestproject-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+setup(
+    name='airtestProject',
+    version='0.1.0',
+    packages=find_packages(include=['airtestProject', 'airtestProject.*']),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "airtest==1.3.3",
+        "openpyxl==1.1.0",
+        "pocoui==1.0.94",
+        "qrcode==7.4.2",
+        "Requests==2.31.0",
+        "watchdog==4.0.0",
+        "easyocr==1.7.1",
+        "paddleocr==2.7.3",
+        "loguru==0.5.3"
+        # 项目依赖项列表
+    ],
+    python_requires='>=3.9',
+    author="mortal_sjh",                                     # 作者
+    author_email="mortal_sjh@qq.com"
+)
