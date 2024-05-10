054c4330770a390134788fd39cb778d822b88be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 15:04:17 +0000

    Add package itwingstestsprojectnn/0.6/itwingstestsprojectnn-0.6-py3-none-any.whl

---
+++ packages/itwingstestsprojectnn/itwingstestsprojectnn-0.6-py3-none-any.whl/mymodule/config.py
@@ -0,0 +1,20 @@
+############################################################################################################
+# Pagespeed
+############################################################################################################
+
+ps_config = {
+    "strategies": ["desktop","mobile"],  # Strategies to use for the assessment
+    "thresholds": {	
+        "LCP": 2500,  # milliseconds
+        "INP": 200,  # milliseconds
+        "CLS": 0.1  # CLS score
+    },
+    "metrics": {
+        "LCP": "LARGEST_CONTENTFUL_PAINT_MS",
+        "INP": "INTERACTION_TO_NEXT_PAINT",
+        "CLS": "CUMULATIVE_LAYOUT_SHIFT_SCORE",
+        "FCP": "FIRST_CONTENTFUL_PAINT_MS",
+        "FID": "FIRST_INPUT_DELAY_MS",
+        "TTFB": "EXPERIMENTAL_TIME_TO_FIRST_BYTE"
+    }
+}
