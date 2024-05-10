abf4536307656bc45e819d35953547bd7a465bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 06:04:01 +0000

    Add package NVRChannel/0.1.6/nvrchannel-0.1.6.tar.gz

---
+++ packages/NVRChannel/nvrchannel-0.1.6.tar.gz/nvrchannel-0.1.6/README.md
@@ -0,0 +1,12 @@
+# NVRChannel
+Network video recorder for iot_devices compatible apps.
+Requires GStreamer to be installed on the system.
+
+Split into a separate lib due to the large model file.
+
+
+## Changes
+### 0.1.6
+
+* Use cv2.dnn because tflite keeps breaking on python version updates.
+* Switch to MobileNetSSD
---
+++ packages/NVRChannel/nvrchannel-0.1.6.tar.gz/nvrchannel-0.1.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name="NVRChannel",
+    version="0.1.6",
+    author="Daniel Dunn",
+    author_email="dannydunn@eternityforest.com",
+    packages=["NVRChannel", "NVRChannel.NVRChannel", "NVRChannel.onvif"],
+    package_data={
+        "": [
+            "**/*.tflite",
+            "**/*.txt",
+            "**.*.json",
+            "*.json",
+            "*.md",
+        ]
+    },
+    license="MIT",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/EternityForest/iot_devices",
+    dependencies=[
+        "opencv-python",
+        "Pillow",
+        "cvlib",
+        "icemedia",
+        "scullery",
+        "zeep",
+    ],
+)
