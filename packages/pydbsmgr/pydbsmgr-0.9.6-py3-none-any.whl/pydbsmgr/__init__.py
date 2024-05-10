21a22c91a37ee8d6d967b0e7612c4b8b59dc5085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 14:04:49 +0000

    Add package pydbsmgr/0.9.6/pydbsmgr-0.9.6-py3-none-any.whl

---
+++ packages/pydbsmgr/pydbsmgr-0.9.6-py3-none-any.whl/pydbsmgr/__init__.py
@@ -0,0 +1,9 @@
+"""
+Authors: J. A. Moreno-Guerra
+Last modification: 05/15/2023
+Corresponding author: jzs.gm27@gmail.com 
+"""
+
+from pydbsmgr.main import *
+from pydbsmgr.utils.azure_sdk import *
+from pydbsmgr.utils.tools import *
