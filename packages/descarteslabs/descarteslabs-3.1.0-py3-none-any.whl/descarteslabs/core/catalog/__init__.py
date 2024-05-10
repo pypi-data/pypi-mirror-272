739c0c28e62fbed177ef74637935f12b1ac6682c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 04:04:27 +0000

    Add package descarteslabs/3.1.0/descarteslabs-3.1.0-py3-none-any.whl

---
+++ packages/descarteslabs/descarteslabs-3.1.0-py3-none-any.whl/descarteslabs/core/catalog/__init__.py
@@ -0,0 +1,144 @@
+"""
+The Catalog Service provides access to products, bands, and images
+available from Descartes Labs.
+"""
+
+# Copyright 2018-2023 Descartes Labs.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .task import TaskState
+from .product import (
+    DeletionTaskStatus,
+    Product,
+    ProductCollection,
+)
+from .band import (
+    Band,
+    BandCollection,
+    BandType,
+    ClassBand,
+    Colormap,
+    DataType,
+    DerivedParamsAttribute,
+    GenericBand,
+    MaskBand,
+    MicrowaveBand,
+    ProcessingLevelsAttribute,
+    ProcessingStepAttribute,
+    SpectralBand,
+)
+from .blob import (
+    Blob,
+    BlobCollection,
+    BlobDeletionTaskStatus,
+    BlobSearch,
+    BlobSummaryResult,
+    StorageType,
+)
+from .image import Image, ImageSearch, ImageSummaryResult
+from .image_types import ResampleAlgorithm, DownloadFileFormat
+from .image_upload import (
+    ImageUpload,
+    ImageUploadEvent,
+    ImageUploadEventSeverity,
+    ImageUploadEventType,
+    ImageUploadOptions,
+    ImageUploadStatus,
+    ImageUploadType,
+    OverviewResampler,
+)
+from .image_collection import ImageCollection
+from .search import (
+    AggregateDateField,
+    GeoSearch,
+    Interval,
+    Search,
+    SummarySearchMixin,
+)
+from .catalog_base import (
+    CatalogClient,
+    CatalogObject,
+    DeletedObjectError,
+    UnsavedObjectError,
+)
+from .named_catalog_base import NamedCatalogObject
+from .attributes import (
+    AttributeValidationError,
+    DocumentState,
+    File,
+    Resolution,
+    ResolutionUnit,
+    StorageState,
+)
+
+from ..common.property_filtering import Properties
+
+properties = Properties()
+
+__all__ = [
+    "AggregateDateField",
+    "AttributeValidationError",
+    "Band",
+    "BandCollection",
+    "BandType",
+    "Blob",
+    "BlobCollection",
+    "BlobDeletionTaskStatus",
+    "BlobSearch",
+    "BlobSummaryResult",
+    "CatalogClient",
+    "CatalogObject",
+    "ClassBand",
+    "Colormap",
+    "DataType",
+    "DeletedObjectError",
+    "DeletionTaskStatus",
+    "DerivedParamsAttribute",
+    "DocumentState",
+    "DownloadFileFormat",
+    "File",
+    "GenericBand",
+    "GeoSearch",
+    "Image",
+    "ImageCollection",
+    "ImageSearch",
+    "ImageUpload",
+    "ImageUploadEvent",
+    "ImageUploadEventSeverity",
+    "ImageUploadEventType",
+    "ImageUploadOptions",
+    "ImageUploadStatus",
+    "ImageUploadType",
+    "ImageSummaryResult",
+    "Interval",
+    "MaskBand",
+    "MicrowaveBand",
+    "NamedCatalogObject",
+    "OverviewResampler",
+    "ProcessingLevelsAttribute",
+    "ProcessingStepAttribute",
+    "Product",
+    "ProductCollection",
+    "properties",
+    "ResampleAlgorithm",
+    "Resolution",
+    "ResolutionUnit",
+    "Search",
+    "SpectralBand",
+    "StorageState",
+    "StorageType",
+    "SummarySearchMixin",
+    "TaskState",
+    "UnsavedObjectError",
+]
---
+++ packages/descarteslabs/descarteslabs-3.1.0-py3-none-any.whl/descarteslabs/core/client/auth/__init__.py
@@ -0,0 +1,5 @@
+# this is a convenience import for backwards compatibility with external existing code
+from descarteslabs.auth import Auth
+
+
+__all__ = ["Auth"]
