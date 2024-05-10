e256fe3a8a73bc29dee8f5eb49a92fd9f760b7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 16:04:44 +0000

    Add package openCV-movement-detection/1.3/openCV_movement_detection-1.3.tar.gz

---
+++ packages/openCV-movement-detection/openCV_movement_detection-1.3.tar.gz/openCV_movement_detection-1.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='openCV_movement_detection',
+    version='1.3',
+    author='Len',
+    description='A Python openCV_movement_detection for movement detection with OpenCV.',
+    packages=find_packages(),
+    install_requires=['opencv-python'],
+)
