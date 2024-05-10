d558b77aea2ba1021b373fb3fb9de9e8015ea592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 14:04:34 +0000

    Add package constellate/0.8.7/constellate-0.8.7-py2.py3-none-any.whl

---
+++ packages/constellate/constellate-0.8.7-py2.py3-none-any.whl/constellate/compression/zstd/__init__.py
@@ -0,0 +1,20 @@
+try:
+    # Prefer pyzstd backend
+    from constellate.compression.zstd.pyzstd import (
+        ZstdCompressor,
+        ZstdDecompressor,
+        ZstdCompressionParameters,
+    )
+except ImportError:
+    # Fallback to zstandard backend
+    # Note: zstandard==0.22.0 fails with fatal exception on all py3x versions
+    try:
+        from constellate.compression.zstd.zstandard import (
+            ZstdCompressor,
+            ZstdDecompressor,
+            ZstdCompressionParameters,
+        )
+    except ImportError:
+        raise
+
+__all__ = ["ZstdCompressor", "ZstdDecompressor", "ZstdCompressionParameters"]
