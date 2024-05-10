16ff3670ffc081cd59a76d7caca03c5008fa518b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 14:05:19 +0000

    Add package somedecorators/1.2.2/somedecorators-1.2.2.tar.gz

---
+++ packages/somedecorators/somedecorators-1.2.2.tar.gz/somedecorators-1.2.2/somedecorators/__init__.py
@@ -0,0 +1,5 @@
+from .retry import retry
+from .email import email_on_exception
+from .wechat import wechat_on_exception
+from .timeit import timeit,timeout,TimeoutError
+from .log import setup_logger
