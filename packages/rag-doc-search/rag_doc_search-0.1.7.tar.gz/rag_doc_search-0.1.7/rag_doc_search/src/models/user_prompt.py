468e9324835704cb85cea01f1da7d04347f81375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 15:04:22 +0000

    Add package rag-doc-search/0.1.7/rag_doc_search-0.1.7.tar.gz

---
+++ packages/rag-doc-search/rag_doc_search-0.1.7.tar.gz/rag_doc_search-0.1.7/rag_doc_search/src/models/user_prompt.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel, Field
+
+
+class UserPrompt(BaseModel):
+    """User Promp schema."""
+    prompt: str= Field("", description="A string that should not be empty or contain only whitespace", min_length=1)
