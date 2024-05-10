af98d703dbcf6e04cb0824856faf8a78b3f0d350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 03:04:25 +0000

    Add package physfs.py/0.2.0/physfs_py-0.2.0.tar.gz

---
+++ packages/physfs.py/physfs_py-0.2.0.tar.gz/physfs_py-0.2.0/tests/test.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import physfs
+
+rootdir = Path(__file__).parent
+assert physfs.__version__ == '0.2.0'
+
+physfs.init()
+assert physfs.ls("/") == []
+
+physfs.mount(str(rootdir / "./example.zip"))
+assert physfs.ls("/") == ["example"]
+assert physfs.cat("example/flag") == b"OqP$IwK7eiZyTWk4Xi3jYECi^IXLdZw1u@H5wOo4\n"
+
+physfs.unmount(str(rootdir / "./example.zip"))
+assert physfs.ls("/") == []
+
+physfs.deinit()
+
+E = None
+try:
+    physfs.ls()
+except physfs.PhysfsUninitialedError as e:
+    E = e
+assert E is not None
