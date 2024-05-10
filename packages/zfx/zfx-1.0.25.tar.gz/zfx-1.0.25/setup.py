dc37fceb2893a70973e5ab1007e585c34f1bb012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 03:04:03 +0000

    Add package zfx/1.0.25/zfx-1.0.25.tar.gz

---
+++ packages/zfx/zfx-1.0.25.tar.gz/zfx-1.0.25/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zfx',
+    version='1.0.25',
+    packages=find_packages(),
+    # 不包含其他文件
+    include_package_data=False,
+    # 作者信息等
+    author='zengfengxiang',
+    author_email='424491679@qq.com',
+    description='自用',
+    # 项目主页
+    url='',
+    # 依赖列表
+    install_requires=[
+        'requests',
+        'pyperclip',
+        "pystray",
+        "psutil",
+        "selenium",
+        "requests",
+        # 添加其他依赖库
+    ],
+)
+
+
