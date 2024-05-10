a1d930216c4f0e0b0150efb3ca0c7acc4b5c3c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 06:03:58 +0000

    Add package snakebids/0.13.1/snakebids-0.13.1-py3-none-any.whl

---
+++ packages/snakebids/snakebids-0.13.1-py3-none-any.whl/snakebids/io/yaml.py
@@ -0,0 +1,26 @@
+import collections
+from pathlib import Path, PosixPath, WindowsPath
+from typing import Any, OrderedDict
+
+from ruamel.yaml import YAML, Dumper
+
+
+def get_yaml_io():
+    """Return yaml loader/dumper configured for snakebids."""
+    yaml = YAML()
+
+    # Represent any PathLikes as str.
+    def path2str(dumper: Dumper, data: Path):
+        return dumper.represent_scalar(
+            "tag:yaml.org,2002:str",
+            str(data),
+        )
+
+    def to_dict(dumper: Dumper, data: OrderedDict[Any, Any]):
+        return dumper.represent_dict(dict(data))
+
+    yaml.representer.add_representer(PosixPath, path2str)
+    yaml.representer.add_representer(WindowsPath, path2str)
+    yaml.representer.add_representer(collections.OrderedDict, to_dict)
+    yaml.representer.add_representer(OrderedDict, to_dict)
+    return yaml
