ce0433e87967264aa8b770fffbf925fff85db939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 04:04:14 +0000

    Add package volcengine-python-sdk/1.0.78/volcengine-python-sdk-1.0.78.tar.gz

---
+++ packages/volcengine-python-sdk/volcengine-python-sdk-1.0.78.tar.gz/volcengine-python-sdk-1.0.78/setup.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+from setuptools import setup, find_packages  # noqa: H301
+
+NAME = "volcengine-python-sdk"
+VERSION = "1.0.78"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = [
+    "certifi>=2017.4.17",
+    "python-dateutil>=2.1",
+    "six>=1.10",
+    "urllib3>=1.23"
+]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    description='Volcengine SDK for Python',
+    license="Apache License 2.0",
+    platforms='any',
+    url='https://github.com/volcengine/volcengine-python-sdk',
+)
