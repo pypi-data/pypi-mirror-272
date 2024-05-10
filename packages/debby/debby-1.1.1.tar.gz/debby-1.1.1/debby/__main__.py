d5a6df7326b112cd68cc3bc4d0b6ffe58b185274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 03:03:52 +0000

    Add package debby/1.1.1/debby-1.1.1.tar.gz

---
+++ packages/debby/debby-1.1.1.tar.gz/debby-1.1.1/debby/__main__.py
@@ -0,0 +1,28 @@
+import sys
+from pathlib import Path
+from typing import Optional, Sequence
+
+from debby.args import Args
+from debby.control_file import ControlFile
+from debby.exceptions import DebbyError
+from debby.files import Files
+from debby.meta import MetaLoaderFactory
+from debby.package import Package
+
+
+def create_package(args: Args) -> Path:
+    meta = MetaLoaderFactory(args).loader().load()
+    files = Files(args.files)
+    package = Package(meta, ControlFile(meta, files, args.template), files)
+    return package.create(args.out_dir)
+
+
+def main(argv: Optional[Sequence[str]] = None):
+    print(create_package(Args.parse(argv)))
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except DebbyError as e:
+        print(e, file=sys.stderr)
