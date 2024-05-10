71c94d33d697038f11e02cfe8259d4519000f2eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 14:04:24 +0000

    Add package ParentLOL/1.0.0/parentlol-1.0.0.tar.gz

---
+++ packages/ParentLOL/parentlol-1.0.0.tar.gz/parentlol-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ParentLOL',
+    version='1.0.0',
+    packages=find_packages(),
+    description='Printing with Ease.',
+    author='parent',
+    author_email='parent@parent.lol',
+    url='https://github.com/parentrb',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
