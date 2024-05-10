c259a7cd98e5cf089c9fafc22934e60ca7e4195a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 15:04:42 +0000

    Add package my-math-unimore-cicd/0.2/my_math_unimore_cicd-0.2.tar.gz

---
+++ packages/my-math-unimore-cicd/my_math_unimore_cicd-0.2.tar.gz/my_math_unimore_cicd-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup 
+  
+setup( 
+    name='my_math_unimore_cicd', 
+    version='0.2', 
+    description='A sample Python package to test CI/CD', 
+    long_description='A sample Python package to test CI/CD',
+    author='Angelo Ferrando', 
+    author_email='angelo.ferrando42@gmail.com', 
+    packages=['my_math_unimore_cicd'], 
+    install_requires=['pytest'], 
+) 
