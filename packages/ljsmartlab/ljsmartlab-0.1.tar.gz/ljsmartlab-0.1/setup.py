9d1e0527f2d09ff7a48d0ea76c347cc659e0e86d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 05:03:52 +0000

    Add package ljsmartlab/0.1/ljsmartlab-0.1.tar.gz

---
+++ packages/ljsmartlab/ljsmartlab-0.1.tar.gz/ljsmartlab-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ljsmartlab',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple test library for Python',
+    long_description="This libray create for ljsmartlab",
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    keywords=['testing', 'example'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
