a4f040b2cada353ec08236b66766ad1da4444281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 04:04:02 +0000

    Add package keyauth-py/1.2/keyauth_py-1.2.tar.gz

---
+++ packages/keyauth-py/keyauth_py-1.2.tar.gz/keyauth_py-1.2/keyauth-py/keyauth.py
@@ -0,0 +1,11 @@
+import requests
+
+def auth(keyslink, keylength, key):
+    keys = requests.get(keyslink)
+    if len(key) == keylength:
+        if key in keys.text:
+            return True
+        else:
+            return False
+    else:
+        return False
