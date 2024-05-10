bbf37bf23950c89ac0064ce1c068feb64a0645f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 16:04:42 +0000

    Add package NatanStanek/0.13.1/NatanStanek-0.13.1-py3-none-any.whl

---
+++ packages/NatanStanek/NatanStanek-0.13.1-py3-none-any.whl/NatanStanek/__init__.py
@@ -0,0 +1,10 @@
+# NatanStanek/__init__.py
+import time
+def greet():
+    print("Hello from NatanStanek!")
+
+def DoesNatanSmell():
+    print("Natan stinks")
+
+def McDonaldsBreak(sec):
+    time.sleep(sec)
