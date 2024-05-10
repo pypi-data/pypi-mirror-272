6c0ba025131c59b36952dc52d9081d99b1618a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 15:04:16 +0000

    Add package ingenialink/7.3.1/ingenialink-7.3.1-py3-none-any.whl

---
+++ packages/ingenialink/ingenialink-7.3.1-py3-none-any.whl/ingenialink/ethercat/register.py
@@ -0,0 +1,12 @@
+from ingenialink.canopen.register import CanopenRegister
+from ingenialink.enums.register import (
+    REG_ACCESS,
+    REG_ADDRESS_TYPE,
+    REG_DTYPE,
+    REG_PHY,
+    RegCyclicType,
+)
+
+
+class EthercatRegister(CanopenRegister):
+    pass
