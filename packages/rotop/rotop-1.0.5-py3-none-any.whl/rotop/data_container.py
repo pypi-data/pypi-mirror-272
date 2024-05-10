3a62c12af53a007df9baca0dbd2d761d3ee4990c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 16:04:59 +0000

    Add package rotop/1.0.5/rotop-1.0.5-py3-none-any.whl

---
+++ packages/rotop/rotop-1.0.5-py3-none-any.whl/rotop/data_container.py
@@ -0,0 +1,131 @@
+# Copyright 2023 iwatake2222
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import annotations
+import datetime
+import time
+import os
+import pandas as pd
+
+from .top_runner import TopRunner
+from .utility import create_logger
+
+
+logger = create_logger(__name__, log_filename='rotop.log')
+
+
+class DataContainer:
+  MAX_ROW_CSV = 1000
+  MAX_NUM_HISTORY = 100
+
+  def __init__(self, write_csv=False):
+    now = datetime.datetime.now()
+    if write_csv:
+      self.csv_dir_name = now.strftime('./rotop_%Y%m%d_%H%M%S')
+      os.mkdir(self.csv_dir_name)
+    else:
+      self.csv_dir_name = None
+    self.csv_index = 0
+    self.df_total = pd.DataFrame()
+    self.df_cpu = pd.DataFrame()
+    self.df_mem = pd.DataFrame()
+    self.df_total_history = pd.DataFrame()
+    self.df_cpu_history = pd.DataFrame()
+    self.df_mem_history = pd.DataFrame()
+
+  def run(self, top_runner: TopRunner, lines: list[str], num_process: int):
+    if top_runner.col_range_command and top_runner.col_range_command[0] > 0:
+      df_total_current, df_cpu_current, df_mem_current = self.create_df_from_top(top_runner, lines, num_process)
+      self.df_total = pd.concat([self.df_total, df_total_current], axis=0)
+      self.df_cpu = pd.concat([self.df_cpu, df_cpu_current], axis=0)
+      self.df_mem = pd.concat([self.df_mem, df_mem_current], axis=0)
+      self.df_total_history = pd.concat([self.df_total_history, df_cpu_current], axis=0, ignore_index=True)
+      self.df_cpu_history = pd.concat([self.df_cpu_history, df_cpu_current], axis=0, ignore_index=True)
+      self.df_mem_history = pd.concat([self.df_mem_history, df_mem_current], axis=0, ignore_index=True)
+      if self.csv_dir_name:
+        self.df_total.to_csv(os.path.join(self.csv_dir_name, f'total_{self.csv_index:03d}.csv'), index=False)
+        self.df_cpu.to_csv(os.path.join(self.csv_dir_name, f'cpu_{self.csv_index:03d}.csv'), index=False)
+        self.df_mem.to_csv(os.path.join(self.csv_dir_name, f'mem_{self.csv_index:03d}.csv'), index=False)
+        if len(self.df_total) >= self.MAX_ROW_CSV:
+          self.df_total = pd.DataFrame()
+          self.df_cpu = pd.DataFrame()
+          self.df_mem = pd.DataFrame()
+          self.csv_index += 1
+      if len(self.df_total_history) >= self.MAX_NUM_HISTORY:
+        self.df_total_history = self.df_total_history[1:]
+        self.df_cpu_history = self.df_cpu_history[1:]
+        self.df_mem_history = self.df_mem_history[1:]
+
+    self.df_cpu_history = self.sort_df_in_column(self.df_cpu_history)
+    self.df_mem_history = self.sort_df_in_column(self.df_mem_history)
+
+    return self.df_total_history, self.df_cpu_history, self.df_mem_history
+
+
+  def reset_history(self):
+    self.df_cpu_history = pd.DataFrame()
+    self.df_mem_history = pd.DataFrame()
+
+
+  @staticmethod
+  def sort_df_in_column(df: pd.DataFrame):
+    df = df.sort_values(by=len(df)-1, axis=1, ascending=False)
+    return df
+
+
+  @staticmethod
+  def create_df_from_top(top_runner: TopRunner, lines: list[str], num_process: int):
+    # now = datetime.datetime.now()
+    now = int(time.time())
+
+    # Get total info
+    total_line = None
+    total_user = ''
+    total_sys = ''
+    total_idle = ''
+    for i, line in enumerate(lines):
+      if '%Cpu' in line:
+        total_line = line
+        break
+    if total_line:
+      total_user = total_line.split('us')[0].split()[-1].strip()
+      total_sys = total_line.split('sy')[0].split()[-1].strip()
+      total_idle = total_line.split('id')[0].split()[-1].strip()
+    df_total_current = pd.DataFrame([[now, total_user, total_sys, total_idle]], columns=['datetime', 'user', 'sys', 'idle'])
+
+    # Move to line containing process info
+    for i, line in enumerate(lines):
+      if 'PID' in line:
+        lines = lines[i + 1:]
+        break
+
+    # Get process info
+    process_list = []
+    cpu_list = []
+    mem_list = []
+    for i, line in enumerate(lines):
+      if i >= num_process:
+        break
+      pid = line[top_runner.col_range_pid[0]:top_runner.col_range_pid[1]].strip()
+      command = line[top_runner.col_range_command[0]:].strip()
+      process_name = str(f'{command} ({pid})')
+      process_list.append(process_name)
+      cpu = float(line[top_runner.col_range_CPU[0]:top_runner.col_range_CPU[1]].strip())
+      cpu_list.append(cpu)
+      mem = float(line[top_runner.col_range_MEM[0]:top_runner.col_range_MEM[1]].strip())
+      mem_list.append(mem)
+
+    df_cpu_current = pd.DataFrame([[now] + cpu_list], columns=['datetime'] + process_list)
+    df_mem_current = pd.DataFrame([[now] + mem_list], columns=['datetime'] + process_list)
+
+    return df_total_current, df_cpu_current, df_mem_current
---
+++ packages/rotop/rotop-1.0.5-py3-none-any.whl/rotop/rotop.py
@@ -0,0 +1,91 @@
+# Copyright 2023 iwatake2222
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import annotations
+import argparse
+import curses
+import time
+
+from .data_container import DataContainer
+from .top_runner import TopRunner
+from .gui_main import gui_main
+from .utility import create_logger
+try:
+  from ._version import version
+except:
+  from .version_dummy import version
+
+
+logger = create_logger(__name__, log_filename='rotop.log')
+
+
+def main_curses(stdscr, args):
+  curses.use_default_colors()
+  # curses.init_color(0, 0, 0, 0)
+  curses.curs_set(0)
+  stdscr.timeout(10)
+
+  top_runner = TopRunner(args.interval, args.filter)
+  data_container = DataContainer(args.csv)
+
+  try:
+    while True:
+      max_y, max_x = stdscr.getmaxyx()
+      result_lines, result_show_all_lines = top_runner.run(max(max_y, args.num_process), max_x>160, args.only_ros)
+      if result_show_all_lines is None:
+        time.sleep(0.1)
+        continue
+
+      _ = data_container.run(top_runner, result_show_all_lines, args.num_process)
+
+      stdscr.clear()
+      for i, line in enumerate(result_lines):
+        if i >= max_y - 1:
+          break
+        stdscr.addstr(i, 0, line[:max_x])
+
+      stdscr.refresh()
+      key = stdscr.getch()
+      if key == ord('q'):
+        break
+  except KeyboardInterrupt:
+    exit(0)
+
+
+def parse_args():
+  parser = argparse.ArgumentParser(
+    description=f'rotop: top for ROS 2, version {version}')
+  parser.add_argument('--interval', type=float, default=2, help="Update interval in seconds. Similar to the -d option of top.")
+  parser.add_argument('--filter', type=str, default='.*', help="Only show processes fitting to this regular expression.")
+  parser.add_argument('--csv', action='store_true', default=False, help="Activate saving data to csv file.")
+  parser.add_argument('--gui', action='store_true', default=False, help="Use GUI including plotting of CPU loads.")
+  parser.add_argument('--num_process', type=int, default=30, help="Maximum number of processes that will be shown.")
+  parser.add_argument('--only_ros', action='store_true', default=False, help="List only ROS 2 node processes.")
+
+  args = parser.parse_args()
+
+  logger.debug(f'filter: {args.filter}')
+  logger.debug(f'csv: {args.csv}')
+  logger.debug(f'gui: {args.gui}')
+  logger.debug(f'num_process: {args.num_process}')
+  logger.debug(f'only_ros: {args.only_ros}')
+
+  return args
+
+
+def main():
+  args = parse_args()
+  if args.gui:
+    gui_main(args)
+  else:
+    curses.wrapper(main_curses, args)
