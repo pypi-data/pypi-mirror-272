8ec25c097600096e68cd5c0bb91a7f1459825b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-10 14:04:50 +0000

    Add package GnDocTrans/0.0.4/GnDocTrans-0.0.4.tar.gz

---
+++ packages/GnDocTrans/GnDocTrans-0.0.4.tar.gz/GnDocTrans-0.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GnDocTrans",
+    version="0.0.4",
+    description='',
+    author="",
+    author_email="",
+    url='',
+    packages=find_packages(),
+    install_requires=['numpy', 'opencv-python'],
+    python_requires=">=3.10,<3.11",
+    keywords=['doc', 'document'],
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
