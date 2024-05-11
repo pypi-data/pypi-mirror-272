6fe4282998bc33189642ff7ab131aeb9bab0034f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 15:04:37 +0000

    Add package proxies-taster/1.2.3/proxies_taster-1.2.3.tar.gz

---
+++ packages/proxies-taster/proxies_taster-1.2.3.tar.gz/proxies_taster-1.2.3/setup.py
@@ -0,0 +1,49 @@
+"""Скрипт для установлки пакета"""
+from setuptools import setup
+
+
+name = 'proxies-taster'
+version = '1.2.3'
+
+
+# Long description
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+url = 'https://github.com/TheWhatis/ProxiesTaster'
+
+
+if __name__ == '__main__':
+    setup(
+        name=name,
+        version=version,
+
+        author='Whatis',
+        author_email='asdwdagwahwabe@gmail.com',
+
+        description='Пакет для проверки прокси',
+
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+
+        license='GPL',
+        platforms='any',
+
+        url=url,
+        download_url=f"{url}/dist/{name}-{version}.zip",
+
+        packages=['proxies_taster'],
+        include_package_data=True,
+        install_requires=[
+            'aiohttp',
+            'aiohttp-proxy',
+            'fake-useragent',
+            'PyEventEmitter'
+        ],
+
+        entry_points={
+            "console_scripts": [
+                'proxies-taster = proxies_taster.__main__:main'
+            ]
+        }
+    )
