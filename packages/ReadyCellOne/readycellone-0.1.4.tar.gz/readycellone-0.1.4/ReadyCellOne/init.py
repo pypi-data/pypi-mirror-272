7471725f57b5119d9d1ec6d73b6a5d33f53e6c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 14:04:17 +0000

    Add package ReadyCellOne/0.1.4/readycellone-0.1.4.tar.gz

---
+++ packages/ReadyCellOne/readycellone-0.1.4.tar.gz/readycellone-0.1.4/ReadyCellOne/init.py
@@ -0,0 +1,3 @@
+from .core import *
+from .plot import *
+from .utils import *
---
+++ packages/ReadyCellOne/readycellone-0.1.4.tar.gz/readycellone-0.1.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='ReadyCellOne',
+      version='0.1.4',
+      description='Computational tools to enable task-centric cell engineering',
+      url='http://github.com/CahanLab/ReadyCellOne/',
+      author='Patrick Cahan',
+      author_email='patrick.cahan@gmail.com',
+      license='MIT',
+      packages=['ReadyCellOne'],
+      install_requires=[
+          'scanpy',
+          'gseapy',
+          'harmonypy',
+          'scikit-learn',
+          'matplotlib',
+          'seaborn'
+      ],
+      zip_safe=False)
