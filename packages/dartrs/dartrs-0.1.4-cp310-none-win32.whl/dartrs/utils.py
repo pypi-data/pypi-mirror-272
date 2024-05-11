0012f93c6a5af6ed394e03a92dec30bbfbb99545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 15:04:29 +0000

    Add package dartrs/0.1.4/dartrs-0.1.4-cp310-none-win32.whl

---
+++ packages/dartrs/dartrs-0.1.4-cp310-none-win32.whl/dartrs/utils.py
@@ -0,0 +1,27 @@
+from . import dartrs
+
+
+def get_generation_config(
+    prompt: str,
+    tokenizer: dartrs.DartTokenizer,
+    device: dartrs.DartDevice = dartrs.DartDevice.Cpu(),
+    eos_token: str | None = None,
+    max_new_tokens: int | None = 256,
+    temperature: float | None = 1.0,
+    top_p: float | None = 1.0,
+    top_k: int | None = 100,
+    ban_token_ids: list[int] | None = None,
+    seed: int | None = None,
+) -> dartrs.GenerationConfig:
+    return dartrs.GenerationConfig(
+        device=device,
+        tokenizer=tokenizer,
+        prompt=prompt,
+        eos_token=eos_token,
+        max_new_tokens=max_new_tokens,
+        temperature=temperature,
+        top_p=top_p,
+        top_k=top_k,
+        ban_token_ids=ban_token_ids,
+        seed=seed,
+    )
