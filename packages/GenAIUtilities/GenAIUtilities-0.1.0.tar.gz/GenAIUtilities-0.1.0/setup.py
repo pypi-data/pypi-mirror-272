b56d17dce364d45d7c991092ed3427bfb6ec1f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 15:04:17 +0000

    Add package GenAIUtilities/0.1.0/GenAIUtilities-0.1.0.tar.gz

---
+++ packages/GenAIUtilities/GenAIUtilities-0.1.0.tar.gz/GenAIUtilities-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+name='GenAIUtilities',
+version='0.1.0',
+author='Arunava Chandra',
+description='A short description of your package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
