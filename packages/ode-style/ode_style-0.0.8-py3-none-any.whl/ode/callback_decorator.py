e05839105689e3342c208e9ea37655365a905205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 05:06:13 +0000

    Add package ode-style/0.0.8/ode_style-0.0.8-py3-none-any.whl

---
+++ packages/ode-style/ode_style-0.0.8-py3-none-any.whl/ode/callback_decorator.py
@@ -0,0 +1,22 @@
+from ode.error_output import ErrorOutput
+from ode.use_case import UseCase
+from ode.output import Output
+from ode.use_case_decorator import UseCaseDecorator
+from typing import Callable, TypeVar
+
+P = TypeVar('P')
+R = TypeVar('R')
+
+class CallbackDecorator(UseCaseDecorator[P, R]):
+    
+    def __init__(self, use_case: UseCase[P, R], callback: Callable[[Output[R]], None]):
+        super().__init__(use_case)
+        self.callback = callback
+
+    def task_done(self, output: Output[R]):
+        if(output.is_success):
+            self.callback(output)
+            return
+        
+        self.callback(ErrorOutput(output.error))
+        
