2dfe9813edad40d5e65987503cc6d20d8216df03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 03:04:55 +0000

    Add package kthcloud/0.1.0a0/kthcloud-0.1.0a0-py3-none-any.whl

---
+++ packages/kthcloud/kthcloud-0.1.0a0-py3-none-any.whl/kthcloud_go_deploy_v2/types/gpu_group_list_response.py
@@ -0,0 +1,9 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List
+
+from .gpu_group import GpuGroup
+
+__all__ = ["GpuGroupListResponse"]
+
+GpuGroupListResponse = List[GpuGroup]
