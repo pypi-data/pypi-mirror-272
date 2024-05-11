0773d58d0a50dcf9d5339433c73b50e9e871723f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 05:06:48 +0000

    Add package sigProfilerPlotting/1.3.23/sigProfilerPlotting-1.3.23.tar.gz

---
+++ packages/sigProfilerPlotting/sigProfilerPlotting-1.3.23.tar.gz/sigProfilerPlotting-1.3.23/sigProfilerPlotting/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILERPLOTTING SETUP.PY
+short_version = '1.3.23'
+version = '1.3.23'
+update = 'Upgrade v1.3.23: Update SBS4608 custom_text_upper, custom_text_lower, custom_text_middle.'
+    
+    
