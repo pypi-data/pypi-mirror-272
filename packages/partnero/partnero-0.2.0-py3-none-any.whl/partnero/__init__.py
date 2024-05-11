2c861ef5576d80ea84f4551b6eb0b11570d8a9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 15:04:38 +0000

    Add package partnero/0.2.0/partnero-0.2.0-py3-none-any.whl

---
+++ packages/partnero/partnero-0.2.0-py3-none-any.whl/partnero/__init__.py
@@ -0,0 +1,17 @@
+from .authentication import Authentication
+from .coupon_api import CouponAPI
+from .customer_api import CustomerAPI
+from .partner_api import PartnerAPI
+from .promotion_code_api import PromotionCodeAPI
+from .transaction_api import TransactionAPI
+from .webhook_api import WebhookAPI
+
+__all__ = [
+    "Authentication",
+    "CouponAPI",
+    "CustomerAPI",
+    "PartnerAPI",
+    "PromotionCodeAPI",
+    "TransactionAPI",
+    "WebhookAPI"
+]
