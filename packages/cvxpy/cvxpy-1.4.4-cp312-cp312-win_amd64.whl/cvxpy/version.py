f9634860b7b8ed00f77e7582bb7c5a2c2fc41a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 14:04:21 +0000

    Add package cvxpy/1.4.4/cvxpy-1.4.4-cp312-cp312-win_amd64.whl

---
+++ packages/cvxpy/cvxpy-1.4.4-cp312-cp312-win_amd64.whl/cvxpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CVXPY SETUP.PY
+short_version = '1.4.4'
+version = '1.4.4'
+full_version = '1.4.4'
+git_revision = '311e79a'
+commit_count = '0'
+release = True
+if not release:
+    version = full_version
