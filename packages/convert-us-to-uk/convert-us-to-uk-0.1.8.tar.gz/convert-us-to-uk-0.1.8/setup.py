c21a4cd48cbf3ebf72fdf9939d8a353d8ae9dde2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 03:04:37 +0000

    Add package convert-us-to-uk/0.1.8/convert-us-to-uk-0.1.8.tar.gz

---
+++ packages/convert-us-to-uk/convert-us-to-uk-0.1.8.tar.gz/convert-us-to-uk-0.1.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='convert-us-to-uk',
+    version='0.1.8',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A simple utility to convert US spelling to UK spelling.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Oliver Blane',
+    author_email='oliverblane72@gmail.com',
+    url='https://github.com/oliverblane/convert-us-to-uk/tree/main',
+    install_requires=[
+    ],
+    python_requires='>=3.10',
+    entry_points={
+        'console_scripts': [
+            'convert_us_to_uk=convert_us_to_uk.converter:main',
+        ],
+    },
+    
+)
