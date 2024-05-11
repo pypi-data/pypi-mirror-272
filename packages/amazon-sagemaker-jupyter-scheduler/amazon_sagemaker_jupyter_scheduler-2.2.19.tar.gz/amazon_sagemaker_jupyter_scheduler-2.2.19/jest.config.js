571e93f70060aa97ecba898c5b0a84a2b07d27f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 05:06:02 +0000

    Add package amazon-sagemaker-jupyter-scheduler/2.2.19/amazon_sagemaker_jupyter_scheduler-2.2.19.tar.gz

---
+++ packages/amazon-sagemaker-jupyter-scheduler/amazon_sagemaker_jupyter_scheduler-2.2.19.tar.gz/amazon_sagemaker_jupyter_scheduler-2.2.19/jest.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const base = require('./jest.config.base.js');
+
+module.exports = {
+  ...base,
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: [
+    '<rootDir>/setupTests/setupEnzyme.js',
+    '<rootDir>/setupTests/setupFetch.js',
+  ],
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
+    '\\.svg$': '<rootDir>/src/__mocks__/fileMock.ts',
+  },
+  globals: {
+    ...base.globals,
+  },
+};
