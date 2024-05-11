cd99f279ec11c4cbc54a03b36f066cae2fd83d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 04:05:39 +0000

    Add package molecularprofiles/1.0.0/molecularprofiles-1.0.0.tar.gz

---
+++ packages/molecularprofiles/molecularprofiles-1.0.0.tar.gz/molecularprofiles-1.0.0/src/molecularprofiles/__init__.py
@@ -0,0 +1,9 @@
+"""
+molecularprofiles
+
+The package provides a set of useful routines
+for the retrieval and analysis of the meteorological data
+from different data assimilation systems
+"""
+
+__version__ = "1.0.0"
