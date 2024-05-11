9965a91f7ce00ae534e16046a6b726eab90bc78d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 03:05:05 +0000

    Add package indent-concluder/1.1.1/indent-concluder-1.1.1.tar.gz

---
+++ packages/indent-concluder/indent-concluder-1.1.1.tar.gz/indent-concluder-1.1.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="indent-concluder",
+    version="1.1.1",
+    author="EvATive7",
+    author_email="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EvATive7/indent-concluder",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            
+        ],
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+    ),
+)
