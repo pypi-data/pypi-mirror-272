5b215301bcf1fdd1786015b99a467b16093c21df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 14:04:10 +0000

    Add package django-dynamic-theme/0.0.8/django_dynamic_theme-0.0.8.tar.gz

---
+++ packages/django-dynamic-theme/django_dynamic_theme-0.0.8.tar.gz/django_dynamic_theme-0.0.8/django_dynamic_theme/admin.py
@@ -0,0 +1,52 @@
+"""Models for Themes"""
+
+from django.contrib import admin
+
+from django_dynamic_theme.models import Background, MediaGallery, Navbar, Theme
+
+
+@admin.register(Theme)
+class ThemeAdmin(admin.ModelAdmin):
+    """
+    The admin model to maange the themes."""
+
+    list_display = ["name", "background"]
+
+
+@admin.register(Background)
+class BackgroundAdmin(admin.ModelAdmin):
+    """
+    The admin model to set the backgrounds.
+    """
+
+    list_display = ["primary_bg"]
+
+
+@admin.register(MediaGallery)
+class MediaGalleryAdmin(admin.ModelAdmin):
+    """
+    The admin model for the media gallery.
+    """
+
+    list_display = [
+        "margin_left",
+        "margin_right",
+        "max_width",
+        "item_align",
+        "row_margin_top",
+    ]
+
+
+@admin.register(Navbar)
+class NavbarAdmin(admin.ModelAdmin):
+    """
+    The admin model for the navigation bar.
+    """
+
+    list_display = [
+        "background_color",
+        "opacity",
+        "font_size",
+        "text_color",
+        "text_opacity",
+    ]
