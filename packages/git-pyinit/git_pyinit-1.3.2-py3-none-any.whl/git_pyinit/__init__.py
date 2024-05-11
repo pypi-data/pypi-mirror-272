de513df5a765a7c7c80422b397f052cedb1e1749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 06:04:44 +0000

    Add package git-pyinit/1.3.2/git_pyinit-1.3.2-py3-none-any.whl

---
+++ packages/git-pyinit/git_pyinit-1.3.2-py3-none-any.whl/git_pyinit/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2024-present Joshua Luckie <luckiejoshu@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from .__about__ import __version__
+from .cli import main
+
+__all__ = ["main", "__version__"]
---
+++ packages/git-pyinit/git_pyinit-1.3.2-py3-none-any.whl/git_pyinit/utils.py
@@ -0,0 +1,39 @@
+from .toml_reader import TomlResults
+
+
+def format_template(results: TomlResults) -> str:
+    template = """
+    name: Linting Stage
+
+    on: [push]
+
+    jobs:
+        build:
+          runs-on: {runs_on}
+          strategy:
+            matrix:
+              python-version: {py_vers}
+          steps:
+          - uses: actions/checkout@v3
+          - name: Set Up Python ${{{{ matrix.python-version }}}}
+            uses: actions/setup-python@v3
+            with:
+              python-version: ${{{{ matrix.python-version }}}}
+          - name: Install Dependencies
+            run: |
+              python -m pip install --upgrade pip
+              pip install {yaml_commands}
+    """.format(
+        runs_on=results.runs_on,
+        py_vers=str(results.py_vers),
+        yaml_commands=" ".join([tool.yaml_command for tool in results.tools if tool.active]),
+    )
+    for tool in results.tools:
+        if not tool.active:
+            continue
+        template += f"""
+          - name: Analyzing code with {tool.name}
+            run: |
+              {tool}
+        """
+    return template
