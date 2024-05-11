81a839100154bb5b692eeb8e0bff5f7d8ed56d1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 15:04:20 +0000

    Add package CTG-Utils/1.1.0/CTG_Utils-1.1.0-py3-none-any.whl

---
+++ packages/CTG-Utils/CTG_Utils-1.1.0-py3-none-any.whl/CTG_Utils/CTG_Func/__init__.py
@@ -0,0 +1,11 @@
+__author__ = 'F. Bertin'
+__license__ = "MIT"
+
+from .CTG_plot import *
+from .CTG_effectif import *
+from .CTG_config import *
+from .CTG_synthese import *
+from .CTGClasses import *
+from .CTG_utility import *
+
+
