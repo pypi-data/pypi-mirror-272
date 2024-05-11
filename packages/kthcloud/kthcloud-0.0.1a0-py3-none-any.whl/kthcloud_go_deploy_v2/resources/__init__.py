87cf144890d196319e8b162382932636dc997da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 03:05:48 +0000

    Add package kthcloud/0.0.1a0/kthcloud-0.0.1a0-py3-none-any.whl

---
+++ packages/kthcloud/kthcloud-0.0.1a0-py3-none-any.whl/kthcloud_go_deploy_v2/resources/__init__.py
@@ -0,0 +1,75 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .vms import (
+    VmsResource,
+    AsyncVmsResource,
+    VmsResourceWithRawResponse,
+    AsyncVmsResourceWithRawResponse,
+    VmsResourceWithStreamingResponse,
+    AsyncVmsResourceWithStreamingResponse,
+)
+from .snapshots import (
+    SnapshotsResource,
+    AsyncSnapshotsResource,
+    SnapshotsResourceWithRawResponse,
+    AsyncSnapshotsResourceWithRawResponse,
+    SnapshotsResourceWithStreamingResponse,
+    AsyncSnapshotsResourceWithStreamingResponse,
+)
+from .gpu_groups import (
+    GpuGroupsResource,
+    AsyncGpuGroupsResource,
+    GpuGroupsResourceWithRawResponse,
+    AsyncGpuGroupsResourceWithRawResponse,
+    GpuGroupsResourceWithStreamingResponse,
+    AsyncGpuGroupsResourceWithStreamingResponse,
+)
+from .gpu_leases import (
+    GpuLeasesResource,
+    AsyncGpuLeasesResource,
+    GpuLeasesResourceWithRawResponse,
+    AsyncGpuLeasesResourceWithRawResponse,
+    GpuLeasesResourceWithStreamingResponse,
+    AsyncGpuLeasesResourceWithStreamingResponse,
+)
+from .vm_actions import (
+    VmActionsResource,
+    AsyncVmActionsResource,
+    VmActionsResourceWithRawResponse,
+    AsyncVmActionsResourceWithRawResponse,
+    VmActionsResourceWithStreamingResponse,
+    AsyncVmActionsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "GpuGroupsResource",
+    "AsyncGpuGroupsResource",
+    "GpuGroupsResourceWithRawResponse",
+    "AsyncGpuGroupsResourceWithRawResponse",
+    "GpuGroupsResourceWithStreamingResponse",
+    "AsyncGpuGroupsResourceWithStreamingResponse",
+    "GpuLeasesResource",
+    "AsyncGpuLeasesResource",
+    "GpuLeasesResourceWithRawResponse",
+    "AsyncGpuLeasesResourceWithRawResponse",
+    "GpuLeasesResourceWithStreamingResponse",
+    "AsyncGpuLeasesResourceWithStreamingResponse",
+    "SnapshotsResource",
+    "AsyncSnapshotsResource",
+    "SnapshotsResourceWithRawResponse",
+    "AsyncSnapshotsResourceWithRawResponse",
+    "SnapshotsResourceWithStreamingResponse",
+    "AsyncSnapshotsResourceWithStreamingResponse",
+    "VmActionsResource",
+    "AsyncVmActionsResource",
+    "VmActionsResourceWithRawResponse",
+    "AsyncVmActionsResourceWithRawResponse",
+    "VmActionsResourceWithStreamingResponse",
+    "AsyncVmActionsResourceWithStreamingResponse",
+    "VmsResource",
+    "AsyncVmsResource",
+    "VmsResourceWithRawResponse",
+    "AsyncVmsResourceWithRawResponse",
+    "VmsResourceWithStreamingResponse",
+    "AsyncVmsResourceWithStreamingResponse",
+]
---
+++ packages/kthcloud/kthcloud-0.0.1a0-py3-none-any.whl/kthcloud_go_deploy_v2/types/__init__.py
@@ -0,0 +1,29 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .shared import VmSnapshotRead as VmSnapshotRead
+from .vm_read import VmRead as VmRead
+from .gpu_group import GpuGroup as GpuGroup
+from .vm_created import VmCreated as VmCreated
+from .vm_deleted import VmDeleted as VmDeleted
+from .vm_updated import VmUpdated as VmUpdated
+from .gpu_lease_read import GpuLeaseRead as GpuLeaseRead
+from .vm_list_params import VmListParams as VmListParams
+from .vm_create_params import VmCreateParams as VmCreateParams
+from .vm_list_response import VmListResponse as VmListResponse
+from .vm_update_params import VmUpdateParams as VmUpdateParams
+from .gpu_lease_created import GpuLeaseCreated as GpuLeaseCreated
+from .gpu_lease_deleted import GpuLeaseDeleted as GpuLeaseDeleted
+from .gpu_lease_updated import GpuLeaseUpdated as GpuLeaseUpdated
+from .vm_action_created import VmActionCreated as VmActionCreated
+from .vm_snapshot_created import VmSnapshotCreated as VmSnapshotCreated
+from .snapshot_list_params import SnapshotListParams as SnapshotListParams
+from .gpu_group_list_params import GpuGroupListParams as GpuGroupListParams
+from .gpu_lease_list_params import GpuLeaseListParams as GpuLeaseListParams
+from .snapshot_list_response import SnapshotListResponse as SnapshotListResponse
+from .gpu_group_list_response import GpuGroupListResponse as GpuGroupListResponse
+from .gpu_lease_create_params import GpuLeaseCreateParams as GpuLeaseCreateParams
+from .gpu_lease_list_response import GpuLeaseListResponse as GpuLeaseListResponse
+from .gpu_lease_update_params import GpuLeaseUpdateParams as GpuLeaseUpdateParams
+from .vm_action_create_params import VmActionCreateParams as VmActionCreateParams
