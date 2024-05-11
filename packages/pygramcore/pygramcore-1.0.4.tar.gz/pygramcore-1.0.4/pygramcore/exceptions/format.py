a64106a1825c3f566236d8ac4c855566147c6afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 04:05:39 +0000

    Add package pygramcore/1.0.4/pygramcore-1.0.4.tar.gz

---
+++ packages/pygramcore/pygramcore-1.0.4.tar.gz/pygramcore-1.0.4/pygramcore/exceptions/format.py
@@ -0,0 +1,8 @@
+from ..constants import MEDIA_FORMATS
+
+
+class InvalidFormat(Exception):
+    def __init__(self) -> None:
+        super().__init__(
+            f"Please insert media of an appropriate format ({', '.join(MEDIA_FORMATS)})."
+        )
