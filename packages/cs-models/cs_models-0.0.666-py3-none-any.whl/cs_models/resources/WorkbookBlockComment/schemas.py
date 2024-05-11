d021cbcf5dddaa50effd6fa36ca1e242218010aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 06:04:43 +0000

    Add package cs-models/0.0.666/cs_models-0.0.666-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.666-py3-none-any.whl/cs_models/resources/WorkbookBlockComment/schemas.py
@@ -0,0 +1,14 @@
+"""Marshmallow Schema for AssistantCommand."""
+from marshmallow import Schema, fields
+
+
+class WorkbookBlockCommentResourceSchema(Schema):
+    """Class for AssistantCommandResource schema"""
+
+    id = fields.Integer(dump_only=True)
+    block_id = fields.Integer(required=True)
+    sequence_number = fields.Integer(required=True)
+    comment = fields.String(allow_none=True)
+    is_deleted = fields.Boolean(allow_none=True)
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
