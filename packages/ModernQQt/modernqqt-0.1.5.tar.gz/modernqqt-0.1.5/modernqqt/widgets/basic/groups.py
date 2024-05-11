f81799b99ee3a84ae449e4e2832e4ca5d4710362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 05:06:17 +0000

    Add package ModernQQt/0.1.5/modernqqt-0.1.5.tar.gz

---
+++ packages/ModernQQt/modernqqt-0.1.5.tar.gz/modernqqt-0.1.5/modernqqt/widgets/basic/groups.py
@@ -0,0 +1,32 @@
+from typing import Optional, TYPE_CHECKING
+
+from PySide6.QtWidgets import (
+    QGroupBox, QButtonGroup
+)
+
+from modernqqt.src.core import Loader, StyleSheetLoader
+
+import os.path
+
+if TYPE_CHECKING:
+    from PySide6.QtWidgets import QWidget
+
+
+class GroupBox(QGroupBox):
+    def __init__(
+            self,
+            title: Optional[str] = None,
+            *,
+            stylesheet: Optional[str] = None,
+            parent: Optional["QWidget"] = None
+    ) -> None:
+        super().__init__(parent)
+
+        self.setObjectName("group-box")
+        self.setStyleSheet(StyleSheetLoader.load_stylesheet(
+            __file__, "styles/group_box.css", 
+            name="GroupBox", obj_name="QGroupBox#group-box",
+            stylesheet=stylesheet
+        ))
+
+        if title is not None: self.setTitle(title)
