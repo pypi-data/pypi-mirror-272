0321b16fc1c1ab82055f8bfa8ac3378d27a8a54d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 04:05:01 +0000

    Add package gradio-toggle/1.0.3/gradio_toggle-1.0.3-py3-none-any.whl

---
+++ packages/gradio-toggle/gradio_toggle-1.0.3-py3-none-any.whl/gradio_toggle/__init__.py
@@ -0,0 +1,9 @@
+# Toggle - A Gradio Custom Component
+# Created by Daniel Ialcin Misser Westergaard
+# https://huggingface.co/dwancin
+# https://github.com/dwancin
+# (c) 2024
+
+from .toggle import toggle as Toggle
+
+__all__ = ['Toggle']
