43bd027ab4fcd9f25f906dd9dce39c9b0bdc7828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 04:05:27 +0000

    Add package python-HackMD/1.0.3/python-HackMD-1.0.3.tar.gz

---
+++ packages/python-HackMD/python-HackMD-1.0.3.tar.gz/python-HackMD-1.0.3/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+
+def readme():
+    with open('README.md', 'r') as fp:
+        return fp.read()
+
+
+setuptools.setup(
+    name='python-HackMD',
+    version='1.0.3',
+    description='A Python interface for HackMD API',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    author='Eugene Yang',
+    author_email='eugene87222@gmail.com',
+    license="MIT",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    keywords='HackMD API',
+    packages=setuptools.find_packages(),
+    package_dir={'': 'src'},
+    install_requires=[
+        'requests'
+    ],
+    url='https://github.com/eugene87222/python-HackMD',
+)
