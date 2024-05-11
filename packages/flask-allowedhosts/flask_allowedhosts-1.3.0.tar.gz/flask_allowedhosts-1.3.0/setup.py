f49a39b23868120abc3d51acda077e8db1f8dff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 05:07:56 +0000

    Add package flask-allowedhosts/1.3.0/flask_allowedhosts-1.3.0.tar.gz

---
+++ packages/flask-allowedhosts/flask_allowedhosts-1.3.0.tar.gz/flask_allowedhosts-1.3.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+long_description = open('README.md').read()
+long_description_content_type = 'text/markdown'
+
+setup(
+    name='flask-allowedhosts',
+    version='1.3.0',
+    packages=['flask_allowedhosts'],
+    install_requires=[
+        'Flask',
+    ],
+    url='https://github.com/riad-azz/flask-allowedhosts',
+    license='MIT',
+    author='Riadh Azzoun',
+    author_email='riadh.azzoun@hotmail.com',
+    description='A Flask extension to limit access to your routes by using allowed hosts.',
+    long_description=long_description,
+    long_description_content_type=long_description_content_type,
+    keywords=["flask", "allowed hosts", "web development", "security"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Security",
+        "Operating System :: OS Independent",
+    ],
+)
