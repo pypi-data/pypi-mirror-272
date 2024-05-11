0482c8579b7e82fb91c26f328ebd3ecfe5459b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 06:04:16 +0000

    Add package ebb-events/0.2.0/ebb_events-0.2.0.tar.gz

---
+++ packages/ebb-events/ebb_events-0.2.0.tar.gz/ebb_events-0.2.0/ebb_events/constants.py
@@ -0,0 +1,15 @@
+TOPIC_HIERARCHY_FIELDS = [
+    "organization",
+    "system_id",
+    "event_type",
+    "subsystem_id",
+    "device_id",
+]
+
+# Payload fields
+ID = "id"
+TIME = "time"
+SOURCE = "source"
+TYPE = "type"
+DATA = "data"
+METADATA = "metadata"
