1c5dc309d533461974e75d8dac826e2ae543ea49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 04:05:32 +0000

    Add package gen-wrappers/0.2.4/gen_wrappers-0.2.4-py3-none-any.whl

---
+++ packages/gen-wrappers/gen_wrappers-0.2.4-py3-none-any.whl/creator/cmfy/request_cmfy.py
@@ -0,0 +1,118 @@
+import json
+from dataclasses import dataclass
+
+from pydantic import Field
+
+from creator.base.base_request import BaseRequest
+
+default_workflow_json = {
+    "3": {
+        "inputs": {
+            "seed": 985289875893903,
+            "steps": 20,
+            "cfg": 7.5,
+            "sampler_name": "uni_pc",
+            "scheduler": "karras",
+            "denoise": 1,
+            "model": [
+                "4",
+                0
+            ],
+            "positive": [
+                "6",
+                0
+            ],
+            "negative": [
+                "7",
+                0
+            ],
+            "latent_image": [
+                "5",
+                0
+            ]
+        },
+        "class_type": "KSampler",
+        "_meta": {
+            "title": "KSampler"
+        }
+    },
+    "4": {
+        "inputs": {
+            "ckpt_name": "sdxl/juggernautXL_v9Rundiffusionphoto2.safetensors"
+        },
+        "class_type": "CheckpointLoaderSimple",
+        "_meta": {
+            "title": "Load Checkpoint"
+        }
+    },
+    "5": {
+        "inputs": {
+            "width": 1024,
+            "height": 768,
+            "batch_size": 1
+        },
+        "class_type": "EmptyLatentImage",
+        "_meta": {
+            "title": "Empty Latent Image"
+        }
+    },
+    "6": {
+        "inputs": {
+            "text": "a photograph of a smiling green alien holding a sign which says \"Run Diffusion\", ultra realistic, street, city, sci-fi, ultra realistic, cute, friendly",
+            "clip": [
+                "4",
+                1
+            ]
+        },
+        "class_type": "CLIPTextEncode",
+        "_meta": {
+            "title": "CLIP Text Encode (Prompt)"
+        }
+    },
+    "7": {
+        "inputs": {
+            "text": "blurry, low quality",
+            "clip": [
+                "4",
+                1
+            ]
+        },
+        "class_type": "CLIPTextEncode",
+        "_meta": {
+            "title": "CLIP Text Encode (Prompt)"
+        }
+    },
+    "8": {
+        "inputs": {
+            "samples": [
+                "3",
+                0
+            ],
+            "vae": [
+                "4",
+                2
+            ]
+        },
+        "class_type": "VAEDecode",
+        "_meta": {
+            "title": "VAE Decode"
+        }
+    },
+    "9": {
+        "inputs": {
+            "filename_prefix": "ComfyUI",
+            "images": [
+                "8",
+                0
+            ]
+        },
+        "class_type": "SaveImage",
+        "_meta": {
+            "title": "Save Image"
+        }
+    }
+}
+
+
+class CmfyWorkflow(BaseRequest):
+    workflow_json: str = Field("", examples=[json.dumps(default_workflow_json)])
