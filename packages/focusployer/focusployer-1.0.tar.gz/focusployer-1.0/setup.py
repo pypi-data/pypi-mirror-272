a8532eca042a58c823660d6baa3bbb8cd20f59a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 04:05:23 +0000

    Add package focusployer/1.0/focusployer-1.0.tar.gz

---
+++ packages/focusployer/focusployer-1.0.tar.gz/focusployer-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='focusployer',
+    version='1.0',
+    packages=['focusployer'],
+    author='uni',
+    install_requires=[
+        'asyncssh',
+        'loguru'
+    ]
+)
