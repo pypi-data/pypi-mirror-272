52adc32900b41a8dedf0f43646efbc793fc0d539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 03:05:41 +0000

    Add package Deep-KAN/0.0.3/Deep-KAN-0.0.3.tar.gz

---
+++ packages/Deep-KAN/Deep-KAN-0.0.3.tar.gz/Deep-KAN-0.0.3/deepkan/__init__.py
@@ -0,0 +1,6 @@
+from .deepkan import SplineLinearLayer
+from .deepkan import DeepKAN
+from .deepkan import ChebyshevKANLayer
+from .deepkan import RBFLinear
+from .deepkan import RBFKANLayer
+from .deepkan import RBFKAN
