65709901a05773574e7c56dea2fb7ef85584d8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 04:05:27 +0000

    Add package secondtrycpp/0.1.5/secondtrycpp-0.1.5.tar.gz

---
+++ packages/secondtrycpp/secondtrycpp-0.1.5.tar.gz/secondtrycpp-0.1.5/secondtrycpp/b.py
@@ -0,0 +1,17 @@
+import ctypes
+
+# Load the shared library
+lib = ctypes.CDLL('a.so')
+
+# Define the argument and return types of the function
+lib.add.argtypes = [ctypes.c_int, ctypes.c_int]
+lib.add.restype = ctypes.c_int
+
+def add(a, b):
+    # Call the C++ function
+    return lib.add(a, b)
+
+# if __name__ == '__main__':
+#     # Test the function
+print(add(3, 5))  # Output should be 8
+
