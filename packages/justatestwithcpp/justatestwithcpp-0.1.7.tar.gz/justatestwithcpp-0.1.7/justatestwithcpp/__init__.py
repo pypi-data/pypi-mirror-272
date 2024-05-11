51ebc692a7b8e2fe4a69608ffe29f3dd791607f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 03:05:11 +0000

    Add package justatestwithcpp/0.1.7/justatestwithcpp-0.1.7.tar.gz

---
+++ packages/justatestwithcpp/justatestwithcpp-0.1.7.tar.gz/justatestwithcpp-0.1.7/justatestwithcpp/__init__.py
@@ -0,0 +1,4 @@
+from .a import sq
+# from .a import somecode
+from .a import myFunc
+from .a import b
---
+++ packages/justatestwithcpp/justatestwithcpp-0.1.7.tar.gz/justatestwithcpp-0.1.7/justatestwithcpp/a.py
@@ -0,0 +1,14 @@
+import cppimport as cpp
+
+# somecode = cpp.imp('somecode') #automatically compiles code
+
+
+b = 22
+
+
+def sq(x):
+    return somecode.square(x)
+
+
+def myFunc(x, y):
+    return x+y
