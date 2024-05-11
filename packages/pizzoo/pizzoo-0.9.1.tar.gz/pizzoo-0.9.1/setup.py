c63433991131c2a603d86759235d07a15e964e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 06:04:20 +0000

    Add package pizzoo/0.9.1/pizzoo-0.9.1.tar.gz

---
+++ packages/pizzoo/pizzoo-0.9.1.tar.gz/pizzoo-0.9.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+# https://packaging.python.org/en/latest/guides/distributing-packages-using-setuptools/
+setup(
+    name="pizzoo",
+    version="0.9.1",
+    author="Pablo Huet",
+    description="Pizzoo is a library for rendering on pixel matrix screens, with direct support for the Divoom Pixoo64 device, and easy integration for any other one. It includes full animation buffer manipulation, a micro game engine, as well as XML/HTML like templates compilation.",
+    license="MIT",
+    keywords="pixoo, pixoo64, divoom, screen, pixel, matrix, render, buffer, LED matrix, LED, raspberry, raspberry pi",
+    url="https://github.com/pabletos/pizzoo#readme",
+    packages=['pizzoo'],
+    install_requires=[
+        'requests ~= 2.31.0',
+        'Pillow ~= 10.0.0',
+		'bdfparser ~= 2.2.0'
+    ],
+	classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Multimedia :: Graphics",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
