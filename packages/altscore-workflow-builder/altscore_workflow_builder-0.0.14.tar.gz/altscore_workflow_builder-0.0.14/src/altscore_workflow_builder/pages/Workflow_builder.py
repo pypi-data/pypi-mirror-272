b352dd8a13257a93daca4186cd7a4fe6179335c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 14:04:08 +0000

    Add package altscore-workflow-builder/0.0.14/altscore_workflow_builder-0.0.14.tar.gz

---
+++ packages/altscore-workflow-builder/altscore_workflow_builder-0.0.14.tar.gz/altscore_workflow_builder-0.0.14/src/altscore_workflow_builder/pages/Workflow_builder.py
@@ -0,0 +1,101 @@
+import streamlit as st
+from altscore_workflow_builder.utils import list_workflows, load_workflow_definition, load_task_definitions, \
+    save_workflow_definition, save_task_definitions, determine_levels, add_item, remove_item
+from altscore_workflow_builder.workflow import task_instance_dropdown, task_instance_graph
+from altscore_workflow_builder.task import task_graph
+from altscore_workflow_builder.utils import hide_deploy_button
+from streamlit_agraph import agraph, Node, Edge, Config
+import json
+
+st.set_page_config(layout="wide")
+hide_deploy_button()
+st.sidebar.header("Graph Configuration")
+workflow = st.sidebar.selectbox("Select Workflow", list_workflows())
+flow_definition = load_workflow_definition(workflow['alias'], workflow['version'])
+task_definitions = load_task_definitions()
+
+# Create nodes, edges, and levels for the agraph
+nodes = []
+edges = []
+task_nodes = flow_definition["task_instances"]
+levels, level_spacing = determine_levels(task_nodes)
+all_task_names = list(task_nodes.keys())
+
+for task_name, task_info in flow_definition["task_instances"].items():
+    inputs = ", ".join([inp['alias'] for inp in task_definitions.get(task_info['type'], {}).get('inputs', [])])
+    outputs = ", ".join([out['alias'] for out in task_definitions.get(task_info['type'], {}).get('outputs', [])])
+    label = f"{task_name}"
+    tooltip = f"Inputs: {inputs}\nOutputs: {outputs}"
+    level = levels[task_name]
+    nodes.append(
+        Node(id=task_name, label=label, color="lightblue", size=50, x=level_spacing[level].pop(0), y=level * 150,
+             title=tooltip))
+
+    for next_task in task_info.get("to", []):
+        edges.append(Edge(source=task_name, target=next_task, type="STRAIGHT"))
+
+# Configuration for agraph
+config = Config(
+    height=900,
+    width='100%',
+    directed=True,
+    nodeHighlightBehavior=True,
+    highlightColor='#88c999',
+    collapsible=True,
+    node={'labelProperty': 'label', 'font_size': 20},
+    link={'labelProperty': 'label', 'renderLabel': True},
+    staticGraph=True,
+    physics={
+        "solver": 'barnesHut',
+        "hierarchical": False
+    }
+)
+
+# Display the graph
+selection = agraph(nodes=nodes, edges=edges, config=config)
+
+if selection:
+    st.sidebar.write(f"Selected task: {selection}")
+    task_info = task_nodes[selection]
+    selected_task = st.sidebar.selectbox("Select Task", all_task_names, index=all_task_names.index(selection))
+    if selected_task:
+        st.sidebar.write(f"Editing: {selected_task}")
+        task_info = task_nodes[selected_task]
+        task_details = task_definitions.get(task_info['type'], {})
+
+        # Inputs, Outputs, Overrides, Conversions management
+        for detail_key in ['inputs', 'outputs', 'overrides', 'conversions']:
+            # Add a Title
+            st.sidebar.title(f"{detail_key.capitalize()} Management")
+            st.sidebar.json(task_details.get(detail_key, []))
+
+            if detail_key in ['overrides', 'conversions']:
+                key_name = st.sidebar.text_input(f"Key for {detail_key[:-1]}", key=f"{detail_key}_key")
+                value_name = st.sidebar.text_input(f"Value for {detail_key[:-1]}", key=f"{detail_key}_value")
+                item_details = {"key": key_name, "value": value_name}
+                button_label = f"Add new {detail_key[:-1]}"
+                if st.sidebar.button(button_label):
+                    add_item(task_details, detail_key, item_details, task_definitions, selected_task, is_key_value=True)
+            else:
+                alias_name = st.sidebar.text_input(f"Alias for {detail_key[:-1]}", key=f"{detail_key}_alias")
+                item_details = {"alias": alias_name}
+                button_label = f"Add new {detail_key[:-1]}"
+                if st.sidebar.button(button_label):
+                    add_item(task_details, detail_key, item_details, task_definitions, selected_task)
+
+            # Deletion interface remains largely the same
+            aliases = [item['alias'] if 'alias' in item else f"{item['key']}:{item['value']}" for item in
+                       task_details.get(detail_key, [])]
+            item_to_remove = st.sidebar.selectbox(f"Select {detail_key[:-1]} to remove", aliases,
+                                                  key=f"{detail_key}_remove")
+            if st.sidebar.button(f"Remove selected {detail_key[:-1]}"):
+                if detail_key in ['overrides', 'conversions']:
+                    task_details[detail_key] = [item for item in task_details[detail_key] if
+                                                f"{item['key']}:{item['value']}" != item_to_remove]
+                else:
+                    task_details[detail_key] = [item for item in task_details[detail_key] if
+                                                item['alias'] != item_to_remove]
+                task_definitions[selected_task] = task_details
+                save_task_definitions(task_definitions)
+                st.sidebar.success(f"{detail_key[:-1].capitalize()} removed successfully!")
+                st.rerun()
---
+++ packages/altscore-workflow-builder/altscore_workflow_builder-0.0.14.tar.gz/altscore_workflow_builder-0.0.14/src/altscore_workflow_builder/utils.py
@@ -0,0 +1,315 @@
+import os
+import json
+from decouple import config
+from pathlib import Path
+import streamlit as st
+
+
+def hide_deploy_button():
+    hide_deploy_button_css = """
+    <style>
+        /* Replace .element-class with the actual class name or ID of the deploy button */
+        .stDeployButton{
+            display: none;
+        }
+    </style>
+    """
+    # Inject the CSS with the markdown component
+    st.markdown(hide_deploy_button_css, unsafe_allow_html=True)
+
+
+def load_task_definitions():
+    # plus native_configuration
+    with open(Path(config("PROJECT_ROOT")) / "app" / "tasks" / "task_definitions.json") as f:
+        custom_configuration = json.load(f)
+    return {**native_configuration, **custom_configuration}
+
+
+def save_task_definitions(task_definitions):
+    with open(Path(config("PROJECT_ROOT")) / "app" / "tasks" / "task_definitions.json", "w") as f:
+        json.dump(task_definitions, f)
+
+
+def load_workflow_definition(workflow_alias: str, workflow_version: str):
+    with open(Path(config(
+            "PROJECT_ROOT")) / "app" / "workflows" / f"{workflow_alias}_{workflow_version}/flow_definition.json") as f:
+        return json.load(f)
+
+
+def save_workflow_definition(workflow_alias: str, workflow_version: str, flow_definition: dict):
+    with open(Path(config(
+            "PROJECT_ROOT")) / "app" / "workflows" / f"{workflow_alias}_{workflow_version}/flow_definition.json",
+              "w") as f:
+        json.dump(flow_definition, f)
+
+
+def list_workflows():
+    workflows = [f for f in os.listdir(Path(config("PROJECT_ROOT")) / "app" / "workflows") if
+                 os.path.isdir(Path(config("PROJECT_ROOT")) / "app" / "workflows" / f)]
+    return [
+        {"alias": workflow.split("_")[0], "version": workflow.split("_")[-1], "label": workflow} for workflow in
+        workflows
+    ]
+
+
+def determine_levels(task_nodes):
+    levels = {}
+    max_width = 1500  # Define the maximum width of the graph
+    level_spacing = {}
+    no_incoming = set(task_nodes.keys())
+
+    # Find tasks with no incoming edges (start nodes)
+    for task, info in task_nodes.items():
+        for next_task in info.get("to", []):
+            if next_task in no_incoming:
+                no_incoming.remove(next_task)
+
+    # Assign levels using a queue
+    queue = [(task, 0) for task in no_incoming]
+    while queue:
+        current, level = queue.pop(0)
+        if current not in levels:
+            levels[current] = level
+        for next_task in task_nodes[current].get("to", []):
+            if next_task not in levels or level + 1 > levels[next_task]:
+                levels[next_task] = level + 1
+                queue.append((next_task, level + 1))
+
+    # Calculate spacing for each level
+    for level in set(levels.values()):
+        level_tasks = [task for task, lvl in levels.items() if lvl == level]
+        width_per_task = max_width / max(1, len(level_tasks))
+        level_spacing[level] = [width_per_task * (i + 0.5) for i in range(len(level_tasks))]
+
+    return levels, level_spacing
+
+
+def add_item(task_details, key, item_details, task_definitions, selected_task, is_key_value=False):
+    """Add an item (input/output/override/conversion) to the task details."""
+    if is_key_value:
+        if all(item_details.values()):  # Check if both key and value are provided
+            task_details[key].append(item_details)
+            task_definitions[selected_task] = task_details
+            save_task_definitions(task_definitions)
+            st.sidebar.success(f"New {key[:-1]} added successfully!")
+        else:
+            st.sidebar.error(f"Both key and value are required for {key[:-1]}.")
+    else:
+        if item_details['alias']:
+            task_details[key].append(item_details)
+            task_definitions[selected_task] = task_details
+            save_task_definitions(task_definitions)
+            st.sidebar.success(f"New {key[:-1]} added successfully!")
+        else:
+            st.sidebar.error(f"{key[:-1].capitalize()} alias cannot be empty.")
+    st.rerun()
+
+
+def remove_item(task_details, key, alias_to_remove, task_definitions, selected_task):
+    """Remove an item (input/output) from the task details."""
+    task_details[key] = [item for item in task_details[key] if item['alias'] != alias_to_remove]
+    task_definitions[selected_task] = task_details
+    save_task_definitions(task_definitions)
+    st.sidebar.success(f"{key[:-1].capitalize()} removed successfully!")
+    st.rerun()
+
+
+native_configuration = {
+    "workflow_args": {
+        "name": "workflow_args",
+        "custom_function": "",
+        "inputs": [
+            {
+                "alias": "arguments"
+            }
+        ],
+        "outputs": [],
+    },
+    "get_borrower_authorization": {
+        "name": "get_borrower_authorization",
+        "custom_function": "",
+        "inputs": [
+            {
+                "alias": "key",
+                "hint": "authorization key",
+            },
+            {
+                "alias": "ensure",
+                "hint": "ensure an authorization exists",
+            },
+            {
+                "alias": "signed",
+                "hint": "ensure there is a signed authorization",
+            },
+            {
+                "alias": "borrower_id",
+            },
+        ],
+        "outputs": [
+            {
+                "alias": "authorization",
+                "hint": "aliased authorization dict",
+            },
+        ],
+    },
+    "persona_source": {
+        "name": "persona_source",
+        "custom_function": "",
+        "inputs": [
+            {
+                "alias": "source_id",
+                "hint": "source id",
+            },
+            {
+                "alias": "source_version",
+                "hint": "source version",
+            },
+            {
+                "alias": "persona_info",
+                "hint": "persona info",
+            },
+            {
+                "alias": "authorization_reference",
+                "hint": "authorization id",
+                "optional": True,
+            },
+            {
+                "alias": "override_keys",
+                "hint": "dictionary specifying custom keys",
+                "optional": True,
+            },
+            {
+                "alias": "sandbox_version",
+                "hint": "sandbox version",
+                "optional": True,
+            },
+        ],
+        "outputs": [
+            {
+                "alias": "source_output_package",
+                "hint": "source output package",
+            },
+        ],
+    },
+    "evaluate_with_evaluator": {
+        "name": "evaluate_with_evaluator",
+        "custom_function": "",
+        "inputs": [
+            {
+                "alias": "evaluator_alias",
+                "hint": "evaluator alias",
+            },
+            {
+                "alias": "evaluator_version",
+                "hint": "evaluator version",
+            },
+            {
+                "alias": "evaluation_instance",
+                "hint": "evaluation instance",
+            },
+            {
+                "alias": "evaluation_contacts",
+                "hint": "evaluation contacts",
+            },
+        ],
+        "outputs": [
+            {
+                "alias": "evaluator_output",
+                "hint": "evaluator output",
+            },
+        ],
+    },
+    "borrower_data_enrichment": {
+        "name": "borrower_data_enrichment",
+        "custom_function": "",
+        "inputs": [
+            {
+                "alias": "borrower_id",
+                "hint": "borrower id",
+            },
+            {
+                "alias": "input_keys",
+                "hint": "input keys",
+            },
+            {
+                "alias": "timeout",
+                "hint": "timeout",
+                "optional": True,
+            },
+            {
+                "alias": "sources_config",
+                "hint": "sources config",
+            },
+            {
+                "alias": "conditions_met",
+                "hint": "conditions met",
+            }
+        ],
+        "outputs": [
+            {
+                "alias": "sources_output_packages",
+                "hint": "sources output packages",
+            },
+        ],
+    },
+    "sat_data_extraction": {
+        "name": "sat_data_extraction",
+        "custom_function": "",
+        "inputs": [
+            {
+                "alias": "rfc",
+                "hint": "RFC of the borrower",
+            },
+            {
+                "alias": "date_to_analyze",
+                "hint": "Date to analyze the SAT data",
+            },
+            {
+                "alias": "days_of_tolerance",
+                "hint": "Days of tolerance to check for coverage",
+            },
+        ],
+        "outputs": [
+            {
+                "alias": "has_coverage",
+                "hint": "indicator that the borrower has coverage",
+            },
+            {
+                "alias": "extraction_running",
+                "hint": "indicator that the extraction is running",
+            }
+        ],
+    },
+    "wait_for_condition": {
+        "name": "wait_for_condition",
+        "custom_function": "",
+        "inputs": [
+            {
+                "alias": "value",
+                "hint": "value to evaluate the conditions",
+            },
+            {
+                "alias": "should_be",
+                "hint": "eq, ne, gt, lt, ge, le",
+            },
+            {
+                "alias": "target",
+                "hint": "target to compare with",
+            },
+            {
+                "alias": "deadline",
+                "hint": "datetime of the deadline to wait for",
+            },
+            {
+                "alias": "abort_on_wait",
+                "hint": "Prevent rest of workflow to run until condition is met",
+            }
+        ],
+        "outputs": [
+            {
+                "alias": "w_schedule_callback",
+                "hint": "indicator that the callback should be scheduled",
+            },
+        ],
+    },
+}
