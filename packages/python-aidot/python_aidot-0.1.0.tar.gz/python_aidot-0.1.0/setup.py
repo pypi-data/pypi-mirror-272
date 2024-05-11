457720eefb381c32b963d8d5d14c027ab2614191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 15:04:23 +0000

    Add package python-aidot/0.1.0/python_aidot-0.1.0.tar.gz

---
+++ packages/python-aidot/python_aidot-0.1.0.tar.gz/python_aidot-0.1.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-aidot",
+    version="0.1.0",
+    author="aidotdev2024",
+    description="aidot control wifi lights",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests",
+        "aiohttp",
+        "setuptools",
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
