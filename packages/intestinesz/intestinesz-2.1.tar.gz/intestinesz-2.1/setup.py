713c46a49c05f9f77c183f4480cc32d4acad162c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 14:04:13 +0000

    Add package intestinesz/2.1/intestinesz-2.1.tar.gz

---
+++ packages/intestinesz/intestinesz-2.1.tar.gz/intestinesz-2.1/setup.py
@@ -0,0 +1,46 @@
+from distutils.core import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+
+setup(
+    name = 'intestinesz',
+    packages = ['intestinesz'],
+    version = '2.1',
+    license='MIT',
+    description = 'intestinesz',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author = 'Mirukutea',
+    author_email = 'tea@masrizky.com',
+    url = 'https://github.com/mirukutea/intestinesz',
+    download_url = 'https://github.com/mirukutea/intestinesz/archive/v_01.tar.gz',
+    project_urls={
+        'Documentation': 'https://github.com/mirukutea/intestinesz',
+        'Funding': 'https://donate.pypi.org',
+        'Say Thanks!': 'http://saythanks.io/to/example',
+        'Source': 'https://github.com/mirukutea/intestinesz/',
+        'Tracker': 'https://github.com/mirukutea/intestinesz/issues',
+    },
+    keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],
+    install_requires=[
+            'validators',
+            'beautifulsoup4',
+            'discordautochat',
+            'discorudo',
+        ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+     dependencies= [
+        'discordautochat',
+        'discorudo'
+    ]
+)
