78743793b041f78107c9c75bb94d5c0825d0de37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 05:06:55 +0000

    Add package SigProfilerExtractor/1.1.24/SigProfilerExtractor-1.1.24-py3-none-any.whl

---
+++ packages/SigProfilerExtractor/SigProfilerExtractor-1.1.24-py3-none-any.whl/SigProfilerExtractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
+short_version = '1.1.24'
+version = '1.1.24'
+Update = 'Upgrade v1.1.24: Add input processing to SigProfilerExtractor'
+    
+    
