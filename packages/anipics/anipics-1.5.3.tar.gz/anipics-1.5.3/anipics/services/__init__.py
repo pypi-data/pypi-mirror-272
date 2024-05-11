69a3b9dec550c908c6f24602da747a20c7190312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 06:04:36 +0000

    Add package anipics/1.5.3/anipics-1.5.3.tar.gz

---
+++ packages/anipics/anipics-1.5.3.tar.gz/anipics-1.5.3/anipics/services/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2024 Daniel <D4n13l3k00@yandex.ru>.
+# SPDX-License-Identifier: AGPL-3.0
+
+__all__ = ["AnimePicsX", "NekosLife", "WaifuPics"]
+
+from .animepicsx import AnimePicsX
+from .nekoslife import NekosLife
+from .waifupics import WaifuPics
