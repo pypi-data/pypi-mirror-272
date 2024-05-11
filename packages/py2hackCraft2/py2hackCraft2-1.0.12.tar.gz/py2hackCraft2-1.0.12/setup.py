052f2fdce01e81c88a5d9cc2759396655990b837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 15:04:33 +0000

    Add package py2hackCraft2/1.0.12/py2hackCraft2-1.0.12.tar.gz

---
+++ packages/py2hackCraft2/py2hackCraft2-1.0.12.tar.gz/py2hackCraft2-1.0.12/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py2hackCraft2",
+    version="1.0.12",
+    packages=find_packages(),
+    install_requires=[
+        "websocket-client"  # websocketライブラリの追加
+        # 他にも依存する外部ライブラリがあれば、同様にリストに追加
+    ],
+    # その他のメタデータ
+    author="Masafumi Terazono",
+    author_email="masafumi_t@0x48lab.com",
+    description="These are APIs that connect to the hackCraft2 server from Python to manipulate pets.",
+    readme = "README.md",
+    license="MIT",
+    keywords="hackCraft2",
+    url="https://pypi.org/project/py2hackCraft2/",   # プロジェクトホームページのURL
+)
