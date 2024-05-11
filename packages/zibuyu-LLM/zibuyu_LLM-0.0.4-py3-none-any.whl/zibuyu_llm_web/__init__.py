a97eab33d96877c685038cc8484e4f48ce8586a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 15:04:34 +0000

    Add package zibuyu-LLM/0.0.4/zibuyu_LLM-0.0.4-py3-none-any.whl

---
+++ packages/zibuyu-LLM/zibuyu_LLM-0.0.4-py3-none-any.whl/zibuyu_llm_web/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+--------------------------------------------
+project: zibuyu_LLM
+author: 子不语
+date: 2024/5/9
+contact: 【公众号】思维兵工厂
+description: 
+--------------------------------------------
+"""
+
+from .qwen import QwenWeb
+from .xinchen import XinChenWeb
+from .xunfei import XunFeiWeb
+from .kimi import KimiWeb
+from .minmax import MinMaxWeb
+from .deepseek import DeepSeekWeb
+from .baichuan import BaiChuanWeb
+from .wanxiang import WanXiangWeb
+
+
+__all__ = [
+    'QwenWeb',
+    'XinChenWeb',
+    'XunFeiWeb',
+    'KimiWeb',
+    'MinMaxWeb',
+    'DeepSeekWeb',
+    'BaiChuanWeb',
+    'WanXiangWeb',
+]
