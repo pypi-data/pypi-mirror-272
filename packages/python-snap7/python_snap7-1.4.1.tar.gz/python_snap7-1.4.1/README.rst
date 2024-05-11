0ef4be408ed94f0e9fc4f95725854aa9f12c43cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 04:05:28 +0000

    Add package python-snap7/1.4.1/python_snap7-1.4.1.tar.gz

---
+++ packages/python-snap7/python_snap7-1.4.1.tar.gz/python_snap7-1.4.1/README.rst
@@ -0,0 +1,21 @@
+About
+=====
+
+This is a ctypes-based Python wrapper for snap7. Snap7 is an open-source,
+32/64 bit, multi-platform Ethernet communication suite for interfacing natively
+with Siemens S7 PLCs.
+
+Python-snap7 is tested with Python 3.9+, on Windows, Linux and OS X.
+
+The full documentation is available on `Read The Docs <https://python-snap7.readthedocs.io/en/latest/>`_.
+
+
+Installation
+============
+
+If you are running Windows, Mac OS X or GNU/Linux on an Intel x64 or ARM 64 compatible platform you can use the binary wheel installation::
+
+   $ pip install python-snap7
+
+
+Otherwise, please read the `online installation documentation <https://python-snap7.readthedocs.io/en/latest/installation.html>`_.
---
+++ packages/python-snap7/python_snap7-1.4.1.tar.gz/python_snap7-1.4.1/tests/test_server.py
@@ -0,0 +1,181 @@
+import ctypes
+import gc
+import logging
+import pytest
+import unittest
+from unittest import mock
+
+from snap7.common import error_text
+from snap7.error import server_errors
+from snap7.server import Server
+from snap7.types import SrvEvent, mkEvent, mkLog, srvAreaDB, LocalPort, WorkInterval, MaxClients, RemotePort
+
+logging.basicConfig(level=logging.WARNING)
+
+
+@pytest.mark.server
+class TestServer(unittest.TestCase):
+    def setUp(self):
+        self.server = Server()
+        self.server.start(tcpport=1102)
+
+    def tearDown(self):
+        self.server.stop()
+        self.server.destroy()
+
+    def test_register_area(self):
+        db1_type = ctypes.c_char * 1024
+        self.server.register_area(srvAreaDB, 3, db1_type())
+
+    def test_error(self):
+        for error in server_errors:
+            error_text(error, context="client")
+
+    def test_event(self):
+        event = SrvEvent()
+        self.server.event_text(event)
+
+    def test_get_status(self):
+        server, cpu, num_clients = self.server.get_status()
+
+    def test_get_mask(self):
+        self.server.get_mask(mkEvent)
+        self.server.get_mask(mkLog)
+        # invalid kind
+        self.assertRaises(Exception, self.server.get_mask, 3)
+
+    def test_lock_area(self):
+        from threading import Thread
+
+        area_code = srvAreaDB
+        index = 1
+        db1_type = ctypes.c_char * 1024
+        # we need to register first
+        self.server.register_area(area_code, index, db1_type())
+        self.server.lock_area(code=area_code, index=index)
+
+        def second_locker():
+            self.server.lock_area(code=area_code, index=index)
+            self.server.unlock_area(code=area_code, index=index)
+
+        thread = Thread(target=second_locker)
+        thread.daemon = True
+        thread.start()
+        thread.join(timeout=1)
+        self.assertTrue(thread.is_alive())
+        self.server.unlock_area(code=area_code, index=index)
+        thread.join(timeout=1)
+        self.assertFalse(thread.is_alive())
+
+    def test_set_cpu_status(self):
+        self.server.set_cpu_status(0)
+        self.server.set_cpu_status(4)
+        self.server.set_cpu_status(8)
+        self.assertRaises(ValueError, self.server.set_cpu_status, -1)
+
+    def test_set_mask(self):
+        self.server.set_mask(kind=mkEvent, mask=10)
+
+    def test_unlock_area(self):
+        area_code = srvAreaDB
+        index = 1
+        db1_type = ctypes.c_char * 1024
+
+        # we need to register first
+        self.assertRaises(Exception, self.server.lock_area, area_code, index)
+
+        self.server.register_area(area_code, index, db1_type())
+        self.server.lock_area(area_code, index)
+        self.server.unlock_area(area_code, index)
+
+    def test_unregister_area(self):
+        area_code = srvAreaDB
+        index = 1
+        db1_type = ctypes.c_char * 1024
+        self.server.register_area(area_code, index, db1_type())
+        self.server.unregister_area(area_code, index)
+
+    def test_events_callback(self):
+        def event_call_back(event):
+            logging.debug(event)
+
+        self.server.set_events_callback(event_call_back)
+
+    def test_read_events_callback(self):
+        def read_events_call_back(event):
+            logging.debug(event)
+
+        self.server.set_read_events_callback(read_events_call_back)
+
+    def test_pick_event(self):
+        event = self.server.pick_event()
+        self.assertEqual(type(event), SrvEvent)
+        event = self.server.pick_event()
+        self.assertFalse(event)
+
+    def test_clear_events(self):
+        self.server.clear_events()
+        self.assertFalse(self.server.clear_events())
+
+    def test_start_to(self):
+        self.server.start_to("0.0.0.0")  # noqa: S104
+        self.assertRaises(ValueError, self.server.start_to, "bogus")
+
+    def test_get_param(self):
+        # check the defaults
+        self.assertEqual(self.server.get_param(LocalPort), 1102)
+        self.assertEqual(self.server.get_param(WorkInterval), 100)
+        self.assertEqual(self.server.get_param(MaxClients), 1024)
+
+        # invalid param for server
+        self.assertRaises(Exception, self.server.get_param, RemotePort)
+
+
+@pytest.mark.server
+class TestServerBeforeStart(unittest.TestCase):
+    """
+    Tests for server before it is started
+    """
+
+    def setUp(self):
+        self.server = Server()
+
+    def test_set_param(self):
+        self.server.set_param(LocalPort, 1102)
+
+
+@pytest.mark.server
+class TestLibraryIntegration(unittest.TestCase):
+    def setUp(self):
+        # replace the function load_library with a mock
+        self.loadlib_patch = mock.patch("snap7.server.load_library")
+        self.loadlib_func = self.loadlib_patch.start()
+
+        # have load_library return another mock
+        self.mocklib = mock.MagicMock()
+        self.loadlib_func.return_value = self.mocklib
+
+        # have the Srv_Create of the mock return None
+        self.mocklib.Srv_Create.return_value = None
+        self.mocklib.Srv_Destroy.return_value = None
+
+    def tearDown(self):
+        # restore load_library
+        self.loadlib_patch.stop()
+
+    def test_create(self):
+        server = Server(log=False)
+        del server
+        gc.collect()
+        self.mocklib.Srv_Create.assert_called_once()
+
+    def test_context_manager(self):
+        with Server(log=False) as _:
+            pass
+
+
+if __name__ == "__main__":
+    import logging
+
+    logging.basicConfig()
+    unittest.main()
