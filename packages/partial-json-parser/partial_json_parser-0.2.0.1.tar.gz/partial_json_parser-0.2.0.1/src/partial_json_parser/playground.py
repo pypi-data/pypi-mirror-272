a34b1b099566c3a8268d2881db9d2fff003bc35b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 04:05:05 +0000

    Add package partial-json-parser/0.2.0.1/partial_json_parser-0.2.0.1.tar.gz

---
+++ packages/partial-json-parser/partial_json_parser-0.2.0.1.tar.gz/partial_json_parser-0.2.0.1/src/partial_json_parser/playground.py
@@ -0,0 +1,32 @@
+from rich.console import Console
+from rich.highlighter import JSONHighlighter
+from rich.style import Style
+from rich.text import Span
+
+from partial_json_parser import ALL, PartialJSON, complete_any
+
+console = Console()
+highlight = JSONHighlighter()
+
+
+def main():
+    while True:
+        try:
+            input_str = console.input("[d]>>> ")
+            result = complete_any(input_str, ALL, is_top_level=True)
+            if result is False:
+                raise PartialJSON
+            index, completion = result
+            json = input_str if completion is True else input_str[:index] + completion
+
+            rich_text = highlight(json)
+            if completion is not True and completion:
+                rich_text.spans.append(Span(index, len(json), Style(dim=True)))
+
+            console.print(" " * 3, rich_text)
+
+        except KeyboardInterrupt:
+            return
+        except Exception as err:
+            console.print(f"{err.__class__.__name__}:", style="bold red", highlight=False, end=" ")
+            console.print(" ".join(map(str, err.args)), style="yellow", highlight=False)
