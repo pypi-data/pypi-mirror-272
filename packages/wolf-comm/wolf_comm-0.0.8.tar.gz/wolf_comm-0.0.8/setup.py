d09b9ec0ab1f3379c9badf0d10460a5e51cd37f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 14:04:09 +0000

    Add package wolf-comm/0.0.8/wolf_comm-0.0.8.tar.gz

---
+++ packages/wolf-comm/wolf_comm-0.0.8.tar.gz/wolf_comm-0.0.8/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='wolf_comm',
+    version='0.0.8',
+    author="Jan Rothkegel",
+    author_email="jan.rothkegel@web.de",
+    description="A package to communicate with Wolf SmartSet Cloud",
+    long_description=long_description,
+    package_data={"wolf_comm": ["py.typed"]},
+    long_description_content_type="text/markdown",
+    url="https://github.com/janrothkegel/wolf-comm",
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'httpx',
+        'lxml',
+        'pkce',
+        'shortuuid'
+    ]
+)
