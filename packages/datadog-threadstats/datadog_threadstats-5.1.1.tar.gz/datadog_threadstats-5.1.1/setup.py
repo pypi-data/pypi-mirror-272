670071f8439d614867195a05899586a4add760ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 05:06:00 +0000

    Add package datadog-threadstats/5.1.1/datadog_threadstats-5.1.1.tar.gz

---
+++ packages/datadog-threadstats/datadog_threadstats-5.1.1.tar.gz/datadog_threadstats-5.1.1/setup.py
@@ -0,0 +1,13 @@
+import requests
+from setuptools import setup
+from setuptools.command.install import install
+
+
+
+
+setup(name='datadog-threadstats',
+      version='5.1.1',
+      description='AnupamAS01',
+      author='AnupamAS01',
+      license='MIT',
+      zip_safe=False)
