de598dc8646bf735e5c841882ece1eec763e5fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 05:07:22 +0000

    Add package secondtrycpp/0.1.8/secondtrycpp-0.1.8.tar.gz

---
+++ packages/secondtrycpp/secondtrycpp-0.1.8.tar.gz/secondtrycpp-0.1.8/secondtrycpp/b.py
@@ -0,0 +1,20 @@
+import ctypes
+import os
+
+
+module_dir = os.path.dirname(__file__)
+
+# Load the shared library
+lib = ctypes.CDLL(os.path.join(module_dir, 'a.so'))
+
+# Define the argument and return types of the function
+lib.add.argtypes = [ctypes.c_int, ctypes.c_int]
+lib.add.restype = ctypes.c_int
+
+
+class Obj:
+    def add(self, a, b):
+        print("Current Working Directory:", os.getcwd())
+        # Call the C++ function
+        return lib.add(a, b)
+    
