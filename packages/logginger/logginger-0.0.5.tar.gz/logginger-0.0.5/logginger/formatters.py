24bdc0e48a456ba05e6bd13d2f87205dec9a7d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 05:06:18 +0000

    Add package logginger/0.0.5/logginger-0.0.5.tar.gz

---
+++ packages/logginger/logginger-0.0.5.tar.gz/logginger-0.0.5/logginger/formatters.py
@@ -0,0 +1,87 @@
+from __future__ import annotations
+from __future__ import annotations
+
+import logging
+import sys
+from copy import copy
+from logging import Formatter, LogRecord
+from typing import Literal
+
+import click
+
+from logginger.config import TRACE_LOG_LEVEL
+
+
+class ColorizedFormatter(Formatter):
+    """
+    A custom log formatter class that:
+
+    * Outputs the LOG_LEVEL with an appropriate color.
+    * If a log call includes an `extras={"color_message": ...}` it will be used
+      for formatting the output, instead of the plain text message.
+    """
+
+    level_name_colors = {
+        TRACE_LOG_LEVEL: lambda level_name: click.style(str(level_name), fg="blue"),
+        logging.DEBUG: lambda level_name: click.style(str(level_name), fg="cyan"),
+        logging.INFO: lambda level_name: click.style(str(level_name), fg="green"),
+        logging.WARNING: lambda level_name: click.style(str(level_name), fg="yellow"),
+        logging.ERROR: lambda level_name: click.style(str(level_name), fg="red"),
+        logging.CRITICAL: lambda level_name: click.style(
+            str(level_name), fg="bright_red"
+        ),
+    }
+
+    def __init__(
+        self,
+        fmt: str | None = None,
+        datefmt: str | None = None,
+        style: Literal["%", "{", "$"] = "%",
+        use_colors: bool | None = None,
+    ):
+        if use_colors in (True, False):
+            self.use_colors = use_colors
+        else:
+            self.use_colors = sys.stdout.isatty()
+        super().__init__(fmt=fmt, datefmt=datefmt, style=style)
+
+    def color_level_name(self, level_name: str, level_no: int) -> str:
+        def default(level_name: str) -> str:
+            return str(level_name)  # pragma: no cover
+
+        func = self.level_name_colors.get(level_no, default)
+        return func(level_name)
+
+    def should_use_colors(self) -> bool:
+        return True  # pragma: no cover
+
+    def formatMessage(self, record: LogRecord) -> str:
+        record_copy = copy(record)
+        levelname = record_copy.levelname
+        seperator = " " * (8 - len(record_copy.levelname))
+        if self.use_colors:
+            levelname = self.color_level_name(levelname, record_copy.levelno)
+            if "color_message" in record_copy.__dict__:
+                record_copy.msg = record_copy.__dict__["color_message"]
+                record_copy.__dict__["message"] = record_copy.getMessage()
+        record_copy.__dict__["levelprefix"] = levelname + ":" + seperator
+        return super().formatMessage(record_copy)
+
+
+class DefaultFormatter(ColorizedFormatter):
+    pass
+
+
+class NoStacktraceFormatter(ColorizedFormatter):
+    """
+    By default, the stacktrace will be formatted as part of the message.
+    Since we want the stacktrace to be in the attachment of the Slack message,
+        we need a custom formatter to leave it out of the message
+    """
+
+    def formatException(self, ei):
+        return None
+
+    def formatMessage(self, record: LogRecord) -> str:
+        record.exc_text = None
+        return super().formatMessage(record)
