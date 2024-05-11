c188c542eb9d85638e90d31e201ad2d7118d6b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 04:04:50 +0000

    Add package layerborn.cdk-ami-builder/0.0.6/layerborn.cdk-ami-builder-0.0.6.tar.gz

---
+++ packages/layerborn.cdk-ami-builder/layerborn.cdk-ami-builder-0.0.6.tar.gz/layerborn.cdk-ami-builder-0.0.6/src/layerborn/cdk_ami_builder/_jsii/__init__.py
@@ -0,0 +1,31 @@
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+import layerborn.cdk_iam_policy_builder_helper._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@layerborn/cdk-ami-builder",
+    "0.0.6",
+    __name__[0:-6],
+    "cdk-ami-builder@0.0.6.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
