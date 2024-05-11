ee9303835af0c9d4756b7e59b31253a600297c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 03:04:52 +0000

    Add package fortls/3.0.0rc6/fortls-3.0.0rc6.tar.gz

---
+++ packages/fortls/fortls-3.0.0rc6.tar.gz/fortls-3.0.0rc6/test/test_parser.py
@@ -0,0 +1,41 @@
+from setup_tests import test_dir
+
+from fortls.parsers.internal.parser import FortranFile
+
+
+def test_line_continuations():
+    file_path = test_dir / "parse" / "line_continuations.f90"
+    file = FortranFile(str(file_path))
+    err_str, _ = file.load_from_disk()
+    assert err_str is None
+    try:
+        file.parse()
+        assert True
+    except Exception as e:
+        print(e)
+        assert False
+
+
+def test_submodule():
+    file_path = test_dir / "parse" / "submodule.f90"
+    file = FortranFile(str(file_path))
+    err_str, _ = file.load_from_disk()
+    assert err_str is None
+    try:
+        ast = file.parse()
+        assert True
+        assert ast.scope_list[0].name == "val"
+        assert ast.scope_list[0].ancestor_name == "p1"
+        assert ast.scope_list[1].name == ""
+        assert ast.scope_list[1].ancestor_name == "p2"
+    except Exception as e:
+        print(e)
+        assert False
+
+
+def test_private_visibility_interfaces():
+    file_path = test_dir / "vis" / "private.f90"
+    file = FortranFile(str(file_path))
+    err_str, _ = file.load_from_disk()
+    file.parse()
+    assert err_str is None
