5e537e60af37dd96eb1d57035bb0298536aef47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 05:06:18 +0000

    Add package calculator-api/1.6.0/calculator_api-1.6.0.tar.gz

---
+++ packages/calculator-api/calculator_api-1.6.0.tar.gz/calculator_api-1.6.0/calculator_api/src/calculator_utils.py
@@ -0,0 +1,14 @@
+from typing import Union
+
+
+def make_addition(first_element: Union[int, float], second_element: Union[int, float]) -> Union[int, float]:
+    """Addition for calculator
+
+    Args:
+        first_element (Union[int, float]): first element of addition
+        second_element (Union[int, float]): second element of addition
+
+    Returns:
+        Union[int, float]: result of addition
+    """
+    return first_element + second_element
