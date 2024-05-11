57f6ddd7f288fd70d22c8fc06a520695d8acd479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 05:06:14 +0000

    Add package increase/0.61.0/increase-0.61.0.tar.gz

---
+++ packages/increase/increase-0.61.0.tar.gz/increase-0.61.0/src/increase/types/account_transfer.py
@@ -0,0 +1,127 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+from datetime import datetime
+from typing_extensions import Literal
+
+from .._models import BaseModel
+
+__all__ = ["AccountTransfer", "Approval", "Cancellation"]
+
+
+class Approval(BaseModel):
+    approved_at: datetime
+    """
+    The [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601) date and time at which
+    the transfer was approved.
+    """
+
+    approved_by: Optional[str] = None
+    """
+    If the Transfer was approved by a user in the dashboard, the email address of
+    that user.
+    """
+
+
+class Cancellation(BaseModel):
+    canceled_at: datetime
+    """
+    The [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601) date and time at which
+    the Transfer was canceled.
+    """
+
+    canceled_by: Optional[str] = None
+    """
+    If the Transfer was canceled by a user in the dashboard, the email address of
+    that user.
+    """
+
+
+class AccountTransfer(BaseModel):
+    id: str
+    """The account transfer's identifier."""
+
+    account_id: str
+    """The Account to which the transfer belongs."""
+
+    amount: int
+    """The transfer amount in the minor unit of the destination account currency.
+
+    For dollars, for example, this is cents.
+    """
+
+    approval: Optional[Approval] = None
+    """
+    If your account requires approvals for transfers and the transfer was approved,
+    this will contain details of the approval.
+    """
+
+    cancellation: Optional[Cancellation] = None
+    """
+    If your account requires approvals for transfers and the transfer was not
+    approved, this will contain details of the cancellation.
+    """
+
+    created_at: datetime
+    """
+    The [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601) date and time at which
+    the transfer was created.
+    """
+
+    currency: Literal["CAD", "CHF", "EUR", "GBP", "JPY", "USD"]
+    """
+    The [ISO 4217](https://en.wikipedia.org/wiki/ISO_4217) code for the destination
+    account currency.
+
+    - `CAD` - Canadian Dollar (CAD)
+    - `CHF` - Swiss Franc (CHF)
+    - `EUR` - Euro (EUR)
+    - `GBP` - British Pound (GBP)
+    - `JPY` - Japanese Yen (JPY)
+    - `USD` - US Dollar (USD)
+    """
+
+    description: str
+    """The description that will show on the transactions."""
+
+    destination_account_id: str
+    """The destination account's identifier."""
+
+    destination_transaction_id: Optional[str] = None
+    """The ID for the transaction receiving the transfer."""
+
+    idempotency_key: Optional[str] = None
+    """The idempotency key you chose for this object.
+
+    This value is unique across Increase and is used to ensure that a request is
+    only processed once. Learn more about
+    [idempotency](https://increase.com/documentation/idempotency-keys).
+    """
+
+    network: Literal["account"]
+    """The transfer's network."""
+
+    pending_transaction_id: Optional[str] = None
+    """The ID for the pending transaction representing the transfer.
+
+    A pending transaction is created when the transfer
+    [requires approval](https://increase.com/documentation/transfer-approvals#transfer-approvals)
+    by someone else in your organization.
+    """
+
+    status: Literal["pending_approval", "canceled", "complete"]
+    """The lifecycle status of the transfer.
+
+    - `pending_approval` - The transfer is pending approval.
+    - `canceled` - The transfer has been canceled.
+    - `complete` - The transfer has been completed.
+    """
+
+    transaction_id: Optional[str] = None
+    """The ID for the transaction funding the transfer."""
+
+    type: Literal["account_transfer"]
+    """A constant representing the object's type.
+
+    For this resource it will always be `account_transfer`.
+    """
