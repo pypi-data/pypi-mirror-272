1e7c720a11d781533cee84609431a57299992e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 15:04:23 +0000

    Add package eXtra-cli/0.2.0/extra_cli-0.2.0-py3-none-any.whl

---
+++ packages/eXtra-cli/extra_cli-0.2.0-py3-none-any.whl/eXtra_cli/app/main.py
@@ -0,0 +1,46 @@
+from typing import Annotated
+
+from typer import Exit, Option, Typer
+
+from .state import console
+from ..commands import add, check, fetch, info, init, install, offload, remove, uninstall
+from ..helpers.common import get_app_version
+
+app = Typer(
+    context_settings={"help_option_names": ["-h", "--help"]},
+    no_args_is_help=True,
+    pretty_exceptions_show_locals=False,
+    rich_markup_mode="rich"
+)
+
+app.command("init")(init.main)
+app.command("add")(add.main)
+app.command("remove")(remove.main)
+app.command("check")(check.main)
+app.command("install")(install.main)
+app.command("uninstall")(uninstall.main)
+
+app.command("fetch", hidden=True)(fetch.main)
+app.command("offload", hidden=True)(offload.main)
+
+app.add_typer(info.app, name="info")
+
+
+def version_callback(value: bool):
+    if value:
+        version = get_app_version()
+        console().print(f"eXtra {version}")
+        raise Exit()
+
+
+@app.callback()
+def main(
+        version: Annotated[bool, Option("--version", "-v",
+                                        callback=version_callback,
+                                        help="Show the version of the program and exit.",
+                                        is_eager=True)] = False
+):
+    """
+    A project management tool for reproducible, portable, and streamlined bioinformatics analysis.
+    """
+    ...
