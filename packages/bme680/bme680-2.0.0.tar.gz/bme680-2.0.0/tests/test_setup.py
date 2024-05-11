1d02794e12f9c640c9820808e2a37c4a628f3b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 03:05:01 +0000

    Add package bme680/2.0.0/bme680-2.0.0.tar.gz

---
+++ packages/bme680/bme680-2.0.0.tar.gz/bme680-2.0.0/tests/test_setup.py
@@ -0,0 +1,14 @@
+import pytest
+
+import bme680
+
+
+def test_setup_not_present(smbus_notpresent):
+    """Mock the adbsence of a BME680 and test initialisation."""
+    with pytest.raises(RuntimeError):
+        sensor = bme680.BME680()  # noqa F841
+
+
+def test_setup_mock_present(smbus):
+    """Mock the presence of a BME680 and test initialisation."""
+    sensor = bme680.BME680()  # noqa F841
