bf750c68b701e94f514192bd06f3f8cb222de1a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 04:05:04 +0000

    Add package djangocms-column/2.1.0/djangocms_column-2.1.0.tar.gz

---
+++ packages/djangocms-column/djangocms_column-2.1.0.tar.gz/djangocms_column-2.1.0/djangocms_column/cms_plugins.py
@@ -0,0 +1,46 @@
+from django.utils.translation import gettext_lazy as _
+
+from cms import api
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+
+from .forms import MultiColumnForm
+from .models import Column, MultiColumns
+
+
+class MultiColumnPlugin(CMSPluginBase):
+    model = MultiColumns
+    module = _("Multi Columns")
+    name = _("Multi Columns")
+    render_template = "cms/plugins/multi_column.html"
+    allow_children = True
+    child_classes = ["ColumnPlugin"]
+    form = MultiColumnForm
+
+    def save_model(self, request, obj, form, change):
+        response = super().save_model(
+            request, obj, form, change
+        )
+        for _x in range(int(form.cleaned_data['create'])):
+            col = api.add_plugin(
+                placeholder=obj.placeholder,
+                plugin_type=ColumnPlugin.__name__,
+                language=obj.language,
+                target=obj,
+                width=form.cleaned_data['create_width'],
+            )
+            col.save()
+        return response
+
+
+class ColumnPlugin(CMSPluginBase):
+    model = Column
+    module = _("Multi Columns")
+    name = _("Column")
+    render_template = "cms/plugins/column.html"
+    parent_classes = ["MultiColumnPlugin"]
+    allow_children = True
+
+
+plugin_pool.register_plugin(MultiColumnPlugin)
+plugin_pool.register_plugin(ColumnPlugin)
---
+++ packages/djangocms-column/djangocms_column-2.1.0.tar.gz/djangocms_column-2.1.0/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+from pathlib import Path
+
+from setuptools import setup
+
+from djangocms_column import __version__
+
+INSTALL_REQUIRES = [
+    'django-cms>=3.8.0',
+    'Django>=2.2'  # the maximum version should be dictated by the cms
+]
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Framework :: Django :: 2.2',
+    'Framework :: Django :: 3.2',
+    'Framework :: Django :: 4.2',
+    'Framework :: Django :: 5.0',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+    'Topic :: Communications',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Message Boards',
+]
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.rst").read_text()
+
+setup(
+    name='djangocms-column',
+    version=__version__,
+    description='Column Plugin for django CMS',
+    author='Django CMS Association and contributors',
+    author_email='info@django-cms.org',
+    url='https://github.com/django-cms/djangocms-column',
+    packages=[
+        'djangocms_column',
+        'djangocms_column.migrations',
+    ],
+    install_requires=INSTALL_REQUIRES,
+    license='LICENSE.txt',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    include_package_data=True,
+    python_requires='>=3.7',
+    zip_safe=False
+)
