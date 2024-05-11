91d39578a68509e554ae24fc6f0ce4110b61a8ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-11 05:06:50 +0000

    Add package taskara/0.1.59/taskara-0.1.59-py3-none-any.whl

---
+++ packages/taskara/taskara-0.1.59-py3-none-any.whl/taskara/server/models.py
@@ -0,0 +1,77 @@
+from typing import Optional, List, Dict, Any
+import uuid
+import time
+
+from threadmem.server.models import V1RoleThread, V1RoleMessage
+from pydantic import BaseModel, Field
+from devicebay.models import V1Device
+from mllm import V1Prompt
+from devicebay import V1Device, V1DeviceType
+
+
+class V1TaskUpdate(BaseModel):
+    status: Optional[str] = None
+    description: Optional[str] = None
+    max_steps: Optional[int] = None
+    error: Optional[str] = None
+    output: Optional[str] = None
+    assigned_to: Optional[str] = None
+    completed: Optional[float] = None
+    version: Optional[str] = None
+
+
+class V1Task(BaseModel):
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    description: str
+    max_steps: int = 30
+    device: Optional[V1Device] = None
+    device_type: Optional[V1DeviceType] = None
+    status: Optional[str] = None
+    threads: Optional[List[V1RoleThread]] = None
+    prompts: Optional[List[V1Prompt]] = None
+    assigned_to: Optional[str] = None
+    assigned_type: Optional[str] = None
+    created: float = Field(default_factory=time.time)
+    started: float = 0.0
+    completed: float = 0.0
+    error: Optional[str] = None
+    output: Optional[str] = None
+    parameters: Optional[Dict[str, Any]] = {}
+    version: Optional[str] = None
+    remote: Optional[str] = None
+    owner_id: Optional[str] = None
+    tags: List[str] = []
+    labels: Dict[str, str] = {}
+    episode_id: Optional[str] = None
+
+
+class V1Tasks(BaseModel):
+    tasks: List[V1Task]
+
+
+class V1UserProfile(BaseModel):
+    email: Optional[str] = None
+    display_name: Optional[str] = None
+    handle: Optional[str] = None
+    picture: Optional[str] = None
+    created: Optional[int] = None
+    updated: Optional[int] = None
+    token: Optional[str] = None
+
+
+class V1AddThread(BaseModel):
+    public: bool
+    name: Optional[str] = None
+    metadata: Optional[dict] = None
+    id: Optional[str] = None
+
+
+class V1RemoveThread(BaseModel):
+    id: str
+
+
+class V1PostMessage(BaseModel):
+    role: str
+    msg: str
+    images: List[str] = []
+    thread: Optional[str] = None
