d307280048da6df0639e11db36ba79ea6da70410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 04:05:16 +0000

    Add package pythonix/1.0.3/pythonix-1.0.3-py3-none-any.whl

---
+++ packages/pythonix/pythonix-1.0.3-py3-none-any.whl/pythonix/prelude.py
@@ -0,0 +1,12 @@
+import pythonix.pipe as pipe
+import pythonix.res as res
+import pythonix.trail as trail
+import pythonix.op as op
+from pythonix.internals.types import Fn, FnOnce
+from pythonix.res import Res
+import pythonix.req as req
+import pythonix.curry as curry
+import pythonix.tup as tup
+import pythonix.dict_utils as dict_utils
+
+from pythonix.res import Ok, Err
