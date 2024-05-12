07b3f979331eb594d671be6c60a450e3ceeb5418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 14:04:59 +0000

    Add package ezside/0.1.39/ezside-0.1.39-py3-none-any.whl

---
+++ packages/ezside/ezside-0.1.39-py3-none-any.whl/ezside/widgets/_settings_window.py
@@ -0,0 +1,5 @@
+"""SettingsWindow provides a scrollable window for displaying and editing
+the contents of the AppSettings class."""
+#  GPL-3.0 license
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
