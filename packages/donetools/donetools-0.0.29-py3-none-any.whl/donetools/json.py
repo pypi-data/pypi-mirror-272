d46559f2e3e96d73ebb0da07fe51cdc5cbe8aa13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 15:04:57 +0000

    Add package donetools/0.0.29/donetools-0.0.29-py3-none-any.whl

---
+++ packages/donetools/donetools-0.0.29-py3-none-any.whl/donetools/json.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+import donetools
+
+def dump(obj: dict | list, path: str, indent: int = 4, mode: str = 'w') -> None:
+    """Dump a JSON file."""
+    os.makedirs(donetools.path.dirname(path), exist_ok=True)
+    with open(path, mode=mode) as file:
+        json.dump(obj, file, indent=indent)
+
+def load(path: str) -> dict | list:
+    """Load a JSON file."""
+    with open(path, mode='r') as file:
+        return json.load(file)
