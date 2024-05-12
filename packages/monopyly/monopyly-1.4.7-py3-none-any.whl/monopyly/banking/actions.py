3dd32406934e0259a5f42c0191b18a822c030db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 14:04:38 +0000

    Add package monopyly/1.4.7/monopyly-1.4.7-py3-none-any.whl

---
+++ packages/monopyly/monopyly-1.4.7-py3-none-any.whl/monopyly/banking/actions.py
@@ -0,0 +1,44 @@
+"""Module describing logical banking actions (to be used in routes)."""
+
+from ..common.utils import convert_date_to_midnight_timestamp
+from .accounts import BankAccountHandler, BankAccountTypeHandler
+
+
+def get_bank_account_type_grouping(bank):
+    """Get a summary of accounts for the given bank, grouped by type."""
+    # Get a grouping (by account type) of accounts at the given bank
+    type_accounts = {}
+    for account_type in BankAccountTypeHandler.get_types_for_bank(bank.id):
+        # Get only accounts for the logged in user and the given bank
+        type_accounts[account_type] = BankAccountHandler.get_accounts(
+            bank_ids=(bank.id,),
+            account_type_ids=(account_type.id,),
+        )
+    return type_accounts
+
+
+def get_balance_chart_data(transactions):
+    """
+    Build a dataset to be passed to a `chartist.js` chart constructor.
+
+    Parameters
+    ----------
+    transactions : list
+        A list of transactions to be used for generating the chart data.
+
+    Returns
+    -------
+    chart_data : list
+        A list of sorted (x, y) pairs consisting of the Unix timestamp
+        (in milliseconds) and the bank account balance.
+    """
+    chart_data = sorted(map(_make_transaction_balance_ordered_pair, transactions))
+    return chart_data
+
+
+def _make_transaction_balance_ordered_pair(transaction):
+    # Create an ordered pair of date (timestamp) and account balance
+    timestamp = convert_date_to_midnight_timestamp(
+        transaction.transaction_date, milliseconds=True
+    )
+    return timestamp, transaction.balance
---
+++ packages/monopyly/monopyly-1.4.7-py3-none-any.whl/monopyly/cli/apps.py
@@ -0,0 +1,106 @@
+"""Application objects for running the app via the CLI."""
+
+import multiprocessing
+import subprocess
+
+from gunicorn.app.base import BaseApplication
+
+from .. import create_app
+
+
+class LocalApplication:
+    """
+    An object for running the application locally.
+
+    This application object will run the Flask application using the
+    built-in Python server on localhost, just like the Flask development
+    mode. However, it will launch from port 5001 to avoid conflicting
+    with other Python servers that may attempt to run on the default
+    port 5000.
+    """
+
+    mode_name = "local"
+    default_port = "5001"
+    command = ["flask"]
+
+    def __init__(self, host=None, port=None, **options):
+        """Initialize the application in development mode."""
+        self._host = host
+        self._port = port
+        if options:
+            raise NotImplementedError(
+                f"Options besides `host` and `port` are not handled in {self.mode_name} mode."
+            )
+
+    def run(self):
+        """Run the Monopyly application in development mode."""
+        instruction = self.command + ["run"]
+        if self._host:
+            instruction += ["--host", self._host]
+        if self._port:
+            instruction += ["--port", self._port]
+        server = subprocess.Popen(instruction)
+
+
+class DevelopmentApplication(LocalApplication):
+    """
+    An object for running the application in development mode.
+
+    This application object will run the Flask application using the
+    built-in Python server on localhost, just like the Flask development
+    mode.
+    """
+
+    mode_name = "development"
+    default_port = "5000"
+    command = LocalApplication.command + ["--debug"]
+
+
+class ProductionApplication(BaseApplication):
+    """
+    An object for running the application in production mode (via Gunicorn).
+
+    This application object will run the Flask application using a
+    Gunicorn server instead of the built-in Python server.
+    """
+
+    default_port = "8000"
+    _default_worker_count = (multiprocessing.cpu_count() * 2) + 1
+
+    def __init__(self, host=None, port=None, **options):
+        """Initialize the application in production mode."""
+        options["bind"] = self._parse_binding(host, port, options.get("bind"))
+        options.setdefault("workers", self._default_worker_count)
+        self.options = options
+        self.application = create_app()
+        super().__init__()
+
+    @staticmethod
+    def _parse_binding(host, port, bind_option):
+        # Parse any socket binding options
+        if (host or port) and bind_option:
+            raise ValueError(
+                "Neither `host` nor `port` parameters can be specified if the "
+                "`bind` option is given."
+            )
+        bind_values = []
+        if host:
+            bind_values.append(host)
+        if port:
+            bind_values.append(port)
+        return bind if (bind := ":".join(bind_values)) else bind_option
+
+    def load_config(self):
+        config = {
+            key: value
+            for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+    def run(self, *args, **kwargs):
+        return super().run(*args, **kwargs)
