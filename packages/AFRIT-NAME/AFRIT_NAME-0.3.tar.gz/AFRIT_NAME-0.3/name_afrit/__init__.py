49bd47597b05b57be5cfec29c1a04a272d7168d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 14:04:55 +0000

    Add package AFRIT-NAME/0.3/AFRIT_NAME-0.3.tar.gz

---
+++ packages/AFRIT-NAME/AFRIT_NAME-0.3.tar.gz/AFRIT_NAME-0.3/name_afrit/__init__.py
@@ -0,0 +1,10 @@
+
+#المشكله بعفريتون
+#مايكعد راحه
+#تمتع بلمكتبه
+#مكتبة اسماء عربيه
+from .OS import fers_fers_name
+from .OS import lsit_name
+from .OS import fers_name
+from .OS import fol2_name
+from .OS import fol_name
