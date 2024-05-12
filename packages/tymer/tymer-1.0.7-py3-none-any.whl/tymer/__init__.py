37f5b7f77b20b452692d3131ef8f183a9a93313c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 04:07:15 +0000

    Add package tymer/1.0.7/tymer-1.0.7-py3-none-any.whl

---
+++ packages/tymer/tymer-1.0.7-py3-none-any.whl/tymer/__init__.py
@@ -0,0 +1,64 @@
+import time as t
+
+class Timer:
+    def __init__(self, seconds, countdown=False):
+        self.sec = seconds
+        self.start_time = None
+        self.run_timer = True
+        self.timer = 0
+        self.start_timer = True
+        self.countdown = countdown
+        self.run_once = True
+    def start(self, seconds='default'):
+        if self.run_once:
+            if seconds != 'default':
+                self.sec = seconds
+            self.run_once = False
+        if self.start_timer:
+            self.start_time = t.time()
+            self.start_timer = False
+        self.current_time = t.time()
+        if self.run_timer:
+            if self.start_time != None:
+                if not self.countdown:
+                    self.timer = self.current_time - self.start_time
+                else:    
+                    self.timer = self.sec - (self.current_time - self.start_time)
+
+        if not self.countdown:
+            if self.timer >= self.sec:
+                self.run_timer = False
+                self.timer = self.sec
+        else:
+            if self.timer <= 0:
+                self.run_timer = False
+                self.timer = 0
+    def stop(self):
+        self.run_timer = False
+        pass
+    def restart(self, seconds='default'):
+        self.new_time = seconds
+        self.start_time = None
+        self.run_timer = True
+        if self.new_time != 'default':
+            self.sec = self.new_time
+        self.start_timer = True
+        pass
+    def time(self, integer=True):
+        if integer:
+            return int(self.timer)
+        else:
+            return self.timer   
+        pass
+    def __bool__(self):
+        if not self.countdown:
+            if self.timer != self.sec:
+                return False
+            else:
+                return True
+        else:
+            if self.timer != 0:
+                return False
+            else:
+                return True
+    pass
