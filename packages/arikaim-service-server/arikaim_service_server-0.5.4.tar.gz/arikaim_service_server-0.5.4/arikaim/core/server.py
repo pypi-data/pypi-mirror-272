75b6a67cdc30eefc28c1cd31e413a0d78149fa4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 04:05:35 +0000

    Add package arikaim-service-server/0.5.4/arikaim_service_server-0.5.4.tar.gz

---
+++ packages/arikaim-service-server/arikaim_service_server-0.5.4.tar.gz/arikaim_service_server-0.5.4/arikaim/core/server.py
@@ -0,0 +1,45 @@
+import uvicorn
+from peewee import *
+
+from arikaim.core.logger import logger
+from arikaim.core.utils import *
+
+class ArikaimServer:
+    _instance = None
+
+    def __init__(self):
+        self._version = '0.5.4'
+        self._config = None
+        
+    def run(self, reload = False):
+
+        self._config = load_module('config',os.path.join(Path.config(),'config.py'))
+        
+        if reload == True:
+            logger.info('Dev mode (reload)')
+
+        uvicorn.run(
+            'arikaim.core.app:app.boot', 
+            reload = reload,
+            factory = True,
+            host = self._config.settings['host'], 
+            port = self._config.settings['port'], 
+            log_level = self._config.settings.get('log_level','info')
+        )
+       
+    @property
+    def server_url(self):
+        return 'http://' + self._config.settings['host'] + ':' + str(self._config.settings['port'])
+    
+    @property 
+    def version(self):
+        return self._version
+
+    @classmethod
+    def instance(cls):
+        if cls._instance is None: 
+            cls._instance = ArikaimServer()
+          
+        return cls._instance
+    
+arikaim_server = ArikaimServer.instance()
