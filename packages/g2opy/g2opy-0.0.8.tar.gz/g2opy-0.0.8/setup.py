68551a3e513d3f8ee7a8e5acbb35f350e5e48b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 14:04:58 +0000

    Add package g2opy/0.0.8/g2opy-0.0.8.tar.gz

---
+++ packages/g2opy/g2opy-0.0.8.tar.gz/g2opy-0.0.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import numpy as np
+
+# read the contents of your README file
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='g2opy',
+    version='0.0.8',
+    package_dir={'g2opy': 'g2opy'},
+    packages=find_packages(),
+    include_package_data=True,
+    description='g2o for python',
+    author='ubicoders',
+    keywords='g2o, SLAM, BA, ICP, optimization, python, binding',
+    author_email='info@ubicoders.com',
+    url='https://github.com/ubicoders/g2opy',  
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This is important for Markdown files
+    python_requires='==3.9',
+)
