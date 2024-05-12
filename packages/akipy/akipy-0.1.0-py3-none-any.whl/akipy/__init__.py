b321e7efa8f4c9a230d8d946e55c4fe9b63a49c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 03:05:37 +0000

    Add package akipy/0.1.0/akipy-0.1.0-py3-none-any.whl

---
+++ packages/akipy/akipy-0.1.0-py3-none-any.whl/akipy/__init__.py
@@ -0,0 +1,9 @@
+"""
+akipy - A Python wrapper library for Akinator
+
+:copyright: (c) 2024, advnpzn
+:license: MIT, see LICENSE for more details.
+"""
+
+from .akinator import Akinator
+from .exceptions import *
