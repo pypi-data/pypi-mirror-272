24172d5eb9101ae2055c718f9510b7a805f64dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 04:05:58 +0000

    Add package CustomTkinterMessagebox/0.0.3/customtkintermessagebox-0.0.3.tar.gz

---
+++ packages/CustomTkinterMessagebox/customtkintermessagebox-0.0.3.tar.gz/customtkintermessagebox-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(name='CustomTkinterMessagebox',
+    version='0.0.3',
+    license='MIT License',
+    author='Jorge Magno',
+    long_description="CustomTkinterMessagebox is a Python library that provides a customizable message box functionality for Tkinter-based applications. It allows developers to easily create message boxes with custom text, buttons, and optional sound effects.",
+    long_description_content_type="text/markdown",
+    author_email='jorge.estudos0@gmail.com',
+    keywords='customtkinter',
+    description=u'Messagebox for CustomTkinter',
+    packages=['CustomTkinterMessagebox'],
+    install_requires=['pillow', 'customtkinter'],)
