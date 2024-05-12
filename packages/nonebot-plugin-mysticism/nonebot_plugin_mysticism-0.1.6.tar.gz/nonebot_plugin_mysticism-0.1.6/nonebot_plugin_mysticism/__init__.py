d520754cd486236b10a8739c25c8f848b06fece6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 15:04:56 +0000

    Add package nonebot-plugin-mysticism/0.1.6/nonebot_plugin_mysticism-0.1.6.tar.gz

---
+++ packages/nonebot-plugin-mysticism/nonebot_plugin_mysticism-0.1.6.tar.gz/nonebot_plugin_mysticism-0.1.6/nonebot_plugin_mysticism/__init__.py
@@ -0,0 +1,18 @@
+from nonebot.plugin import PluginMetadata
+
+from .config import Config
+from .tarot import tarot
+
+__version__ = "0.1.1"
+__plugin_meta__ = PluginMetadata(
+    name="神秘学助手",
+    description="虽然现在只有塔罗派相关的工具喵",
+    usage="""相关指令：
+/tarot [formations]
+/s.tarot [type]
+""",
+    homepage="https://yan-zero.github.io/docs/coffee/mysticism",
+    type="application",
+    config=Config,
+    supported_adapters=["~onebot.v11"],
+)
