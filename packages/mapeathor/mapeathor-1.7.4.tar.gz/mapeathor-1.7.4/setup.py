db486bd5160b7f8edc273264dc17b378083ff483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 03:05:12 +0000

    Add package mapeathor/1.7.4/mapeathor-1.7.4.tar.gz

---
+++ packages/mapeathor/mapeathor-1.7.4.tar.gz/mapeathor-1.7.4/setup.py
@@ -0,0 +1,42 @@
+import os
+import re 
+
+from setuptools import setup, find_packages
+
+
+# borrowed from SQLAlchemy
+with open(os.path.join(os.path.dirname(__file__), 'src', 'mapeathor', '_version.py')) as file:
+    version = (re.compile(r""".*__version__ = ["'](.*?)['"]""", re.S).match(file.read()).group(1))
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as r:
+    requirements = r.read().split("\n")[0:-1]
+
+setup(
+    name="mapeathor",
+    version=version,
+    author="Ana Iglesias-Molina",
+    author_email="ana.iglesiasm@upm.es",
+    license_files = ('LICENSE'),
+    description="Mapeathor is a simple spreadsheet parser able to generate mapping rules in three mapping languages: R2RML, RML (releases on 2014 and 2023) and YARRRML.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/oeg-upm/Mapeathor",
+    project_urls={
+        'Documentation': 'https://github.com/oeg-upm/Mapeathor/wiki',
+        'Source code': 'https://github.com/oeg-upm/Mapeathor/tree/master/src/mapeathor',
+        'Issue tracker': 'https://github.com/oeg-upm/Mapeathor/issues',
+    },
+    include_package_data=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements,
+    python_requires='>=3.6',
+)
---
+++ packages/mapeathor/mapeathor-1.7.4.tar.gz/mapeathor-1.7.4/src/mapeathor/__init__.py
@@ -0,0 +1,47 @@
+import sys
+import argparse
+
+from . import global_config
+from . import source
+from . import mapping_generator
+from ._version import __version__
+
+
+def main():
+    parser = argparse.ArgumentParser("mapeathor")
+    parser.add_argument("-i", "--input_file", required=True, help="Input Excel file or Google SpreadSheet URL")
+    parser.add_argument("-o", "--output_file", required=False, help="Name and path for output file", default="output")
+    parser.add_argument("-l", "--language", required=True, help=("Supported Languages: " + str(global_config.supportedLanguages)))
+    parser.add_argument("-v", "--version", help=("Version"), action="version", version="Mapeathor " + __version__)
+    args = parser.parse_args()
+    inputFile = ''
+
+    # Local file
+    if(source.checkFile(args.input_file)):
+        inputFile = str(args.input_file)
+    elif (args.input_file[0:8] != 'https://'):
+        print('ERROR: File not found')
+        sys.exit()
+
+    # Google Spreadsheet file
+    else:
+        temp = source.gdriveToXMLX(args.input_file)
+        if source.checkFile(temp):
+            inputFile = temp
+        else:
+            print("ERROR: The downloaded document is not a spreadsheet")
+            sys.exit()
+
+    # Wrong language or not supported
+    if(args.language.lower() not in global_config.supportedLanguages):
+        print("ERROR: The selected Language is not supported at the moment.")
+        print("Supporteds Languages: " + str(global_config.supportedLanguages))
+        sys.exit()
+    else: # Create mapping
+        global_config.setMappingLanguage(args.language)
+        print('Generating mapping file')
+        outputFile = mapping_generator.generateMapping(inputFile, args.output_file)
+        print("Your mapping file is in "+outputFile)
+
+if __name__ == '__main__':
+    main()
