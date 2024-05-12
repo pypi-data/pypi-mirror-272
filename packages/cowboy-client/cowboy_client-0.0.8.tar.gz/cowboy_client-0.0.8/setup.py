e0b2b4a1811a883886d77a727a02b7aaa91a7835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 14:05:16 +0000

    Add package cowboy-client/0.0.8/cowboy_client-0.0.8.tar.gz

---
+++ packages/cowboy-client/cowboy_client-0.0.8.tar.gz/cowboy_client-0.0.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import Extension, find_packages, setup
+
+setup(
+    name="cowboy-client",
+    version="0.0.8",
+    packages=find_packages(),
+    description="Cowboy Client Interface",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    author="John Peng",
+    author_email="kongyijipeng@gmail.com",
+    install_requires=[
+        "click",
+        "pyyaml",
+        "pydantic",
+        "gitpython==3.0.6",
+        "requests",
+    ],
+    entry_points={
+        "console_scripts": [
+            "cowboy = cowboy.cli:entry_point",
+        ],
+    },
+    python_requires=">=3.8",
+)
