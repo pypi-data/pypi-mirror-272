cf81571e8ff120b35da6de3f13d74f01b3b2a352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 03:05:28 +0000

    Add package spatium/1.5.5/spatium-1.5.5-cp311-cp311-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/spatium/spatium-1.5.5-cp311-cp311-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/spatium/__init__.py
@@ -0,0 +1,12 @@
+from ._spatium import Vec2, Vec3, Vec4, Vec2i, Vec3i, Vec4i, Transform2D, Transform3D
+
+__all__ = (
+    "Vec2",
+    "Vec3",
+    "Vec4",
+    "Vec2i",
+    "Vec3i",
+    "Vec4i",
+    "Transform2D",
+    "Transform3D",
+)
