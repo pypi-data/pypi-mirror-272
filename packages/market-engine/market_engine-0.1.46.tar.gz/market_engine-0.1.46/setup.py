7659198d1d193f29ae5309f27ae09789b9c11afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 04:05:47 +0000

    Add package market-engine/0.1.46/market_engine-0.1.46.tar.gz

---
+++ packages/market-engine/market_engine-0.1.46.tar.gz/market_engine-0.1.46/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='market-engine',
+    version='0.1.46',
+    description='Engine for easily getting the orders, statistics, and other stats from warframe.market.',
+    author='Jacob McBride',
+    author_email='jake55111@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'aiohttp~=3.8.4',
+        'discord~=2.2.2',
+        'aiolimiter~=1.0.0',
+        'redis~=4.5.4',
+        'requests~=2.29.0',
+        'beautifulsoup4~=4.12.2',
+        'PyMySQL~=1.0.3',
+        'fuzzywuzzy~=0.18.0',
+        'pytz~=2023.3',
+        'python-Levenshtein~=0.21.0',
+        'beautifulsoup4~=4.12.2',
+        'Markdown~=3.4.3',
+        'cryptography~=40.0.2',
+        'tenacity~=8.2.2'
+    ],
+)
