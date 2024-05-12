fb58f6defdb15bed7ef2da2549edb32acfc0f15a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 03:04:59 +0000

    Add package bibla/2.0.1/bibla-2.0.1.tar.gz

---
+++ packages/bibla/bibla-2.0.1.tar.gz/bibla-2.0.1/test/test_base.py
@@ -0,0 +1,22 @@
+import unittest
+
+from bibla.lint import lint
+
+
+class TestBase(unittest.TestCase):
+
+    def test_run(self):
+        lint("test_data/mit.bib", verbose=False)
+
+    def test_jabref(self):
+        lint("test_data/bci.bib", verbose=False)
+
+    def test_incorrect_syntax(self):
+        lint("test_data/syntax.bib", verbose=False)
+        
+    def test_correct_bibLaTex(self):
+        lint("test_data/bibLaTex.bib", verbose=False)
+
+
+if __name__ == '__main__':
+    unittest.main()
