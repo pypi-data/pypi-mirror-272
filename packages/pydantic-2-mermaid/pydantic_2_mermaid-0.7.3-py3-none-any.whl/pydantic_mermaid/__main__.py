46b754b108512518e1d9bdbd5a890153bda05038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 14:04:51 +0000

    Add package pydantic-2-mermaid/0.7.3/pydantic_2_mermaid-0.7.3-py3-none-any.whl

---
+++ packages/pydantic-2-mermaid/pydantic_2_mermaid-0.7.3-py3-none-any.whl/pydantic_mermaid/__main__.py
@@ -0,0 +1,103 @@
+import argparse
+import importlib
+import logging
+import sys
+from importlib.abc import Loader
+from importlib.machinery import ModuleSpec
+from importlib.util import module_from_spec, spec_from_file_location
+from pathlib import Path
+from types import ModuleType
+from uuid import uuid4
+
+from pydantic_mermaid.mermaid_generator import MermaidGenerator
+from pydantic_mermaid.models import Relations
+
+logger = logging.getLogger("pydantic-mermaid")
+
+
+def import_module(path: str) -> ModuleType:
+    """
+    Helper which allows modules to be specified by either dotted path notation or by filepath.
+    If we import by filepath, we must also assign a name to it and add it to sys.modules BEFORE
+    calling 'spec.loader.exec_module' because there is code in pydantic which requires that the
+    definition exist in sys.modules under that name.
+    """
+    try:
+        if Path(path).exists():
+            name = uuid4().hex
+            spec = spec_from_file_location(name, path, submodule_search_locations=[])
+
+            if isinstance(spec, ModuleSpec):
+                module = module_from_spec(spec)
+                sys.modules[name] = module
+                if isinstance(spec.loader, Loader):
+                    spec.loader.exec_module(module)
+                    return module
+
+            logger.error(f"Failed to import module from file {path}")  # pragma: no cover
+            sys.exit(1)  # pragma: no cover
+
+        return importlib.import_module(path)
+    except ModuleNotFoundError as e:  # pragma: no cover
+        logger.error("The --module argument must be a module path separated by dots or a valid filepath")
+        logger.exception(e)
+        sys.exit(1)
+
+
+def _parse_cli_args() -> argparse.Namespace:
+    """
+    Parses the command-line arguments passed to pydantic-mermaid.
+    """
+    parser = argparse.ArgumentParser(
+        prog="pydantic-mermaid",
+        description="Generate mermaid charts from pydantic models.",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument(
+        "-m",
+        "--module",
+        type=str,
+        required=True,
+        help="name or filepath of the python module.\n" "Discoverable submodules will also be checked.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        required=True,
+        help="name of the file the mermaid chart should be written to.",
+    )
+    parser.add_argument(
+        "-n",
+        "--root",
+        type=str,
+        help="Root node for dependency chart or inheritance chart",
+        default="",
+    )
+    parser.add_argument(
+        "-e",
+        "--relations",
+        type=Relations.parse,
+        # list(Relations) does not contain Relations.Both in python3.11 and python3.12
+        choices=list(Relations.__members__.values()),
+        help="Dependency or Inheritance chart",
+        default="dependency",
+    )
+    return parser.parse_args()
+
+
+def main() -> None:
+    """
+    CLI entrypoint
+    """
+    args = _parse_cli_args()
+    module_type = import_module(args.module)
+
+    mg = MermaidGenerator(module_type)
+
+    chart_content = mg.generate_chart(root=args.root, relations=args.relations)
+    Path(args.output).write_text(chart_content)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
---
+++ packages/pydantic-2-mermaid/pydantic_2_mermaid-0.7.3-py3-none-any.whl/pydantic_mermaid/models.py
@@ -0,0 +1,61 @@
+from enum import Flag, auto
+from typing import Dict, List, Set
+
+from pydantic import BaseModel, Field
+
+
+class Relations(Flag):
+    """Enum for representing the different types of relationships between classes."""
+
+    Inheritance = auto()
+    Dependency = auto()
+    Both = Inheritance | Dependency
+
+    def __str__(self) -> str:
+        return str(self.name).lower()
+
+    def __repr__(self) -> str:  # pragma: no cover
+        return str(self)
+
+    @staticmethod
+    def parse(s: str) -> "Relations":
+        """Convert a string to a Relations enum"""
+
+        return Relations[s.title()]
+
+
+class Property(BaseModel):
+    """A class representing a property of a MermaidClass."""
+
+    name: str
+    type: str
+    default_value: str = ""  # str is surrounded by quotes, so empty string "''"
+
+    def __str__(self) -> str:
+        s = f"{self.name}: {self.type}"
+        if self.default_value:
+            s = s + f" = {self.default_value}"
+
+        return s
+
+
+class MermaidClass(BaseModel):
+    """We call it mermaid class because 'class' is a keyword"""
+
+    name: str
+    properties: List[Property]
+    annotation: str = ""
+
+
+class MermaidGraph(BaseModel):
+    """A graph of mermaid classes and their relationships"""
+
+    class_names: List[str] = Field(
+        description="A list of all pydantic classes that is topologically sorted",
+        default_factory=list,
+    )
+    class_dict: Dict[str, MermaidClass] = Field(default_factory=dict)
+    service_clients: Dict[str, Set[str]] = Field(default_factory=dict)
+    client_services: Dict[str, Set[str]] = Field(default_factory=dict)
+    parent_children: Dict[str, Set[str]] = Field(default_factory=dict)
+    child_parents: Dict[str, Set[str]] = Field(default_factory=dict)
