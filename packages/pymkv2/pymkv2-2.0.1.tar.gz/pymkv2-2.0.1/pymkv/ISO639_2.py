d0024d5ba0bef7f470567e53519a72bce0537139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 04:05:17 +0000

    Add package pymkv2/2.0.1/pymkv2-2.0.1.tar.gz

---
+++ packages/pymkv2/pymkv2-2.0.1.tar.gz/pymkv2-2.0.1/pymkv/ISO639_2.py
@@ -0,0 +1,27 @@
+# sheldon woodward
+# 3/18/18
+
+"""ISO639-2 Three Character Language Codes"""
+
+from typing import Optional
+
+from iso639 import languages
+
+
+def is_iso639_2(language) -> Optional[bool]:
+    """The `is_iso639_2` function checks if a given language code is a valid ISO 639-2 language code.
+
+    Args:
+        language: The language code to be checked.
+
+    Returns:
+        bool: True if the language code is a valid ISO 639-2 language code, False otherwise.
+
+    Raises:
+        None
+    """
+    try:
+        languages.get(part2b=language)
+        return True
+    except KeyError:
+        return False
