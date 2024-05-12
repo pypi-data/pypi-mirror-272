117a3cf9d363444699cf69f2c352afd07b066b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 04:05:54 +0000

    Add package denoisers/0.1.8/denoisers-0.1.8.tar.gz

---
+++ packages/denoisers/denoisers-0.1.8.tar.gz/denoisers-0.1.8/denoisers/modeling/waveunet/__init__.py
@@ -0,0 +1,5 @@
+"""WaveUnet model."""
+from denoisers.modeling.waveunet.config import WaveUNetConfig
+from denoisers.modeling.waveunet.model import WaveUNetModel
+
+__all__ = ["WaveUNetConfig", "WaveUNetModel"]
