ae6cbf4a567380ea5003f4122ff28e6ff1cd5a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 04:07:16 +0000

    Add package tymer/1.0.6/tymer-1.0.6.tar.gz

---
+++ packages/tymer/tymer-1.0.6.tar.gz/tymer-1.0.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+   name='tymer',
+   version='1.0.6',
+   description='A simple timer',
+   author='Morris El Helou',
+   author_email='morrishelou@gmail.com',
+   include_package_data=True,
+   packages=['tymer'], 
+   package_data={"tymer": ["*.ico"]},
+   install_requires=[], #external packages as dependencies
+)
