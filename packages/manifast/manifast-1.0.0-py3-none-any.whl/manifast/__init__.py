b917571cf9809f4067a5611513567519a65a6dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 14:05:17 +0000

    Add package manifast/1.0.0/manifast-1.0.0-py3-none-any.whl

---
+++ packages/manifast/manifast-1.0.0-py3-none-any.whl/manifast/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+brief        :  
+Author       : knightdby knightdby@163.com
+Date         : 2023-02-24 15:53:51
+FilePath     : /manifast/manifast/__init__.py
+Description  : 
+LastEditTime : 2023-06-18 14:16:18
+LastEditors  : knightdby
+Copyright (c) 2023 by Inc, All Rights Reserved.
+'''
+from .iostream import *
+from .import_pkg import *
+
+
+__version__ = '0.0.14'
