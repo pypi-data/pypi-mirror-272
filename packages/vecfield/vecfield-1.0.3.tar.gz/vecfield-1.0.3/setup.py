d2c0eab9abad34fb54f982ea38c957afd45fb858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 14:04:46 +0000

    Add package vecfield/1.0.3/vecfield-1.0.3.tar.gz

---
+++ packages/vecfield/vecfield-1.0.3.tar.gz/vecfield-1.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+setup(
+  name='vecfield',
+  version='1.0.3',
+  author='SorbetKipit',
+  author_email='ibk07@yandex.ru',
+  description='Module for working with vector and scalar fields',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/SorbetKipit/jubilant-barnacle/tree/main',
+  packages=find_packages(),
+  install_requires=['sympy>=1.12','matplotlib>=3.8.4','numpy>=1.26.4'],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='python vectorifeld scalarfield',
+  project_urls={
+    'Documentation': 'https://github.com/SorbetKipit/jubilant-barnacle/blob/main/README.md'
+  },
+  python_requires='>=3.11'
+)
