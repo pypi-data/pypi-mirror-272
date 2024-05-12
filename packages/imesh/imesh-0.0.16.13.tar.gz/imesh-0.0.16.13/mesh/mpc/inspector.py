fabed94664aa287c074e7d639faf51c2362347f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 03:04:50 +0000

    Add package imesh/0.0.16.13/imesh-0.0.16.13.tar.gz

---
+++ packages/imesh/imesh-0.0.16.13.tar.gz/imesh-0.0.16.13/mesh/mpc/inspector.py
@@ -0,0 +1,42 @@
+#
+# Copyright (c) 2000, 2099, trustbe and/or its affiliates. All rights reserved.
+# TRUSTBE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+#
+#
+import asyncio
+from typing import Type, Any
+
+from mesh.macro import Inspector, mpi, T
+from asgiref.sync import async_to_sync
+
+
+loop = asyncio.get_event_loop()
+
+
+class MethodInspector(Inspector):
+
+    def __init__(self, macro: mpi, kind: Type[T], method: Any):
+        self.macro = macro
+        self.kind = kind
+        self.method = method
+
+    def get_signature(self) -> str:
+        return self.method.__name__
+
+    def get_type(self) -> Type[T]:
+        return self.kind
+
+    def get_name(self) -> str:
+        return self.method
+
+    def get_annotation(self, kind: Type[T]) -> T:
+        return self.method.__annotations__.get(kind)
+
+    def get_return_type(self) -> Type[T]:
+        return self.method.__annotations__.get("return")
+
+    def invoke(self, obj: Any, args: [Any]) -> Any:
+        if asyncio.iscoroutinefunction(self.method):
+            return loop.run_until_complete(self.method(*args))
+        else:
+            return self.method(*args)
