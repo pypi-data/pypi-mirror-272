ee91e287e17de8283e8c7454639141860e9e9aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 04:05:36 +0000

    Add package lens-metric/0.2.0/lens_metric-0.2.0.tar.gz

---
+++ packages/lens-metric/lens_metric-0.2.0.tar.gz/lens_metric-0.2.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lens_metric',
+    version='0.2.0',
+    description='A learnable evaluation metric for text simplification',
+    author='Mounica Maddela',
+    author_email='mmaddela3@gatech.edu',
+    url='https://github.com/Yao-Dou/LENS',
+    download_url='https://github.com/Yao-Dou/LENS/archive/refs/tags/v0.2.0.tar.gz',
+    license='Apache license',
+    packages=find_packages(),
+    install_requires=[
+        "sentencepiece >= 0.1.96",
+        "pandas >= 1.5.0, < 2.0.0",
+        "transformers >= 4.8",
+        "pytorch-lightning == 2.0.9",
+        "jsonargparse == 3.13.1",
+        "torch >= 2.0.1",
+        "numpy >= 1.20.0",
+        "torchmetrics >= 0.7",
+        "sacrebleu >= 2.0.0",
+        "scipy >=1.5.4" 
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable ',
+        'Intended Audience :: Science/Research',
+    ],
+)
