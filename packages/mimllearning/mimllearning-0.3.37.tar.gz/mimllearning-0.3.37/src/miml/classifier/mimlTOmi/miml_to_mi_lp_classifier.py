7d45bae57168bd4cb66870d26ab149acc59464f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 03:04:40 +0000

    Add package mimllearning/0.3.37/mimllearning-0.3.37.tar.gz

---
+++ packages/mimllearning/mimllearning-0.3.37.tar.gz/mimllearning-0.3.37/src/miml/classifier/mimlTOmi/miml_to_mi_lp_classifier.py
@@ -0,0 +1,90 @@
+import numpy as np
+from copy import deepcopy
+
+from .miml_to_mi_classifier import MIMLtoMIClassifier
+from ...transformation import LabelPowersetTransformation
+from ...data import Bag
+from ...data import MIMLDataset
+
+
+class MIMLtoMILPClassifier(MIMLtoMIClassifier):
+    """
+    Class to represent a multiinstance classifier
+    """
+
+    def __init__(self, classifier) -> None:
+        """
+        Constructor of the class MIMLtoMILPClassifier
+
+        Parameters
+        ----------
+        classifier
+            Specific classifier to be used
+        """
+        super().__init__(classifier)
+        self.transformation = LabelPowersetTransformation()
+
+    def fit_internal(self, dataset_train: MIMLDataset) -> None:
+        """
+        Training the classifier
+
+        Parameters
+        ----------
+        dataset_train: MIMLDataset
+            Dataset to train the classifier
+        """
+
+        dataset = self.transformation.transform_dataset(dataset_train)
+
+        self.classifier.fit(dataset.get_features_by_bag(), dataset.get_labels_by_bag())
+
+    def predict(self, x: np.ndarray) -> np.ndarray:
+        """
+        Predict labels of given data
+
+        Parameters
+        ----------
+        x : ndarray of shape (n_instances, n_features)
+            Data to predict their labels
+
+        Returns
+        -------
+        results : ndarray of shape (n_labels)
+            Predicted labels
+        """
+        # Prediction of each label
+        results = self.classifier.predict(x)
+        binary_str = np.binary_repr(results, width=self.transformation.dataset.get_number_labels())
+        return np.array([int(bit) for bit in binary_str])
+
+    def predict_proba(self, dataset_test: MIMLDataset):
+        # TODO: DOC
+        results = np.zeros((dataset_test.get_number_bags(), dataset_test.get_number_labels()))
+        dataset = self.transformation.transform_dataset(dataset_test)
+        results = self.classifier.predict_proba(dataset.get_features_by_bag())
+        return results
+
+    def evaluate(self, dataset_test: MIMLDataset) -> np.ndarray:
+        """
+        Evaluate the model on a test dataset
+
+        Parameters
+        ----------
+        dataset_test : MIMLDataset
+            Test dataset to evaluate the model on
+
+        Returns
+        ----------
+        results : ndarray of shape (n_bags, n_labels)
+            Predicted labels of dataset_test
+        """
+        # super().evaluate(dataset_test)
+
+        dataset = self.transformation.transform_dataset(dataset_test)
+
+        results = np.zeros((dataset_test.get_number_bags(), dataset_test.get_number_labels()))
+        # Features are the same in all datasets
+        for i, bag in enumerate(dataset.get_features_by_bag()):
+            results[i] = self.predict(bag)
+
+        return results
