e277abc856a940904bdc1b36947cd49eb1be95fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 14:04:53 +0000

    Add package nonebot-plugin-mysticism/0.1.5/nonebot_plugin_mysticism-0.1.5-py3-none-any.whl

---
+++ packages/nonebot-plugin-mysticism/nonebot_plugin_mysticism-0.1.5-py3-none-any.whl/nonebot_plugin_mysticism/utils.py
@@ -0,0 +1,34 @@
+from io import BytesIO
+import os
+import aiohttp
+import hashlib
+import tempfile
+import pathlib
+
+
+# 返回BytesIO对象图片
+async def send_image_as_bytes(url: str, cache: bool = True):
+    if cache:
+        path = pathlib.Path(tempfile.gettempdir()) / "tarot"
+        if not os.path.exists(path):
+            os.mkdir(path)
+        path /= hashlib.sha256(url.encode()).hexdigest()
+
+        if os.path.exists(path):
+            with open(path, mode="rb") as f:
+                buffered = BytesIO(f.read())
+                buffered.seek(0)
+                return buffered
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                image_data = await response.read()
+                if cache:
+                    with open(path, mode="wb+") as f:
+                        f.write(image_data)
+                buffered = BytesIO(image_data)
+                buffered.seek(0)
+                return buffered
+            else:
+                return None
