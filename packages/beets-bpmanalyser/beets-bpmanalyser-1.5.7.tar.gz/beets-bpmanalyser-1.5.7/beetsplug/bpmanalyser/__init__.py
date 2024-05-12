2bc79c7a5fe4c5c714f58050a16227d0eff7a15a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 04:05:37 +0000

    Add package beets-bpmanalyser/1.5.7/beets-bpmanalyser-1.5.7.tar.gz

---
+++ packages/beets-bpmanalyser/beets-bpmanalyser-1.5.7.tar.gz/beets-bpmanalyser-1.5.7/beetsplug/bpmanalyser/__init__.py
@@ -0,0 +1,44 @@
+#  Copyright: Copyright (c) 2020., Adam Jakab
+#
+#  Author: Adam Jakab <adam at jakab dot pro>
+#  Created: 2/23/20, 10:50 PM
+#  License: See LICENSE.txt
+
+import logging
+
+from beets.plugins import BeetsPlugin
+from beetsplug.bpmanalyser.command import BpmAnalyserCommand
+
+log = logging.getLogger('beets.bpmanalyser')
+
+
+class BpmAnalyserPlugin(BeetsPlugin):
+
+    def __init__(self):
+        super(BpmAnalyserPlugin, self).__init__()
+        self.config.add({
+            'auto': False,
+            'dry-run': False,
+            'write': True,
+            'threads': "AUTO",
+            'force': False,
+            'quiet': False
+        })
+
+        # On-import analysis.
+        if self.config['auto']:
+            self.import_stages = [self.imported]
+
+    def commands(self):
+        return [BpmAnalyserCommand(self.config)]
+
+    def imported(self, session, task):
+        cmd = BpmAnalyserCommand(self.config)
+        # Add BPM for imported items.
+        for item in task.imported_items():
+            if not self.config['force'] and item['bpm'] != 0:
+                item_path = item.get("path").decode("utf-8")
+                log.debug("Skipped. Item({}) has already got BPM: {}".format(item_path, item['bpm']))
+                return
+            else:
+                cmd.runAnalyser(item)
