c1f875875727d6c6e4ed801fe03b519e05411d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 14:04:51 +0000

    Add package QtHarmony/0.1.9/qtharmony-0.1.9.tar.gz

---
+++ packages/QtHarmony/qtharmony-0.1.9.tar.gz/qtharmony-0.1.9/qtharmony/widgets/basic/groups.py
@@ -0,0 +1,51 @@
+from typing import Optional, TYPE_CHECKING
+
+from PySide6.QtWidgets import (
+    QGroupBox, QButtonGroup
+)
+
+from qtharmony.src.core import StyleSheetLoader
+from qtharmony.src.core.theme import ThemeManager 
+
+if TYPE_CHECKING:
+    from PySide6.QtWidgets import QWidget
+
+
+class GroupBox(QGroupBox):
+    """
+    Custom QGroupBox widget for grouping widgets.
+
+    Methods:
+    - __init__(title: Optional[str] = None, *, stylesheet: Optional[str] = None, parent: Optional["QWidget"] = None): None
+              - Initializes the GroupBox widget with optional title and stylesheet.
+    """
+
+    def __init__(
+            self,
+            title: Optional[str] = None,
+            size: tuple[int, int] = (400, 200),
+            *,
+            stylesheet: Optional[str] = None,
+            parent: Optional["QWidget"] = None
+    ) -> None:
+        """
+        Initializes the GroupBox widget with optional title and stylesheet.
+
+        Args:
+            title (Optional[str], optional): The title of the group box. Defaults to None.
+            stylesheet (Optional[str], optional): Custom stylesheet for the group box widget. Defaults to None.
+            parent (Optional["QWidget"], optional): Parent widget of the group box. Defaults to None.
+        """
+
+        super().__init__(parent)
+        ThemeManager.add_widgets(self)
+
+        self.setObjectName("group-box")
+        self.stylesheet = StyleSheetLoader.load_stylesheet(
+            __file__, "styles/group_box.css", 
+            name="GroupBox", obj_name="QGroupBox#group-box",
+            stylesheet=stylesheet
+        )
+
+        if title is not None: self.setTitle(title)
+        self.setFixedSize(*size)
