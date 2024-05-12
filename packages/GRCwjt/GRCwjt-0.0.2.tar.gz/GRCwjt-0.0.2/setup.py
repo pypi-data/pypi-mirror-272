74a8b313356085b4fb6465f5ca74619c9c48b1b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 14:04:56 +0000

    Add package GRCwjt/0.0.2/GRCwjt-0.0.2.tar.gz

---
+++ packages/GRCwjt/GRCwjt-0.0.2.tar.gz/GRCwjt-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GRCwjt",
+    packages=find_packages(),
+    version="0.0.2",
+    install_requires=[
+        "rpyc",
+        "pyelftools",
+        "find_libpython"
+    ],
+    author="",
+    author_email="",
+)
