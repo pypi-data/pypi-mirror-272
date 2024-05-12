ef5b9688ae771a67fceb1b87f96ec43a849194b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 15:04:58 +0000

    Add package portfolio-tracker/0.1.1.1/portfolio_tracker-0.1.1.1.tar.gz

---
+++ packages/portfolio-tracker/portfolio_tracker-0.1.1.1.tar.gz/portfolio_tracker-0.1.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='portfolio_tracker',
+    python_requires='>=3.10',
+    packages=find_packages(include=['portfolio_tracker']),
+    version='0.1.1.1',
+    description='A portfolio tracker library, that allows to track a portfolio of stocks and their performance.',
+    author='Poli Luca',
+    install_requires=['pandas', 'yfinance', 'numpy', 'pandas_market_calendars', 'openpyxl'],
+    package_data={'docs': ['manager.html', 'price_repo.html', 'utils.html', 'xlsx_example.xlsx', 'example.ipynb']},
+)
