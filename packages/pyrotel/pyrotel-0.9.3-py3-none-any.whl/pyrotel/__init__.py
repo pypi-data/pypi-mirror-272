f2a66c639830b56a9ac2befcb0ef27fc16e954ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 04:07:28 +0000

    Add package pyrotel/0.9.3/pyrotel-0.9.3-py3-none-any.whl

---
+++ packages/pyrotel/pyrotel-0.9.3-py3-none-any.whl/pyrotel/__init__.py
@@ -0,0 +1,10 @@
+from .client import Client
+from .util import message
+
+__title__: str = 'pyrotel'
+__version__: str = '0.9.3'
+__license__: str = 'MIT license'
+__author__: str = 'Erfan Bafandeh'
+__url__: str = 'https://github.com/Erfan-Bafandeh/pyrotel'
+__author_email__: str = 'user.enbh@gmail.com'
+__description__: str = 'pyrotel is a library for telegram bots.'
