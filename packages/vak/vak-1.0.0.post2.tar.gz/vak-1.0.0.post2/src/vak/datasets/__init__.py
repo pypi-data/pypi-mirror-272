3d43a8d88eb3dbc750dd6eb09372ae44e9aa342f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 03:05:13 +0000

    Add package vak/1.0.0.post2/vak-1.0.0.post2.tar.gz

---
+++ packages/vak/vak-1.0.0.post2.tar.gz/vak-1.0.0.post2/src/vak/datasets/__init__.py
@@ -0,0 +1,15 @@
+from . import biosoundsegbench
+from .biosoundsegbench import BioSoundSegBench, SplitsMetadata
+from .get import get
+
+__all__ = [
+    "biosoundsegbench",
+    "BioSoundSegBench",
+    "get",
+    "SplitsMetadata",
+]
+
+# TODO: make this a proper registry
+DATASETS = {
+    "BioSoundSegBench": BioSoundSegBench
+}
