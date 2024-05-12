abb2d91ccfa40c5262431e869784a2b330670dab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 14:04:59 +0000

    Add package zafkiel/0.1.2/zafkiel-0.1.2.tar.gz

---
+++ packages/zafkiel/zafkiel-0.1.2.tar.gz/zafkiel-0.1.2/zafkiel/__init__.py
@@ -0,0 +1,14 @@
+from airtest.core.helper import G
+
+from zafkiel.config import Config
+from zafkiel.device.api import API
+from zafkiel.device.template import Template
+from zafkiel.device.win import WindowsPlatform
+from zafkiel.timer import Timer
+from zafkiel.report import simple_report
+from zafkiel.logger import logger
+
+__version__ = '0.1.2'
+
+
+G.register_custom_device(WindowsPlatform)
