9917132d18c5b5555f5607d8ecdd7d30457a4da2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 15:04:58 +0000

    Add package v2share/0.1.0a6/v2share-0.1.0a6.tar.gz

---
+++ packages/v2share/v2share-0.1.0a6.tar.gz/v2share-0.1.0a6/v2share/__init__.py
@@ -0,0 +1,6 @@
+from .data import V2Data
+from .clash import ClashConfig
+from .singbox import SingBoxConfig
+from .clashmeta import ClashMetaConfig
+
+__all__ = [V2Data, SingBoxConfig, ClashConfig, ClashMetaConfig]
