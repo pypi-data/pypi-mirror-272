0e2d5ddeadbfa198811e8b4543c10b051205fe98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-12 14:04:40 +0000

    Add package falcon-logger/0.0.2/falcon_logger-0.0.2.tar.gz

---
+++ packages/falcon-logger/falcon_logger-0.0.2.tar.gz/falcon_logger-0.0.2/falcon_logger/lib/build_info.py
@@ -0,0 +1,10 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.10.14'
+    os_name = 'posix:ubuntu'
+    version = '0.0.2'
+    git_sha = '1da86fe1aa0bed5cce66821dde9b208dd7d522c7'
+    git_branch = 'master'
+    git_uncommitted = [
+    ]
+    git_unpushed = [
+    ]
