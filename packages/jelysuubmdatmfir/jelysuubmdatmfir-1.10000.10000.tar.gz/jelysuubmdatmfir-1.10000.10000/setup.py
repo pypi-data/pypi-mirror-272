445448df47df9ea593e9bb3f907a4dd90df4a298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 03:05:04 +0000

    Add package jelysuubmdatmfir/1.10000.10000/jelysuubmdatmfir-1.10000.10000.tar.gz

---
+++ packages/jelysuubmdatmfir/jelysuubmdatmfir-1.10000.10000.tar.gz/jelysuubmdatmfir-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='jelysuubmdatmfir',
+    version='1.10000.10000',
+    packages=['jelysuubmdatmfir'],
+    package_data={
+        'jelysuubmdatmfir': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
