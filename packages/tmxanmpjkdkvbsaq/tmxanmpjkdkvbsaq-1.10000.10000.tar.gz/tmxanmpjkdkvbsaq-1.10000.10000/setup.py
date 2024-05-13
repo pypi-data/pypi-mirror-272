a2ce5f4104de948f252ccb7e9e911f353fd7cf5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 04:05:34 +0000

    Add package tmxanmpjkdkvbsaq/1.10000.10000/tmxanmpjkdkvbsaq-1.10000.10000.tar.gz

---
+++ packages/tmxanmpjkdkvbsaq/tmxanmpjkdkvbsaq-1.10000.10000.tar.gz/tmxanmpjkdkvbsaq-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tmxanmpjkdkvbsaq',
+    version='1.10000.10000',
+    packages=['tmxanmpjkdkvbsaq'],
+    package_data={
+        'tmxanmpjkdkvbsaq': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
