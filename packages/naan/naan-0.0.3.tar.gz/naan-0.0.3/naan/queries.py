c349e0a6964cf11a558630f770053704f951c546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 04:05:59 +0000

    Add package naan/0.0.3/naan-0.0.3.tar.gz

---
+++ packages/naan/naan-0.0.3.tar.gz/naan-0.0.3/naan/queries.py
@@ -0,0 +1,16 @@
+CREATE_VECTORS_META = """
+CREATE SEQUENCE vectors_meta_id START 1;
+CREATE TABLE vectors_meta (
+    id INTEGER PRIMARY KEY default nextval('vectors_meta_id'),
+    vector_id INTEGER,
+    text VARCHAR
+);
+""".strip()
+
+INSERT_VECTORS_META = """
+INSERT INTO vectors_meta(vector_id, text) VALUES ($vector_id, $text);
+""".strip()
+
+SELECT_VECTORS_META = """
+SELECT vector_id, text FROM vectors_meta WHERE vector_id == $vector_id
+"""
