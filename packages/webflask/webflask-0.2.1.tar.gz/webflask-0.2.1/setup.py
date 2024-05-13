dba4c7d0ca30bec6f3969b8d123dc81cf04f0a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 03:05:19 +0000

    Add package webflask/0.2.1/webflask-0.2.1.tar.gz

---
+++ packages/webflask/webflask-0.2.1.tar.gz/webflask-0.2.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='webflask',
+    version='0.2.1',
+    packages=['webflask'],
+    install_requires=[],
+)
+#pypi-AgEIcHlwaS5vcmcCJDFiNTI1Yzg0LTM2ZGYtNGYxNC05YWZhLTNlYmNjNTliZGM3NwACKlszLCIzMmEyY2E5ZS1lYjExLTQxM2ItYTY0NS1jNmIzZDBjMmNlODYiXQAABiDPeAfbu_Xj4ciLk8msWV4rj_3QKmDPR_zyvIFpv02xPA
