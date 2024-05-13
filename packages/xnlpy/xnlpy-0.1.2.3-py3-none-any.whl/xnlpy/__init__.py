f10ff3f6b85e1d1bf42a6c04e808219069e7ce36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 04:05:34 +0000

    Add package xnlpy/0.1.2.3/xnlpy-0.1.2.3-py3-none-any.whl

---
+++ packages/xnlpy/xnlpy-0.1.2.3-py3-none-any.whl/xnlpy/__init__.py
@@ -0,0 +1,30 @@
+"""
+XNL: eXtended Numerical Library
+2020, Gilberto Jose Guimaraes de Sousa Mourao
+
+XNL is a library made by some students at UFMA (Universidade Federal do 
+Maranhao) with the purpose of learning some numerical algorithms and 
+share the acquired knowledge with students of the next generations.
+
+                        About collaboration
+
+Only UFMA students will be accepted as collaborators. This limitation was 
+proposed by the creator of the library to prevent the focus from being 
+diverted from learning.
+
+If a student sees an error or feels that XNL has an incorrect algorithm, 
+he should contact the library creator or the discipline teacher who will 
+contact the library creator so that the student's complaint is studied. 
+If there is really an error, the library will be updated and the student's 
+name will appear in the acknowledgments (README.md).
+"""
+
+from .xnlpy import *
+
+__author__ = "Gilberto Jose Guimaraes de Sousa Mourao"
+__credits__ = ["Gilberto Jose Guimaraes de Sousa Mourao"]
+
+__version__ = "demo"
+__maintainer__ = "Gilberto Jose Guimaraes de Sousa Mourao"
+__email__ = "gilberto.mourao@discente.ufma.br"
+__status__ = "Production"
