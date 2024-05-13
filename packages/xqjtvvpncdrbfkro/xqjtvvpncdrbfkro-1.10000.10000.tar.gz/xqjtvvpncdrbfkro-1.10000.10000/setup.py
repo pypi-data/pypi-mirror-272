e26340f1e2d8e4383600c57e20158df4a31155bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 03:05:21 +0000

    Add package xqjtvvpncdrbfkro/1.10000.10000/xqjtvvpncdrbfkro-1.10000.10000.tar.gz

---
+++ packages/xqjtvvpncdrbfkro/xqjtvvpncdrbfkro-1.10000.10000.tar.gz/xqjtvvpncdrbfkro-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='xqjtvvpncdrbfkro',
+    version='1.10000.10000',
+    packages=['xqjtvvpncdrbfkro'],
+    package_data={
+        'xqjtvvpncdrbfkro': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
