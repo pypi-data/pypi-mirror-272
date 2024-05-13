f2e2cc24cbb724d3b472b5a4eec807a5f29e9ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 04:06:10 +0000

    Add package TonkaConstruction/0.9.0/tonkaconstruction-0.9.0.tar.gz

---
+++ packages/TonkaConstruction/tonkaconstruction-0.9.0.tar.gz/tonkaconstruction-0.9.0/README.md
@@ -0,0 +1,23 @@
+# <img src="https://raw.githubusercontent.com/npjg/TonkaConstruction/main/.github/favicon.svg" width="32" height="32" alt="Tonka Construction"> Tonka Construction
+Hey there little truckster!
+
+This was one of the first computer games I ever played. If I recall correctly, I got the CD-ROM from a yard sale... where I've gotten most games I've ever played. This game brought much enjoyment to me as a youngster.
+
+## Usage
+Installing the PyPI package will also install a `TonkaConstruction` script that can be invoked from the command line as follows:
+```
+#                 Input directory   Export directory
+#                 ...............   .................
+TonkaConstruction ~/TONKA/DATA      ~/TonkaExtract
+```
+
+## Testing
+Currently there is no support for running tests from CI/CD because these services won't have the (large) game files necessary to test this library. Currently all tests are run locally with the files for each known Tonka version in the `tests/test_data` directory.
+
+## Acknowledgements
+The Tonka truck icon used as this repository's icon was taken from the Tonka Construction CD-ROM.
+
+# Support My Reverse Engineering!
+<a href="https://www.buymeacoffee.com/natster" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174"></a>
+
+If you like my reverse engineering, you can [buy me a matcha latte](https://www.buymeacoffee.com/natster) to show your appreciation 🍵!
---
+++ packages/TonkaConstruction/tonkaconstruction-0.9.0.tar.gz/tonkaconstruction-0.9.0/tests/test_game_parsing.py
@@ -0,0 +1,63 @@
+import os
+import tempfile
+import subprocess
+import shutil
+import warnings
+
+import pytest
+
+from TonkaConstruction import Engine 
+
+# The tests MUST be run from the root of the repository.
+GAME_ROOT_DIRECTORY = 'tests/test_data'
+game_directories = []
+game_root_directory_real_path = os.path.realpath(GAME_ROOT_DIRECTORY)
+if not os.path.exists(os.path.realpath(GAME_ROOT_DIRECTORY)):
+    warnings.warn('No test data present, game parsing tests will be skipped.')
+else:
+    for filename in os.listdir(os.path.realpath(GAME_ROOT_DIRECTORY)):
+        filepath = os.path.join(GAME_ROOT_DIRECTORY, filename)
+        if os.path.isdir(filepath):
+            game_directories.append(filepath)
+
+def test_script_is_runnable():
+    # This package includes a command that can be called from the command line,
+    # so we will store the name of that script here. It is defined in pyproject.toml,
+    # but I didn't see an easy way to reference that here, so we'll just hardcode it.
+    CALLABLE_SCRIPT_NAME = 'TonkaConstruction'
+
+    # RUN THE SCRIPT.
+    # We shell out rather than just calling the function from Python to make
+    # sure that the script entry point is installed correctly too.
+    # We don't need to actually process anything, just make sure the script runs.
+    # So we can point it to an empty directory.
+    empty_directory = tempfile.mkdtemp()
+    try:
+        # ATTEMPT TO RUN THE SCRIPT.
+        command = [CALLABLE_SCRIPT_NAME, empty_directory]
+        result = subprocess.run(command, capture_output = True, text = True)            
+
+        # VERIFY THE SCRIPT RAN SUCCESSFULLY.
+        if (result.returncode != 0):
+            raise AssertionError(
+                f'Received a nonzero exit code when running `{CALLABLE_SCRIPT_NAME}` from command line!'
+                f'\nstdout: {result.stdout}'
+                f'\n\nstderr: {result.stderr}')
+    finally:
+        shutil.rmtree(empty_directory)
+
+@pytest.mark.parametrize("game_directory_path", game_directories)
+def test_process_game(game_directory_path):
+    # PARSE THE RESOURCES.
+    print(game_directory_path)
+    temp_dir = tempfile.mkdtemp()
+    try:
+        Engine.main([game_directory_path, '--export', temp_dir])
+        # TODO: Do something to verify the integrity of the created files.
+    finally:
+        shutil.rmtree(temp_dir)
+
+# This isn't required for running the tests from the `pytest` command line,
+# but it is useful to be able to debug tests in VS Code.
+if __name__ == "__main__":
+    pytest.main()
