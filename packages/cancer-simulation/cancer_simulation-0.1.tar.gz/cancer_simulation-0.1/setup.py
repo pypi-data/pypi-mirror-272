bca8b2268dd40d59db70d2b13e50f83e9670eeea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 04:05:58 +0000

    Add package cancer-simulation/0.1/cancer_simulation-0.1.tar.gz

---
+++ packages/cancer-simulation/cancer_simulation-0.1.tar.gz/cancer_simulation-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cancer_simulation",
+    version='0.1',
+    packages=find_packages(),
+    install_requires = [
+        'pygame==2.5.2',
+        'pygame-chart==1.0.0',
+        'numpy==1.26.3'
+    ]
+)
