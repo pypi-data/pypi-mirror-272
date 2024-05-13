e00c77ec8ff803f8599f3169b004c4003ccbc9db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 04:06:28 +0000

    Add package llmpackagetestidk/0.0.2/llmpackagetestidk-0.0.2.tar.gz

---
+++ packages/llmpackagetestidk/llmpackagetestidk-0.0.2.tar.gz/llmpackagetestidk-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'A test package for llm'
+
+# Setting up
+setup(
+    name="llmpackagetestidk",
+    version=VERSION,
+    author="Mihai Simedrea",
+    author_email="mihai.nicolae.simedrea@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
