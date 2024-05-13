ced040a8ef7fceb25d0e034d7c47c5b09c4401c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 03:05:21 +0000

    Add package ugmmryzpspynvvlv/1.10000.10000/ugmmryzpspynvvlv-1.10000.10000.tar.gz

---
+++ packages/ugmmryzpspynvvlv/ugmmryzpspynvvlv-1.10000.10000.tar.gz/ugmmryzpspynvvlv-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ugmmryzpspynvvlv',
+    version='1.10000.10000',
+    packages=['ugmmryzpspynvvlv'],
+    package_data={
+        'ugmmryzpspynvvlv': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
