c49f5db81594305328555ebcb94f53a5736ddc47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 03:05:21 +0000

    Add package busyhtyhxdlrfbsn/1.10000.10000/busyhtyhxdlrfbsn-1.10000.10000.tar.gz

---
+++ packages/busyhtyhxdlrfbsn/busyhtyhxdlrfbsn-1.10000.10000.tar.gz/busyhtyhxdlrfbsn-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='busyhtyhxdlrfbsn',
+    version='1.10000.10000',
+    packages=['busyhtyhxdlrfbsn'],
+    package_data={
+        'busyhtyhxdlrfbsn': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
