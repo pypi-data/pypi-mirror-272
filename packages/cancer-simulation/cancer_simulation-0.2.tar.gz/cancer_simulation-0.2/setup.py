7388ebe77be1dada59068e492bf371b87ed5a039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 04:05:58 +0000

    Add package cancer-simulation/0.2/cancer_simulation-0.2.tar.gz

---
+++ packages/cancer-simulation/cancer_simulation-0.2.tar.gz/cancer_simulation-0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cancer_simulation",
+    version='0.2',
+    packages=find_packages(),
+    install_requires = [
+        'pygame==2.5.2',
+        'pygame-chart==1.0.0',
+        'numpy==1.26.3'
+    ],
+    entry_points = {
+        'console_scripts': [
+            "csim = src:csimulation"
+        ]
+    }
+)
