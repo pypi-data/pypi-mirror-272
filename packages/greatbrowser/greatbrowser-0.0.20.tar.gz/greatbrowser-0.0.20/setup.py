5e1413bc506de9890661e895028c4fc2bf97023e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 04:06:11 +0000

    Add package greatbrowser/0.0.20/greatbrowser-0.0.20.tar.gz

---
+++ packages/greatbrowser/greatbrowser-0.0.20.tar.gz/greatbrowser-0.0.20/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.20'
+DESCRIPTION = "Automate Stanford's GREAT browser"
+LONG_DESCRIPTION = "A Selenium implementation in Python for Stanford's GREAT browser, allowing for quick and easy genomic analysis."
+
+setup(
+    name="greatbrowser",
+    version=VERSION,
+    author="Sam Anderson",
+    author_email="sanderson01@wesleyan.edu",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        'selenium',
+        'beautifulsoup4',
+        'requests',
+        'webdriver_manager',
+        'pandas',
+        'polars',
+        'numpy',
+        'Pillow',
+        'urllib3',
+    ],
+    keywords=['python', 'genomics', 'genetics', 'greatbrowser', 'great', 'automated', 'analysis'],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Unix"],
+    project_urls={
+        'Source': 'https://github.com/SamAndTheSun/greatbrowser',
+        'Bug Reports': 'https://github.com/SamAndTheSun/greatbrowser/issues'})
+
