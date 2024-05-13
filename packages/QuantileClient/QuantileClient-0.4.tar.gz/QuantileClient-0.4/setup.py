95e012757ffffb646cf9e9280fe7448dc5bfb164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 04:05:44 +0000

    Add package QuantileClient/0.4/QuantileClient-0.4.tar.gz

---
+++ packages/QuantileClient/QuantileClient-0.4.tar.gz/QuantileClient-0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='QuantileClient',
+    version='0.4',
+    packages=find_packages(),
+    install_requires=[
+        'python-dotenv',
+        'requests',
+    ],
+    author='QuantileaiDev',
+    author_email='quantileai@gmail.com',
+    description='Its a wrapper lib from quantile ai api service',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Quantileaidev/QuantileClientLib',
+    license='MIT',
+)
