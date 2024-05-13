28e8a67ff0aa91d94508f39323f3a8c0503aa6c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 04:06:18 +0000

    Add package pazok/0.2/pazok-0.2.tar.gz

---
+++ packages/pazok/pazok-0.2.tar.gz/pazok-0.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+
+name= 'pazok',
+version= '0.2',
+author= 'b_azo',
+description= '',
+packages=setuptools.find_packages(),
+classifiers=[
+"Programming Language :: Python :: 3.9",
+"Operating System :: OS Independent",
+"License :: OSI Approved :: MIT License"
+]
+)
