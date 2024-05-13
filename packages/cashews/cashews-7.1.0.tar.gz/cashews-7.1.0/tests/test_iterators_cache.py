0e184528ff892f57e2f5a261ddf9c95234574340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 03:04:43 +0000

    Add package cashews/7.1.0/cashews-7.1.0.tar.gz

---
+++ packages/cashews/cashews-7.1.0.tar.gz/cashews-7.1.0/tests/test_iterators_cache.py
@@ -0,0 +1,31 @@
+import asyncio
+from unittest.mock import Mock
+
+from cashews import Cache
+
+
+async def test_iterator(cache: Cache):
+    chunks = [b"a", b"b", b"c", b"d"]
+    call = Mock()
+
+    @cache.iterator(ttl=10, key="iterator")
+    async def func():
+        for chunk in chunks:
+            call()
+            await asyncio.sleep(0)
+            yield chunk
+
+    i = 0
+    async for chunk in func():
+        assert chunk == chunks[i]
+        i += 1
+    assert call.call_count == 4
+
+    call.reset_mock()
+
+    i = 0
+    async for chunk in func():
+        assert chunk == chunks[i]
+        i += 1
+
+    assert call.call_count == 0
---
+++ packages/cashews/cashews-7.1.0.tar.gz/cashews-7.1.0/tests/test_middleware.py
@@ -0,0 +1,74 @@
+from cashews import Cache
+from cashews.helpers import add_prefix, all_keys_lower, memory_limit
+
+
+async def test_all_keys_lower(cache: Cache, target):
+    cache._add_backend(target, (all_keys_lower(),))
+    await cache.get(key="KEY")
+    target.get.assert_called_once_with(key="key", default=None)
+
+    await cache.set(key="KEY", value="value")
+    target.set.assert_called_once_with(
+        key="key",
+        value="value",
+        exist=None,
+        expire=None,
+    )
+    await cache.set_many({"KEY": "value"})
+    target.set_many.assert_called_once_with(
+        pairs={"key": "value"},
+        expire=None,
+    )
+    await cache.ping()
+    target.ping.assert_called_once_with(message=b"PING")
+
+
+async def test_memory_limit(cache: Cache, target):
+    cache._add_backend(target, (memory_limit(min_bytes=52, max_bytes=75),))
+
+    await cache.set(key="key", value="v")
+    target.set.assert_not_called()
+
+    await cache.set(key="key", value="v" * 35)
+    target.set.assert_not_called()
+
+    await cache.set(key="key", value="v" * 15)
+    target.set.assert_called_once()
+
+    await cache.ping()
+    target.ping.assert_called_once_with(message=b"PING")
+
+
+async def test_add_prefix(cache: Cache, target):
+    cache._add_backend(target, (add_prefix("prefix!"),))
+
+    await cache.get(key="key")
+    target.get.assert_called_once_with(key="prefix!key", default=None)
+
+    await cache.set(key="key", value="value")
+    target.set.assert_called_once_with(
+        key="prefix!key",
+        value="value",
+        exist=None,
+        expire=None,
+    )
+    await cache.ping()
+    target.ping.assert_called_once_with(message=b"PING")
+
+
+async def test_add_prefix_get_many(cache: Cache, target):
+    cache._add_backend(target, (add_prefix("prefix!"),))
+    await cache.get_many("key")
+    target.get_many.assert_called_once_with("prefix!key")
+
+
+async def test_add_prefix_set_many(cache: Cache, target):
+    cache._add_backend(target, (add_prefix("prefix!"),))
+    await cache.set_many({"key": "value"})
+    target.set_many.assert_called_once_with(pairs={"prefix!key": "value"}, expire=None)
+
+
+async def test_add_prefix_delete_match(cache: Cache, target):
+    cache._add_backend(target, (add_prefix("prefix!"),))
+    await cache.delete_match("key")
+    target.delete_match.assert_called_once_with(pattern="prefix!key")
