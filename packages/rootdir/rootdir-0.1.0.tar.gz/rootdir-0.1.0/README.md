ee0b5114bc5dcef5eaed18d49c19828afdee258f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 03:05:24 +0000

    Add package rootdir/0.1.0/rootdir-0.1.0.tar.gz

---
+++ packages/rootdir/rootdir-0.1.0.tar.gz/rootdir-0.1.0/README.md
@@ -0,0 +1,11 @@
+## Find root path simply
+
+### Step 1
+
+add `__root__.py` to your root path.
+
+### Step 2
+
+import by 
+
+
---
+++ packages/rootdir/rootdir-0.1.0.tar.gz/rootdir-0.1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name = 'rootdir',
+    version = '0.1.0',
+    description = 'get python root directory easily',
+    author = 'meansoup',
+    author_email = 'alsrnr1210@gmail.com',
+    long_description = open('README.md').read(),
+    long_description_content_type = "text/markdown",
+    url = 'https://github.com/meansoup/root',
+    include_package_data = True,
+    packages = setuptools.find_packages()
+    # install_requires = [
+    # ],
+)
