4c8db329fbde933b4ca134f21ef592ff64d39df9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 04:10:37 +0000

    Add package rootdir/0.1.2.post3/rootdir-0.1.2.post3.tar.gz

---
+++ packages/rootdir/rootdir-0.1.2.post3.tar.gz/rootdir-0.1.2.post3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name = 'rootdir',
+    version = '0.1.2-3',
+    description = 'get python root directory easily',
+    author = 'meansoup',
+    author_email = 'alsrnr1210@gmail.com',
+    long_description = open('README.md').read(),
+    long_description_content_type = "text/markdown",
+    url = 'https://github.com/meansoup/rootdir',
+    include_package_data = True,
+    packages=setuptools.find_packages(),
+    # package_dir = {'':'rootdir'},
+    # install_requires = [
+    # ],
+)
