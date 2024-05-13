77228bf87f9fab46344b8ec037fbd62800c1f72a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 04:06:28 +0000

    Add package rootdir/0.1.2.post5/rootdir-0.1.2.post5.tar.gz

---
+++ packages/rootdir/rootdir-0.1.2.post5.tar.gz/rootdir-0.1.2.post5/rootdir/__init__.py
@@ -0,0 +1,18 @@
+import os
+
+class FailToFindRootException(Exception):
+    pass
+
+
+def rootdir():
+    current_directory = os.path.dirname(__file__)
+    print("current: " + current_directory)
+
+    while True:
+        if os.path.exists(os.path.join(current_directory, '__root__.py')):
+            return current_directory
+
+        if current_directory == "/" or current_directory == os.path.dirname(current_directory):
+            raise FailToFindRootException("There is no _root__.py, " + current_directory)
+
+        current_directory = os.path.dirname(current_directory)
