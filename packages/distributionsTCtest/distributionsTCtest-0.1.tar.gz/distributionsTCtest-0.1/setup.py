646bbef0423682b8cbab82ce74302083ddb2ee04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 03:05:23 +0000

    Add package distributionsTCtest/0.1/distributionsTCtest-0.1.tar.gz

---
+++ packages/distributionsTCtest/distributionsTCtest-0.1.tar.gz/distributionsTCtest-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name = 'distributionsTCtest',
+      version = '0.1',
+      description = 'Gaussian distributions',
+      packages = ['distributionsTCtest'],
+      zip_safe=False)
