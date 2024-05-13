8419d28b6166a07d43d000478326948c4274252b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 04:06:00 +0000

    Add package sequentialdw/2.2/sequentialdw-2.2.tar.gz

---
+++ packages/sequentialdw/sequentialdw-2.2.tar.gz/sequentialdw-2.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sequentialdw',
+    version='2.2',
+    packages=find_packages(),
+    install_requires=['requests'],
+    entry_points={
+        'console_scripts': [
+            'seqdownload = sequentialdw.seq_download:main'  
+        ]
+    },
+    author='wweziza',
+    description='A simple website scrapper with sequential files in it.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
