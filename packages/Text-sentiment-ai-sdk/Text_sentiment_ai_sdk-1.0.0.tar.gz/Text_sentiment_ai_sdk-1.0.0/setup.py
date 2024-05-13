e55d9ad5e9630e76218435aad5f23d8ad59eccec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 04:06:20 +0000

    Add package Text-sentiment-ai-sdk/1.0.0/Text_sentiment_ai_sdk-1.0.0.tar.gz

---
+++ packages/Text-sentiment-ai-sdk/Text_sentiment_ai_sdk-1.0.0.tar.gz/Text_sentiment_ai_sdk-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Text_sentiment_ai_sdk",
+    version="1.0.0",
+    author="Abdulrafiu Izuafa",
+    author_email="izuafa123abdulrafiu@gmail.com",
+    description="A Python SDK for AI Text sentiment analysis",
+    url="https://github.com/Ramseyxlil/Text_Sentiment_analysis",
+    packages=find_packages(),
+    install_requires=[
+        "requests"
+    ],
+)
