a9511e15b044cf26ef288c6fe66cf0817880f8ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 03:05:18 +0000

    Add package malet/0.1.13/malet-0.1.13.tar.gz

---
+++ packages/malet/malet-0.1.13.tar.gz/malet-0.1.13/src/malet/utils.py
@@ -0,0 +1,46 @@
+import os, shutil
+from ast import literal_eval
+
+from rich.table import Table
+
+def create_dir(dir):
+  if os.path.exists(dir):
+    for f in os.listdir(dir):
+      if os.path.isdir(os.path.join(dir, f)):
+        shutil.rmtree(os.path.join(dir, f))
+      else:
+        os.remove(os.path.join(dir, f))
+  else:
+    os.makedirs(dir)
+    
+
+def df2richtable(df):
+  table = Table(title='Metric Summary Table')
+  df = df.reset_index()
+  
+  table.add_column('id')
+  for f in list(df):       
+    table.add_column(f)
+  
+  for row in df.itertuples(name=None):
+    table.add_row(*(str(i) for i in row))
+    
+  return table
+
+
+def list2tuple(l):
+  if isinstance(l, list):
+    return tuple(map(list2tuple, l))
+  if isinstance(l, dict):
+    return {k: list2tuple(v) for k, v in l.items()}
+  return l
+    
+
+def str2value(value_str):
+    """Casts string back to standard python types"""
+    if not isinstance(value_str, str): return value_str
+    value_str = value_str.replace('inf', '2e+308')
+    try:
+      return literal_eval(value_str)
+    except:
+      return value_str
