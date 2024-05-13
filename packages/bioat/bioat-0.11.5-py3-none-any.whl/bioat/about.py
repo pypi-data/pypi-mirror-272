90f2ada2367a79ea987a9662ad96a631416eb123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 04:05:43 +0000

    Add package bioat/0.11.5/bioat-0.11.5-py3-none-any.whl

---
+++ packages/bioat/bioat-0.11.5-py3-none-any.whl/bioat/about.py
@@ -0,0 +1,29 @@
+from bioat.version import (__version__, __author__, __upgrade_date__, __email__, __doc_address__, __repo_address__,
+                           __issue_address__)
+from bioat import __name__ as name
+
+about = f"""
+BioAT ({name})
+    - {name} version:
+        {__version__}
+    - last update:
+        - {__upgrade_date__}
+    - repository page:
+        {__repo_address__}
+    - doc page:
+        {__doc_address__}
+    - issue / new feature page:
+        {__issue_address__}
+    - author:
+        - name: {__author__}
+        - email: {__email__}
+    ---
+        
+    **Copyright**:
+        For researchers: the authors appreciate the citations to this tool (citations are not mandatory),
+            please cite my papers:
+                1. aaaaaaa
+                2. bbbbbbb
+        For commercial use:
+            NOT PERMITTED unless permission is obtained from the author
+"""
---
+++ packages/bioat/bioat-0.11.5-py3-none-any.whl/bioat/cli.py
@@ -0,0 +1,65 @@
+from bioat import (
+    __version__,
+    about,
+    BamTools,
+    BedTools,
+    CrisprTools,
+    FastxTools,
+    HiCTools,
+    MetaTools,
+    SearchTools,
+    # SystemTools,
+    TableTools,
+    TargetSeq
+)
+
+
+class Cli(object):
+    """Cli interface of bioat
+
+    - "bioat" is short for "Bioinformatic Analysis Tools" and is a commandline toolkit and a python package. It can be used through this cli interface in terminal or the `import` way in python codes.
+    - "bioat" has many subcommand to deal with different bio-format: BED, BAM, FASTA, FASTQ, VCF, et al.
+    - "bioat" can be used for mining CRISPRs, downloading metagenome and even report a Google Scholar search result!
+    - for more information, run `bioat about`
+    """
+
+    # - Citation:
+    #     - Citation is not forcible and I would be appreciated if you wann to do this.
+    #     - citation reference format:
+    #         [1] Huanan ZHAO. bioat, a Bioinformatic Analysis Tool kit (2023), https://github.com/hermanzhaozzzz/bioat
+
+    # """
+
+    def __init__(self):
+        self.bam = BamTools()
+        self.bed = BedTools()
+        self.crispr = CrisprTools()
+        self.fastx = FastxTools()
+        self.hic = HiCTools()
+        self.meta = MetaTools()
+        self.search = SearchTools()
+        # self.system = SystemTools()
+        self.table = TableTools()
+        self.target_seq = TargetSeq()
+
+    @classmethod
+    def about(self):
+        """Print information about `bioat`."""
+        return about
+
+    def list(self):  # not class method because some attr and methods do exit when obj instantiation
+        """Print commands and subcommands."""
+        out = ""
+        for att in dir(self):
+            if not att.startswith('_'):
+                out += f'{att}\n'
+
+                for sub_att in dir(getattr(self, att)):
+                    if not sub_att.startswith('_'):
+                        out += f'  ├── {sub_att}\n'
+        return out
+
+    @classmethod
+    def version(self):
+        """Print version information."""
+        return __version__
