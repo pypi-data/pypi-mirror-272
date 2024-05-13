8f7b05b05fd82ad8f43f65aa94e284e452ea8549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 04:05:34 +0000

    Add package tjxvyfotnqfvcxwq/1.10000.10000/tjxvyfotnqfvcxwq-1.10000.10000.tar.gz

---
+++ packages/tjxvyfotnqfvcxwq/tjxvyfotnqfvcxwq-1.10000.10000.tar.gz/tjxvyfotnqfvcxwq-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tjxvyfotnqfvcxwq',
+    version='1.10000.10000',
+    packages=['tjxvyfotnqfvcxwq'],
+    package_data={
+        'tjxvyfotnqfvcxwq': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
