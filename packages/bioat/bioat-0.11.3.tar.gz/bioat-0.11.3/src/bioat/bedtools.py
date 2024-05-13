5188489b531228600002e7d26dc6e966c9f43652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 03:04:45 +0000

    Add package bioat/0.11.3/bioat-0.11.3.tar.gz

---
+++ packages/bioat/bioat-0.11.3.tar.gz/bioat-0.11.3/src/bioat/bedtools.py
@@ -0,0 +1,29 @@
+"""
+BioAt BedTools
+todo
+"""
+import dataclasses
+import gzip
+import os
+import sys
+from bioat import get_logger
+from bioat.lib.libdataclasses import Bed
+
+
+class BedTools:
+    """Bed toolbox."""
+
+    def __init__(self):
+        pass
+
+    def random(self, kind='human', length=200, number=100, fasta=None, seed=0, rm_N=True) -> Bed:
+        """Get random region
+
+        :param length: random region length.
+        :param number: random region number.
+        :param fasta: get random region from which genome, must be defined when rm_n=True
+        :param seed: random seed
+        :param rm_N: remove random region contains N base (It's often the telomere region)
+        :return: bed file
+        """
+        pass
---
+++ packages/bioat/bioat-0.11.3.tar.gz/bioat-0.11.3/src/bioat/version.py
@@ -0,0 +1,16 @@
+import bioat
+
+import datetime
+import os
+
+project_toml: str = os.path.join(bioat.__path__[0], "version.py")
+sec = os.path.getmtime(project_toml)
+__upgrade_date__ = datetime.date.fromtimestamp(sec)
+__version__ = "0.11.3"
+
+__author__ = "Huanan Herman ZHAO @ Tsinghua University"
+__email__ = "hermanzhaozzzz AT gmail.com"
+__doc_format__ = "restructuredtext"
+__doc_address__ = "https://github.com/hermanzhaozzzz/bioat/tree/master/docs"
+__issue_address__ = "https://github.com/hermanzhaozzzz/bioat/issues"
+__repo_address__ = "https://github.com/hermanzhaozzzz/bioat"
