a264db47d5dd1ed6669fd4d7e8a86b1cfc143894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 03:04:55 +0000

    Add package django-admin-logentry-extension/0.1.1/django-admin-logentry-extension-0.1.1.tar.gz

---
+++ packages/django-admin-logentry-extension/django-admin-logentry-extension-0.1.1.tar.gz/django-admin-logentry-extension-0.1.1/django_admin_logentry_extension/__init__.py
@@ -0,0 +1,6 @@
+default_app_config = (
+    "django_admin_logentry_extension.apps.DjangoAdminLogentryExtensionExampleConfig"
+)
+app_requires = [
+    "django_admin_daterange_listfilter",
+]
---
+++ packages/django-admin-logentry-extension/django-admin-logentry-extension-0.1.1.tar.gz/django-admin-logentry-extension-0.1.1/django_admin_logentry_extension/utils.py
@@ -0,0 +1,7 @@
+from django.contrib.admin.models import LogEntry
+
+
+def add_extra_action_flags(extra_action_flags):
+    action_flag_field = LogEntry._meta.get_field("action_flag")
+    for item in extra_action_flags:
+        action_flag_field.choices.append(item)
