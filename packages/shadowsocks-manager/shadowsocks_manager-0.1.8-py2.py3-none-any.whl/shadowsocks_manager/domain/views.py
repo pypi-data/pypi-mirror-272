2a04143c1e6a9ab6afbfb7657a86560c5a2861f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 03:04:50 +0000

    Add package shadowsocks-manager/0.1.8/shadowsocks_manager-0.1.8-py2.py3-none-any.whl

---
+++ packages/shadowsocks-manager/shadowsocks_manager-0.1.8-py2.py3-none-any.whl/shadowsocks_manager/domain/views.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+# py2.7 and py3 compatibility imports
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+from shadowsocks_manager.utils.viewsets import CompatModelViewSet
+
+from . import models, serializers
+
+
+# Create your views here.
+
+class NameServerViewSet(CompatModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
+    queryset = models.NameServer.objects.all()
+    serializer_class = serializers.NameServerSerializer
+    filter_fields = ['name', 'api_cls_name', 'user']
+
+
+class DomainViewSet(CompatModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
+    queryset = models.Domain.objects.all()
+    serializer_class = serializers.DomainSerializer
+    filter_fields = ['name', 'nameserver', 'nameserver__name']
+
+
+class RecordViewSet(CompatModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
+    queryset = models.Record.objects.all()
+    serializer_class = serializers.RecordSerializer
+    filter_fields = ['host', 'domain', 'domain__name', 'type', 'answer', 'site', 'site__name', 'site__domain']
