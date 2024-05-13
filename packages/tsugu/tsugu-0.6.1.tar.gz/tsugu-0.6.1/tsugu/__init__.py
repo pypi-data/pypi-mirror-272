01d2564d78bd090ff4c09b66c87192cd62dd2c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 03:05:18 +0000

    Add package tsugu/0.6.1/tsugu-0.6.1.tar.gz

---
+++ packages/tsugu/tsugu-0.6.1.tar.gz/tsugu-0.6.1/tsugu/__init__.py
@@ -0,0 +1,7 @@
+from .handler import handler, handler_old
+from .universal_utils import config
+from .local.utils import database
+from .router import router, interior_local_method, interior_remote_method
+
+
+
