1ee78f8cb145f57586eabbca94f6fa939371172f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 03:04:44 +0000

    Add package pi-log/0.5.8.1/pi_log-0.5.8.1-py3-none-any.whl

---
+++ packages/pi-log/pi_log-0.5.8.1-py3-none-any.whl/pi_log/__init__.py
@@ -0,0 +1,6 @@
+from pi_log import logs as logs
+from pi_log.logs import get_app_logger as get_app_logger
+from pi_log.logs import getLogger as getLogger
+from pi_log.logs import set_app_level as set_app_level
+from pi_log.logs import set_app_root as set_app_root
+from pi_log.logs import set_log_level as set_log_level
