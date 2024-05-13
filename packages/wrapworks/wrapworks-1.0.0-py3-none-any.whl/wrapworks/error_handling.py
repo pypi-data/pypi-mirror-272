77f3dcdb86924b53fa45c74891130467474b72a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 03:05:14 +0000

    Add package wrapworks/1.0.0/wrapworks-1.0.0-py3-none-any.whl

---
+++ packages/wrapworks/wrapworks-1.0.0-py3-none-any.whl/wrapworks/error_handling.py
@@ -0,0 +1,17 @@
+"""
+Contains helpers related to error handling
+"""
+
+from typing import Callable
+import traceback
+
+from rich import print
+
+
+def eprint(e: Exception, func: Callable, no_tracebacks: True):
+    """
+    Prints an error with traceback and type
+    """
+    print(f"Error on '{func.__name__}'. {type(e).__name__} - {e}.")
+    if not no_tracebacks:
+        traceback.print_tb(e.__traceback__, limit=5)
