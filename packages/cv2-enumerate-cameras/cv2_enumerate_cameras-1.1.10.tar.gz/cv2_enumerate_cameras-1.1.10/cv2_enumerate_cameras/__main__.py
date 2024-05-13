2b72bde22587967e0cff7278b5758d302d6ed363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 03:05:21 +0000

    Add package cv2-enumerate-cameras/1.1.10/cv2_enumerate_cameras-1.1.10.tar.gz

---
+++ packages/cv2-enumerate-cameras/cv2_enumerate_cameras-1.1.10.tar.gz/cv2_enumerate_cameras-1.1.10/cv2_enumerate_cameras/__main__.py
@@ -0,0 +1,17 @@
+from cv2_enumerate_cameras import enumerate_cameras, supported_backends
+from cv2_enumerate_cameras.camera_info import CAP_ANY
+
+
+try:
+    from cv2.videoio_registry import getBackendName
+except ModuleNotFoundError:
+    def getBackendName(api):
+        return str(api)
+
+
+if __name__ == '__main__':
+    for backend in [CAP_ANY, *supported_backends]:
+        print(f'Enumerate using {getBackendName(backend)} backend:')
+        for i in enumerate_cameras(backend):
+            print(i)
+        print()
